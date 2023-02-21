b4bde16bf9a6a74a6f55639f31ea11bd56cc813f
Micah Halter <micah@mehalter.com>
2023-02-21 11:27:37 -0500

    feat(colorscheme): add nightfox

---
+++ lua/astrocommunity/colorscheme/nightfox/nightfox.lua
@@ -0,0 +1,24 @@
+return {
+  "EdenEast/nightfox.nvim",
+  opts = {
+    options = {
+      module_default = false,
+      modules = {
+        aerial = true,
+        cmp = true,
+        ["dap-ui"] = true,
+        dashboard = true,
+        diagnostic = true,
+        gitsigns = true,
+        native_lsp = true,
+        neotree = true,
+        notify = true,
+        symbol_outline = true,
+        telescope = true,
+        treesitter = true,
+        whichkey = true,
+      },
+    },
+    groups = { all = { NormalFloat = { link = "Normal" } } },
+  },
+}
