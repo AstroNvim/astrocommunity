f5ccde5d95da09ce48751809435ed1baa3fb1922
luxus <luxuspur@gmail.com>
2023-02-22 00:37:42 +0100

    feat(media): drop-nvim

---
+++ lua/astrocommunity/media/drop-nvim/drop-nvim.lua
@@ -0,0 +1,12 @@
+return {
+  -- silly drops
+  {
+    "folke/drop.nvim",
+    event = "VeryLazy",
+    config = function()
+      math.randomseed(os.time())
+      local theme = ({ "stars", "snow" })[math.random(1, 3)]
+      require("drop").setup { theme = theme }
+    end,
+  },
+}
