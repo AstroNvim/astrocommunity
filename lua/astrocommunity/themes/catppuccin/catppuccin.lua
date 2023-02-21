02c1284545b3888e64c156d9ab9c64849fd11977
Micah Halter <micah@mehalter.com>
2023-02-21 10:03:31 -0500

    feat(themes): add catppuccin

---
+++ lua/astrocommunity/themes/catppuccin/catppuccin.lua
@@ -0,0 +1,19 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  opts = {
+    integrations = {
+      nvimtree = false,
+      ts_rainbow = false,
+      aerial = true,
+      dap = { enabled = true, enable_ui = true },
+      mason = true,
+      neotree = true,
+      notify = true,
+      semantic_tokens = true,
+      symbols_outline = true,
+      telescope = true,
+      which_key = true,
+    },
+  },
+}
