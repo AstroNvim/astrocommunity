b47895d3dd50acbb7cb43be0b998d148d090a792
luxus <luxuspur@gmail.com>
2023-02-27 20:25:37 +0100

    feat(motion): Add Mini.move

---
+++ lua/astrocommunity/motion/mini-move/mini-move.lua
@@ -0,0 +1,22 @@
+return {
+  "echasnovski/mini.move",
+  config = function(_, opts) require("mini.move").setup(opts) end,
+  keys = {
+    { "<A-l>", mode = { "n", "v" } },
+    { "<A-k>", mode = { "n", "v" } },
+    { "<A-j>", mode = { "n", "v" } },
+    { "<A-h>", mode = { "n", "v" } },
+  },
+  opts = {
+    mappings = {
+      left = "<A-h>",
+      right = "<A-l>",
+      down = "<A-j>",
+      up = "<A-k>",
+      line_left = "<A-h>",
+      line_right = "<A-l>",
+      line_down = "<A-j>",
+      line_up = "<A-k>",
+    },
+  },
+}
