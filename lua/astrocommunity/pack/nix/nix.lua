804e0189077872c54c29419a9387f469edf44f81
luxus <luxuspur@gmail.com>
2023-02-27 04:16:31 +0100

    feat(pack): Add nix

---
+++ lua/astrocommunity/pack/nix/nix.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table or string "all".
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      elseif opts.ensure_installed == "all" then
+        return
+      end
+      -- Add the "nix" language to opts.ensure_installed.
+      table.insert(opts.ensure_installed, "nix")
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      table.insert(opts.ensure_installed, "rnix")
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require "null-ls"
+      if type(opts.sources) == "table" then
+        vim.list_extend(opts.sources, {
+          nls.builtins.code_actions.statix,
+          nls.builtins.formatting.alejandra,
+          nls.builtins.diagnostics.deadnix,
+        })
+      end
+    end,
+  },
+}
