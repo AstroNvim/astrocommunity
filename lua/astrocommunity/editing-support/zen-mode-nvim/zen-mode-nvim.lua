28a7d11b058afcda3ea7b40d6a50c4da71f41c90
luxus <luxuspur@gmail.com>
2023-02-27 04:56:08 +0100

    fix(editing-support): Hide more in Zen-Mode

--- lua/astrocommunity/editing-support/zen-mode-nvim/zen-mode-nvim.lua
+++ lua/astrocommunity/editing-support/zen-mode-nvim/zen-mode-nvim.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     window = {
       backdrop = 1,
-      width = 0.5,
+      width = function() return math.min(120, vim.o.columns * 0.75) end,
       height = 0.9,
       options = {
         number = false,
@@ -21,7 +21,17 @@ return {
         laststatus = 0,
       },
     },
-    on_open = function() pcall(vim.cmd.IndentBlanklineDisable) end,
-    on_close = function() pcall(vim.cmd.IndentBlanklineEnable) end,
+    on_open = function() -- disable diagnostics and indent blankline
+      vim.g.diagnostics_mode_old = vim.g.diagnostics_mode
+      vim.g.diagnostics_mode = 0
+      vim.diagnostic.config(require("astronvim.utils.lsp").diagnostics[0])
+      vim.g.indent_blankline_enabled_old = vim.g.indent_blankline_enabled
+      vim.g.indent_blankline_enabled = false
+    end,
+    on_close = function() -- restore diagnostics and indent blankline
+      vim.g.diagnostics_mode = vim.g.diagnostics_mode_old
+      vim.diagnostic.config(require("astronvim.utils.lsp").diagnostics[vim.g.diagnostics_mode])
+      vim.g.indent_blankline_enabled = vim.g.indent_blankline_enabled_old
+    end,
   },
 }
