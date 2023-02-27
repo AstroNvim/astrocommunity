60b5b056a36090b26bf9eae93508f057d9d7d93a
luxus <luxuspur@gmail.com>
2023-02-27 07:05:21 +0100

    feat(utility): Add Neodim

---
+++ lua/astrocommunity/utility/neodim/neodim.lua
@@ -0,0 +1,17 @@
+return {
+  "zbirenbaum/neodim",
+  event = "LspAttach",
+  opts = {
+    alpha = 0.75,
+    blend_color = "#000000",
+    update_in_insert = {
+      enable = true,
+      delay = 100,
+    },
+    hide = {
+      virtual_text = true,
+      signs = true,
+      underline = true,
+    },
+  },
+}
