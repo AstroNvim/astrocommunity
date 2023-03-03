9a99d7a1cbdd1785ce2034430e19229e18c729d8
luxus <luxuspur@gmail.com>
2023-03-04 00:18:10 +0100

    feat(motion): add mini.surround

---
+++ lua/astrocommunity/motion/mini-surround/minisurround.lua
@@ -0,0 +1,22 @@
+local prefix = "gz"
+return {
+  { "machakann/vim-sandwich", enabled = false },
+  {
+    "echasnovski/mini.surround",
+    keys = {
+      { prefix, desc = "Surround" },
+    },
+    opts = {
+      mappings = {
+        add = prefix .. "a", -- Add surrounding in Normal and Visual modes
+        delete = prefix .. "d", -- Delete surrounding
+        find = prefix .. "f", -- Find surrounding (to the right)
+        find_left = prefix .. "F", -- Find surrounding (to the left)
+        highlight = prefix .. "h", -- Highlight surrounding
+        replace = prefix .. "r", -- Replace surrounding
+        update_n_lines = prefix .. "n", -- Update `n_lines`
+      },
+    },
+    config = function(_, opts) require("mini.surround").setup(opts) end,
+  },
+}
