df5769d6c49b748125df612299e8e7c61912d016
Micah Halter <micah@mehalter.com>
2023-03-06 10:43:15 -0500

    feat(pack): add Julia language pack

---
+++ lua/astrocommunity/pack/julia/README.md
@@ -0,0 +1,11 @@
+# Julia Language Pack
+
+**Requirements:** `julia` must be in your `PATH` and executable
+
+This plugin pack does the following:
+
+- Adds `julia` and `toml` Treesitter parsers
+- Adds `julials` and `taplo` language servers
+    - `julials` - the language server for Julia
+    - `taplo` - the language server for Toml
+- Adds [cmp-latex-symbols](https://github.com/kdheepak/cmp-latex-symbols) for easily inserting unicode symbols into code with latex notation with autocompletion with `cmp`
---
+++ lua/astrocommunity/pack/julia/julia.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table or string "all".
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      elseif opts.ensure_installed == "all" then
+        return
+      end
+      -- Add the "julia" and "toml" language to opts.ensure_installed.
+      vim.list_extend(opts.ensure_installed, { "julia", "toml" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      -- Add julia lsp and toml lsp to ensure_installed
+      vim.list_extend(opts.ensure_installed, { "julials", "taplo" })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    -- add cmp latex symbols for easier julia editing
+    dependencies = { "kdheepak/cmp-latex-symbols" },
+    opts = function(_, opts)
+      if not opts.sources then opts.sources = {} end
+      table.insert(opts.sources, { name = "latex_symbols", priority = 700 })
+    end,
+  },
+}
