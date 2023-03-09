15661ab07284786505cca72573250e215ee31317
Micah Halter <micah@mehalter.com>
2023-03-09 13:40:11 -0500

    feat(pack): add markdown language pack

---
+++ lua/astrocommunity/pack/markdown/markdown.lua
@@ -0,0 +1,31 @@
+local utils = require "astrocommunity.utils"
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table or string "all".
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      elseif opts.ensure_installed == "all" then
+        return
+      end
+      utils.list_insert_unique(opts.ensure_installed, { "markdown", "markdown_inline" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      utils.list_insert_unique(opts.ensure_installed, "marksman")
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      utils.list_insert_unique(opts.ensure_installed, "prettier")
+    end,
+  },
+}
