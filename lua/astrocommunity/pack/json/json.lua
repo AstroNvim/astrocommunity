d4ae46bc3a9934fbcc51be0a8ce5f67e79085a60
Micah Halter <micah@mehalter.com>
2023-03-09 12:53:58 -0500

    feat(pack): add json language pack

---
+++ lua/astrocommunity/pack/json/json.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table or string "all".
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      elseif opts.ensure_installed == "all" then
+        return
+      end
+      vim.list_extend(opts.ensure_installed, { "json", "jsonc" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      table.insert(opts.ensure_installed, "jsonls")
+    end,
+  },
+}
