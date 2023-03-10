cd2eb14cba254e77c8b6860c84d58be860bc855f
Micah Halter <micah@mehalter.com>
2023-03-10 16:05:02 -0500

    feat(pack): add php language pack

---
+++ lua/astrocommunity/pack/php/README.md
@@ -0,0 +1,7 @@
+# PHP Language Pack
+
+This plugin pack does the following:
+
+- Adds `php` Treesitter parsers
+- Adds `phpactor` language servers
+- Adds `php-debug-adapter` for debugging
---
+++ lua/astrocommunity/pack/php/php.lua
@@ -0,0 +1,34 @@
+local utils = require "astrocommunity.utils"
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table or string "all".
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      elseif opts.ensure_installed == "all" then
+        return
+      end
+      -- Add the "python" and "toml" language to opts.ensure_installed.
+      utils.list_insert_unique(opts.ensure_installed, "php")
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      -- Add pyright lsp to opts.ensure_installed using table.insert.
+      utils.list_insert_unique(opts.ensure_installed, "phpactor")
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      -- Add to opts.ensure_installed using table.insert.
+      utils.list_insert_unique(opts.ensure_installed, "php")
+    end,
+  },
+}
