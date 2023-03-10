74473d883f2070a7856cbaa382eb545090aa1d7c
Micah Halter <micah@mehalter.com>
2023-03-10 16:14:14 -0500

    feat(pack): add yaml language pack

---
+++ lua/astrocommunity/pack/yaml/yaml.lua
@@ -0,0 +1,32 @@
+local utils = require "astrocommunity.utils"
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table or string "all".
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      elseif opts.ensure_installed == "all" then
+        return
+      end
+      utils.list_insert_unique(opts.ensure_installed, "yaml")
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      utils.list_insert_unique(opts.ensure_installed, "yamlls")
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      -- Add go lsps to opts.ensure_installed using vim.list_extend.
+      utils.list_insert_unique(opts.ensure_installed, "prettier")
+    end,
+  },
+}
