47959094e8e085ab349698562bd305c4a90d2d5a
Pete Kazmier <pete@kazmier.com>
2023-03-12 13:58:10 -0400

    fix(mini-ai): explicitly call setup()
    The mini-ai astrocommunity plug-in was not working because the
    setup function was not being called.  According to lazy docs, it
    seems to imply that it should automatically call setup, but this
    plug-in did not work without explicitly defining our own config
    that calls setup.

--- lua/astrocommunity/motion/mini-ai/mini-ai.lua
+++ lua/astrocommunity/motion/mini-ai/mini-ai.lua
@@ -1,1 +1,5 @@
-return { "echasnovski/mini.ai", event = "User AstroFile" }
+return {
+  "echasnovski/mini.ai",
+  event = "User AstroFile",
+  config = function(_, opts) require("mini.ai").setup(opts) end,
+}
