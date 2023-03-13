3a3231280c50bcbc0bef9f5327a41ca974b8909b
Micah Halter <micah@mehalter.com>
2023-03-13 07:53:18 -0400

    feat(color): add modes.nvim plugin

---
+++ lua/astrocommunity/color/modes-nvim/modes-nvim.lua
@@ -0,0 +1,9 @@
+return {
+  { "folke/which-key.nvim", opts = { plugins = { presets = { operators = false } } } },
+  {
+    "mvllow/modes.nvim",
+    version = "^0.2",
+    event = "VeryLazy",
+    opts = {},
+  },
+}
