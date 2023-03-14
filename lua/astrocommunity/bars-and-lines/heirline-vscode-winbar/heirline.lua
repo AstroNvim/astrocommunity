cdc12ce6a15e3bc4a19dfb849c559331110df24e
Micah Halter <micah@mehalter.com>
2023-03-14 08:05:05 -0400

    feat(bars-and-lines): add vs code style winbar recipe

---
+++ lua/astrocommunity/bars-and-lines/heirline-vscode-winbar/heirline.lua
@@ -0,0 +1,50 @@
+return {
+  "rebelot/heirline.nvim",
+  opts = function(_, opts)
+    local status = require "astronvim.utils.status"
+
+    opts.winbar = { -- create custom winbar
+      -- store the current buffer number
+      init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+      fallthrough = false, -- pick the correct winbar based on condition
+      -- inactive winbar
+      {
+        condition = function() return not status.condition.is_active() end,
+        -- show the path to the file relative to the working directory
+        status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
+        -- add the file name and icon
+        status.component.file_info {
+          file_icon = { hl = status.hl.file_icon "winbar", padding = { left = 0 } },
+          file_modified = false,
+          file_read_only = false,
+          hl = status.hl.get_attributes("winbarnc", true),
+          surround = false,
+          update = "BufEnter",
+        },
+      },
+      -- active winbar
+      {
+        -- show the path to the file relative to the working directory
+        status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
+        -- add the file name and icon
+        status.component.file_info { -- add file_info to breadcrumbs
+          file_icon = { hl = status.hl.filetype_color, padding = { left = 0 } },
+          file_modified = false,
+          file_read_only = false,
+          hl = status.hl.get_attributes("winbar", true),
+          surround = false,
+          update = "BufEnter",
+        },
+        -- show the breadcrumbs
+        status.component.breadcrumbs {
+          icon = { hl = true },
+          hl = status.hl.get_attributes("winbar", true),
+          prefix = true,
+          padding = { left = 0 },
+        },
+      },
+    }
+
+    return opts
+  end,
+}
