8998365fe4cd01744b20bc1a5908f5a96d97d385
Jiří Štefka <jiri.stefka@atlas.cz>
2023-03-14 19:05:12 +0100

    fix(code-runner): fix style in overseer.nvim to be in line with stylua

--- lua/astrocommunity/code-runner/overseer-nvim/overseer-nvim.lua
+++ lua/astrocommunity/code-runner/overseer-nvim/overseer-nvim.lua
@@ -1,10 +1,21 @@
-return
-{
+return {
   {
     "stevearc/overseer.nvim",
-    cmd = { "OverseerOpen", "OverseerClose", "OverseerToggle", "OverseerSaveBundle", "OverseerLoadBundle",
-      "OverseerDeleteBundle", "OverseerRunCmd", "OverseerRun", "OverseerInfo", "OverseerBuild", "OverseerQuickAction",
-      "OverseerTaskAction ", "OverseerClearCache" },
-    opts = {}
-  }
+    cmd = {
+      "OverseerOpen",
+      "OverseerClose",
+      "OverseerToggle",
+      "OverseerSaveBundle",
+      "OverseerLoadBundle",
+      "OverseerDeleteBundle",
+      "OverseerRunCmd",
+      "OverseerRun",
+      "OverseerInfo",
+      "OverseerBuild",
+      "OverseerQuickAction",
+      "OverseerTaskAction ",
+      "OverseerClearCache",
+    },
+    opts = {},
+  },
 }
