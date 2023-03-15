38e9b1a5260101216ee1bbb23f16308c3bbb1969
Samuel Cobb <sjcobb2003@gmail.com>
2023-03-15 13:12:52 +0000

    attach jdtls

--- lua/astrocommunity/pack/java/java.lua
+++ lua/astrocommunity/pack/java/java.lua
@@ -28,7 +28,6 @@ return {
       utils.list_insert_unique(opts.ensure_installed, { "java" })
     end,
   },
-
   {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
@@ -40,26 +39,85 @@ return {
       if not opts.setup then opts.setup = {} end
 
       utils.list_insert_unique(opts.setup, {
-        jdtls = function(_, opts)
+        jdtls = function(_, jdopts)
           local ut = require "astronvim.utils"
 
           ut.notify("opts")
-          ut.notify(dump(opts))
+          ut.notify(dump(jdopts))
           print("opts")
-          print(dump(opts))
+          print(dump(jdopts))
         end,
       })
+
+      local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
+      local root_dir = require("jdtls.setup").find_root(root_markers)
+
+      -- calculate workspace dir
+      local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+      local workspace_dir = vim.fn.stdpath "data" .. "/site/java/workspace-root/" .. project_name
+      os.execute("mkdir " .. workspace_dir)
+
+      -- get the mason install path
+      local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
+
+      -- get the current OS
+      local os
+      if vim.fn.has "macunix" then
+        os = "mac"
+      elseif vim.fn.has "win32" then
+        os = "win"
+      else
+        os = "linux"
+      end
+
+      -- return the server config
+      return {
+        cmd = {
+          "java",
+          "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+          "-Dosgi.bundles.defaultStartLevel=4",
+          "-Declipse.product=org.eclipse.jdt.ls.core.product",
+          "-Dlog.protocol=true",
+          "-Dlog.level=ALL",
+          "-javaagent:" .. install_path .. "/lombok.jar",
+          "-Xms1g",
+          "--add-modules=ALL-SYSTEM",
+          "--add-opens",
+          "java.base/java.util=ALL-UNNAMED",
+          "--add-opens",
+          "java.base/java.lang=ALL-UNNAMED",
+          "-jar",
+          vim.fn.glob(install_path .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
+          "-configuration",
+          install_path .. "/config_" .. os,
+          "-data",
+          workspace_dir,
+        },
+        root_dir = root_dir,
+      }
     end,
+    setup_handlers = {
+      jdtls = function(_, opts)
+        vim.api.nvim_create_autocmd("Filetype", {
+          pattern = "java", -- autocmd to start jdtls
+          callback = function()
+            if opts.root_dir and opts.root_dir ~= "" then require("jdtls").start_or_attach(opts) end
+          end,
+        })
+      end
+    },
   },
 
   {
     "mfussenegger/nvim-jdtls",
-    init = function() utils.list_insert_unique(astronvim.lsp.skip_setup, "jdtls") end,
-    opts = {
-      config = {
-        cmd = "java"
-      }
-    }
+    init = function()
+      utils.list_insert_unique(astronvim.lsp.skip_setup, "jdtls")
+    end,
+    -- opts = {
+    --   config = {
+    --     cmd = "java"
+    --   }
+    -- }
     -- opts = function()
     --   return {
     --     setup = {
