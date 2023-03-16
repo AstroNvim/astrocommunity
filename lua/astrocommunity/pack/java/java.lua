847095ba72c76521edc13b914bcfdfe757fb9f95
Samuel Cobb <sjcobb2003@gmail.com>
2023-03-16 15:26:07 +0000

    pass opts to start_or_attach

--- lua/astrocommunity/pack/java/java.lua
+++ lua/astrocommunity/pack/java/java.lua
@@ -45,12 +45,58 @@ return {
     init = function()
       utils.list_insert_unique(astronvim.lsp.skip_setup, "jdtls")
     end,
-    opts = {
-      test = "stinky poopy"
-    },
+    opts = function()
+      -- use this function notation to build some variables
+      local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
+      local root_dir = require("jdtls.setup").find_root(root_markers)
+
+      -- calculate workspace dir
+      local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+      local workspace_dir = vim.fn.stdpath "data" .. "/site/java/workspace-root/" .. project_name
+      os.execute("mkdir " .. workspace_dir)
+
+      -- get the mason install path
+      local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
+
+      -- get the current OS
+      local os
+      if vim.fn.has "macunix" then
+        os = "mac"
+      elseif vim.fn.has "win32" then
+        os = "win"
+      else
+        os = "linux"
+      end
+
+      -- return the server config
+      return {
+        cmd = {
+          "java",
+          "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+          "-Dosgi.bundles.defaultStartLevel=4",
+          "-Declipse.product=org.eclipse.jdt.ls.core.product",
+          "-Dlog.protocol=true",
+          "-Dlog.level=ALL",
+          "-javaagent:" .. install_path .. "/lombok.jar",
+          "-Xms1g",
+          "--add-modules=ALL-SYSTEM",
+          "--add-opens",
+          "java.base/java.util=ALL-UNNAMED",
+          "--add-opens",
+          "java.base/java.lang=ALL-UNNAMED",
+          "-jar",
+          vim.fn.glob(install_path .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
+          "-configuration",
+          install_path .. "/config_" .. os,
+          "-data",
+          workspace_dir,
+        },
+        root_dir = root_dir,
+      }
+    end,
     config = function(_, opts)
       local ut = require "astronvim.utils"
-      ut.notify(opts.test)
+      require("jdtls").start_or_attach(opts)
     end
     -- opts = {
     --   server = require("astronvim.utils.lsp").config "jdtls"
