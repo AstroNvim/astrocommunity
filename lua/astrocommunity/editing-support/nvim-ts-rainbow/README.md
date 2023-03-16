a06a3bfe908165680b72b6160fbcd72b41a6d5f0
Micah Halter <micah@mehalter.com>
2023-03-16 09:09:13 -0400

    feat(editing-support): add nvim-ts-rainbow

---
+++ lua/astrocommunity/editing-support/nvim-ts-rainbow/README.md
@@ -0,0 +1,5 @@
+# nvim-ts-rainbow
+
+**Repository:** https://github.com/mrjones2014/nvim-ts-rainbow
+
+Rainbow parentheses for neovim using tree-sitter.
---
+++ lua/astrocommunity/editing-support/nvim-ts-rainbow/nvim-ts-rainbow.lua
@@ -0,0 +1,5 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = { "mrjones2014/nvim-ts-rainbow" },
+  opts = { rainbow = { enable = true } },
+}
