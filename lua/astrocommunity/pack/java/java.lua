6d4ae6396b9ddad51eb0ab29abf564c38cfc56ef
Samuel Cobb <sjcobb2003@gmail.com>
2023-03-16 17:52:47 +0000

    rm ensure installed

--- lua/astrocommunity/pack/java/java.lua
+++ lua/astrocommunity/pack/java/java.lua
@@ -42,7 +42,7 @@ return {
     "mfussenegger/nvim-jdtls",
     -- ft = { "java" },
     init = function()
-      utils.list_insert_unique(astronvim.lsp.skip_setup, "jdtls")
+      -- utils.list_insert_unique(astronvim.lsp.skip_setup, "jdtls")
     end,
     -- opts = function() return require("astronvim.utils.lsp").config "jdtls" end,
     -- opts = function()
