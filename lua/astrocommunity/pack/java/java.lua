f8ed7f30691530f654030efbcfd59deccdebd3f5
Samuel Cobb <sjcobb2003@gmail.com>
2023-03-16 16:18:15 +0000

    server config

--- lua/astrocommunity/pack/java/java.lua
+++ lua/astrocommunity/pack/java/java.lua
@@ -59,41 +59,40 @@ return {
 
       -- return the server config
       return {
-        -- server = require("astronvim.utils.lsp").config "jdtls",
-        -- cmd = {
-        --   "java",
-        --   "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-        --   "-Dosgi.bundles.defaultStartLevel=4",
-        --   "-Declipse.product=org.eclipse.jdt.ls.core.product",
-        --   "-Dlog.protocol=true",
-        --   "-Dlog.level=ALL",
-        --   "-javaagent:" .. install_path .. "/lombok.jar",
-        --   "-Xms1g",
-        --   "--add-modules=ALL-SYSTEM",
-        --   "--add-opens",
-        --   "java.base/java.util=ALL-UNNAMED",
-        --   "--add-opens",
-        --   "java.base/java.lang=ALL-UNNAMED",
-        --   "-jar",
-        --   vim.fn.glob(install_path .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
-        --   "-configuration",
-        --   install_path .. "/config_" .. "linux",
-        --   "-data",
-        --   workspace_dir,
-        -- },
-        -- root_dir = root_dir,
-        -- settings = {
-        --   java = {},
-        -- },
-        -- init_options = {
-        --   bundles = {},
-        -- },
+        cmd = {
+          "java",
+          "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+          "-Dosgi.bundles.defaultStartLevel=4",
+          "-Declipse.product=org.eclipse.jdt.ls.core.product",
+          "-Dlog.protocol=true",
+          "-Dlog.level=ALL",
+          "-javaagent:" .. install_path .. "/lombok.jar",
+          "-Xms1g",
+          "--add-modules=ALL-SYSTEM",
+          "--add-opens",
+          "java.base/java.util=ALL-UNNAMED",
+          "--add-opens",
+          "java.base/java.lang=ALL-UNNAMED",
+          "-jar",
+          vim.fn.glob(install_path .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
+          "-configuration",
+          install_path .. "/config_" .. "linux",
+          "-data",
+          workspace_dir,
+        },
+        root_dir = root_dir,
+        settings = {
+          java = {},
+        },
+        init_options = {
+          bundles = {},
+        },
       }
     end,
     config = function(_, opts)
       -- local ut = require "astronvim.utils"
       vim.api.nvim_create_autocmd("Filetype", {
-        pattern = "java",   -- autocmd to start jdtls
+        pattern = "java", -- autocmd to start jdtls
         callback = function()
           if opts.root_dir and opts.root_dir ~= "" then require("jdtls").start_or_attach(opts) end
         end,
