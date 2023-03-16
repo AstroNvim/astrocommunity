ef9a9239099fb14ccb1360dd90cefb6c76e9693b
Samuel Cobb <sjcobb2003@gmail.com>
2023-03-16 16:38:55 +0000

    try lsp method

--- lua/astrocommunity/pack/java/java.lua
+++ lua/astrocommunity/pack/java/java.lua
@@ -26,6 +26,77 @@ return {
   },
 
   {
+    "neovim/nvim-lspconfig",
+    dependencies = { "mfussenegger/nvim-jdtls" },
+    opts = {
+      setup = {
+        jdtls = function(_, opts)
+          vim.api.nvim_create_autocmd("FileType", {
+            pattern = "java",
+            callback = function()
+              -- use this function notation to build some variables
+              local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle", ".project" }
+              local root_dir = require("jdtls.setup").find_root(root_markers)
+
+              -- calculate workspace dir
+              local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+              local workspace_dir = vim.fn.stdpath "data" .. "/site/java/workspace-root/" .. project_name
+              os.execute("mkdir " .. workspace_dir)
+
+              -- ut.notify("" .. workspace_dir)
+
+              -- get the mason install path
+              local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
+
+              local config = {
+                cmd = {
+                  "java",
+                  "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+                  "-Dosgi.bundles.defaultStartLevel=4",
+                  "-Declipse.product=org.eclipse.jdt.ls.core.product",
+                  "-Dlog.protocol=true",
+                  "-Dlog.level=ALL",
+                  "-javaagent:" .. install_path .. "/lombok.jar",
+                  "-Xms1g",
+                  "--add-modules=ALL-SYSTEM",
+                  "--add-opens",
+                  "java.base/java.util=ALL-UNNAMED",
+                  "--add-opens",
+                  "java.base/java.lang=ALL-UNNAMED",
+                  "-jar",
+                  vim.fn.glob(install_path .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
+                  "-configuration",
+                  install_path .. "/config_" .. "linux",
+                  "-data",
+                  workspace_dir,
+                },
+                root_dir = root_dir,
+                settings = {
+                  java = {},
+                },
+                init_options = {
+                  bundles = {},
+                },
+                -- filetypes = { "java" },
+                handlers = {
+                  ["language/status"] = function(_, result)
+                    -- print(result)
+                  end,
+                  ["$/progress"] = function(_, result, ctx)
+                    -- disable progress updates.
+                  end,
+                },
+              }
+              require("jdtls").start_or_attach(config)
+            end,
+          })
+          return true
+        end,
+      },
+    },
+  },
+
+  {
     "mfussenegger/nvim-jdtls",
     ft = { "java" },
     init = function()
@@ -42,7 +113,7 @@ return {
       local workspace_dir = vim.fn.stdpath "data" .. "/site/java/workspace-root/" .. project_name
       os.execute("mkdir " .. workspace_dir)
 
-      ut.notify("" .. workspace_dir)
+      -- ut.notify("" .. workspace_dir)
 
       -- get the mason install path
       local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
