a303ebbbe369911558b49c92d8038e58220467fa
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-03-17 20:01:38 +0530

    fix(nvim-ts-rainbow): add autocmd to refresh on save/focus

--- lua/astrocommunity/editing-support/nvim-ts-rainbow/nvim-ts-rainbow.lua
+++ lua/astrocommunity/editing-support/nvim-ts-rainbow/nvim-ts-rainbow.lua
@@ -2,4 +2,14 @@ return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = { "mrjones2014/nvim-ts-rainbow" },
   opts = { rainbow = { enable = true } },
+  config = function(_, opts)
+    -- HACK: https://github.com/p00f/nvim-ts-rainbow/issues/112#issuecomment-1310835936
+    vim.api.nvim_create_autocmd({ "BufWritePost", "FocusGained" }, {
+      callback = function()
+        vim.cmd "TSDisable rainbow"
+        vim.cmd "TSEnable rainbow"
+      end,
+    })
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
