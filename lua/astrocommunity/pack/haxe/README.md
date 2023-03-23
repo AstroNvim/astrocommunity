6ff998948a8555f10f6c2322bfabb74cb2ebcf69
Micah Halter <micah@mehalter.com>
2023-03-23 13:23:15 -0400

    feat(pack): add haxe language pack

---
+++ lua/astrocommunity/pack/haxe/README.md
@@ -0,0 +1,6 @@
+# Haxe Language Pack
+
+This plugin pack does the following:
+
+- Adds `haxe` Treesitter parser: https://github.com/vantreeseba/tree-sitter-haxe
+- Adds `haxe` language server
---
+++ lua/astrocommunity/pack/haxe/haxe.lua
@@ -0,0 +1,22 @@
+local utils = require "astrocommunity.utils"
+return {
+  {
+    "vantreeseba/tree-sitter-haxe",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = "cp -r queries ../nvim-treesitter/queries/haxe",
+    config = function()
+      vim.filetype.add { extension = { hx = "haxe" } }
+      require("nvim-treesitter.parsers").get_parser_configs().haxe =
+        { install_info = { url = vim.fn.stdpath "data" .. "/lazy/tree-sitter-haxe", files = { "src/parser.c" } } }
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      utils.list_insert_unique(opts.ensure_installed, "haxe_language_server")
+    end,
+  },
+}
