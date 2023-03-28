483d4a9847aeb0283492bb45f9c47f6524e4c90b
Micah Halter <micah@mehalter.com>
2023-03-28 13:37:57 -0400

    fix(project): fix telescope lazy loading with project.nvim

--- lua/astrocommunity/project/project-nvim/project-nvim.lua
+++ lua/astrocommunity/project/project-nvim/project-nvim.lua
@@ -1,10 +1,9 @@
 return {
-  "ahmedkhalf/project.nvim",
-  event = "VeryLazy",
-  dependencies = "nvim-telescope/telescope.nvim",
-  opts = { ignore_lsp = { "lua_ls" } },
-  config = function(_, opts)
-    require("project_nvim").setup(opts)
-    require("telescope").load_extension "projects"
-  end,
+  {
+    "ahmedkhalf/project.nvim",
+    event = "VeryLazy",
+    opts = { ignore_lsp = { "lua_ls" } },
+    config = function(_, opts) require("project_nvim").setup(opts) end,
+  },
+  { "nvim-telescope/telescope.nvim", opts = function() require("telescope").load_extension "projects" end },
 }
