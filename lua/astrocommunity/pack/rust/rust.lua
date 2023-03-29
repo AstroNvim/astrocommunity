2005cf25dd0281c4ee50f31ffc7e016c16670c48
Zachary Glenn <57847330+ZackGlenn@users.noreply.github.com>
2023-03-29 08:55:20 -0600

    feat(pack): switch to codelldb for rust debugging (#106)
    * feat!: switch to codelldb for rust debugging
    
    * fix: use mason's API for installation path
    
    * fix: cleaner use of the mason api
    
    Co-authored-by: Micah Halter <micah@mehalter.com>
    
    * refactor: clean up code a bit
    
    Co-authored-by: Oli <57495944+owittek@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Micah Halter <micah@mehalter.com>
    Co-authored-by: Oli <57495944+owittek@users.noreply.github.com>

--- lua/astrocommunity/pack/rust/rust.lua
+++ lua/astrocommunity/pack/rust/rust.lua
@@ -18,7 +18,21 @@ return {
     "simrat39/rust-tools.nvim",
     ft = { "rust" },
     init = function() utils.list_insert_unique(astronvim.lsp.skip_setup, "rust_analyzer") end,
-    opts = function() return { server = require("astronvim.utils.lsp").config "rust_analyzer" } end,
+    opts = function()
+      local package_path = require("mason-registry").get_package("codelldb"):get_install_path()
+      local codelldb_path = package_path .. "/codelldb"
+      local liblldb_path = package_path .. "/extension/lldb/lib/liblldb"
+
+      return {
+        server = require("astronvim.utils.lsp").config "rust_analyzer",
+        dap = {
+          adapter = require("rust-tools.dap").get_codelldb_adapter(
+            codelldb_path,
+            liblldb_path .. (vim.loop.os_uname().sysname == "Linux" and ".so" or ".dylib")
+          ),
+        },
+      }
+    end,
     dependencies = {
       {
         "jay-babu/mason-nvim-dap.nvim",
