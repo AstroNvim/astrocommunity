cfcf2a93508794c6c6b13aebdffc416f589936b5
Charles C <Cretezy@users.noreply.github.com>
2023-03-30 10:26:52 -0400

    feat(motion): add nvim-surround (#110)
    * Add nvim-surround
    
    * Update lua/astrocommunity/motion/nvim-surround/nvim-surround.lua
    
    ---------
    
    Co-authored-by: Micah Halter <micah@mehalter.com>

---
+++ lua/astrocommunity/motion/nvim-surround/nvim-surround.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    opts = {}
+  },
+}
