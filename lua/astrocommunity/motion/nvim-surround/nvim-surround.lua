c1eec6011894d539cb3f3a85306bc172e82687aa
Micah Halter <micah@mehalter.com>
2023-03-30 10:26:32 -0400

    Update lua/astrocommunity/motion/nvim-surround/nvim-surround.lua

--- lua/astrocommunity/motion/nvim-surround/nvim-surround.lua
+++ lua/astrocommunity/motion/nvim-surround/nvim-surround.lua
@@ -3,6 +3,6 @@ return {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-    config = function(_, opts) require("nvim-surround").setup(opts) end,
+    opts = {}
   },
 }
