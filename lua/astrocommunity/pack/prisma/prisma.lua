5f63310d789ac3b8d09bb6e3e4a7fa417a5cc6f9
Miguel Casqueira <20k-ultra@users.noreply.github.com>
2023-03-31 16:05:13 -0400

    feat(pack): add prisma language pack
    Signed-off-by: Miguel Casqueira <20k-ultra@users.noreply.github.com>

---
+++ lua/astrocommunity/pack/prisma/prisma.lua
@@ -0,0 +1,23 @@
+local utils = require "astrocommunity.utils"
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table or string "all".
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      elseif opts.ensure_installed == "all" then
+        return
+      end
+      utils.list_insert_unique(opts.ensure_installed, { "prisma" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      utils.list_insert_unique(opts.ensure_installed, "prismals")
+    end,
+  },
+}
