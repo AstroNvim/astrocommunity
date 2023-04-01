01066bbbd69d022d3162759f1039835cf5273db4
luxus <luxuspur@gmail.com>
2023-04-01 23:18:52 +0200

    fix(motion): make stylua happy

--- lua/astrocommunity/motion/nvim-surround/nvim-surround.lua
+++ lua/astrocommunity/motion/nvim-surround/nvim-surround.lua
@@ -3,6 +3,6 @@ return {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-    opts = {}
+    opts = {},
   },
 }
