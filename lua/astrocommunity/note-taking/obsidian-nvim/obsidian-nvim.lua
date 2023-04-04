2a0423431f5554ef920e085320da0f9713e656de
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-04-04 12:54:13 +0200

    feat(note-taking): add obsidian-nvim

---
+++ lua/astrocommunity/note-taking/obsidian-nvim/obsidian-nvim.lua
@@ -0,0 +1,17 @@
+return {
+  "epwalsh/obsidian.nvim",
+  cmd = {
+    "ObsdianBacklinks",
+    "ObsidianToday",
+    "ObsidianYesterday",
+    "ObsidianOpen",
+    "ObsidianNew",
+    "ObsidianSearch",
+    "ObsidianQuickSwitch",
+    "ObsidianLink",
+    "ObsidianLinkNew",
+    "ObsidianFollowLink",
+    "ObsidianTemplate",
+  },
+  opts = { completion = { nvim_cmp = true } },
+}
