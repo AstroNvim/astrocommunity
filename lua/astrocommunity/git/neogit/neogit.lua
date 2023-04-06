55dba820dfbf2bccdbfae38905fea612440cc00a
taskirel <75871323+taskylizard@users.noreply.github.com>
2023-04-07 00:34:28 +0530

    Update lua/astrocommunity/git/neogit/neogit.lua
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/git/neogit/neogit.lua
+++ lua/astrocommunity/git/neogit/neogit.lua
@@ -7,7 +7,7 @@ return {
     event = "User AstroGitFile",
     keys = {
       { "<leader>gn", desc = "Neogit", mode = { "n" } },
-      { "<leader>gnt", "<cmd>Neogit<CR>", desc = "Open Neoget Tab Page" },
+      { "<leader>gnt", "<cmd>Neogit<CR>", desc = "Open Neogit Tab Page" },
       { "<leader>gnc", "<cmd>Neogit commit<CR>", desc = "Open Neogit Commit Page" },
       { "<leader>gnd", ":Neogit cwd=", desc = "Open Neogit Override CWD" },
       { "<leader>gnk", ":Neogit kind=", desc = "Open Neogit Override Kind" },
