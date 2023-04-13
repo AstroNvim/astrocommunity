76e1a396500008b850403f2ddea038608468693d
434b <admin@0x434b.dev>
2023-04-13 19:46:37 +0200

    fix: key bindings to be more intuitive

--- lua/astrocommunity/motion/harpoon/harpoon.lua
+++ lua/astrocommunity/motion/harpoon/harpoon.lua
@@ -7,7 +7,7 @@ return {
     { prefix, desc = "Harpoon" },
     { prefix .. "a", function() require("harpoon.mark").add_file() end, desc = "Add file" },
     { prefix .. "e", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
-    { prefix .. "h", function() require("harpoon.ui").nav_next() end, desc = "Goto previous mark" },
-    { prefix .. "l", function() require("harpoon.ui").nav_prev() end, desc = "Goto next mark" },
+    { "<C-k>", function() require("harpoon.ui").nav_next() end, desc = "Goto next mark" },
+    { "<C-j>", function() require("harpoon.ui").nav_prev() end, desc = "Goto previous mark" },
   },
 }
