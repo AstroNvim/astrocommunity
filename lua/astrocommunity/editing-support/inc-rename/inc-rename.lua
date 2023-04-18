47b50349315c9a82d6cc59d6b4812a23aa9bff78
434b <admin@0x434b.dev>
2023-04-18 17:31:53 +0200

    feat: add inc-rename

---
+++ lua/astrocommunity/editing-support/inc-rename/inc-rename.lua
@@ -0,0 +1,18 @@
+return {
+  "smjonas/inc-rename.nvim",
+  config = function() 
+    require("inc_rename").setup()
+  end,
+  opts = {},
+  keys = {
+    {
+      "<leader>lr",
+      function()
+        require "inc_rename"
+        return ":IncRename " .. vim.fn.expand "<cword>"
+      end,
+      expr = true,
+      desc = "IncRename",
+    },
+  },
+}
