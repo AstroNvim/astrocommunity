b983cd0c9c26819ae50bbb576777949e1f86d6ec
sunlin92 <40912737+sunlin92@users.noreply.github.com>
2023-04-26 22:25:13 +0800

    feat(pack): add vue (#164)

---
+++ lua/astrocommunity/pack/vue/vue.lua
@@ -0,0 +1,34 @@
+local utils = require "astrocommunity.utils"
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table or string "all".
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      elseif opts.ensure_installed == "all" then
+        return
+      end
+      -- Add the required file types to opts.ensure_installed.
+      utils.list_insert_unique(opts.ensure_installed, "vue")
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      -- Add tsserver to opts.ensure_installed using vim.list_extend.
+      utils.list_insert_unique(opts.ensure_installed, "volar")
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      -- Add to opts.ensure_installed using table.insert.
+      utils.list_insert_unique(opts.ensure_installed, "js")
+    end,
+  },
+}
