dc9f3a8f21031f2fa1e762633fe2a56fc13c9331
Santiago Garcia <santiago.garcia@posteo.de>
2023-04-27 05:46:50 +0200

    Format with stylua

--- lua/astrocommunity/pack/python/python.lua
+++ lua/astrocommunity/pack/python/python.lua
@@ -49,7 +49,7 @@ return {
     -- This is needed for pylint to work in a virtualenv. See https://github.com/williamboman/mason.nvim/issues/668#issuecomment-1320859097
     "williamboman/mason.nvim",
     opts = {
-      PATH = "append"
-    }
-  }
+      PATH = "append",
+    },
+  },
 }
