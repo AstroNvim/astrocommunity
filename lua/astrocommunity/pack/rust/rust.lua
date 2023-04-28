b7e2ced31b35a768104eb5be16a5016331578f01
xoffio <marquessricardo@gmail.com>
2023-04-27 23:16:28 -0400

    fix: codelldb pointing to different path in win

--- lua/astrocommunity/pack/rust/rust.lua
+++ lua/astrocommunity/pack/rust/rust.lua
@@ -22,13 +22,23 @@ return {
       local package_path = require("mason-registry").get_package("codelldb"):get_install_path()
       local codelldb_path = package_path .. "/codelldb"
       local liblldb_path = package_path .. "/extension/lldb/lib/liblldb"
+      local this_os = vim.loop.os_uname().sysname;
+
+      -- The path in windows is different
+      if this_os:find 'Windows' then
+        codelldb_path = package_path .. "\\extension\\adapter\\codelldb.exe"
+        liblldb_path = package_path .. "\\extension\\lldb\\bin\\liblldb.dll"
+      else
+        -- The liblldb extension is .so for linux and .dylib for macOS
+        liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+      end
 
       return {
         server = require("astronvim.utils.lsp").config "rust_analyzer",
         dap = {
           adapter = require("rust-tools.dap").get_codelldb_adapter(
             codelldb_path,
-            liblldb_path .. (vim.loop.os_uname().sysname == "Linux" and ".so" or ".dylib")
+            liblldb_path
           ),
         },
       }
