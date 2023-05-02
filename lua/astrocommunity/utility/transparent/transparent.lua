9c2209768d821c3e95e88f4985799a887407ae9a
434b <admin@0x434b.dev>
2023-05-02 08:47:02 +0200

    fix: lint

--- lua/astrocommunity/utility/transparent/transparent.lua
+++ lua/astrocommunity/utility/transparent/transparent.lua
@@ -3,14 +3,35 @@ return {
   "xiyaowong/transparent.nvim",
   lazy = false,
   opts = {
-    groups = { -- table: default groups
-      'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
-      'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
-      'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
-      'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+    -- table: default groups
+    groups = {
+      "Normal",
+      "NormalNC",
+      "Comment",
+      "Constant",
+      "Special",
+      "Identifier",
+      "Statement",
+      "PreProc",
+      "Type",
+      "Underlined",
+      "Todo",
+      "String",
+      "Function",
+      "Conditional",
+      "Repeat",
+      "Operator",
+      "Structure",
+      "LineNr",
+      "NonText",
+      "SignColumn",
+      "CursorLineNr",
+      "EndOfBuffer",
     },
-    extra_groups = {},   -- table: additional groups that should be cleared
-    exclude_groups = {}, -- table: groups you don't want to clear
+    -- table: additional groups that should be cleared
+    extra_groups = {},
+    -- table: groups you don't want to clear
+    exclude_groups = {},
   },
   keys = {
     { prefix .. "T", "<cmd>TransparentToggle<CR>", desc = "Toggle transparency" },
