49e9a3961bba079d7f413b8d5567382dd6f55392
434b <17012133+0xricksanchez@users.noreply.github.com>
2023-05-02 11:15:00 +0200

    feat: add transparent plugin (#180)

---
+++ lua/astrocommunity/utility/transparent-nvim/transparent-nvim.lua
@@ -0,0 +1,42 @@
+local prefix = "<leader>u"
+return {
+  "xiyaowong/transparent.nvim",
+  lazy = false,
+  opts = {
+    -- table: default groups
+    groups = {
+      "Normal",
+      "NormalNC",
+      "Comment",
+      "Constant",
+      "Special",
+      "Identifier",
+      "Statement",
+      "PreProc",
+      "Type",
+      "Underlined",
+      "Todo",
+      "String",
+      "Function",
+      "Conditional",
+      "Repeat",
+      "Operator",
+      "Structure",
+      "LineNr",
+      "NonText",
+      "SignColumn",
+      "CursorLineNr",
+      "EndOfBuffer",
+    },
+    -- table: additional groups that should be cleared
+    extra_groups = {
+      "NormalFloat",
+      "NvimTreeNormal",
+    },
+    -- table: groups you don't want to clear
+    exclude_groups = {},
+  },
+  keys = {
+    { prefix .. "T", "<cmd>TransparentToggle<CR>", desc = "Toggle transparency" },
+  },
+}
