c0b2d2f024162bc368b424718963d7aabcb950ec
Samuel Cobb <sjcobb2003@gmail.com>
2023-05-02 11:16:57 +0100

    fix: working haskell debug adapter

--- lua/astrocommunity/pack/haskell/haskell.lua
+++ lua/astrocommunity/pack/haskell/haskell.lua
@@ -1,43 +1,70 @@
-local utils = require "astronvim.utils"
+local utils = require "astrocommunity.utils"
 return {
   { import = "astrocommunity.pack.yaml" }, -- stack.yaml
   { import = "astrocommunity.pack.json" }, -- hls.json
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "haskell")
+      -- Ensure that opts.ensure_installed exists and is a table or string "all".
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      elseif opts.ensure_installed == "all" then
+        return
       end
+      -- ensure haskell treesitter installed
+      utils.list_insert_unique(opts.ensure_installed, "haskell")
     end,
   },
+
   {
     "mrcjkb/haskell-tools.nvim",
     ft = { "haskell" },
     branch = "1.x.x", -- reccomended by haskell-tools
-    init = function() astronvim.lsp.skip_setup = utils.list_insert_unique(astronvim.lsp.skip_setup, "hls") end,
+    init = function() utils.list_insert_unique(astronvim.lsp.skip_setup, "hls") end,
     opts = {
       hls = {
-        on_attach = function(client, bufnr) require("astronvim.utils.lsp").on_attach(client, bufnr) end,
+        on_attach = function(client, bufnr)
+          local ht = require "haskell-tools"
+          require("astronvim.utils.lsp").on_attach(client, bufnr)
+        end,
       },
     },
     config = function(_, opts)
       vim.api.nvim_create_autocmd("Filetype", {
         pattern = "haskell", -- autocmd to start haskell-tools
-        callback = function(_) require("haskell-tools").start_or_attach(opts) end,
+        callback = function() require("haskell-tools").start_or_attach(opts) end,
+      })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        pattern = "*.hs", -- autocmd to start haskell-tools
+        callback = function(args)
+          local ht = require "haskell-tools"
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client.name == "haskell-tools.nvim" then ht.dap.discover_configurations(args.buf) end
+        end,
       })
     end,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim", -- optional
-      -- TODO: Need haskell to be pulled into mason-nvim-dap (haskell hopefully coming soon)
-      -- {
-      --   "jay-babu/mason-nvim-dap.nvim",
-      --   opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "haskell") end,
-      -- },
     },
   },
+
   {
     "williamboman/mason-lspconfig.nvim",
-    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "hls") end,
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      utils.list_insert_unique(opts.ensure_installed, "hls")
+    end,
+  },
+
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table.
+      if not opts.ensure_installed then opts.ensure_installed = {} end
+      utils.list_insert_unique(opts.ensure_installed, "haskell")
+    end,
   },
 }
