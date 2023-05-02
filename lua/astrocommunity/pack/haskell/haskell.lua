d9fb1e9c26c9abbed6be038f58291fa63d7b4470
Samuel Cobb <sjcobb2003@gmail.com>
2023-05-02 12:03:46 +0100

    fix: api changes

--- lua/astrocommunity/pack/haskell/haskell.lua
+++ lua/astrocommunity/pack/haskell/haskell.lua
@@ -1,26 +1,20 @@
-local utils = require "astrocommunity.utils"
+local utils = require "astronvim.utils"
 return {
   { import = "astrocommunity.pack.yaml" }, -- stack.yaml
   { import = "astrocommunity.pack.json" }, -- hls.json
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      -- Ensure that opts.ensure_installed exists and is a table or string "all".
-      if not opts.ensure_installed then
-        opts.ensure_installed = {}
-      elseif opts.ensure_installed == "all" then
-        return
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "haskell")
       end
-      -- ensure haskell treesitter installed
-      utils.list_insert_unique(opts.ensure_installed, "haskell")
     end,
   },
-
   {
     "mrcjkb/haskell-tools.nvim",
     ft = { "haskell" },
     branch = "1.x.x", -- reccomended by haskell-tools
-    init = function() utils.list_insert_unique(astronvim.lsp.skip_setup, "hls") end,
+    init = function() astronvim.lsp.skip_setup = utils.list_insert_unique(astronvim.lsp.skip_setup, "hls") end,
     opts = {
       hls = {
         on_attach = function(client, bufnr) require("astronvim.utils.lsp").on_attach(client, bufnr) end,
@@ -45,22 +39,12 @@ return {
       "nvim-telescope/telescope.nvim", -- optional
     },
   },
-
   {
     "williamboman/mason-lspconfig.nvim",
-    opts = function(_, opts)
-      -- Ensure that opts.ensure_installed exists and is a table.
-      if not opts.ensure_installed then opts.ensure_installed = {} end
-      utils.list_insert_unique(opts.ensure_installed, "hls")
-    end,
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "hls") end,
   },
-
   {
     "jay-babu/mason-nvim-dap.nvim",
-    opts = function(_, opts)
-      -- Ensure that opts.ensure_installed exists and is a table.
-      if not opts.ensure_installed then opts.ensure_installed = {} end
-      utils.list_insert_unique(opts.ensure_installed, "haskell")
-    end,
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "haskell") end,
   },
 }
