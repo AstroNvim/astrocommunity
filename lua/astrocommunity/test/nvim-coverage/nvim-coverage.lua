99a484fd5564551130c375a37df259ef1001e1ee
434b <17012133+0xricksanchez@users.noreply.github.com>
2023-05-04 11:54:46 +0200

    Update lua/astrocommunity/test/nvim-coverage/nvim-coverage.lua
    Co-authored-by: Oli <57495944+owittek@users.noreply.github.com>

--- lua/astrocommunity/test/nvim-coverage/nvim-coverage.lua
+++ lua/astrocommunity/test/nvim-coverage/nvim-coverage.lua
@@ -4,5 +4,4 @@ return {
   requires = { "nvim-lua/plenary.nvim" },
   -- Optional: needed for PHP when using the cobertura parser
   rocks = { "lua-xmlreader" },
-  config = function() require("coverage").setup() end,
 }
