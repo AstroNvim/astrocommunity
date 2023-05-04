29e95504138f913f101f5ff392c9aaa2e0250de4
434b <admin@0x434b.dev>
2023-05-04 11:57:47 +0200

    fix(nvim-coverage): requested changes

--- lua/astrocommunity/test/nvim-coverage/nvim-coverage.lua
+++ lua/astrocommunity/test/nvim-coverage/nvim-coverage.lua
@@ -2,6 +2,4 @@ return {
   "andythigpen/nvim-coverage",
   event = "User AstroFile",
   requires = { "nvim-lua/plenary.nvim" },
-  -- Optional: needed for PHP when using the cobertura parser
-  rocks = { "lua-xmlreader" },
 }
