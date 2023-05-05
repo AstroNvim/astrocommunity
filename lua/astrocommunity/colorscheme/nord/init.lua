08f1f1a6e9be41d3092f417a82bff92a28e24d0a
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2023-05-05 13:59:07 +0530

    Update init.lua

--- lua/astrocommunity/colorscheme/nord/init.lua
+++ lua/astrocommunity/colorscheme/nord/init.lua
@@ -1,19 +1,3 @@
 return {
   'shaunsingh/nord.nvim',
-  opts = function(_, opts)
-     markdown = {
-        headline_highlights = {
-            "Headline1",
-            "Headline2",
-            "Headline3",
-            "Headline4",
-            "Headline5",
-            "Headline6",
-        },
-        codeblock_highlight = "CodeBlock",
-        dash_highlight = "Dash",
-        quote_highlight = "Quote",
-    }
-    return opts
-  end,
 }
