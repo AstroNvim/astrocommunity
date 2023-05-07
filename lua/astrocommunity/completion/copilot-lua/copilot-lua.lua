da920bd3b0ba0e302c6dfeceb5d9c29f648f3251
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-05-07 13:36:31 +0200

    fix(completion): use list_insert_unique in copilot-lua

--- lua/astrocommunity/completion/copilot-lua/copilot-lua.lua
+++ lua/astrocommunity/completion/copilot-lua/copilot-lua.lua
@@ -1,6 +1,9 @@
+local utils = require "astronvim.utils"
 return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   event = "User AstroFile",
-  opts = { suggestion = { auto_trigger = true, debounce = 150 } },
+  opts = function(_, opts)
+    opts.suggestion = utils.list_insert_unique(opts.suggestion, { auto_trigger = true, debounce = 150 })
+  end,
 }
