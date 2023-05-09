73e105b0e918a9c511d8e4321be0dd2abdb3694d
Sourav Pakhira <pakhira55@gmail.com>
2023-05-09 21:25:45 +0530

    feat: added docker pack

--- lua/astrocommunity/pack/docker/docker.lua
+++ lua/astrocommunity/pack/docker/docker.lua
@@ -1,15 +1,14 @@
 local utils = require "astronvim.utils"
 return {
-	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = function(_, opts)
-			opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "docker-compose-language-service", "dockerfile-language-server")
-		end,
-	},
-	{
-		"jay-babu/mason-null-ls.nvim",
-		opts = function(_, opts)
-			opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "hadolint")
-		end,
-	},
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed =
+        utils.list_insert_unique(opts.ensure_installed, "docker-compose-language-service", "dockerfile-language-server")
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "hadolint") end,
+  },
 }
