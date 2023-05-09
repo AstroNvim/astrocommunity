f074d564db7310d2c309086eb3cfc3e15e2d637e
Souravpakhira <pakhira55@gmail.com>
2023-05-09 21:53:29 +0530

    feat(pack): added docker pack (#193)
    * feat: added docker pack
    
    * feat: added docker pack
    
    feat: added docker pack
    
    * feat: added docker pack
    
    * feat: added docker pack
    
    * feat: added docker pack

---
+++ lua/astrocommunity/pack/docker/README.md
@@ -0,0 +1,7 @@
+# Docker Language Pack
+
+This plugin pack does the following:
+
+- Adds `dockerfile` Treesitter parser
+- Adds `dockerfile-language-server` and `docker-compose-language-service` language servers
+- Adds `hadolint` linter
---
+++ lua/astrocommunity/pack/docker/docker.lua
@@ -0,0 +1,24 @@
+local utils = require "astronvim.utils"
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "dockerfile")
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = utils.list_insert_unique(
+        opts.ensure_installed,
+        { "docker-compose-language-service", "dockerfile-language-server" }
+      )
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "hadolint") end,
+  },
+}
