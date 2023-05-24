ae48403d88fe5b34bd5413744386bcd658abd107
Jörg Thalheim <Mic92@users.noreply.github.com>
2023-05-24 12:59:29 +0100

    fix(pack): gracefully rust plugin if codelldb is not available (#216)
    * fix(pack): gracefully rust plugin if codelldb is not available
    
    We have this issue on NixOS but potentiall also other operating systems
    such as *BSDs and non-mainstream cpu architectures are affected by this.
    
    * refactor(pack): improve rust debugger logic performance
    
    ---------
    
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/pack/rust/rust.lua
+++ lua/astrocommunity/pack/rust/rust.lua
@@ -14,26 +14,28 @@ return {
     ft = { "rust" },
     init = function() astronvim.lsp.skip_setup = utils.list_insert_unique(astronvim.lsp.skip_setup, "rust_analyzer") end,
     opts = function()
-      local package_path = require("mason-registry").get_package("codelldb"):get_install_path()
-      local codelldb_path = package_path .. "/codelldb"
-      local liblldb_path = package_path .. "/extension/lldb/lib/liblldb"
-      local this_os = vim.loop.os_uname().sysname
+      local adapter
+      local success, package = pcall(function() return require("mason-registry").get_package "codelldb" end)
+      if success then
+        local package_path = package:get_install_path()
+        local codelldb_path = package_path .. "/codelldb"
+        local liblldb_path = package_path .. "/extension/lldb/lib/liblldb"
+        local this_os = vim.loop.os_uname().sysname
 
-      -- The path in windows is different
-      if this_os:find "Windows" then
-        codelldb_path = package_path .. "\\extension\\adapter\\codelldb.exe"
-        liblldb_path = package_path .. "\\extension\\lldb\\bin\\liblldb.dll"
+        -- The path in windows is different
+        if this_os:find "Windows" then
+          codelldb_path = package_path .. "\\extension\\adapter\\codelldb.exe"
+          liblldb_path = package_path .. "\\extension\\lldb\\bin\\liblldb.dll"
+        else
+          -- The liblldb extension is .so for linux and .dylib for macOS
+          liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+        end
+        adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
       else
-        -- The liblldb extension is .so for linux and .dylib for macOS
-        liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+        adapter = require("rust-tools.dap").get_codelldb_adapter()
       end
 
-      return {
-        server = require("astronvim.utils.lsp").config "rust_analyzer",
-        dap = {
-          adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-        },
-      }
+      return { server = require("astronvim.utils.lsp").config "rust_analyzer", dap = { adapter = adapter } }
     end,
     dependencies = {
       {
