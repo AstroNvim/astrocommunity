03dda0bb00490cce602cd1583831a2cb0f7fabd8
Zeioth <zeioth@hotmail.com>
2023-05-26 04:06:19 +0200

    Let's not give the html_output option, so it's detected automatically for windows too.

--- lua/astrocommunity/markdown-and-latex/markmap/markmap-nvim.lua
+++ lua/astrocommunity/markdown-and-latex/markmap/markmap-nvim.lua
@@ -5,9 +5,7 @@ return {
   build = "yarn global add markmap-cli",
   cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
   opts = {
-    html_output = "/tmp/markmap.html",
     hide_toolbar = "false",
-    grace_period = 3600000,
   },
   config = function(_, opts) require("markmap").setup(opts) end,
 }
