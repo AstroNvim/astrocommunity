ef8eee381efc25b22d46bc27d7c346a06f1e8508
Micah Halter <micah@mehalter.com>
2023-05-26 11:02:41 -0400

    fix(markmap-nvim): fix issues with markmap.nvim plugin

--- lua/astrocommunity/markdown-and-latex/markmap-nvim/markmap.lua
+++ lua/astrocommunity/markdown-and-latex/markmap-nvim/markmap.lua
@@ -2,7 +2,6 @@
 --  https://github.com/Zeioth/markmap.nvim
 return {
   "Zeioth/markmap.nvim",
-  build = "npm install -g markmap-cli",
   cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
   opts = {
     hide_toolbar = "false",
--- lua/astrocommunity/markdown-and-latex/markmap/README.md
+++
@@ -1,7 +0,0 @@
-# markmap.nvim
-
-**Repository:** <https://github.com/Zeioth/markmap.nvim>
-
-Visualize your Markdown as mindmaps with markmap.
-
-**Note:** This will install `markmap-cli` globally on your system with `npm`.
