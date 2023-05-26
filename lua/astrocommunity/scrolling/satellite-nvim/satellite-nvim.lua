391eaccf6020178e5cd1d5350394583aa9a4f06d
Micah Halter <micah@mehalter.com>
2023-05-26 16:15:04 -0400

    feat(satellite-nvim): add interoperability with zen-mode

--- lua/astrocommunity/scrolling/satellite-nvim/satellite-nvim.lua
+++ lua/astrocommunity/scrolling/satellite-nvim/satellite-nvim.lua
@@ -1,5 +1,22 @@
-return { -- scrollbar
-  "lewis6991/satellite.nvim",
-  event = "User AstroFile",
-  opts = { excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify", "neo-tree" } },
+return {
+  { -- scrollbar
+    "lewis6991/satellite.nvim",
+    event = "User AstroFile",
+    opts = { excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify", "neo-tree" } },
+  },
+  {
+    "folke/zen-mode.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local old_on_open, old_on_close = opts.on_open, opts.on_close
+      opts.on_open = function()
+        old_on_open()
+        vim.cmd.SatelliteDisable()
+      end
+      opts.on_close = function()
+        old_on_close()
+        vim.cmd.SatelliteEnable()
+      end
+    end,
+  },
 }
