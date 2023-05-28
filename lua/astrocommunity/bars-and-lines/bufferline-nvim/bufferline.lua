399db566d51e713b84a1a200d63d657727cf6f1a
TheSast <27977196+TheSast@users.noreply.github.com>
2023-05-28 15:08:33 +0200

    chore: add 'optional' where applicable

--- lua/astrocommunity/bars-and-lines/bufferline-nvim/bufferline.lua
+++ lua/astrocommunity/bars-and-lines/bufferline-nvim/bufferline.lua
@@ -2,6 +2,7 @@ return {
   { "akinsho/bufferline.nvim", event = "VeryLazy", opts = {} },
   {
     "rebelot/heirline.nvim",
+    optional = true,
     opts = function(opts) opts.tabline = nil end,
   },
 }
