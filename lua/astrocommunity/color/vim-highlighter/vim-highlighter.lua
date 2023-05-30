0bf3dbffff118f374ca594bdc79213ff90f418e7
Hugo-CASTELL <hugo.castell@outlook.fr>
2023-05-30 08:33:15 +0200

    feat: add vim-highlighter

--- lua/astrocommunity/color/vim-highlighter/vim-highlighter.lua
+++ lua/astrocommunity/color/vim-highlighter/vim-highlighter.lua
@@ -4,13 +4,13 @@ return {
   keys = {
     -- These are basing keymaps to guide new users
     { "f<Enter>", desc = "Highlight" },
-    { "f<BS>",    desc = "Remove Highlight" },
-    { "f<C-L>",   desc = "Clear Highlight" },
-    { "f<Tab>",   desc = "Find Highlight (similar to Telescope grep)" },
+    { "f<BS>", desc = "Remove Highlight" },
+    { "f<C-L>", desc = "Clear Highlight" },
+    { "f<Tab>", desc = "Find Highlight (similar to Telescope grep)" },
     -- They are derivated from the default keymaps, see README.md to github repo for documentation
-    { "nn",       "<cmd>Hi><CR>",                                     desc = "Next Recently Set Highlight" },
-    { "ng",       "<cmd>Hi<<CR>",                                     desc = "Previous Recently Set Highlight" },
-    { "n[",       "<cmd>Hi{<CR>",                                     desc = "Next Nearest Highlight" },
-    { "n]",       "<cmd>Hi}<CR>",                                     desc = "Previous Nearest Highlight" },
+    { "nn", "<cmd>Hi><CR>", desc = "Next Recently Set Highlight" },
+    { "ng", "<cmd>Hi<<CR>", desc = "Previous Recently Set Highlight" },
+    { "n[", "<cmd>Hi{<CR>", desc = "Next Nearest Highlight" },
+    { "n]", "<cmd>Hi}<CR>", desc = "Previous Nearest Highlight" },
   },
 }
