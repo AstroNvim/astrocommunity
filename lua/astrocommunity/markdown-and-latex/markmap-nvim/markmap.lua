abd7777e9d50feed3c56b8e446b89596b4025784
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-06-04 17:48:11 -0400

    feat(markdown-and-latex): install markmap from mason (#258)

--- lua/astrocommunity/markdown-and-latex/markmap-nvim/markmap.lua
+++ lua/astrocommunity/markdown-and-latex/markmap-nvim/markmap.lua
@@ -1,3 +1,5 @@
+local utils = require "astronvim.utils"
+
 --  [markdown markmap]
 --  https://github.com/Zeioth/markmap.nvim
 return {
@@ -5,5 +7,10 @@ return {
   cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
   opts = {
     hide_toolbar = "false",
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "markmap" }) end,
   },
 }
