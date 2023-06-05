a6fb3506d7999204233e37d03ac1b25d3ee993e3
Oli <57495944+owittek@users.noreply.github.com>
2023-06-05 13:07:39 +0200

    feat(pack): add html-css base pack (#260)
    * feat(pack): add html-css base pack
    
    * feat(pack): remove html-css base pack from typescript packs

---
+++ lua/astrocommunity/pack/html-css/html-css.lua
@@ -0,0 +1,19 @@
+local utils = require "astronvim.utils"
+
+return {
+  { import = "astrocommunity.pack.json" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "html", "css" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "html", "cssls" })
+    end,
+  },
+}
