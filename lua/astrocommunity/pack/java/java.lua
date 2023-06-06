89f0f756ad382e991ed9d0233c28cc74e926ad0e
Samuel Cobb <sjcobb2003@gmail.com>
2023-06-06 20:12:51 +0100

    no dependencies test

--- lua/astrocommunity/pack/java/java.lua
+++ lua/astrocommunity/pack/java/java.lua
@@ -32,13 +32,11 @@ return {
     "mfussenegger/nvim-jdtls",
     ft = { "java" },
     init = function() astronvim.lsp.skip_setup = utils.list_insert_unique(astronvim.lsp.skip_setup, "jdtls") end,
-    -- dependencies = { "williamboman/mason-lspconfig.nvim",  },
+    -- dependencies = { "williamboman/mason-lspconfig.nvim" },
     opts = function(_, opts)
       -- use this function notation to build some variables
       local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle", ".project" }
       local root_dir = require("jdtls.setup").find_root(root_markers)
-      require "mason-lspconfig"
-
       -- calculate workspace dir
       local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
       local workspace_dir = vim.fn.stdpath "data" .. "/site/java/workspace-root/" .. project_name
