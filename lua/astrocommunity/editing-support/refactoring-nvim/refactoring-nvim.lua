e438b4b2cffceff2a60b131e495bb34311138a7c
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-06-06 15:37:28 -0400

    feat(editing-support): add keymaps to refactoring-nvim (#264)
    * feat(editing-support): add keymaps to refactoring-nvim
    
    * Update lua/astrocommunity/editing-support/refactoring-nvim/refactoring-nvim.lua
    
    Co-authored-by: Micah Halter <micah@mehalter.com>
    
    * fix formatting
    
    ---------
    
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/editing-support/refactoring-nvim/refactoring-nvim.lua
+++ lua/astrocommunity/editing-support/refactoring-nvim/refactoring-nvim.lua
@@ -3,4 +3,97 @@ return {
   dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
   event = "User AstroFile",
   opts = {},
+  keys = {
+    {
+      "<leader>re",
+      [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR>]],
+      { silent = true, expr = false },
+      mode = {
+        "v",
+      },
+      desc = "Extract Function",
+    },
+    {
+      "<leader>rf",
+      [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>]],
+      { silent = true, expr = false },
+      mode = {
+        "v",
+      },
+      desc = "Extract Function To File",
+    },
+    {
+      "<leader>rv",
+      [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]],
+      { silent = true, expr = false },
+      mode = {
+        "v",
+      },
+      desc = "Extract Variable",
+    },
+    {
+      "<leader>ri",
+      [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]],
+      { silent = true, expr = false },
+      mode = {
+        "v",
+      },
+      desc = "Inline Variable",
+    },
+    {
+      "<leader>rb",
+      function() require("refactoring").refactor "Extract Block" end,
+      { silent = true, expr = false },
+      mode = {
+        "n",
+      },
+      desc = "Extract Block",
+    },
+    {
+      "<leader>rbf",
+      function() require("refactoring").refactor "Extract Block To File" end,
+      { silent = true, expr = false },
+      mode = {
+        "n",
+      },
+      desc = "Extract Block To File",
+    },
+    {
+      "<leader>ri",
+      function() require("refactoring").refactor "Inline Variable" end,
+      { silent = true, expr = false },
+      mode = { "n" },
+      desc = "Inline Variable",
+    },
+    {
+      "<leader>rr",
+      function() require("refactoring").select_refactor() end,
+      { silent = true, expr = false },
+      desc = "Select Refactor",
+    },
+    {
+      "<leader>rp",
+      function() require("refactoring").debug.printf { below = false } end,
+      mode = { "n" },
+      desc = "Debug: Print Function",
+    },
+    {
+      "<leader>rv",
+      function() require("refactoring").debug.print_var { normal = true, below = false } end,
+      mode = { "n" },
+      desc = "Debug: Print Variable",
+    },
+    {
+      "<leader>rv",
+      function() require("refactoring").debug.print_var { below = false } end,
+      mode = { "v" },
+      desc = "Debug: Print Variable",
+    },
+    {
+      "<leader>rc",
+      function() require("refactoring").debug.cleanup {} end,
+      mode = { "n" },
+      desc = "Debug: Clean Up",
+    },
+  },
 }
