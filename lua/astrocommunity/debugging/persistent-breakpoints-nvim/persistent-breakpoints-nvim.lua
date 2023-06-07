3e335966e591dbcef90df2e7dcac9d8f39f02728
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-06-06 20:35:39 -0400

    feat(debugging): add persistent-breakpoints.nvim

---
+++ lua/astrocommunity/debugging/persistent-breakpoints-nvim/persistent-breakpoints-nvim.lua
@@ -0,0 +1,28 @@
+return {
+  "Weissle/persistent-breakpoints.nvim",
+  opts = function(_, opts)
+    return require("astronvim.utils").extend_tbl(opts, {
+      load_breakpoints_event = { "BufReadPost" },
+    })
+  end,
+  keys = {
+    {
+      "<leader>db",
+      function() require("persistent-breakpoints.api").toggle_breakpoint() end,
+      { silent = true },
+      desc = "Toggle Breakpoint",
+    },
+    {
+      "<leader>dB",
+      function() require("persistent-breakpoints.api").clear_all_breakpoints() end,
+      { silent = true },
+      desc = "Clear Breakpoints",
+    },
+    {
+      "<leader>dC",
+      function() require("persistent-breakpoints.api").set_conditional_breakpoint() end,
+      { silent = true },
+      desc = "Conditional Breakpoint",
+    },
+  },
+}
