2e2d54be1bd761df8860f8140a4a420efae48900
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-06-07 22:38:19 -0400

    fix: return opts

--- lua/astrocommunity/editing-support/cutlass-nvim/cutlass-nvim.lua
+++ lua/astrocommunity/editing-support/cutlass-nvim/cutlass-nvim.lua
@@ -6,5 +6,6 @@ return {
   opts = function(_, opts)
     if utils.is_available "leap.nvim" then opts.exclude = utils.list_insert_unique(opts.exclude, { "ns", "nS" }) end
     if utils.is_available "hop.nvim" then opts.exclude = utils.list_insert_unique(opts.exclude, { "ns", "nS" }) end
+    return opts
   end,
 }
