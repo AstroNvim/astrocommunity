569f4f8ca83e8d3233e0d43944e8ae0ea3c0cb07
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-06-09 17:17:19 +0600

    feat(pack): add luacheck linter to lua pack (#270)

--- lua/astrocommunity/pack/lua/lua.lua
+++ lua/astrocommunity/pack/lua/lua.lua
@@ -14,6 +14,8 @@ return {
   },
   {
     "jay-babu/mason-null-ls.nvim",
-    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "stylua") end,
+    opts = function(_, opts)
+      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "stylua", "luacheck" })
+    end,
   },
 }
