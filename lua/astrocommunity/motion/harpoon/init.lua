ba26fe2badb941da9eb80981585b5c79a3409e02
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-06-20 23:00:17 +0600

    fix(harpoon): which key display fix

--- lua/astrocommunity/motion/harpoon/init.lua
+++ lua/astrocommunity/motion/harpoon/init.lua
@@ -1,4 +1,13 @@
 local prefix = "<leader><leader>"
+local dynamic_tmux_keymap_desc
+local running_tmux_seesion = vim.fn.exists "$TMUX" == 1
+if running_tmux_seesion then
+  dynamic_tmux_keymap_desc = "Goto to TMUX window"
+else
+  dynamic_tmux_keymap_desc = "Goto to terminal window"
+end
+local icon
+if vim.g.icons_enabled then icon = "󱡀 " else icon = "" end
 return {
   "ThePrimeagen/harpoon",
   dependencies = {
@@ -7,7 +16,7 @@ return {
   },
   cmd = { "Harpoon" },
   keys = {
-    { prefix, desc = "Harpoon" },
+    { prefix, function() end, desc = icon .. "Harpoon" },
     { prefix .. "a", function() require("harpoon.mark").add_file() end, desc = "Add file" },
     { prefix .. "e", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
     { "<C-p>", function() require("harpoon.ui").nav_prev() end, desc = "Goto previous mark" },
