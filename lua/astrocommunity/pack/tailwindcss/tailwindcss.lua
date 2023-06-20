87ca14d35947c488e31b45d8ebed2c990051806b
Charles C <Cretezy@users.noreply.github.com>
2023-06-20 16:20:44 -0400

    Remove rustywind from tailwindcss pack

--- lua/astrocommunity/pack/tailwindcss/tailwindcss.lua
+++ lua/astrocommunity/pack/tailwindcss/tailwindcss.lua
@@ -17,7 +17,7 @@ return {
   {
     "jay-babu/mason-null-ls.nvim",
     opts = function(_, opts)
-      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "prettierd", "rustywind" })
+      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "prettierd" })
     end,
   },
   {
