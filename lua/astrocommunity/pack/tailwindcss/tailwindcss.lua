fa75e42670aecbe5dec84039a3e567e285396409
Josh <56745535+Subjective@users.noreply.github.com>
2023-06-21 11:01:03 -0700

    fix(pack/tailwindcss): use icon when color is not available for cmp

--- lua/astrocommunity/pack/tailwindcss/tailwindcss.lua
+++ lua/astrocommunity/pack/tailwindcss/tailwindcss.lua
@@ -28,6 +28,7 @@ return {
       opts.formatting.format = function(entry, item)
         if item.kind == "Color" then
           item = require("cmp-tailwind-colors").format(entry, item)
+          if item.kind == "Color" then return format_kinds(entry, item) end
           return item
         end
         return format_kinds(entry, item)
