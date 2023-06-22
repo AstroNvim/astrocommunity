9bb074739673af2fa35a2f0f30908291f4fb48b9
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2023-06-23 00:06:03 +0600

    feat(harpoon)!: remove "j" keymap, make "t" keymap dynamic based on $TMUX (#292)
    * fix(harpoon): which key display fix
    
    * fix(harpoon): error when user doesn't input terminal number
    
    * feat(harpoon)!: remove "j" keymap, make "t" keymap dynamic based on $TMUX
    
    * feat(harpoon): add keymap to jump mark index
    
    * chore(harpoon): fix typos, simplify variable setting, change formatting
    
    Co-authored-by: Micah Halter <micah@mehalter.com>
    
    ---------
    
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/motion/harpoon/init.lua
+++ lua/astrocommunity/motion/harpoon/init.lua
@@ -1,4 +1,7 @@
 local prefix = "<leader><leader>"
+local running_tmux_session = vim.fn.exists "$TMUX" == 1
+local dynamic_tmux_keymap_desc = "Go to " .. running_tmux_session and "TMUX" or "terminal" .. " window"
+local icon = vim.g.icons_enabled and "󱡀 " or ""
 return {
   "ThePrimeagen/harpoon",
   dependencies = {
@@ -7,27 +10,35 @@ return {
   },
   cmd = { "Harpoon" },
   keys = {
-    { prefix, desc = "Harpoon" },
+    { prefix, function() end, desc = icon .. "Harpoon" },
     { prefix .. "a", function() require("harpoon.mark").add_file() end, desc = "Add file" },
     { prefix .. "e", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
+    {
+      "<C-t>",
+      function()
+        local num = tonumber(vim.fn.input "Go to mark index: ")
+        if num == nil then return end
+        require("harpoon.ui").nav_file(num)
+      end,
+      desc = "Goto index of mark",
+    },
     { "<C-p>", function() require("harpoon.ui").nav_prev() end, desc = "Goto previous mark" },
     { "<C-n>", function() require("harpoon.ui").nav_next() end, desc = "Goto next mark" },
     { prefix .. "m", "<cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" },
     {
       prefix .. "t",
       function()
-        local num = tonumber(vim.fn.input "GoTo terminal window number: ")
-        require("harpoon.term").gotoTerminal(num)
+        if running_tmux_session then
+          local num = tonumber(vim.fn.input "GoTo Tmux window number: ")
+          if num == nil then return end
+          require("harpoon.tmux").gotoTerminal(num)
+        else
+          local num = tonumber(vim.fn.input "GoTo terminal window number: ")
+          if num == nil then return end
+          require("harpoon.term").gotoTerminal(num)
+        end
       end,
-      desc = "Goto to terminal window",
-    },
-    {
-      prefix .. "j",
-      function()
-        local num = tonumber(vim.fn.input "GoTo Tmux window number: ")
-        require("harpoon.tmux").gotoTerminal(num)
-      end,
-      desc = "Goto to TMUX tmux window",
+      desc = dynamic_tmux_keymap_desc,
     },
   },
 }
