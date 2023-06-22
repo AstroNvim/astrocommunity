289482bcadc381d49c1fbd7c13718de1745d8a1f
Micah Halter <micah@mehalter.com>
2023-06-22 12:24:17 -0400

    simplify implementation

--- lua/astrocommunity/code-runner/compiler-nvim/compiler-nvim.lua
+++ lua/astrocommunity/code-runner/compiler-nvim/compiler-nvim.lua
@@ -1,24 +1,22 @@
 return {
-  {
-    "Zeioth/compiler.nvim",
-    dependencies = { "stevearc/overseer.nvim" },
-    cmd = {"CompilerOpen", "CompilerToggleResults"},
-    config = function(_, opts) require("compiler").setup(opts) end,
-  },
-  {
-    "stevearc/overseer.nvim",
-    cmd = {"CompilerOpen", "CompilerToggleResults"},
-    opts = {
-      task_list = { -- this refers to the window that shows the result
-        direction = "bottom",
-        min_height = 25,
-        max_height = 25,
-        default_detail = 1,
-        bindings = {
-          ["q"] = function() vim.cmd("OverseerClose") end ,
-        }
+  "Zeioth/compiler.nvim",
+  dependencies = {
+    {
+      "stevearc/overseer.nvim",
+      opts = {
+        task_list = { -- this refers to the window that shows the result
+          direction = "bottom",
+          min_height = 25,
+          max_height = 25,
+          default_detail = 1,
+          bindings = {
+            ["q"] = function() vim.cmd "OverseerClose" end,
+          },
+        },
       },
+      config = function(_, opts) require("overseer").setup(opts) end,
     },
-    config = function(_, opts) require("overseer").setup(opts) end,
   },
+  cmd = { "CompilerOpen", "CompilerToggleResults" },
+  config = function(_, opts) require("compiler").setup(opts) end,
 }
