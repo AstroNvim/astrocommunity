81474abf1d1e5bb2bdecbbd1795e5a6f2c254f43
Zeioth <Zeioth@hotmail.com>
2023-06-22 09:28:34 +0200

    Update compiler-nvim.lua

--- lua/astrocommunity/code-runner/compiler-nvim/compiler-nvim.lua
+++ lua/astrocommunity/code-runner/compiler-nvim/compiler-nvim.lua
@@ -1,6 +1,7 @@
 return {
   {
-    "https://github.com/Zeioth/compiler.nvim",
+    "Zeioth/compiler.nvim",
+    dependencies = { "stevearc/overseer.nvim" },
     cmd = {"CompilerOpen", "CompilerToggleResults"},
     config = function(_, opts) require("compiler").setup(opts) end,
   },
@@ -8,9 +9,6 @@ return {
     "stevearc/overseer.nvim",
     cmd = {"CompilerOpen", "CompilerToggleResults"},
     opts = {
-      -- Tasks are disposed 5 minutes after running to free resources.
-      -- If you need to close a task inmediatelly:
-      -- press ENTER in the outut menu on the task you wanna close.
       task_list = { -- this refers to the window that shows the result
         direction = "bottom",
         min_height = 25,
@@ -20,16 +18,6 @@ return {
           ["q"] = function() vim.cmd("OverseerClose") end ,
         }
       },
-      -- component_aliases = { -- uncomment this to disable notifications
-      --   -- Components included in default will apply to all tasks
-      --   default = {
-      --     { "display_duration", detail_level = 2 },
-      --     "on_output_summarize",
-      --     "on_exit_set_status",
-      --     "on_complete_notify",
-      --     "on_complete_dispose",
-      --   },
-      -- },
     },
     config = function(_, opts) require("overseer").setup(opts) end,
   },
