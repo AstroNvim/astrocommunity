39d374c5d8f57108521f6950941ef6781e632a65
Micah Halter <micah@mehalter.com>
2023-06-23 07:28:45 -0400

    feat(split-and-window): add edgy.nvim

---
+++ lua/astrocommunity/split-and-window/edgy-nvim/README.md
@@ -0,0 +1,5 @@
+# edgy.nvim
+
+Easily create and manage predefined window layouts, bringing a new edge to your workflow
+
+**Repository:** <https://github.com/folke/edgy.nvim>
---
+++ lua/astrocommunity/split-and-window/edgy-nvim/init.lua
@@ -0,0 +1,75 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      source_selector = {
+        winbar = false,
+        statusline = false,
+      },
+    },
+  },
+  {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    keys = {
+      -- stylua: ignore
+      { "<leader>E", function() require("edgy").toggle() end, desc = "Toggle Sidebars" },
+      { "<leader>F", function() require("edgy").select() end, desc = "Pick Sidebar" },
+    },
+    opts = {
+      exit_when_last = true,
+      bottom = {
+        { ft = "qf", title = "QuickFix" },
+        {
+          ft = "help",
+          size = { height = 20 },
+          -- don't open help files in edgy that we're editing
+          filter = function(buf) return vim.bo[buf].buftype == "help" end,
+        },
+      },
+      left = {
+        {
+          title = "Files",
+          ft = "neo-tree",
+          filter = function(buf) return vim.b[buf].neo_tree_source == "filesystem" end,
+          pinned = true,
+          open = "Neotree position=left filesystem",
+          size = { height = 0.5 },
+        },
+        {
+          title = "Git Status",
+          ft = "neo-tree",
+          filter = function(buf) return vim.b[buf].neo_tree_source == "git_status" end,
+          pinned = true,
+          open = "Neotree position=right git_status",
+        },
+        {
+          title = "Buffers",
+          ft = "neo-tree",
+          filter = function(buf) return vim.b[buf].neo_tree_source == "buffers" end,
+          pinned = true,
+          open = "Neotree position=top buffers",
+        },
+        "neo-tree",
+      },
+      right = {
+        {
+          ft = "aerial",
+          title = "Symbol Outline",
+          pinned = true,
+          open = function() require("aerial").open() end,
+        },
+      },
+      keys = {
+        -- increase width
+        ["<C-Right>"] = function(win) win:resize("width", 2) end,
+        -- decrease width
+        ["<C-Left>"] = function(win) win:resize("width", -2) end,
+        -- increase height
+        ["<C-Up>"] = function(win) win:resize("height", 2) end,
+        -- decrease height
+        ["<C-Down>"] = function(win) win:resize("height", -2) end,
+      },
+    },
+  },
+}
