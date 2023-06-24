bb2189b332e09831dd88f6ecda4de47b6890d3ca
Ben Woodward <ben@benwoodward.me.uk>
2023-06-24 15:53:47 +0100

    refactor(python): Remove isort in favor of ruff_lsp (#310)
    Remove isort as ruff_lsp does the same job

--- lua/astrocommunity/pack/python/init.lua
+++ lua/astrocommunity/pack/python/init.lua
@@ -16,9 +16,7 @@ return {
   },
   {
     "jay-babu/mason-null-ls.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "isort", "black" })
-    end,
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "black") end,
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
