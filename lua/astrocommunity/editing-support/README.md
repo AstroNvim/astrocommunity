901a6dd51c357dfd2ed4369a8a942a54f50b1c52
windowsrefund <mtf8>
2023-06-24 23:20:52 -0400

    feat(editing-support): add ultimate-autopair.nvim

---
+++ lua/astrocommunity/editing-support/README.md
@@ -0,0 +1,6 @@
+# ultimate-autopair.nvim
+
+- Disable nvim-autopairs.
+- Install [ultimate-autopair.nvim](https://github.com/altermo/ultimate-autopair.nvim).
+- Setup rule to automatically disable pairing while editing comments.
+- Add `<leader>ua` keymap to toggle the plugin off and on.
---
+++ lua/astrocommunity/editing-support/ultimate-autopair-nvim/init.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "windwp/nvim-autopairs",
+    enabled = false,
+  },
+  {
+    "altermo/ultimate-autopair.nvim",
+    lazy = false,
+    opts = {
+      cmap = false,
+      extensions = {
+        rules = {
+          rules = {
+            {
+              "call",
+              function(o)
+                -- disable in comments including markdown style
+                local status, node = pcall(vim.treesitter.get_node, { pos = { o.linenr - 1, o.col - 2 } })
+                return o.incmd
+                  or o.col == 1
+                  or not status
+                  or not node
+                  or node:type() ~= "comment" and node:type() ~= "html_block"
+              end,
+            },
+          },
+        },
+        -- get fly mode working on strings: https://github.com/altermo/ultimate-autopair.nvim/issues/17
+        fly = {
+          nofilter = true,
+        },
+      },
+      { '"', '"', fly = true, p = 11 },
+      { "'", "'", fly = true, p = 11 },
+    },
+    keys = {
+      {
+        "<leader>ua",
+        function() require("ultimate-autopair").toggle() end,
+        desc = "Toggle ultimate-autopair",
+      },
+    },
+  },
+}
