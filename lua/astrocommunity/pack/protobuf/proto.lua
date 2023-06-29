0be8092635b2285c5b8c513be08111eed60aafa6
Miguel Casqueira <20k-ultra@users.noreply.github.com>
2023-06-29 15:31:35 -0400

    feat(pack): add ProtoBuf pack
    Signed-off-by: Miguel Casqueira <20k-ultra@users.noreply.github.com>

---
+++ lua/astrocommunity/pack/protobuf/proto.lua
@@ -0,0 +1,20 @@
+local utils = require "astronvim.utils"
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table or string "all".
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "proto")
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "bufls") end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "buf") end,
+  },
+}
