d517dcfc9762a4d7a9b00d49194fb41c550cb713
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-06-29 18:22:02 -0400

    fix(motion): vim-matchup - matchup_matchparen_offscreen method='popup' #279

--- lua/astrocommunity/motion/vim-matchup/init.lua
+++ lua/astrocommunity/motion/vim-matchup/init.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = { "andymass/vim-matchup" },
+  init = function() vim.g.matchup_matchparen_offscreen = { method = "popup" } end,
   opts = { matchup = { enable = true } },
 }
