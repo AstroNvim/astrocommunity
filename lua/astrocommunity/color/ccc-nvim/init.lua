c3d5871bf9856493e46934c9452e2d6f0bfbf7f2
zhaown <51357674+chaozwn@users.noreply.github.com>
2023-07-01 09:35:22 +0800

    add cccConvert and CccPick default mapping
    add cccConvert and CccPick default mapping

--- lua/astrocommunity/color/ccc-nvim/init.lua
+++ lua/astrocommunity/color/ccc-nvim/init.lua
@@ -3,7 +3,11 @@ return {
   {
     "uga-rosa/ccc.nvim",
     event = "User AstroFile",
-    keys = { { "<leader>uC", "<cmd>CccPick<cr>", desc = "Toggle colorizer" } },
+    keys = {
+      { "<leader>uC", "<cmd>CccPick<cr>", desc = "Toggle colorizer" },
+      { "<leader>zc", "<cmd>CccConvert<cr>", desc = "Convert color" },
+      { "<leader>zp", "<cmd>CccPick<cr>", desc = "Pick Color" },
+    },
     opts = {
       highlighter = {
         auto_enable = true,
