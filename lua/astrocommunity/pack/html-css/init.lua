9f492db7dfd12a124edf85425ba14103a987ca27
zhaown <51357674+chaozwn@users.noreply.github.com>
2023-07-02 14:27:47 +0800

    feat(pack): add emmet_ls to html-css supprt syntax like `li * 5`

--- lua/astrocommunity/pack/html-css/init.lua
+++ lua/astrocommunity/pack/html-css/init.lua
@@ -13,7 +13,7 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
-      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "html", "cssls" })
+      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "html", "cssls", "emmet_ls" })
     end,
   },
 }
