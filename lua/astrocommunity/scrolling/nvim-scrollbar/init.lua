01f136623b1c7b49f82e0900e42f155a46d9d7b3
Uzair Aftab <uzaaft@outlook.com>
2023-07-02 22:15:57 +0200

    fix(nvim-scrollbar): Fix issue with is_available calls

--- lua/astrocommunity/scrolling/nvim-scrollbar/init.lua
+++ lua/astrocommunity/scrolling/nvim-scrollbar/init.lua
@@ -2,8 +2,8 @@ return {
   "petertriho/nvim-scrollbar",
   opts = {
     handlers = {
-      gitsigns = require("astronvim.utils").is_available "gitsigns",
-      search = require("astronvim.utils").is_available "hlslens",
+      gitsigns = require("astronvim.utils").is_available "gitsigns.nvim",
+      search = require("astronvim.utils").is_available "nvim-hlslens",
       ale = require("astronvim.utils").is_available "ale",
     },
   },
