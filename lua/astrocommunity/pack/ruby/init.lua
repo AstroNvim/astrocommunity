f97e762316318fe1c129722489e7ee6a23998206
Kai Löhnert <luxuspur@gmail.com>
2023-07-02 15:32:08 +0200

    Update lua/astrocommunity/pack/ruby/init.lua
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/pack/ruby/init.lua
+++ lua/astrocommunity/pack/ruby/init.lua
@@ -5,7 +5,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "ruby" })
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "ruby")
       end
     end,
   },
