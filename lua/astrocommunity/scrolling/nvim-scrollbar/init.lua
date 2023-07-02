ba830b78e7cc3003c9e6caa5a2a20f757343a5eb
Uzair Aftab <uzaaft@outlook.com>
2023-07-02 22:48:11 +0200

    fix(nvim-scrollbar): extend opts with function

--- lua/astrocommunity/scrolling/nvim-scrollbar/init.lua
+++ lua/astrocommunity/scrolling/nvim-scrollbar/init.lua
@@ -1,11 +1,15 @@
+local utils = require "astronvim.utils"
+
 return {
   "petertriho/nvim-scrollbar",
-  opts = {
-    handlers = {
-      gitsigns = require("astronvim.utils").is_available "gitsigns.nvim",
-      search = require("astronvim.utils").is_available "nvim-hlslens",
-      ale = require("astronvim.utils").is_available "ale",
-    },
-  },
+  opts = function(_, opts)
+    utils.extend_tbl(opts, {
+      handlers = {
+        gitsigns = require("astronvim.utils").is_available "gitsigns.nvim",
+        search = require("astronvim.utils").is_available "nvim-hlslens",
+        ale = require("astronvim.utils").is_available "ale",
+      },
+    })
+  end,
   event = "User AstroFile",
 }
