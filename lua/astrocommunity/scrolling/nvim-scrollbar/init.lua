d77645817bb2cd1ad8291be44cd02e1fd4e52893
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2023-07-02 23:00:23 +0200

    fix(nvim-scrollbar): Fix issue with is_available calls (#351)
    * fix(nvim-scrollbar): Fix issue with is_available calls
    
    * fix(nvim-scrollbar): extend opts with function
    
    * chore(nvim-scrollbar): Remove unnessecary import

--- lua/astrocommunity/scrolling/nvim-scrollbar/init.lua
+++ lua/astrocommunity/scrolling/nvim-scrollbar/init.lua
@@ -1,11 +1,13 @@
 return {
   "petertriho/nvim-scrollbar",
-  opts = {
-    handlers = {
-      gitsigns = require("astronvim.utils").is_available "gitsigns.nvim",
-      search = require("astronvim.utils").is_available "nvim-hlslens",
-      ale = require("astronvim.utils").is_available "ale",
-    },
-  },
+  opts = function(_, opts)
+    require("astronvim.utils").extend_tbl(opts, {
+      handlers = {
+        gitsigns = require("astronvim.utils").is_available "gitsigns.nvim",
+        search = require("astronvim.utils").is_available "nvim-hlslens",
+        ale = require("astronvim.utils").is_available "ale",
+      },
+    })
+  end,
   event = "User AstroFile",
 }
