6d9d46f3e600a8d0c0a00576e13e094f152542f2
Kai Löhnert <luxuspur@gmail.com>
2023-07-02 15:32:00 +0200

    Update lua/astrocommunity/pack/ruby/init.lua
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/pack/ruby/init.lua
+++ lua/astrocommunity/pack/ruby/init.lua
@@ -21,7 +21,8 @@ return {
     "mfussenegger/nvim-dap",
     dependencies = {
       "suketa/nvim-dap-ruby",
-      config = function() require("dap-ruby").setup() end,
+      name = "dap-ruby",
+      opts = {},
     },
   },
 }
