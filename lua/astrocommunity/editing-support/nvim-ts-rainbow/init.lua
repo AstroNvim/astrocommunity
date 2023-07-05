052923f253a0a00e391717b3fa6102dcdee40e92
Micah Halter <micah@mehalter.com>
2023-07-05 08:49:47 -0400

    fix: add deprecation notices to relevant AstroCommunity entries

--- lua/astrocommunity/editing-support/nvim-ts-rainbow/init.lua
+++ lua/astrocommunity/editing-support/nvim-ts-rainbow/init.lua
@@ -2,6 +2,12 @@ return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
     "mrjones2014/nvim-ts-rainbow",
+    init = function()
+      require("astronvim.utils").notify(
+        "`nvim-ts-rainbow` is deprecated!\nPlease use `rainbow-delimiters`",
+        vim.log.levels.WARN
+      )
+    end,
     config = function()
       -- HACK: https://github.com/p00f/nvim-ts-rainbow/issues/112#issuecomment-1310835936
       vim.api.nvim_create_autocmd({ "BufWritePost", "FocusGained" }, {
