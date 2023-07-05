8f37aab709fed9111542896778f32ebd36c70f45
Kenro <kenro2002@gmail.com>
2023-07-05 17:07:59 +0000

    feat(dadbod) added vim-dadbod

---
+++ lua/astrocommunity/sql/dadbod/init.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = "tpope/vim-dadbod",
+    lazy = false,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "kristijanhusak/vim-dadbod-completion",
+        config = function()
+          vim.api.nvim_create_autocmd("FileType", {
+            desc = "dadbod completion",
+            group = vim.api.nvim_create_augroup("dadbod_cmp", { clear = true }),
+            pattern = { "sql", "mysql", "plsql" },
+            callback = function() require("cmp").setup.buffer { sources = { { name = "vim-dadbod-completion" } } } end,
+          })
+        end,
+      },
+    },
+  },
+}
