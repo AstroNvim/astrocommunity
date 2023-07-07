cf3dde079a7d70b1fb72871bc35c5c9b130e761f
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-07 11:24:06 -0700

    fix(refactoring-nvim): improve lazy loading

--- lua/astrocommunity/editing-support/refactoring-nvim/init.lua
+++ lua/astrocommunity/editing-support/refactoring-nvim/init.lua
@@ -1,7 +1,6 @@
 return {
   "ThePrimeagen/refactoring.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
-  event = "User AstroFile",
   opts = {},
   keys = {
     {
