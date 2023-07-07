50cf8ea6e89625de29c5fef64326ade3a9e4416b
zhao_wei_nan <326747337@qq.com>
2023-07-07 10:39:36 +0800

    feat(debugging): add telescope dap extension

---
+++ lua/astrocommunity/debugging/telescope-dap-nvim/README.md
@@ -0,0 +1,8 @@
+# telescope-dap.nvim
+
+Integration for `nvim-dap` with `telescope.nvim`.
+
+This plugin is also overriding `dap` internal `ui`, so running any `dap` command, which makes use of the internal ui, will result in a `telescope` prompt.
+
+**Repository:** <https://github.com/nvim-telescope/telescope-dap.nvim>
+
---
+++ lua/astrocommunity/debugging/telescope-dap-nvim/init.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    "nvim-telescope/telescope-dap.nvim",
+    name = "telescope_dap",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>fdc",
+        "<Cmd>lua require('telescope').extensions.dap.commands{}<CR>",
+        { silent = true, expr = false },
+        mode = {
+          "n",
+        },
+        desc = "Telescope DAP commands",
+      },
+      {
+        "<leader>fdg",
+        "<Cmd>lua require('telescope').extensions.dap.configurations{}<CR>",
+        { silent = true, expr = false },
+        mode = {
+          "n",
+        },
+        desc = "Telescope DAP configurations",
+      },
+      {
+        "<leader>fdl",
+        "<Cmd>lua require('telescope').extensions.dap.list_breakpoints{}<CR>",
+        { silent = true, expr = false },
+        mode = {
+          "n",
+        },
+        desc = "Telescope DAP list breakpoints",
+      },
+      {
+        "<leader>fdv",
+        "<Cmd>lua require('telescope').extensions.dap.variables{}<CR>",
+        { silent = true, expr = false },
+        mode = {
+          "n",
+        },
+        desc = "Telescope DAP variables",
+      },
+      {
+        "<leader>fdf",
+        "<Cmd>lua require('telescope').extensions.dap.frames{}<CR>",
+        { silent = true, expr = false },
+        mode = {
+          "n",
+        },
+        desc = "Telescope DAP frames",
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    optional = true,
+    dependencies = { "telescope_dap" },
+    opts = function() require("telescope").load_extension "dap" end,
+  },
+}
