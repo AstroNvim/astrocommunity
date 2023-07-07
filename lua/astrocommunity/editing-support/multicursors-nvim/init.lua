19e0bb491495a1b0db8b598829bd233d555aedb1
Fynn Flügge <fynnfluegge@gmx.de>
2023-07-07 19:27:17 +0200

    feat(editing-support): add multicursors.nvim plugin (#379)
    add multicursors.nvim

---
+++ lua/astrocommunity/editing-support/multicursors-nvim/init.lua
@@ -0,0 +1,12 @@
+return {
+  "smoka7/multicursors.nvim",
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    {
+      "<Leader>m",
+      "<cmd>MCstart<cr>",
+      desc = "Create a selection for word under the cursor",
+    },
+  },
+}
