db49182628cb6a5f6ae8675e269e339ef30008f6
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-07-08 10:49:24 +0200

    refactor(obsidian-nvim): return to opts table from function

--- lua/astrocommunity/note-taking/obsidian-nvim/init.lua
+++ lua/astrocommunity/note-taking/obsidian-nvim/init.lua
@@ -23,21 +23,21 @@ return {
     "hrsh7th/nvim-cmp",
     "nvim-telescope/telescope.nvim",
   },
-  opts = function(_, opts)
-    opts.dir = vim.env.HOME .. "/obsidian-vault" -- specify the vault location. no need to call 'vim.fn.expand' here
-    opts.completion = { nvim_cmp = true }
-    opts.disable_frontmatter = false
-    opts.use_advanced_uri = true
-    opts.open_app_foreground = false
-    opts.finder = "telescope.nvim"
+  opts = {
+    dir = vim.env.HOME .. "/obsidian-vault", -- specify the vault location. no need to call 'vim.fn.expand' here
+    completion = { nvim_cmp = true },
+    disable_frontmatter = false,
+    use_advanced_uri = true,
+    open_app_foreground = false,
+    finder = "telescope.nvim",
 
-    opts.templates = {
+    templates = {
       subdir = "templates",
       date_format = "%Y-%m-%d-%a",
       time_format = "%H:%M",
-    }
+    },
 
-    opts.note_frontmatter_func = function(note)
+    note_frontmatter_func = function(note)
       -- This is equivalent to the default frontmatter function.
       local out = { id = note.id, aliases = note.aliases, tags = note.tags }
       -- `note.metadata` contains any manually added fields in the frontmatter.
@@ -48,11 +48,11 @@ return {
         end
       end
       return out
-    end
+    end,
 
     -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
     -- URL it will be ignored but you can customize this behavior here.
-    opts.follow_url_func = function(url)
+    follow_url_func = function(url)
       local this_os = vim.loop.os_uname().sysname
       -- Open the URL in the default web browser.
       if this_os == "Darwin" then
@@ -60,6 +60,6 @@ return {
       elseif this_os == "Linux" then
         vim.fn.jobstart { "xdg-open", url }
       end
-    end
-  end,
+    end,
+  },
 }
