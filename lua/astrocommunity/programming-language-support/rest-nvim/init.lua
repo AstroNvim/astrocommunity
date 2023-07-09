4ff91b1082e5232d925e114789d74a7315a528d1
Uzair Aftab <uzaaft@outlook.com>
2023-07-09 11:25:00 +0200

    feat(programming-language-support): Add support for rest-nvim

---
+++ lua/astrocommunity/programming-language-support/rest-nvim/init.lua
@@ -0,0 +1,27 @@
+local prefix = "<leader>r"
+
+return {
+  {
+    "rest-nvim/rest.nvim",
+    ft = { "http", "json" },
+    cmd = {
+      "RestNvim",
+      "RestNvimPreview",
+      "RestNvimLast",
+    },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { prefix, desc = "RestNvim" },
+      { prefix .. "r", "<Plug>RestNvim", desc = "Run request" },
+    },
+    config = function(_, opts) require("rest-nvim").setup(opts) end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, { "http", "json" })
+      end
+    end,
+  },
+}
