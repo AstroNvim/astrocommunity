4af2d859ce212de2e67c5bdd7221e826e9ef7876
Uzair Aftab <uzaaft@outlook.com>
2023-07-09 22:05:13 +0200

    feat(split-and-window): Add windows.nvim

---
+++ lua/astrocommunity/split-and-window/windows-nvim/README.md
@@ -0,0 +1,5 @@
+# Windows.nvim
+
+Automatically expand width of the current window. Maximizes and restore it. And all this with nice animations!
+
+**Repository:** <https://github.com/anuvyklack/windows.nvim>
---
+++ lua/astrocommunity/split-and-window/windows-nvim/init.lua
@@ -0,0 +1,22 @@
+return {
+  "anuvyklack/windows.nvim",
+  dependencies = {
+    "anuvyklack/middleclass",
+    "anuvyklack/animation.nvim",
+  },
+  opts = {},
+  cmd = {
+    "WindowsMaximize",
+    "WindowsMaximizeVertically",
+    "WindowsMaximizeHorizontally",
+    "WindowsEqualize",
+    "WindowsEnableAutowidth",
+    "WindowsDisableAutowidth",
+    "WindowsToggleAutowidth",
+  },
+  init = function()
+    vim.o.winwidth = 10
+    vim.o.winminwidth = 10
+    vim.o.equalalways = false
+  end,
+}
