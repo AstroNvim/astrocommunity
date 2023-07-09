994e5e2d7a9ecb3287c58e721f376275b8c847a4
Uzair Aftab <uzaaft@outlook.com>
2023-07-09 18:39:54 +0200

    feat(code-runner): Add executor.nvim

---
+++ lua/astrocommunity/code-runner/executor-nvim/init.lua
@@ -0,0 +1,16 @@
+return {
+  "google/executor.nvim",
+  dependencies = "MunifTanjim/nui.nvim",
+  opts = {},
+  cmd = {
+    "ExecutorRun",
+    "ExecutorSetCommand",
+    "ExecutorShowDetail",
+    "ExecutorHideDetail",
+    "ExecutorToggleDetail",
+    "ExecutorSwapToSplit",
+    "ExecutorSwapToPopup",
+    "ExecutorToggleDetail",
+    "ExecutorReset",
+  },
+}
