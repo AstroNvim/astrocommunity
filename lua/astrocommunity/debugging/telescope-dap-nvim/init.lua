a29ed59a18bf88405388869c4da54218c3bb88b4
Micah Halter <micah@mehalter.com>
2023-07-10 05:41:49 -0400

    fix(telescope-dap-nvim): rewrite plugin spec

--- lua/astrocommunity/debugging/telescope-dap-nvim/init.lua
+++ lua/astrocommunity/debugging/telescope-dap-nvim/init.lua
@@ -1,62 +1,22 @@
 local prefix = "<leader>fd"
 
 return {
-  {
-    "nvim-telescope/telescope-dap.nvim",
-    name = "telescope_dap",
-    event = "User AstroFile",
-    keys = {
-      {
-        prefix .. "c",
-        "<Cmd>lua require('telescope').extensions.dap.commands{}<CR>",
-        { silent = true, expr = false },
-        mode = {
-          "n",
-        },
-        desc = "Telescope DAP commands",
-      },
-      {
-        prefix .. "g",
-        "<Cmd>lua require('telescope').extensions.dap.configurations{}<CR>",
-        { silent = true, expr = false },
-        mode = {
-          "n",
-        },
-        desc = "Telescope DAP configurations",
-      },
-      {
-        prefix .. "l",
-        "<Cmd>lua require('telescope').extensions.dap.list_breakpoints{}<CR>",
-        { silent = true, expr = false },
-        mode = {
-          "n",
-        },
-        desc = "Telescope DAP list breakpoints",
-      },
-      {
-        prefix .. "v",
-        "<Cmd>lua require('telescope').extensions.dap.variables{}<CR>",
-        { silent = true, expr = false },
-        mode = {
-          "n",
-        },
-        desc = "Telescope DAP variables",
-      },
-      {
-        prefix .. "f",
-        "<Cmd>lua require('telescope').extensions.dap.frames{}<CR>",
-        { silent = true, expr = false },
-        mode = {
-          "n",
-        },
-        desc = "Telescope DAP frames",
-      },
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    { prefix .. "c", function() require("telescope").extensions.dap.commands() end, desc = "Telescope DAP commands" },
+    { prefix .. "f", function() require("telescope").extensions.dap.frames() end, desc = "Telescope DAP frames" },
+    {
+      prefix .. "g",
+      function() require("telescope").extensions.dap.configurations() end,
+      desc = "Telescope DAP configurations",
     },
+    {
+      prefix .. "l",
+      function() require("telescope").extensions.dap.list_breakpoints() end,
+      desc = "Telescope DAP list breakpoints",
+    },
+    { prefix .. "v", function() require("telescope").extensions.dap.variables() end, desc = "Telescope DAP variables" },
   },
-  {
-    "nvim-telescope/telescope.nvim",
-    optional = true,
-    dependencies = { "telescope_dap" },
-    opts = function() require("telescope").load_extension "dap" end,
-  },
+  dependencies = { "nvim-telescope/telescope-dap.nvim" },
+  opts = function() require("telescope").load_extension "dap" end,
 }
