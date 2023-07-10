954dc5b72a2c0262a6c8aa5796e742576c4f661a
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2023-07-10 11:40:36 +0200

    Update lua/astrocommunity/utility/telescope-live-grep-args-nvim/init.lua
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/utility/telescope-live-grep-args-nvim/init.lua
+++ lua/astrocommunity/utility/telescope-live-grep-args-nvim/init.lua
@@ -1,9 +1,5 @@
 return {
-  { "nvim-telescope/telescope-live-grep-args.nvim" },
-  {
-    "nvim-telescope/telescope.nvim",
-    optional = true,
-    dependencies = { "telescope_dap" },
-    opts = function() require("telescope").load_extension "live_grep_args" end,
-  },
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-telescope/telescope-live-grep-args.nvim" },
+  opts = function() require("telescope").load_extension "live_grep_args" end,
 }
