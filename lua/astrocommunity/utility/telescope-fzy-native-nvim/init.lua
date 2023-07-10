2a04a3d6a2e50e46937ee7b2c6d5f13cce18c53b
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2023-07-10 11:40:53 +0200

    Update lua/astrocommunity/utility/telescope-fzy-native-nvim/init.lua
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/utility/telescope-fzy-native-nvim/init.lua
+++ lua/astrocommunity/utility/telescope-fzy-native-nvim/init.lua
@@ -1,9 +1,5 @@
 return {
-  { "nvim-telescope/telescope-fzy-native.nvim" },
-  {
-    "nvim-telescope/telescope.nvim",
-    optional = true,
-    dependencies = { "telescope_dap" },
-    opts = function() require("telescope").load_extension "fzy_native" end,
-  },
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-telescope/telescope-fzy-native.nvim" },
+  opts = function() require("telescope").load_extension "fzy_native" end,
 }
