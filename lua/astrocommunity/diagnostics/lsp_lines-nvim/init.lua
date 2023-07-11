5899355e3241ad317bf14229ac7e08cb5d5a540e
TheSast <27977196+TheSast@users.noreply.github.com>
2023-07-11 20:45:57 +0000

    fix(lsp_lines-nvim): duplicate diagnostics
    This should be later improved by silencing the notification

--- lua/astrocommunity/diagnostics/lsp_lines-nvim/init.lua
+++ lua/astrocommunity/diagnostics/lsp_lines-nvim/init.lua
@@ -1,1 +1,6 @@
-return { "https://git.sr.ht/~whynothugo/lsp_lines.nvim", event = "LspAttach", opts = {} }
+return {
+  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  event = "LspAttach",
+  init = function() require("astronvim.utils.ui").toggle_diagnostics() end,
+  opts = {},
+}
