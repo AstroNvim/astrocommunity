45ae8f9bc11eb6a655e584898b2decec30195f74
TheSast <27977196+TheSast@users.noreply.github.com>
2023-07-11 18:35:42 +0000

    fix(leap-nvim): duplicate cursor when leaping (#407)
    https://github.com/ggandor/leap.nvim/issues/70

--- lua/astrocommunity/motion/leap-nvim/init.lua
+++ lua/astrocommunity/motion/leap-nvim/init.lua
@@ -8,6 +8,22 @@ return {
     { "gs", "<Plug>(leap-from-window)", mode = { "n", "x", "o" }, desc = "Leap from window" },
   },
   opts = {},
+  init = function() -- https://github.com/ggandor/leap.nvim/issues/70#issuecomment-1521177534
+    vim.api.nvim_create_autocmd("User", {
+      callback = function()
+        vim.cmd.hi("Cursor", "blend=100")
+        vim.opt.guicursor:append { "a:Cursor/lCursor" }
+      end,
+      pattern = "LeapEnter",
+    })
+    vim.api.nvim_create_autocmd("User", {
+      callback = function()
+        vim.cmd.hi("Cursor", "blend=0")
+        vim.opt.guicursor:remove { "a:Cursor/lCursor" }
+      end,
+      pattern = "LeapLeave",
+    })
+  end,
   dependencies = {
     "tpope/vim-repeat",
   },
