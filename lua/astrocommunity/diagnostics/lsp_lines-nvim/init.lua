2a078cac4bfa9ab8c97b7ed2888d4dc20abed421
TheSast <27977196+TheSast@users.noreply.github.com>
2023-07-11 20:49:53 +0000

    chore(lsp_lines.nvim): add `<leader>uD` mapping
    Ideally this would be impemented as one of the cycled options in `<leader>ud` (aka astronvim.ui.toggle_diagnostics)

--- lua/astrocommunity/diagnostics/lsp_lines-nvim/init.lua
+++ lua/astrocommunity/diagnostics/lsp_lines-nvim/init.lua
@@ -1,6 +1,17 @@
 return {
   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
   event = "LspAttach",
+  keys = {
+    {
+      "<Leader>uD",
+      function()
+        vim.diagnostic.config {
+          virtual_text = not require("lsp_lines").toggle(),
+        }
+      end,
+      desc = "Toggle virtual diagnostic lines",
+    },
+  },
   init = function() require("astronvim.utils.ui").toggle_diagnostics() end,
   opts = {},
 }
