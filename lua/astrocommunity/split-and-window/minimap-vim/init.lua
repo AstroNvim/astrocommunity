1c8cabd7c1fd5441e2b2c3687975438dad5b0ff8
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2023-07-13 21:27:30 +0200

    Update lua/astrocommunity/split-and-window/minimap-vim/init.lua
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/split-and-window/minimap-vim/init.lua
+++ lua/astrocommunity/split-and-window/minimap-vim/init.lua
@@ -3,8 +3,8 @@ return {
   lazy = false,
   cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
   init = function()
-    vim.cmd "let g:minimap_width = 10"
-    vim.cmd "let g:minimap_auto_start = 1"
-    vim.cmd "let g:minimap_auto_start_win_enter = 1"
+    vim.g.minimap_width = 10
+    vim.g.minimap_auto_start = 1
+    vim.g.minimap_auto_start_win_enter = 1
   end,
 }
