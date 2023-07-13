1fc42c8344967dba7db1ed1696bfcc693e24d08c
Micah Halter <micah@mehalter.com>
2023-07-13 07:56:20 -0400

    Restructure treesitter dependencies

--- lua/astrocommunity/pack/wgsl/init.lua
+++ lua/astrocommunity/pack/wgsl/init.lua
@@ -1,19 +1,24 @@
 local utils = require "astronvim.utils"
 return {
   {
-    "szebniok/tree-sitter-wgsl",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = "cp -r queries ../nvim-treesitter/queries/wgsl",
-    config = function()
-      vim.filetype.add { extension = { wgsl = "wgsl" } }
-      require("nvim-treesitter.parsers").get_parser_configs().wgsl = {
-        install_info = {
-          url = vim.fn.stdpath "data" .. "/lazy/tree-sitter-wgsl",
-          files = { "src/parser.c", "src/scanner.c" },
-        },
-      }
-    end,
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    dependencies = {
+      {
+        "szebniok/tree-sitter-wgsl",
+        build = "cp -r queries ../nvim-treesitter/queries/wgsl",
+        init = function() vim.filetype.add { extension = { wgsl = "wgsl" } } end,
+        config = function()
+          require("nvim-treesitter.parsers").get_parser_configs().wgsl = {
+            install_info = {
+              url = vim.fn.stdpath "data" .. "/lazy/tree-sitter-wgsl",
+              files = { "src/parser.c", "src/scanner.c" },
+            },
+            filetype = "wgsl",
+          }
+        end,
+      },
+    },
     opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "wgsl") end,
   },
   {
