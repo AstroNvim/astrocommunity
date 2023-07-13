71cd3dcd99f875ab48d732b3731b86d4f20065cf
A-Lamia <git@lamia.io>
2023-07-13 20:20:24 +0000

    feat(godot): add godot language pack

---
+++ lua/astrocommunity/pack/godot/init.lua
@@ -0,0 +1,29 @@
+local utils = require "astronvim.utils"
+return {
+  {
+    "QuickGD/quickgd.nvim",
+    ft = { "gdshader", "gdshaderinc" },
+    cmd = {
+      "GodotRun",
+      "GodotRunLast",
+      "GodotStart",
+    },
+    init = function()
+      vim.filetype.add {
+        extension = {
+          gdshaderinc = "gdshaderinc",
+        },
+      }
+    end,
+    config = true,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed =
+          utils.list_insert_unique(opts.ensure_installed, { "gdscript", "glsl", "godot_resource" })
+      end
+    end,
+  },
+}
