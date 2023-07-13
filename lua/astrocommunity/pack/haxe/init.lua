97b41c54dce8c06bd50eca034aefd3da3680c875
Micah Halter <micah@mehalter.com>
2023-07-13 07:48:47 -0400

    fix(haxe): fix treesitter detection for haxe

--- lua/astrocommunity/pack/haxe/init.lua
+++ lua/astrocommunity/pack/haxe/init.lua
@@ -1,21 +1,19 @@
 local utils = require "astronvim.utils"
 return {
-  {
-    "vantreeseba/tree-sitter-haxe",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = "cp -r queries ../nvim-treesitter/queries/haxe",
-    config = function()
-      vim.filetype.add { extension = { hx = "haxe" } }
-      require("nvim-treesitter.parsers").get_parser_configs().haxe =
-        { install_info = { url = vim.fn.stdpath "data" .. "/lazy/tree-sitter-haxe", files = { "src/parser.c" } } }
-      vim.cmd.TSInstall "haxe"
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  optional = true,
+  dependencies = {
+    {
+      "vantreeseba/tree-sitter-haxe",
+      build = "cp -r queries ../nvim-treesitter/queries/haxe",
+      init = function() vim.filetype.add { extension = { hx = "haxe" } } end,
+      config = function()
+        require("nvim-treesitter.parsers").get_parser_configs().haxe = {
+          install_info = { url = vim.fn.stdpath "data" .. "/lazy/tree-sitter-haxe", files = { "src/parser.c" } },
+          filetype = "haxe",
+        }
+      end,
+    },
   },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "haxe_language_server")
-    end,
-  },
+  opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "haxe") end,
 }
