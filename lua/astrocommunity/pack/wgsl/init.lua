febbbad7bdba5d0c62e6b1d89cb408c7991a609f
Mariano Lambir <117318+mlambir@users.noreply.github.com>
2023-07-13 08:57:26 -0300

    feat(pack): add wgsl pack (#415)
    * feat(pack): add wgsl pack
    
    * Restructure treesitter dependencies
    
    ---------
    
    Co-authored-by: Micah Halter <micah@mehalter.com>

---
+++ lua/astrocommunity/pack/wgsl/init.lua
@@ -0,0 +1,28 @@
+local utils = require "astronvim.utils"
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    dependencies = {
+      {
+        "szebniok/tree-sitter-wgsl",
+        build = "cp -r queries ../nvim-treesitter/queries/wgsl",
+        init = function() vim.filetype.add { extension = { wgsl = "wgsl" } } end,
+        config = function()
+          require("nvim-treesitter.parsers").get_parser_configs().wgsl = {
+            install_info = {
+              url = vim.fn.stdpath "data" .. "/lazy/tree-sitter-wgsl",
+              files = { "src/parser.c", "src/scanner.c" },
+            },
+            filetype = "wgsl",
+          }
+        end,
+      },
+    },
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "wgsl") end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "wgsl_analyzer") end,
+  },
+}
