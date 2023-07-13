0a812dec3c3b54d16d9789d8bd4459af71c58d1d
Uzair Aftab <uzaaft@outlook.com>
2023-07-13 13:19:23 +0200

    fix(distant.nvim): Use opts instead of config

--- lua/astrocommunity/remote-development/distant-nvim/init.lua
+++ lua/astrocommunity/remote-development/distant-nvim/init.lua
@@ -14,14 +14,5 @@ return {
     "DistantSessionInfo",
     "DistantCopy",
   },
-  config = function()
-    require("distant").setup {
-      -- Applies Chip's personal settings to every machine you connect to
-
-      -- 1. Ensures that distant servers terminate with no connections
-      -- 2. Provides navigation bindings for remote directories
-      -- 3. Provides keybinding to jump into a remote file's parent directory
-      ["*"] = require("distant.settings").chip_default(),
-    }
-  end,
+  opts = function(_, opts) require("astronvim.utils").extend_tbl(opts, require("distant.settings").chip_default()) end,
 }
