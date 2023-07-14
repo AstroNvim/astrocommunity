78ada15fdd1643bc43225a1f47d082fe92b73a22
Micah Halter <micah@mehalter.com>
2023-07-14 16:00:37 -0400

    Simplify setup call

--- lua/astrocommunity/git/neogit/init.lua
+++ lua/astrocommunity/git/neogit/init.lua
@@ -10,7 +10,7 @@ return {
       { "nvim-lua/plenary.nvim" },
     },
     event = "User AstroGitFile",
-    config = function(_, opts) require("neogit").setup(opts) end,
+    opts = {},
     keys = {
       { prefix .. "nt", "<cmd>Neogit<CR>", desc = "Open Neogit Tab Page" },
       { prefix .. "nc", "<cmd>Neogit commit<CR>", desc = "Open Neogit Commit Page" },
