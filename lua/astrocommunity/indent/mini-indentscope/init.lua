c88072ab2cd9e13688620cefe0b9b16e75d951f3
Josh <56745535+Subjective@users.noreply.github.com>
2023-07-15 14:50:52 -0700

    fix(mini-indentscope): fix alphabetical sorting of filetypes

--- lua/astrocommunity/indent/mini-indentscope/init.lua
+++ lua/astrocommunity/indent/mini-indentscope/init.lua
@@ -4,18 +4,18 @@ return {
   opts = { symbol = "│", options = { try_as_border = true } },
   init = function()
     vim.api.nvim_create_autocmd("FileType", {
-      pattern = {
-        "alpha",
+      pattern = {      
         "aerial",
+        "alpha",
         "dashboard",
         "fzf",
         "help",
-        "neo-tree",
-        "Trouble",
+        "neo-tree",  
         "lazy",
         "lspinfo",
         "mason",
         "starter",
+        "Trouble",
         "undotree",
       },
       callback = function() vim.b.miniindentscope_disable = true end,
