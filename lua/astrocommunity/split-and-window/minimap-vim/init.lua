4722f6ab26b05c4f2a430450ba7136bd7b1955c4
TheSast <27977196+TheSast@users.noreply.github.com>
2023-07-15 09:58:52 +0000

    feat(mini-map): add `<leader>um` keybind

--- lua/astrocommunity/split-and-window/minimap-vim/init.lua
+++ lua/astrocommunity/split-and-window/minimap-vim/init.lua
@@ -2,6 +2,9 @@ return {
   "wfxr/minimap.vim",
   event = "User AstroFile",
   cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+  keys = {
+    { "<leader>um", "<cmd>MinimapToggle<CR>", desc = "Toggle minimap", mode = { "n" } },
+  },
   init = function()
     vim.g.minimap_width = 10
     vim.g.minimap_auto_start = 1
