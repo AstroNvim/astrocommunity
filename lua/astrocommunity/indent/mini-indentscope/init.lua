9b1b8e6507e10ee52d53214a1908405b77f99b2c
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-15 12:47:52 -0700

    chore(mini-indentscope): ignore aerial and sort filetypes alphabetically

--- lua/astrocommunity/indent/mini-indentscope/init.lua
+++ lua/astrocommunity/indent/mini-indentscope/init.lua
@@ -5,16 +5,17 @@ return {
   init = function()
     vim.api.nvim_create_autocmd("FileType", {
       pattern = {
-        "fzf",
-        "starter",
-        "help",
         "alpha",
+        "aerial",
         "dashboard",
+        "fzf",
+        "help",
         "neo-tree",
         "Trouble",
         "lazy",
         "lspinfo",
         "mason",
+        "starter",
         "undotree",
       },
       callback = function() vim.b.miniindentscope_disable = true end,
