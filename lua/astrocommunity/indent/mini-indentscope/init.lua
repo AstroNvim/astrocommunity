dc7ea3b358eafd508261b8108a37dc3ca11b414e
Josh <56745535+Subjective@users.noreply.github.com>
2023-07-16 06:36:45 -0700

    fix(mini-indentscope): ignore more filetypes (#442)
    * chore(mini-indentscope): remove config function and ignore lsp-info
    
    * chore(mini-indentscope): ignore aerial and sort filetypes alphabetically
    
    * fix(mini-indentscope): fix alphabetical sorting of filetypes
    
    * fix(mini-indentscope): remove spacing for style check

--- lua/astrocommunity/indent/mini-indentscope/init.lua
+++ lua/astrocommunity/indent/mini-indentscope/init.lua
@@ -2,11 +2,23 @@ return {
   "echasnovski/mini.indentscope",
   event = "User AstroFile",
   opts = { symbol = "│", options = { try_as_border = true } },
-  config = function(_, opts)
+  init = function()
     vim.api.nvim_create_autocmd("FileType", {
-      pattern = { "fzf", "starter", "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason", "undotree" },
+      pattern = {
+        "aerial",
+        "alpha",
+        "dashboard",
+        "fzf",
+        "help",
+        "neo-tree",
+        "lazy",
+        "lspinfo",
+        "mason",
+        "starter",
+        "Trouble",
+        "undotree",
+      },
       callback = function() vim.b.miniindentscope_disable = true end,
     })
-    require("mini.indentscope").setup(opts)
   end,
 }
