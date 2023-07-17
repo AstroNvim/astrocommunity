5e6de7a1b94d6d807d064b969b63f152ae5ced4d
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2023-07-17 15:47:35 +0200

    incorporate suggestions

--- lua/astrocommunity/editing-support/nvim-neoclip-lua/init.lua
+++ lua/astrocommunity/editing-support/nvim-neoclip-lua/init.lua
@@ -1,11 +1,11 @@
 return {
   "AckslD/nvim-neoclip.lua",
-  event = { "User AstroFile" },
+  event = { "User AstroFile", "InsertEnter" },
   dependencies = {
     { "nvim-telescope/telescope.nvim" },
   },
-  config = function()
-    require("neoclip").setup()
+  config = function(_, opts)
+    require("neoclip").setup(opts)
     require("telescope").load_extension "neoclip"
   end,
   keys = {
