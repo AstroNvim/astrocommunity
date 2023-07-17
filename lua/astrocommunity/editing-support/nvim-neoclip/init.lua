707f025917cf2406a0f42b7c31eea68e37494fb7
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2023-07-17 14:45:42 +0200

    feat(editing-support): add nvim-neoclip

---
+++ lua/astrocommunity/editing-support/nvim-neoclip/init.lua
@@ -0,0 +1,14 @@
+return {
+  "AckslD/nvim-neoclip.lua",
+  event = { "User AstroFile" },
+  dependencies = {
+    {'nvim-telescope/telescope.nvim'},
+  },
+  config = function()
+      require("neoclip").setup()
+      require('telescope').load_extension('neoclip')
+  end,
+  keys = {
+    { "<leader>fy", "<cmd>Telescope neoclip<cr>", desc = "Find yanks (neoclip)" },
+  },
+}
