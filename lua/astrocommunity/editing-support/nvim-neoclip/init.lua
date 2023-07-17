30928a2682234766159c4980e993b6349459fcc5
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2023-07-17 15:35:58 +0200

    forgot stylua formatting

--- lua/astrocommunity/editing-support/nvim-neoclip/init.lua
+++ lua/astrocommunity/editing-support/nvim-neoclip/init.lua
@@ -2,11 +2,11 @@ return {
   "AckslD/nvim-neoclip.lua",
   event = { "User AstroFile" },
   dependencies = {
-    {'nvim-telescope/telescope.nvim'},
+    { "nvim-telescope/telescope.nvim" },
   },
   config = function()
-      require("neoclip").setup()
-      require('telescope').load_extension('neoclip')
+    require("neoclip").setup()
+    require("telescope").load_extension "neoclip"
   end,
   keys = {
     { "<leader>fy", "<cmd>Telescope neoclip<cr>", desc = "Find yanks (neoclip)" },
