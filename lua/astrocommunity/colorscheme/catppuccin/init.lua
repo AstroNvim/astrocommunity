d1a190ad412eae66db42b8c842c6f7c31e951daf
Ben Woodward <ben@benwoodward.me.uk>
2023-07-19 11:35:51 +0100

    refactor(catppuccin): sort integrations

--- lua/astrocommunity/colorscheme/catppuccin/init.lua
+++ lua/astrocommunity/colorscheme/catppuccin/init.lua
@@ -3,16 +3,16 @@ return {
   name = "catppuccin",
   opts = {
     integrations = {
-      nvimtree = false,
-      ts_rainbow = false,
       aerial = true,
       dap = { enabled = true, enable_ui = true },
       mason = true,
       neotree = true,
       notify = true,
+      nvimtree = false,
       semantic_tokens = true,
       symbols_outline = true,
       telescope = true,
+      ts_rainbow = false,
       which_key = true,
     },
   },
