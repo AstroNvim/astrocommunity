a06f6e7e9f8dd77fe8fac0357b947e0696bd7f93
Ben Woodward <ben@benwoodward.me.uk>
2023-07-20 21:19:26 +0100

    fix(scope): Register extension with Telescope

--- lua/astrocommunity/bars-and-lines/scope-nvim/init.lua
+++ lua/astrocommunity/bars-and-lines/scope-nvim/init.lua
@@ -1,1 +1,5 @@
-return { "tiagovla/scope.nvim", event = "VeryLazy", opts = {} }
+return {
+  "tiagovla/scope.nvim",
+  event = "VeryLazy",
+  opts = function() require("telescope").load_extension "scope" end,
+}
