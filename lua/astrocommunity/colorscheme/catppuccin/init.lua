70135e4dd245a5a74d743ab9203603c3ccc28378
Ben Woodward <ben@benwoodward.me.uk>
2023-07-21 17:31:58 +0100

    fix(catppuccin): add recommended telescope config

--- lua/astrocommunity/colorscheme/catppuccin/init.lua
+++ lua/astrocommunity/colorscheme/catppuccin/init.lua
@@ -1,20 +1,32 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  opts = {
-    integrations = {
-      alpha = true,
-      aerial = true,
-      dap = { enabled = true, enable_ui = true },
-      mason = true,
-      neotree = true,
-      notify = true,
-      nvimtree = false,
-      semantic_tokens = true,
-      symbols_outline = true,
-      telescope = true,
-      ts_rainbow = false,
-      which_key = true,
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        alpha = true,
+        aerial = true,
+        dap = { enabled = true, enable_ui = true },
+        mason = true,
+        neotree = true,
+        notify = true,
+        nvimtree = false,
+        semantic_tokens = true,
+        symbols_outline = true,
+        telescope = true,
+        ts_rainbow = false,
+        which_key = true,
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    optional = true,
+    opts = {
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
     },
   },
 }
