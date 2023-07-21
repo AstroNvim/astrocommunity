8225f4101902a815964451e826f597562bbc8909
Juniar Rakhman <jrakhman@skynet.local>
2023-07-21 23:10:29 +0700

    feat(lsp-signature): add ray-x/lsp_signature.nvim

---
+++ lua/astrocommunity/lsp/lsp-signature-nvim/README.md
@@ -0,0 +1,5 @@
+# lsp_signature.nvim
+
+**Repository:** <https://github.com/ray-x/lsp_signature.nvim>
+
+Show function signature when you type.
---
+++ lua/astrocommunity/lsp/lsp-signature-nvim/init.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    opts = {
+      hint_enable = false, -- disable hints as it will crash in some terminal
+    },
+  },
+}
