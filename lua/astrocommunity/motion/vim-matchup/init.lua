4e3e74177bccc4b653f5eac063d25adf70e83697
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-22 13:48:12 -0700

    feat(vim-matchup): improve offscreen popup style

--- lua/astrocommunity/motion/vim-matchup/init.lua
+++ lua/astrocommunity/motion/vim-matchup/init.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = { "andymass/vim-matchup" },
   init = function()
-    vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    vim.g.matchup_matchparen_offscreen = { method = "popup", fullwidth = 1, highlight = "Normal", syntax_hl = 1 }
     vim.g.matchup_matchparen_deferred = 1
   end,
   opts = { matchup = { enable = true } },
