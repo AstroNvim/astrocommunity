9bcd159668ae6c69157281c011237e2f5a99ac62
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2023-07-23 20:27:37 +0200

    Update lua/astrocommunity/pack/php/init.lua
    Co-authored-by: Oli <57495944+owittek@users.noreply.github.com>

--- lua/astrocommunity/pack/php/init.lua
+++ lua/astrocommunity/pack/php/init.lua
@@ -19,7 +19,7 @@ return {
   {
     "jay-babu/mason-null-ls.nvim",
     opts = function(_, opts)
-      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "php-cs-fixer" })
+      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "php-cs-fixer")
     end,
   },
 }
