6f60c0f95f3ae8008a15bb401559cd726a0d2e11
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2023-07-23 21:39:25 +0200

    Update lua/astrocommunity/pack/typescript/deno.lua
    Co-authored-by: Oli <57495944+owittek@users.noreply.github.com>

--- lua/astrocommunity/pack/typescript/deno.lua
+++ lua/astrocommunity/pack/typescript/deno.lua
@@ -11,7 +11,6 @@ return {
       port = "${port}",
       executable = {
         command = "node",
-        -- 💀 Make sure to update this path to point to your installation
         args = {
           require("mason-registry").get_package("js-debug-adapter"):get_install_path()
             .. "/js-debug/src/dapDebugServer.js",
