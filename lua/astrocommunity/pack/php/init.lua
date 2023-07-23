db2a4b5c9861e62747f377ce9bd2a8519c7e6ab2
Uzair Aftab <uzaaft@outlook.com>
2023-07-23 21:08:35 +0200

    chore(php): Fix styling

--- lua/astrocommunity/pack/php/init.lua
+++ lua/astrocommunity/pack/php/init.lua
@@ -18,8 +18,6 @@ return {
   },
   {
     "jay-babu/mason-null-ls.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "php-cs-fixer")
-    end,
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "php-cs-fixer") end,
   },
 }
