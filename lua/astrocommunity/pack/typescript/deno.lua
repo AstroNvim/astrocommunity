57d8edb35327cc784e744cbf4e8d5b1a3121e207
Uzair Aftab <uzaaft@outlook.com>
2023-07-23 17:52:07 +0200

    feat(typescript): Add support for dap for JS

---
+++ lua/astrocommunity/pack/typescript/deno.lua
@@ -0,0 +1,45 @@
+local utils = require "astronvim.utils"
+
+return {
+  "mfussenegger/nvim-dap",
+  optional = true,
+  config = function()
+    local dap = require "dap"
+    dap.adapters["pwa-node"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "node",
+        -- 💀 Make sure to update this path to point to your installation
+        args = {
+          require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+            .. "/js-debug/src/dapDebugServer.js",
+          "${port}",
+        },
+      },
+    }
+    local js_config = {
+      {
+        type = "pwa-node",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        cwd = "${workspaceFolder}",
+      },
+      {
+        type = "pwa-node",
+        request = "attach",
+        name = "Attach",
+        processId = require("dap.utils").pick_process,
+        cwd = "${workspaceFolder}",
+      },
+    }
+
+    if not dap.configurations.javascript then
+      dap.configurations.javascript = js_config
+    else
+      utils.extend_tbl(dap.configurations.javascript, js_config)
+    end
+  end,
+}
