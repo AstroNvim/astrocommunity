20975a66e5e4e68907ff0ef332a60dd7cdd9797f
Uzair Aftab <uzaaft@outlook.com>
2023-07-26 19:04:16 +0200

    feat(deno): Migrate deno pack to astrolsp

--- lua/astrocommunity/pack/typescript-deno/init.lua
+++ lua/astrocommunity/pack/typescript-deno/init.lua
@@ -1,6 +1,12 @@
 local utils = require "astrocore.utils"
 
 return {
+  {
+    "AstroNvim/astrolsp",
+    opts = {
+      setup_handlers = { denols = false },
+    },
+  },
   { import = "astrocommunity.pack.json" },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -20,8 +26,7 @@ return {
   },
   {
     "sigmasd/deno-nvim",
-    init = function() astrocore.lsp.skip_setup = utils.list_insert_unique(astrocore.lsp.skip_setup, "denols") end,
     ft = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-    opts = function() return { server = require("astrocore.utils.lsp").config "denols" } end,
+    opts = function() return { server = require("astrolsp").config "denols" } end,
   },
 }
