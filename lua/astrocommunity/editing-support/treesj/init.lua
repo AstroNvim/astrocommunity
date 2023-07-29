8546e603c5c6455b5348b24dc5f13e2ebb77dcd9
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-07-29 15:26:22 -0400

    feat(editing-support): TreeSJ supports join length of 9999

--- lua/astrocommunity/editing-support/treesj/init.lua
+++ lua/astrocommunity/editing-support/treesj/init.lua
@@ -2,5 +2,5 @@ return {
   "Wansmer/treesj",
   keys = { { "<leader>m", "<CMD>TSJToggle<CR>", desc = "Toggle Treesitter Join" } },
   cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
-  opts = { use_default_keymaps = false },
+  opts = { use_default_keymaps = false, max_join_length = 9999, },
 }
