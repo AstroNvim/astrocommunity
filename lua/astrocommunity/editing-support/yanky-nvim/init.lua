a87f6a3643b314b37ba15ab7805290727ed21b15
fynnfluegge <fynnfluegge@gmx.de>
2023-07-29 19:33:59 +0200

    remove stylua:ignore

--- lua/astrocommunity/editing-support/yanky-nvim/init.lua
+++ lua/astrocommunity/editing-support/yanky-nvim/init.lua
@@ -17,8 +17,11 @@ return {
     }
   end,
   keys = {
-        -- stylua: ignore
-      { "<leader>p", function() require("telescope").extensions.yank_history.yank_history({ }) end, desc = "Open Yank History" },
+    {
+      "<leader>p",
+      function() require("telescope").extensions.yank_history.yank_history {} end,
+      desc = "Open Yank History",
+    },
     { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
     { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
     { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
