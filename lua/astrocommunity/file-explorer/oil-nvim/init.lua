2458093e2db31db9f239c25e8f0fecd1242e3279
Uzair Aftab <uzaaft@outlook.com>
2023-07-29 21:34:13 +0200

    fix(oil): Rewrite keymaps

--- lua/astrocommunity/file-explorer/oil-nvim/init.lua
+++ lua/astrocommunity/file-explorer/oil-nvim/init.lua
@@ -1,9 +1,16 @@
 return {
-  "stevearc/oil.nvim",
-  opts = {},
-  enabled = true,
-  cmd = "Oil",
-  keys = {
-    { "<leader>O", function() require("oil").open() end, desc = "Open folder in Oil" },
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        ["<leader>O"] = { function() require("oil").open() end, desc = "Open folder in Oil" },
+      },
+    },
+  },
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    enabled = true,
+    cmd = "Oil",
   },
 }
