bc42b9855b2989c0a5a07234f1387e40de4fdb3c
Uzair Aftab <uzaaft@outlook.com>
2023-07-29 21:34:06 +0200

    fix(mini-files): Rewrite keymaps

--- lua/astrocommunity/file-explorer/mini-files/init.lua
+++ lua/astrocommunity/file-explorer/mini-files/init.lua
@@ -1,17 +1,18 @@
 return {
   {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        ["<leader>e"] = { function() require("mini.files").open() end, desc = "Explorer" },
+      },
+    },
+  },
+  {
     "neo-tree.nvim",
     enabled = false,
   },
   {
     "echasnovski/mini.files",
-    keys = {
-      {
-        "<leader>e",
-        function() require("mini.files").open() end,
-        desc = "Explorer",
-      },
-    },
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
