6ba17c77a1d805dbd395407b795d190daf92e6a7
Uzair Aftab <uzaaft@outlook.com>
2023-07-29 20:52:38 +0200

    fix(transparent-nvim): Rewrite key bindigns

--- lua/astrocommunity/utility/transparent-nvim/init.lua
+++ lua/astrocommunity/utility/transparent-nvim/init.lua
@@ -1,45 +1,54 @@
 local prefix = "<leader>u"
 return {
-  "xiyaowong/transparent.nvim",
-  lazy = false,
-  opts = {
-    -- table: default groups
-    groups = {
-      "Normal",
-      "NormalNC",
-      "Comment",
-      "Constant",
-      "Special",
-      "Identifier",
-      "Statement",
-      "PreProc",
-      "Type",
-      "Underlined",
-      "Todo",
-      "String",
-      "Function",
-      "Conditional",
-      "Repeat",
-      "Operator",
-      "Structure",
-      "LineNr",
-      "NonText",
-      "SignColumn",
-      "CursorLineNr",
-      "EndOfBuffer",
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          [prefix .. "T"] = { desc = "Toggle transparency" },
+        },
+      },
     },
-    -- table: additional groups that should be cleared
-    extra_groups = {
-      "NormalFloat",
-      "NvimTreeNormal",
-      "NeoTreeNormal",
-      "NeoTreeFloatBorder",
-      "NeoTreeNormalNC",
-    },
-    -- table: groups you don't want to clear
-    exclude_groups = {},
   },
-  keys = {
-    { prefix .. "T", "<cmd>TransparentToggle<CR>", desc = "Toggle transparency" },
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    opts = {
+      -- table: default groups
+      groups = {
+        "Normal",
+        "NormalNC",
+        "Comment",
+        "Constant",
+        "Special",
+        "Identifier",
+        "Statement",
+        "PreProc",
+        "Type",
+        "Underlined",
+        "Todo",
+        "String",
+        "Function",
+        "Conditional",
+        "Repeat",
+        "Operator",
+        "Structure",
+        "LineNr",
+        "NonText",
+        "SignColumn",
+        "CursorLineNr",
+        "EndOfBuffer",
+      },
+      -- table: additional groups that should be cleared
+      extra_groups = {
+        "NormalFloat",
+        "NvimTreeNormal",
+        "NeoTreeNormal",
+        "NeoTreeFloatBorder",
+        "NeoTreeNormalNC",
+      },
+      -- table: groups you don't want to clear
+      exclude_groups = {},
+    },
   },
 }
