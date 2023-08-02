ac679a2c37beb3fdc7b1f2de1f104c8c531205cf
Uzair Aftab <uzaaft@outlook.com>
2023-08-02 21:23:32 +0200

    fix(trouble-nvim): Rewrite keymaps

--- lua/astrocommunity/diagnostics/trouble-nvim/init.lua
+++ lua/astrocommunity/diagnostics/trouble-nvim/init.lua
@@ -5,14 +5,33 @@ maps.n[prefix] = { desc = icon .. "Trouble" }
 require("astrocore").set_mappings(maps)
 return {
   {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          [prefix .. "X"] = {
+            "<cmd>TroubleToggle workspace_diagnostics<cr>",
+            desc = "Workspace Diagnostics (Trouble)",
+          },
+          [prefix .. "x"] = {
+            "<cmd>TroubleToggle document_diagnostics<cr>",
+            desc = "Document Diagnostics (Trouble)",
+          },
+          [prefix .. "l"] = {
+            "<cmd>TroubleToggle loclist<cr>",
+            desc = "Location List (Trouble)",
+          },
+          [prefix .. "q"] = {
+            "<cmd>TroubleToggle quickfix<cr>",
+            desc = "Quickfix List (Trouble)",
+          },
+        },
+      },
+    },
+  },
+  {
     "folke/trouble.nvim",
     cmd = { "TroubleToggle", "Trouble" },
-    keys = {
-      { prefix .. "X", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
-      { prefix .. "x", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
-      { prefix .. "l", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
-      { prefix .. "q", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
-    },
     opts = {
       use_diagnostic_signs = true,
       action_keys = {
