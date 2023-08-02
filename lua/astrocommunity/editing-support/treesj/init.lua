9bdd4b19778a1c411627de712abb55e1ae53e08e
Uzair Aftab <uzaaft@outlook.com>
2023-08-02 20:54:47 +0200

    fix(treesj): Rewrite keymaps

--- lua/astrocommunity/editing-support/treesj/init.lua
+++ lua/astrocommunity/editing-support/treesj/init.lua
@@ -1,6 +1,18 @@
 return {
-  "Wansmer/treesj",
-  keys = { { "<leader>m", "<CMD>TSJToggle<CR>", desc = "Toggle Treesitter Join" } },
-  cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
-  opts = { use_default_keymaps = false },
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<leader>m"] = { "<CMD>TSJToggle<CR>", desc = "Toggle Treesitter Join" },
+        },
+      },
+    },
+  },
+
+  {
+    "Wansmer/treesj",
+    cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
+    opts = { use_default_keymaps = false },
+  },
 }
