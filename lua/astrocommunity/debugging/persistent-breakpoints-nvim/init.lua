e1498ba9a4dccb85d3bb9cc78947170cb2a211b8
Uzair Aftab <uzaaft@outlook.com>
2023-08-02 21:22:04 +0200

    fix(persistent-breakpoints-nvim): Rewrite keymaps

--- lua/astrocommunity/debugging/persistent-breakpoints-nvim/init.lua
+++ lua/astrocommunity/debugging/persistent-breakpoints-nvim/init.lua
@@ -1,29 +1,41 @@
 return {
-  "Weissle/persistent-breakpoints.nvim",
-  event = "BufReadPost",
-  opts = function(_, opts)
-    return require("astrocore").extend_tbl(opts, {
-      load_breakpoints_event = { "BufReadPost" },
-    })
-  end,
-  keys = {
-    {
-      "<leader>db",
-      function() require("persistent-breakpoints.api").toggle_breakpoint() end,
-      { silent = true },
-      desc = "Toggle Breakpoint",
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<leader>db"] = {
+            function() require("persistent-breakpoints.api").toggle_breakpoint() end,
+            {
+              silent = true,
+            },
+            desc = "Toggle Breakpoint",
+          },
+          ["<leader>dB"] = {
+            function() require("persistent-breakpoints.api").clear_all_breakpoints() end,
+            {
+              silent = true,
+            },
+            desc = "Clear Breakpoints",
+          },
+          ["<leader>dC"] = {
+            function() require("persistent-breakpoints.api").set_conditional_breakpoint() end,
+            {
+              silent = true,
+            },
+            desc = "Conditional Breakpoint",
+          },
+        },
+      },
     },
-    {
-      "<leader>dB",
-      function() require("persistent-breakpoints.api").clear_all_breakpoints() end,
-      { silent = true },
-      desc = "Clear Breakpoints",
-    },
-    {
-      "<leader>dC",
-      function() require("persistent-breakpoints.api").set_conditional_breakpoint() end,
-      { silent = true },
-      desc = "Conditional Breakpoint",
-    },
+  },
+  {
+    "Weissle/persistent-breakpoints.nvim",
+    event = "BufReadPost",
+    opts = function(_, opts)
+      return require("astrocore").extend_tbl(opts, {
+        load_breakpoints_event = { "BufReadPost" },
+      })
+    end,
   },
 }
