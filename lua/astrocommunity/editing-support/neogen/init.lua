7ba8d9e71b9dc62012bf33e3e47138c27a4e4dad
Uzair Aftab <uzaaft@outlook.com>
2023-08-02 21:28:36 +0200

    fix(neogen): Rewrite keymaps

--- lua/astrocommunity/editing-support/neogen/init.lua
+++ lua/astrocommunity/editing-support/neogen/init.lua
@@ -4,21 +4,30 @@ local icon = vim.g.icons_enabled and "󰷉 " or ""
 maps.n[prefix] = { desc = icon .. "Annotation" }
 require("astrocore").set_mappings(maps)
 return {
-  "danymat/neogen",
-  cmd = "Neogen",
-  opts = {
-    snippet_engine = "luasnip",
-    languages = {
-      lua = { template = { annotation_convention = "ldoc" } },
-      typescript = { template = { annotation_convention = "tsdoc" } },
-      typescriptreact = { template = { annotation_convention = "tsdoc" } },
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          [prefix .. "<cr>"] = { function() require("neogen").generate { type = "current" } end, desc = "Current" },
+          [prefix .. "c"] = { function() require("neogen").generate { type = "class" } end, desc = "Class" },
+          [prefix .. "f"] = { function() require("neogen").generate { type = "func" } end, desc = "Function" },
+          [prefix .. "t"] = { function() require("neogen").generate { type = "type" } end, desc = "Type" },
+          [prefix .. "F"] = { function() require("neogen").generate { type = "file" } end, desc = "File" },
+        },
+      },
     },
   },
-  keys = {
-    { prefix .. "<cr>", function() require("neogen").generate { type = "current" } end, desc = "Current" },
-    { prefix .. "c", function() require("neogen").generate { type = "class" } end, desc = "Class" },
-    { prefix .. "f", function() require("neogen").generate { type = "func" } end, desc = "Function" },
-    { prefix .. "t", function() require("neogen").generate { type = "type" } end, desc = "Type" },
-    { prefix .. "F", function() require("neogen").generate { type = "file" } end, desc = "File" },
+  {
+    "danymat/neogen",
+    cmd = "Neogen",
+    opts = {
+      snippet_engine = "luasnip",
+      languages = {
+        lua = { template = { annotation_convention = "ldoc" } },
+        typescript = { template = { annotation_convention = "tsdoc" } },
+        typescriptreact = { template = { annotation_convention = "tsdoc" } },
+      },
+    },
   },
 }
