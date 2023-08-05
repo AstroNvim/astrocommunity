5bc519e1908bf6dbaa1191c83c26bc6d953c3bf3
Uzair Aftab <uzaaft@outlook.com>
2023-08-05 16:10:11 +0200

    fix(true-zen-nvim): Rewrite keymaps

--- lua/astrocommunity/editing-support/true-zen-nvim/init.lua
+++ lua/astrocommunity/editing-support/true-zen-nvim/init.lua
@@ -1,55 +1,58 @@
 local prefix = "<leader>z"
 local utils = require "astrocore"
 return {
-  "Pocco81/true-zen.nvim",
-  opts = function(_, opts)
-    return utils.extend_tbl(opts, {
-      integrations = {
-        tmux = os.getenv "TMUX" ~= nil, -- hide tmux status bar in (minimalist, ataraxis)
-        twilight = utils.is_available "twilight.nvim", -- enable twilight (ataraxis)
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        v = {
+          [prefix .. "n"] = {
+            function()
+              local truezen = require "true-zen"
+              local first = vim.fn.line "v"
+              local last = vim.fn.line "."
+              truezen.narrow(first, last)
+            end,
+            desc = "Narrow (True Zen)",
+            mode = { "v" },
+          },
+        },
+        n = {
+          [prefix] = { desc = "True Zen" },
+          [prefix .. "f"] = {
+            function() require("true-zen").focus() end,
+            desc = "Focus (True Zen)",
+          },
+          [prefix .. "m"] = {
+            function() require("true-zen").minimalist() end,
+            desc = "Minimalist (True Zen)",
+          },
+          [prefix .. "a"] = {
+            function() require("true-zen").ataraxis() end,
+            desc = "Ataraxis (True Zen)",
+          },
+          [prefix .. "n"] = {
+            function()
+              local truezen = require "true-zen"
+              local first = 0
+              local last = vim.api.nvim_buf_line_count(0)
+              truezen.narrow(first, last)
+            end,
+            desc = "Narrow (True Zen)",
+          },
+        },
       },
-    })
-  end,
-  keys = {
-    {
-      prefix,
-      desc = "True Zen",
     },
-    {
-      prefix .. "f",
-      function() require("true-zen").focus() end,
-      desc = "Focus (True Zen)",
-    },
-    {
-      prefix .. "m",
-      function() require("true-zen").minimalist() end,
-      desc = "Minimalist (True Zen)",
-    },
-    {
-      prefix .. "a",
-      function() require("true-zen").ataraxis() end,
-      desc = "Ataraxis (True Zen)",
-    },
-    {
-      prefix .. "n",
-      function()
-        local truezen = require "true-zen"
-        local first = 0
-        local last = vim.api.nvim_buf_line_count(0)
-        truezen.narrow(first, last)
-      end,
-      desc = "Narrow (True Zen)",
-    },
-    {
-      prefix .. "n",
-      function()
-        local truezen = require "true-zen"
-        local first = vim.fn.line "v"
-        local last = vim.fn.line "."
-        truezen.narrow(first, last)
-      end,
-      desc = "Narrow (True Zen)",
-      mode = { "v" },
-    },
+  },
+  {
+    "Pocco81/true-zen.nvim",
+    opts = function(_, opts)
+      return utils.extend_tbl(opts, {
+        integrations = {
+          tmux = os.getenv "TMUX" ~= nil, -- hide tmux status bar in (minimalist, ataraxis)
+          twilight = utils.is_available "twilight.nvim", -- enable twilight (ataraxis)
+        },
+      })
+    end,
   },
 }
