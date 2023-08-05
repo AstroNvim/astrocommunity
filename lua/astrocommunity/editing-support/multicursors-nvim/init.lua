22f852842be653f572f0f14c4b893962bc613781
Uzair Aftab <uzaaft@outlook.com>
2023-08-05 14:04:35 +0200

    fix(multicursors-nvim): Rewrite keymaps

--- lua/astrocommunity/editing-support/multicursors-nvim/init.lua
+++ lua/astrocommunity/editing-support/multicursors-nvim/init.lua
@@ -1,14 +1,27 @@
 return {
-  "smoka7/multicursors.nvim",
-  event = "VeryLazy",
-  dependencies = { "smoka7/hydra.nvim" },
-  opts = {},
-  keys = {
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<leader>m"] = {
+            "<cmd>MCstart<cr>",
+            desc = "Create a selection for word under the cursor",
+          },
+        },
+        v = {
+          ["<leader>m"] = {
+            "<cmd>MCstart<cr>",
+            desc = "Create a selection for word under the cursor",
+          },
+        },
+      },
+    },
     {
-      mode = { "v", "n" },
-      "<Leader>m",
-      "<cmd>MCstart<cr>",
-      desc = "Create a selection for word under the cursor",
+      "smoka7/multicursors.nvim",
+      event = "VeryLazy",
+      dependencies = { "smoka7/hydra.nvim" },
+      opts = {},
     },
   },
 }
