3ee078e5aec845669bd2c9dd4c088f46201e1ad0
Zeioth <zeioth@hotmail.com>
2023-08-05 18:33:00 +0200

    ok: when opts = {}, the config line is not necessary.

--- lua/astrocommunity/programming-tools/dooku-nvim/init.lua
+++ lua/astrocommunity/programming-tools/dooku-nvim/init.lua
@@ -2,6 +2,6 @@ return {
   {
     "Zeioth/dooku.nvim",
     cmd = { "DookuGenerate", "DookuOpen", "DookuAutoSetup" },
-    config = function(_, opts) require("dooku").setup(opts) end,
+    opts = {}
   }
 }
