5fc2c2b7920ea729ce9e7061a711a0099dba27e3
Uzair Aftab <uzaaft@outlook.com>
2023-08-05 18:40:48 +0200

    fix(dial-nvim): Rewrite keymaps

--- lua/astrocommunity/editing-support/dial-nvim/init.lua
+++ lua/astrocommunity/editing-support/dial-nvim/init.lua
@@ -1,60 +1,56 @@
 return {
-  "monaqa/dial.nvim",
-  keys = {
-    {
-      "<C-a>",
-      mode = { "v" },
-      function() return require("dial.map").inc_visual() end,
-      expr = true,
-      desc = "Increment",
-    },
-    {
-      "<C-x>",
-      mode = { "v" },
-      function() return require("dial.map").dec_visual() end,
-      expr = true,
-      desc = "Decrement",
-    },
-    {
-      "g<C-a>",
-      mode = { "v" },
-      function() return require("dial.map").inc_gvisual() end,
-      expr = true,
-      desc = "Increment",
-    },
-    {
-      "g<C-x>",
-      mode = { "v" },
-      function() return require("dial.map").dec_gvisual() end,
-      expr = true,
-      desc = "Decrement",
-    },
-    {
-      "<C-a>",
-      function() return require("dial.map").inc_normal() end,
-      expr = true,
-      desc = "Increment",
-    },
-    {
-      "<C-x>",
-      function() return require("dial.map").dec_normal() end,
-      expr = true,
-      desc = "Decrement",
-    },
-  },
-  config = function()
-    local augend = require "dial.augend"
-    require("dial.config").augends:register_group {
-      default = {
-        augend.integer.alias.decimal,
-        augend.integer.alias.hex,
-        augend.date.alias["%Y/%m/%d"],
-        augend.constant.alias.bool,
-        augend.semver.alias.semver,
-        augend.case.new {
-          types = { "camelCase", "PascalCase", "snake_case", "SCREAMING_SNAKE_CASE" },
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        v = {
+          ["<C-a>"] = {
+            function() return require("dial.map").inc_visual() end,
+            desc = "Increment",
+          },
+          ["<C-x>"] = {
+            function() return require("dial.map").dec_visual() end,
+            desc = "Decrement",
+          },
+          ["g<C-a>"] = {
+            function() return require("dial.map").inc_gvisual() end,
+            desc = "Increment",
+          },
+
+          ["g<C-x>"] = {
+            function() return require("dial.map").dec_gvisual() end,
+            desc = "Decrement",
+          },
+        },
+        n = {
+          ["<C-a>"] = {
+            function() return require("dial.map").inc_normal() end,
+            desc = "Increment",
+          },
+          ["<C-x>"] = {
+            function() return require("dial.map").dec_normal() end,
+            desc = "Decrement",
+          },
         },
       },
-    }
-  end,
+    },
+  },
+  {
+    "monaqa/dial.nvim",
+    config = function()
+      local augend = require "dial.augend"
+      require("dial.config").augends:register_group {
+        default = {
+          augend.integer.alias.decimal,
+          augend.integer.alias.hex,
+          augend.date.alias["%Y/%m/%d"],
+          augend.constant.alias.bool,
+          augend.semver.alias.semver,
+          augend.case.new {
+            types = { "camelCase", "PascalCase", "snake_case", "SCREAMING_SNAKE_CASE" },
+          },
+        },
+      }
+    end,
+  },
 }
