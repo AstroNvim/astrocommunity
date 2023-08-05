2b800b520fefa4250dc02515f27aec8c47dbb3a9
Uzair Aftab <uzaaft@outlook.com>
2023-08-05 14:09:43 +0200

    fix(grapple-nvim): Rewrite keymaps

--- lua/astrocommunity/motion/grapple-nvim/init.lua
+++ lua/astrocommunity/motion/grapple-nvim/init.lua
@@ -1,20 +1,27 @@
 local prefix = "<leader><leader>"
-local maps = { n = {} }
 local icon = vim.g.icons_enabled and "󰛢 " or ""
-maps.n[prefix] = { desc = icon .. "Grapple" }
-require("astrocore").set_mappings(maps)
 return {
-  "cbochs/grapple.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  cmd = { "Grapple" },
-  keys = {
-    { prefix .. "a", "<cmd>GrappleTag<CR>", desc = "Add file" },
-    { prefix .. "d", "<cmd>GrappleUntag<CR>", desc = "Remove file" },
-    { prefix .. "t", "<cmd>GrappleToggle<CR>", desc = "Toggle a file" },
-    { prefix .. "e", "<cmd>GrapplePopup tags<CR>", desc = "Select from tags" },
-    { prefix .. "s", "<cmd>GrapplePopup scopes<CR>", desc = "Select a project scope" },
-    { prefix .. "x", "<cmd>GrappleReset<CR>", desc = "Clear tags from current project" },
-    { "<C-n>", "<cmd>GrappleCycle forward<CR>", desc = "Select next tag" },
-    { "<C-p>", "<cmd>GrappleCycle backward<CR>", desc = "Select previous tag" },
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          [prefix] = { desc = icon .. "Grapple" },
+          [prefix .. "a"] = { "<cmd>GrappleTag<CR>", desc = "Add file" },
+          [prefix .. "d"] = { "<cmd>GrappleUntag<CR>", desc = "Remove file" },
+          [prefix .. "t"] = { "<cmd>GrappleToggle<CR>", desc = "Toggle a file" },
+          [prefix .. "e"] = { "<cmd>GrapplePopup tags<CR>", desc = "Select from tags" },
+          [prefix .. "s"] = { "<cmd>GrapplePopup scopes<CR>", desc = "Select a project scope" },
+          [prefix .. "x"] = { "<cmd>GrappleReset<CR>", desc = "Clear tags from current project" },
+          ["<C-n>"] = { "<cmd>GrappleCycle forward<CR>", desc = "Select next tag" },
+          ["<C-p>"] = { "<cmd>GrappleCycle backward<CR>", desc = "Select previous tag" },
+        },
+      },
+    },
+  },
+  {
+    "cbochs/grapple.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "Grapple" },
   },
 }
