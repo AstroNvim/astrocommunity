d9407f113f8fb16fefaf647ae39c70c8d8c9f5e5
Uzair Aftab <uzaaft@outlook.com>
2023-08-05 13:05:55 +0200

    fix(lsp_lines-nvim): Rewrite keymaps

--- lua/astrocommunity/diagnostics/lsp_lines-nvim/init.lua
+++ lua/astrocommunity/diagnostics/lsp_lines-nvim/init.lua
@@ -1,23 +1,25 @@
 return {
-  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-  event = "LspAttach",
-  keys = {
-    {
-      "<Leader>uD",
-      function()
-        vim.diagnostic.config {
-          virtual_text = not require("lsp_lines").toggle(),
-        }
-      end,
-      desc = "Toggle virtual diagnostic lines",
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<leader>uD"] = { function() require("lsp_lines").toggle() end, desc = "Toggle virtual diagnostic lines" },
+        },
+      },
     },
   },
-  opts = {},
-  config = function(_, opts)
-    -- disable diagnostic virtual text
-    local lsp_utils = require "astrolsp"
-    lsp_utils.diagnostics[3].virtual_text = false
-    vim.diagnostic.config(lsp_utils.diagnostics[vim.g.diagnostics_mode])
-    require("lsp_lines").setup(opts)
-  end,
+
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    event = "LspAttach",
+    opts = {},
+    config = function(_, opts)
+      -- disable diagnostic virtual text
+      local lsp_utils = require "astrolsp"
+      lsp_utils.diagnostics[3].virtual_text = false
+      vim.diagnostic.config(lsp_utils.diagnostics[vim.g.diagnostics_mode])
+      require("lsp_lines").setup(opts)
+    end,
+  },
 }
