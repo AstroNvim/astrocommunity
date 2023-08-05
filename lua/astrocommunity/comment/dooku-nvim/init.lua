35134589c4f3b3412ad31a6a1874eddcdf74c897
Zeioth <Zeioth@hotmail.com>
2023-08-05 18:13:08 +0200

    Changes proposed by Uzaaft - nº1
    Removed empty opts = {}.

--- lua/astrocommunity/comment/dooku-nvim/init.lua
+++ lua/astrocommunity/comment/dooku-nvim/init.lua
@@ -2,9 +2,6 @@ return {
   {
     "Zeioth/dooku.nvim",
     cmd = { "DookuGenerate", "DookuOpen", "DookuAutoSetup" },
-    opts = {
-      -- your config options here
-    },
     config = function(_, opts) require("dooku").setup(opts) end,
   }
 }
