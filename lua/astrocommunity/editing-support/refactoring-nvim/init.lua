b5e73fa5aaffc7db73147ac2dafdc6705f76a256
Uzair Aftab <uzaaft@outlook.com>
2023-08-05 21:03:33 +0200

    fix(refactoring-nvim): rewrite keymaps

--- lua/astrocommunity/editing-support/refactoring-nvim/init.lua
+++ lua/astrocommunity/editing-support/refactoring-nvim/init.lua
@@ -1,96 +1,101 @@
 return {
-  "ThePrimeagen/refactoring.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
-  opts = {},
-  keys = {
-    {
-      "<leader>re",
-      [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR>]],
-      { silent = true, expr = false },
-      mode = {
-        "v",
-        "x",
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<leader>rb"] = {
+            require("refactoring").refactor "Extract Block",
+            desc = "Extract Block",
+          },
+          ["<leader>ri"] = {
+            require("refactoring").refactor "Inline Variable",
+            desc = "Inline Variable",
+          },
+          ["<leader>rp"] = {
+            require("refactoring").debug.printf { below = false },
+            desc = "Debug: Print Function",
+          },
+          ["<leader>rc"] = {
+            require("refactoring").debug.cleanup {},
+            desc = "Debug: Clean Up",
+          },
+          ["<leader>rd"] = {
+            require("refactoring").debug.print_var { below = false },
+            desc = "Debug: Print Variable",
+          },
+          ["<leader>rbf"] = {
+            require("refactoring").refactor "Extract Block To File",
+            desc = "Extract Block To File",
+          },
+        },
+        x = {
+          ["<leader>re"] = {
+            function() require("refactoring").refactor "Extract Function" end,
+            desc = "Extract Function",
+          },
+          ["<leader>rf"] = {
+            function() require("refactoring").refactor "Extract Function To File" end,
+            desc = "Extract Function To File",
+          },
+          ["<leadeer>rv"] = {
+            require("refactoring").refactor "Extract Variable",
+            desc = "Extract Variable",
+          },
+          ["<leader>ri"] = {
+            require("refactoring").refactor "Inline Variable",
+            desc = "Inline Variable",
+          },
+        },
+        v = {
+          ["<leader>re"] = {
+            function() require("refactoring").refactor "Extract Function" end,
+            desc = "Extract Function",
+          },
+          ["<leader>rf"] = {
+            function() require("refactoring").refactor "Extract Function To File" end,
+            desc = "Extract Function To File",
+          },
+          ["<leadeer>rv"] = {
+            require("refactoring").refactor "Extract Variable",
+            desc = "Extract Variable",
+          },
+          ["<leader>ri"] = {
+            require("refactoring").refactor "Inline Variable",
+            desc = "Inline Variable",
+          },
+          ["<leader>rb"] = {
+            require("refactoring").refactor "Extract Block",
+            desc = "Extract Block",
+          },
+          ["<leader>rbf"] = {
+            require("refactoring").refactor "Extract Block To File",
+            desc = "Extract Block To File",
+          },
+          ["<leader>rr"] = {
+            require("refactoring").select_refactor,
+            desc = "Select Refactor",
+          },
+          ["<leader>rp"] = {
+            require("refactoring").debug.printf { below = false },
+            desc = "Debug: Print Function",
+          },
+          ["<leader>rc"] = {
+            require("refactoring").debug.cleanup {},
+            desc = "Debug: Clean Up",
+          },
+          ["<leader>rd"] = {
+            require("refactoring").debug.print_var { below = false },
+            desc = "Debug: Print Variable",
+          },
+        },
       },
-      desc = "Extract Function",
     },
-    {
-      "<leader>rf",
-      [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>]],
-      { silent = true, expr = false },
-      mode = {
-        "v",
-        "x",
-      },
-      desc = "Extract Function To File",
-    },
-    {
-      "<leader>rv",
-      [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]],
-      { silent = true, expr = false },
-      mode = {
-        "v",
-        "x",
-      },
-      desc = "Extract Variable",
-    },
-    {
-      "<leader>ri",
-      [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]],
-      { silent = true, expr = false },
-      mode = {
-        "n",
-        "v",
-        "x",
-      },
-      desc = "Inline Variable",
-    },
-    {
-      "<leader>rb",
-      function() require("refactoring").refactor "Extract Block" end,
-      { silent = true, expr = false },
-      mode = {
-        "n",
-      },
-      desc = "Extract Block",
-    },
-    {
-      "<leader>rbf",
-      function() require("refactoring").refactor "Extract Block To File" end,
-      { silent = true, expr = false },
-      mode = {
-        "n",
-      },
-      desc = "Extract Block To File",
-    },
-    {
-      "<leader>rr",
-      function() require("refactoring").select_refactor() end,
-      { silent = true, expr = false },
-      desc = "Select Refactor",
-    },
-    {
-      "<leader>rp",
-      function() require("refactoring").debug.printf { below = false } end,
-      mode = { "n" },
-      desc = "Debug: Print Function",
-    },
-    {
-      "<leader>rd",
-      function() require("refactoring").debug.print_var { normal = true, below = false } end,
-      mode = { "n" },
-      desc = "Debug: Print Variable",
-    },
-    {
-      "<leader>rd",
-      function() require("refactoring").debug.print_var { below = false } end,
-      mode = { "v" },
-      desc = "Debug: Print Variable",
-    },
-    {
-      "<leader>rc",
-      function() require("refactoring").debug.cleanup {} end,
-      mode = { "n" },
-      desc = "Debug: Clean Up",
-    },
+  },
+
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
+    opts = {},
   },
 }
