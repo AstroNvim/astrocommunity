2ec3316f93ab2ef311e34ff508075bdc33ec72ef
Uzair Aftab <uzaaft@outlook.com>
2023-08-05 14:04:50 +0200

    fix(telescope-zoxide): Rewrite keymaps

--- lua/astrocommunity/fuzzy-finder/telescope-zoxide/init.lua
+++ lua/astrocommunity/fuzzy-finder/telescope-zoxide/init.lua
@@ -12,13 +12,6 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "jvgrootveld/telescope-zoxide" },
-    keys = {
-      {
-        "<leader>fz",
-        "<cmd>Telescope zoxide list<CR>",
-        desc = "Find directories",
-      },
-    },
     opts = function() require("telescope").load_extension "zoxide" end,
   },
 }
