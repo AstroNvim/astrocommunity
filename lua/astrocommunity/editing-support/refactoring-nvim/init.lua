b2703c4e2490c3b8223adf728abc43767f0de2e9
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-08-18 16:41:20 -0700

    chore(refactoring-nvim): convert cmd string into table

--- lua/astrocommunity/editing-support/refactoring-nvim/init.lua
+++ lua/astrocommunity/editing-support/refactoring-nvim/init.lua
@@ -1,7 +1,7 @@
 return {
   "ThePrimeagen/refactoring.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
-  cmd = "Refactor",
+  cmd = { "Refactor" },
   opts = {},
   keys = {
     {
