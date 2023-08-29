f6546b684462e0a46f6e3e723ddf3521e4534cdb
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-08-29 16:13:38 -0700

    chore(mini-indentscope): inherit excluded filetypes/buftypes from indent_blankline

--- lua/astrocommunity/indent/mini-indentscope/init.lua
+++ lua/astrocommunity/indent/mini-indentscope/init.lua
@@ -5,31 +5,13 @@ return {
     opts = { symbol = "│", options = { try_as_border = true } },
     init = function()
       vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          "Trouble",
-          "aerial",
-          "alpha",
-          "checkhealth",
-          "dashboard",
-          "fzf",
-          "help",
-          "lazy",
-          "lspinfo",
-          "man",
-          "mason",
-          "neo-tree",
-          "notify",
-          "null-ls-info",
-          "starter",
-          "toggleterm",
-          "undotree",
-        },
-        callback = function() vim.b.miniindentscope_disable = true end,
-      })
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "*" },
+        pattern = "*",
         callback = function()
-          if vim.tbl_contains({ "nofile", "prompt", "quickfix", "terminal" }, vim.bo["buftype"]) then
+          if
+            vim.tbl_contains(vim.g.indent_blankline_filetype_exclude, vim.bo["filetype"])
+            or vim.tbl_contains(vim.g.indent_blankline_buftype_exclude, vim.bo["buftype"])
+            or vim.tbl_contains(vim.g.indent_blankline_bufname_exclude, vim.api.nvim_buf_get_name(0))
+          then
             vim.b.miniindentscope_disable = true
           end
         end,
