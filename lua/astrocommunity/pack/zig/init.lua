086f3c78be03b58416fbd1248dcdc70f3d4c67d1
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2023-09-06 07:55:54 +0200

    feat(pack): Add zig pack (#557)

---
+++ lua/astrocommunity/pack/zig/init.lua
@@ -0,0 +1,27 @@
+local utils = require "astronvim.utils"
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "zig" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "zls") end,
+  },
+
+  {
+    "NTBBloodbath/zig-tools.nvim",
+    -- Load zig-tools.nvim only in Zig buffers
+    ft = { "zig" },
+    opts = {},
+    dependencies = {
+      "akinsho/toggleterm.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+  },
+}
