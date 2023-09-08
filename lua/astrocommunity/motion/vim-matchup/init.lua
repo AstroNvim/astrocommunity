751812af94ccc9e6279a6d73da7a7a32b785f79e
Kaka Ruto <borecollins@gmail.com>
2023-09-08 22:59:35 +0100

    fix(vim-matchup): remove matchup_match_paren_deferred option (#561)
    Remove matchup_match_paren_deferred option
    
    This will leave the option set to 0 by default
    
    It was causing issues, by printing matching content on top of buffers in split screens
    
    Relevant issue: https://github.com/AstroNvim/AstroNvim/issues/2276

--- lua/astrocommunity/motion/vim-matchup/init.lua
+++ lua/astrocommunity/motion/vim-matchup/init.lua
@@ -3,7 +3,6 @@ return {
   dependencies = { "andymass/vim-matchup" },
   init = function()
     vim.g.matchup_matchparen_offscreen = { method = "popup", fullwidth = 1, highlight = "Normal", syntax_hl = 1 }
-    vim.g.matchup_matchparen_deferred = 1
   end,
   opts = { matchup = { enable = true } },
 }
