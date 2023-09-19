d4cda05a333eccd8610c951e4dd6346a876e5dee
Uzair Aftab <uzaaft@outlook.com>
2023-09-19 08:03:52 +0200

    fix(neosolarised): Add missing dependencies

--- lua/astrocommunity/colorscheme/neosolarized-nvim/init.lua
+++ lua/astrocommunity/colorscheme/neosolarized-nvim/init.lua
@@ -1,1 +1,9 @@
-return { { "svrana/neosolarized.nvim", opts = { comment_italics = true, background_set = false } } }
+return {
+  {
+    "svrana/neosolarized.nvim",
+    dependencies = {
+      "tjdevries/colorbuddy.nvim",
+    },
+    opts = { comment_italics = true, background_set = false },
+  },
+}
