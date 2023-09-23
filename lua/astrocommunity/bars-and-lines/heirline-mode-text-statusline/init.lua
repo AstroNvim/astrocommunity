dd6ce301082f7aaace340c2ce64f2afce57031de
Austin Horstman <khaneliman12@gmail.com>
2023-09-22 22:10:26 -0500

    fix(heirline): require astroui.status

--- lua/astrocommunity/bars-and-lines/heirline-mode-text-statusline/init.lua
+++ lua/astrocommunity/bars-and-lines/heirline-mode-text-statusline/init.lua
@@ -1,7 +1,7 @@
 return {
   "rebelot/heirline.nvim",
   opts = function(_, opts)
-    local status = require "astrocore.status"
+    local status = require "astroui.status"
 
     opts.statusline = { -- statusline
       hl = { fg = "fg", bg = "bg" },
