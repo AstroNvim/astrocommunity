5c3ce63f9bc2c286c0e12922972ea1e1ef06f294
valentino-sm <5059499+valentino-sm@users.noreply.github.com>
2023-10-16 19:17:29 +0500

    fix(rainbow-delimiters-nvim): fix setup function due to breaking change  (#610)
    * fix(rainbow-delimiters-nvim): fix setup function
    
    * Update lua/astrocommunity/editing-support/rainbow-delimiters-nvim/init.lua
    
    Co-authored-by: Micah Halter <micah@mehalter.com>
    
    ---------
    
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/editing-support/rainbow-delimiters-nvim/init.lua
+++ lua/astrocommunity/editing-support/rainbow-delimiters-nvim/init.lua
@@ -3,7 +3,7 @@ return {
     "HiPhish/rainbow-delimiters.nvim",
     dependencies = "nvim-treesitter/nvim-treesitter",
     event = "User AstroFile",
-    config = function(_, opts) require "rainbow-delimiters.setup"(opts) end,
+    main = "rainbow-delimiters.setup",
   },
   {
     "catppuccin/nvim",
