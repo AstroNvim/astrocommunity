c2a9056d07841b45fb27f6aa796227bb89b4eae1
Uzair Aftab <uzaaft@outlook.com>
2023-11-21 09:15:52 +0100

    feat(typescript): Add jsdoc treesitter ensured_installed table

--- lua/astrocommunity/pack/typescript/init.lua
+++ lua/astrocommunity/pack/typescript/init.lua
@@ -42,7 +42,8 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "javascript", "typescript", "tsx" })
+        opts.ensure_installed =
+          utils.list_insert_unique(opts.ensure_installed, { "javascript", "typescript", "tsx", "jsdoc" })
       end
     end,
   },
