22b24e79d18559fa84392c56115cd078553264b8
Andrea C from The App <3269984+gacallea@users.noreply.github.com>
2023-11-27 10:06:34 +0100

    feat(note-taking): add support for zettelkasten with zk-nvim (#654)
    * feat(note-taking): add support for zettelkasten with zk-nvim
    
    * Update lua/astrocommunity/note-taking/zk-nvim/README.md
    
    Co-authored-by: Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
    
    * feat(zk-nvim): Use opts instead of config
    
    * chore(zk-nvim): Remove empty line
    
    * chore(zk-nvim): Fix style
    
    ---------
    
    Co-authored-by: gacallea <gacallea@users.noreply.github.com>
    Co-authored-by: Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
    Co-authored-by: Uzair Aftab <uzaaft@outlook.com>

---
+++ lua/astrocommunity/note-taking/zk-nvim/README.md
@@ -0,0 +1,9 @@
+# zk-nvim
+
+Neovim extension for the [zk](https://github.com/mickael-menu/zk) plain text note-taking assistant.
+
+**Repository:** <https://github.com/mickael-menu/zk-nvim>
+
+> :warning: This plugin will setup and start the LSP server for you, do *not* call `require("lspconfig").zk.setup()`.
+
+
---
+++ lua/astrocommunity/note-taking/zk-nvim/init.lua
@@ -0,0 +1,15 @@
+local utils = require "astronvim.utils"
+
+return {
+  {
+    "mickael-menu/zk-nvim",
+    opts = {
+      picker = "telescope",
+    },
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "zk") end,
+  },
+}
