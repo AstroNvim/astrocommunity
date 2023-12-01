08285ae53903be19c625b88c000328db2c95ed52
Micah Halter <micah@mehalter.com>
2023-12-01 08:11:10 -0500

    fix(swift): update switch pack to AstroNvim v4

--- lua/astrocommunity/pack/swift/init.lua
+++ lua/astrocommunity/pack/swift/init.lua
@@ -3,19 +3,13 @@ local utils = require "astronvim.utils"
 return {
   {
     "xbase-lab/xbase",
-    ft = {
-      "swift",
-      "objcpp",
-      "objc",
-    },
+    ft = { "swift", "objcpp", "objc" },
     run = "make install", -- or "make install && make free_space" (not recommended, longer build time)
     dependencies = {
-      "neovim/nvim-lspconfig",
-      -- "nvim-telescope/telescope.nvim", -- optional
-      -- "nvim-lua/plenary.nvim", -- optional/requirement of telescope.nvim
-      -- "stevearc/dressing.nvim", -- optional (in case you don't use telescope but something else)
+      "AstroNvim/astrolsp",
+      opts = function(_, opts) opts.servers = require("astrocore").list_insert_unique(opts.servers, "sourcekit-lsp") end,
     },
-    init = function() require("astronvim.utils.lsp").setup "sourcekit-lsp" end,
+    opts = {},
   },
   {
     "nvimtools/none-ls.nvim",
