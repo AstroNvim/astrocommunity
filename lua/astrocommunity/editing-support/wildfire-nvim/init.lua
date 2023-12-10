e50eedd5dfe12e9652cf5c5877bc25c41262889b
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2023-12-10 13:28:57 +0100

    fix(wildfire-nvim): Use opts instead of config function

--- lua/astrocommunity/editing-support/wildfire-nvim/init.lua
+++ lua/astrocommunity/editing-support/wildfire-nvim/init.lua
@@ -2,5 +2,5 @@ return {
   "sustech-data/wildfire.nvim",
   event = "BufEnter",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  config = function() require("wildfire").setup() end,
+  opts = {},
 }
