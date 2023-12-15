a92bdaacaffd94e6b84a74eaa4ecfd207efeb358
Micah Halter <micah@mehalter.com>
2023-12-15 14:39:27 -0500

    Update lua/astrocommunity/pack/typescript/init.lua

--- lua/astrocommunity/pack/typescript/init.lua
+++ lua/astrocommunity/pack/typescript/init.lua
@@ -50,7 +50,8 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
-      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "tsserver", "eslint")
+      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "tsserver", "eslint" })
+
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("eslint_fix_creator", { clear = true }),
         desc = "Create autocommand in buffers where eslint attaches",
