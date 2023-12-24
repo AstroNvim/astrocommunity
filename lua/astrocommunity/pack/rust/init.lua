75e4e692fb84fce57960b0f92ac2a48e653c4419
zhaown <51357674+chaozwn@users.noreply.github.com>
2023-12-24 19:30:20 +0800

    feat(pack): add ron tree-sitter for rust develop.

--- lua/astrocommunity/pack/rust/init.lua
+++ lua/astrocommunity/pack/rust/init.lua
@@ -5,7 +5,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "rust")
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, {"rust", "ron"})
       end
     end,
   },
