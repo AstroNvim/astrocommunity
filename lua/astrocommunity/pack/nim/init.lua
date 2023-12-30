0040d94745495bef242e889be285ce785ec5ce02
Uzair Aftab <uzaaft@outlook.com>
2023-12-30 21:51:10 +0100

    feat(nim): Add formatter

--- lua/astrocommunity/pack/nim/init.lua
+++ lua/astrocommunity/pack/nim/init.lua
@@ -13,4 +13,8 @@ return {
       end
     end,
   },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "nimpretty") end,
+  },
 }
