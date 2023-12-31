5ae9eed83715c9501f1763c81e1091bc5473b12e
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2024-01-01 00:41:31 +0100

    feat(dial-nvim): Migrate to new mapping api, and add g<C-x> g<C-a> mappings (#700)

--- lua/astrocommunity/editing-support/dial-nvim/init.lua
+++ lua/astrocommunity/editing-support/dial-nvim/init.lua
@@ -1,43 +1,57 @@
 return {
   "monaqa/dial.nvim",
   keys = {
+    -- Visual mode mappings
     {
       "<C-a>",
       mode = { "v" },
-      function() return require("dial.map").inc_visual() end,
+      function() return require("dial.map").manipulate("increment", "visual") end,
       expr = true,
       desc = "Increment",
     },
     {
       "<C-x>",
       mode = { "v" },
-      function() return require("dial.map").dec_visual() end,
+      function() return require("dial.map").manipulate("decrement", "visual") end,
       expr = true,
       desc = "Decrement",
     },
     {
       "g<C-a>",
       mode = { "v" },
-      function() return require("dial.map").inc_gvisual() end,
+      function() return require("dial.map").manipulate("increment", "gvisual") end,
       expr = true,
       desc = "Increment",
     },
     {
       "g<C-x>",
       mode = { "v" },
-      function() return require("dial.map").dec_gvisual() end,
+      function() return require("dial.map").manipulate("decrement", "gvisual") end,
       expr = true,
       desc = "Decrement",
     },
+    -- Normal mode mappings
     {
       "<C-a>",
-      function() return require("dial.map").inc_normal() end,
+      function() return require("dial.map").manipulate("increment", "normal") end,
       expr = true,
       desc = "Increment",
     },
     {
       "<C-x>",
-      function() return require("dial.map").dec_normal() end,
+      function() return require("dial.map").manipulate("decrement", "normal") end,
+      expr = true,
+      desc = "Decrement",
+    },
+    {
+      "g<C-a>",
+      function() return require("dial.map").manipulate("increment", "gnormal") end,
+      expr = true,
+      desc = "Increment",
+    },
+    {
+      "g<C-x>",
+      function() return require("dial.map").manipulate("decrement", "gnormal") end,
       expr = true,
       desc = "Decrement",
     },
