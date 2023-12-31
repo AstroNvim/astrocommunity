6b8aef261a4821987b764ad5a866e1b80dddaf1d
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2023-12-31 02:23:16 +0100

    feat(pack): Add nim pack (#699)
    * feat(pack): Add nim pack
    
    * feat(nim): Add formatter

---
+++ lua/astrocommunity/pack/nim/init.lua
@@ -0,0 +1,20 @@
+local utils = require "astronvim.utils"
+
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "nim_langserver") end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "nim", "nim_format_string" })
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "nimpretty") end,
+  },
+}
