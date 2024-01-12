5459c88765976ad488b7a935ddfee801ee6e7471
Micah Halter <micah@mehalter.com>
2024-01-12 05:29:53 -0500

    fix(rust): fix missing astrocore import

--- lua/astrocommunity/pack/rust/init.lua
+++ lua/astrocommunity/pack/rust/init.lua
@@ -28,7 +28,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "rust")
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "rust")
       end
     end,
   },
