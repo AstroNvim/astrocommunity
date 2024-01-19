a808e3d43a5ee669b1b3f8d2b049d2be558c34cd
Micah Halter <micah@mehalter.com>
2024-01-19 11:43:10 -0500

    feat(neotest): add default mappings to neotest

--- lua/astrocommunity/test/neotest/init.lua
+++ lua/astrocommunity/test/neotest/init.lua
@@ -1,8 +1,34 @@
+local prefix = "<Leader>T"
 return {
   {
     "nvim-neotest/neotest",
-    ft = { "go", "rust", "python" },
+    lazy = true,
     dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          mappings = {
+            n = {
+              [prefix] = { desc = "󰗇 Tests" },
+              [prefix .. "t"] = { function() require("neotest").run.run() end, desc = "Run test" },
+              [prefix .. "d"] = { function() require("neotest").run.run { strategy = "dap" } end, desc = "Debug test" },
+              [prefix .. "f"] = {
+                function() require("neotest").run.run(vim.fn.expand "%") end,
+                desc = "Run all tests in file",
+              },
+              [prefix .. "p"] = {
+                function() require("neotest").run.run(vim.fn.getcwd()) end,
+                desc = "Run all tests in project",
+              },
+              [prefix .. "<CR>"] = { function() require("neotest").summary.toggle() end, desc = "Test Summary" },
+              [prefix .. "o"] = { function() require("neotest").output.open() end, desc = "Output hover" },
+              [prefix .. "O"] = { function() require("neotest").output_panel.toggle() end, desc = "Output window" },
+              ["]T"] = { function() require("neotest").jump.next() end, desc = "Next test" },
+              ["[T"] = { function() require("neotest").jump.prev() end, desc = "previous test" },
+            },
+          },
+        },
+      },
       {
         "folke/neodev.nvim",
         opts = function(_, opts)
