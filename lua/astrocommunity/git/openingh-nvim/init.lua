149ab9c83764f41bbd9642fcfdb4dd9a95edda9d
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2024-01-23 23:02:06 +0300

    feat(openingh-nvim): added mappings to copy url (#733)

--- lua/astrocommunity/git/openingh-nvim/init.lua
+++ lua/astrocommunity/git/openingh-nvim/init.lua
@@ -4,7 +4,10 @@ return {
   cmd = { "OpenInGHRepo", "OpenInGHFile", "OpenInGHFileLines" },
   keys = {
     { prefix .. "o", "<cmd>OpenInGHRepo<CR>", desc = "Open git repo in web", mode = { "n" } },
+    { prefix .. "O", "<cmd>OpenInGHRepo+<CR>", desc = "Copy git repo url", mode = { "n" } },
     { prefix .. "f", "<cmd>OpenInGHFile<CR>", desc = "Open git file in web", mode = { "n" } },
+    { prefix .. "F", "<cmd>OpenInGHFile+<CR>", desc = "Copy git file url", mode = { "n" } },
     { prefix .. "f", "<cmd>OpenInGHFileLines<CR>", desc = "Open git lines in web", mode = { "x" } },
+    { prefix .. "F", "<cmd>OpenInGHFileLines+<CR>", desc = "Copy git lines url", mode = { "x" } },
   },
 }
