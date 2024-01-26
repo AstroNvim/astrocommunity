e310e613b76c6b8caaaa73f8984506635e872211
Micah Halter <micah@mehalter.com>
2024-01-26 02:53:26 -0500

    feat(pack): add `blade` pack for Laravel Blade support (#734)

---
+++ lua/astrocommunity/pack/blade/init.lua
@@ -0,0 +1,57 @@
+vim.filetype.add {
+  pattern = {
+    [".*.php.blade"] = "blade",
+  },
+}
+
+return {
+  {
+    "AstroNvim/astrolsp",
+    ---@param opts AstroLSPOpts
+    opts = function(_, opts)
+      opts.config.blade = {
+        cmd = { "laravel-dev-generators", "lsp" },
+        filetypes = { "blade" },
+        root_dir = function(fname) return require("lspconfig").util.find_git_ancestor(fname) end,
+      }
+
+      if vim.fn.executable "laravel-dev-generators" == 1 then
+        opts.servers = require("astrocore").list_insert_unique(opts.servers, "blade")
+      end
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "blade")
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "blade_formatter")
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        blade = { "blade-formatter" },
+      },
+    },
+  },
+}
