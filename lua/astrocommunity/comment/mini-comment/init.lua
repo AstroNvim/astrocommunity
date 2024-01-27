fb36d798b76c8372ea00591d39d30b062625e04b
Kyle King <KyleKing@users.noreply.github.com>
2024-01-27 12:18:52 -0500

    update mini.comment
    Following: https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#minicomment

--- lua/astrocommunity/comment/mini-comment/init.lua
+++ lua/astrocommunity/comment/mini-comment/init.lua
@@ -2,12 +2,12 @@ return {
   { "numToStr/Comment.nvim", enabled = false },
   {
     "echasnovski/mini.comment",
-    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-    event = "User AstroFile",
+    event = "BufReadPost",
+    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring", opts = { enable_autocmd = false } },
     opts = {
-      hooks = {
-        pre = function() require("ts_context_commentstring.internal").update_commentstring {} end,
-      },
+      custom_commentstring = function()
+        return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+      end,
     },
   },
   {
