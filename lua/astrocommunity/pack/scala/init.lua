a5c297c7fc6fff18c1875a362c95d1ea3675bc89
Micah Halter <micah@mehalter.com>
2024-01-29 11:40:09 -0500

    refactor(scala): improve `nvim-metals` implementation

--- lua/astrocommunity/pack/scala/init.lua
+++ lua/astrocommunity/pack/scala/init.lua
@@ -18,27 +18,25 @@ return {
   },
   {
     "scalameta/nvim-metals",
-    init = function()
+    ft = { "scala", "sbt", "java" },
+    opts = function()
+      local metals = require "metals"
+      local user_config = require("astrolsp").lsp_opts "metals"
+      if require("astrocore").is_available "nvim-dap" then
+        local on_attach = user_config.on_attach
+        user_config.on_attach = function(...)
+          on_attach(...)
+          metals.setup_dap()
+        end
+      end
+      return require("astrocore").extend_tbl(metals.bare_config(), user_config)
+    end,
+    config = function(self, opts)
       vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "scala", "sbt" },
-        callback = function()
-          if vim.fn.executable "metals" ~= 1 and vim.fn.executable "coursier" ~= 1 then
-            vim.notify "`metals` or `coursier` are required to be installed for `nvim-metals` to work."
-            return
-          end
-
-          local metals = require "metals"
-
-          local user_config = require("astrolsp").lsp_opts "metals"
-          local old_on_attach = user_config.on_attach
-          user_config.on_attach = function(...)
-            old_on_attach(...)
-            metals.setup_dap()
-          end
-
-          metals.initialize_or_attach(require("astrocore").extend_tbl(metals.bare_config(), user_config))
-        end,
+        pattern = self.ft,
         group = vim.api.nvim_create_augroup("nvim-metals", { clear = true }),
+        desc = "Initialize and attach nvim-metals",
+        callback = function() require("metals").initialize_or_attach(opts) end,
       })
     end,
   },
