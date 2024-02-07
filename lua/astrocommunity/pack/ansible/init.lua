d71489cf766a6575900ebad367dae63933a53a6c
Micah Halter <micah@mehalter.com>
2024-02-07 09:46:30 -0500

    feat(ansible): add `nvim-lint` to pack

--- lua/astrocommunity/pack/ansible/init.lua
+++ lua/astrocommunity/pack/ansible/init.lua
@@ -51,4 +51,13 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        ansible = { "ansible_lint" },
+      },
+    },
+  },
 }
