be266537b94997fee82b7de828d0004720283076
Micah Halter <micah@mehalter.com>
2024-02-07 10:18:50 -0500

    feat(nix): add `nvim-lint` and `conform`

--- lua/astrocommunity/pack/nix/init.lua
+++ lua/astrocommunity/pack/nix/init.lua
@@ -29,4 +29,22 @@ return {
       end
     end,
   },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        nix = { "alejandra" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        nix = { "statix" },
+      },
+    },
+  },
 }
