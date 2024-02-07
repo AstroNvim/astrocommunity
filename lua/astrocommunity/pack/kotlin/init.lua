e59bdf90b3067f1b3af6f3184a3a9b88e93afb09
Micah Halter <micah@mehalter.com>
2024-02-07 10:18:34 -0500

    feat(kotlin): add `nvim-lint`

--- lua/astrocommunity/pack/kotlin/init.lua
+++ lua/astrocommunity/pack/kotlin/init.lua
@@ -31,4 +31,13 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "kotlin")
     end,
   },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        kotlin = { "ktlint" },
+      },
+    },
+  },
 }
