4fb8d59ac829462644726f628ee0d5e5dcf4d19c
Micah Halter <micah@mehalter.com>
2024-02-07 10:19:39 -0500

    feat(proto): add `conform` and `nvim-lint` support

--- lua/astrocommunity/pack/proto/init.lua
+++ lua/astrocommunity/pack/proto/init.lua
@@ -23,4 +23,22 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "buf")
     end,
   },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        proto = { "buf" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        proto = { "buf_lint" },
+      },
+    },
+  },
 }
