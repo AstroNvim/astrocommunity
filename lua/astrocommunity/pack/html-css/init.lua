344efbeee4f1f739ee1f8d10656da5db5906164c
Micah Halter <micah@mehalter.com>
2024-02-07 10:18:06 -0500

    feat(html-css): add `conform` support

--- lua/astrocommunity/pack/html-css/init.lua
+++ lua/astrocommunity/pack/html-css/init.lua
@@ -17,4 +17,23 @@ return {
         require("astrocore").list_insert_unique(opts.ensure_installed, "html", "cssls", "emmet_ls")
     end,
   },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "prettierd")
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        html = { { "prettierd", "prettier" } },
+        css = { { "prettierd", "prettier" } },
+        scss = { { "prettierd", "prettier" } },
+        less = { { "prettierd", "prettier" } },
+      },
+    },
+  },
 }
