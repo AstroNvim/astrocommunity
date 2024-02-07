008cc92a91f8c1125248831334e4cd7231eade36
Micah Halter <micah@mehalter.com>
2024-02-07 10:19:30 -0500

    feat(php): add `conform` support

--- lua/astrocommunity/pack/php/init.lua
+++ lua/astrocommunity/pack/php/init.lua
@@ -29,4 +29,13 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "php-cs-fixer")
     end,
   },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { "php_cs_fixer" },
+      },
+    },
+  },
 }
