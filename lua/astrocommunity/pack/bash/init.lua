5ea323691feca75d078ccbfe94ef5d0b143ac5b7
Micah Halter <micah@mehalter.com>
2024-02-07 09:46:39 -0500

    feat(bash): add `nvim-lint` to pack

--- lua/astrocommunity/pack/bash/init.lua
+++ lua/astrocommunity/pack/bash/init.lua
@@ -38,4 +38,13 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        sh = { "shellcheck" },
+      },
+    },
+  },
 }
