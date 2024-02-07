dd0cb7c6b8e08e2befb36d3f4807b71fb2c87014
Micah Halter <micah@mehalter.com>
2024-02-07 10:17:23 -0500

    feat(elm): add `conform` support

--- lua/astrocommunity/pack/elm/init.lua
+++ lua/astrocommunity/pack/elm/init.lua
@@ -7,18 +7,25 @@ return {
       end
     end,
   },
-
   {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "elmls")
     end,
   },
-
   {
     "jay-babu/mason-null-ls.nvim",
     opts = function(_, opts)
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "elm_format")
     end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        elm = { "elm_format" },
+      },
+    },
   },
 }
