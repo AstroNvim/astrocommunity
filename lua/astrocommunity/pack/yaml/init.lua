580c75434cf0e22cf48e2aee4970acae645dc7ed
Micah Halter <micah@mehalter.com>
2024-02-08 10:24:39 -0500

    feat(yaml): add `mason-tool-installer` config

--- lua/astrocommunity/pack/yaml/init.lua
+++ lua/astrocommunity/pack/yaml/init.lua
@@ -47,6 +47,14 @@ return {
     end,
   },
   {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, "yaml-language-server", "prettierd")
+    end,
+  },
+  {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
