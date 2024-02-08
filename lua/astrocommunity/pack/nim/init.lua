c5661cc6e69e4b81c27668b251aabdaf6ac1d3d9
Micah Halter <micah@mehalter.com>
2024-02-08 10:23:09 -0500

    feat(nim): add `mason-tool-installer` config

--- lua/astrocommunity/pack/nim/init.lua
+++ lua/astrocommunity/pack/nim/init.lua
@@ -15,9 +15,10 @@ return {
     end,
   },
   {
-    "jay-babu/mason-null-ls.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "nimpretty")
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "nimlangserver")
     end,
   },
 }
