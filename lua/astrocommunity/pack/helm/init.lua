3ef290e471e1cc6b091c6d9a3b0e3353216d61c6
Micah Halter <micah@mehalter.com>
2024-02-08 10:22:32 -0500

    feat(helm): add `mason-tool-installer` config

--- lua/astrocommunity/pack/helm/init.lua
+++ lua/astrocommunity/pack/helm/init.lua
@@ -24,25 +24,13 @@ return {
     optional = true,
     opts = function(_, opts)
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "helm_ls")
-
-      local configs = require "lspconfig.configs"
-      local lspconfig = require "lspconfig"
-      local util = require "lspconfig.util"
-
-      if not configs.helm_ls then
-        configs.helm_ls = {
-          default_config = {
-            cmd = { "helm_ls", "serve" },
-            filetypes = { "helm" },
-            root_dir = function(fname) return util.root_pattern "Chart.yaml"(fname) end,
-          },
-        }
-      end
-
-      lspconfig.helm_ls.setup {
-        filetypes = { "helm" },
-        cmd = { "helm_ls", "serve" },
-      }
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "helm-ls")
     end,
   },
   {
