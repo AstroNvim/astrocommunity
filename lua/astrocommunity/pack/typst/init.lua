b6382b5e646afde3def5021ad064d4034a508547
Micah Halter <micah@mehalter.com>
2024-02-08 10:24:27 -0500

    feat(typst): add `mason-tool-installer` config

--- lua/astrocommunity/pack/typst/init.lua
+++ lua/astrocommunity/pack/typst/init.lua
@@ -6,4 +6,11 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "typst_lsp")
     end,
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "typst-lsp")
+    end,
+  },
 }
