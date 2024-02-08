aa034a0a9c2d85c5faedb45c6d979a03b44fd964
Micah Halter <micah@mehalter.com>
2024-02-08 10:23:30 -0500

    feat(ps1): add `mason-tool-installer` config

--- lua/astrocommunity/pack/ps1/init.lua
+++ lua/astrocommunity/pack/ps1/init.lua
@@ -6,5 +6,13 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "powershell_es")
     end,
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, "powershell-editor-services")
+    end,
+  },
   { "PProvost/vim-ps1", ft = "ps1" },
 }
