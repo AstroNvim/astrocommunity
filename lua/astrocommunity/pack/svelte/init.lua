2b5be438a412322cf2b42eb6716eb1536e634454
Micah Halter <micah@mehalter.com>
2024-02-08 10:23:53 -0500

    feat(svelte): add `mason-tool-installer` config

--- lua/astrocommunity/pack/svelte/init.lua
+++ lua/astrocommunity/pack/svelte/init.lua
@@ -22,4 +22,12 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "js")
     end,
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, "svelte-language-server", "js-debug-adapter")
+    end,
+  },
 }
