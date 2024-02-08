cb7674e750694d2d2f9310762f6241b77209ea80
Micah Halter <micah@mehalter.com>
2024-02-08 10:21:41 -0500

    feat(clojure): add `mason-tool-installer` config

--- lua/astrocommunity/pack/clojure/init.lua
+++ lua/astrocommunity/pack/clojure/init.lua
@@ -8,6 +8,13 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "clojure_lsp")
     end,
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "clojure-lsp")
+    end,
+  },
   -- Clojure parser
   {
     "nvim-treesitter/nvim-treesitter",
