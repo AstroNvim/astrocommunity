fe67144df7d2533b1eeae2374606c053dee70893
Micah Halter <micah@mehalter.com>
2024-02-08 10:22:46 -0500

    feat(json): add `mason-tool-installer` config

--- lua/astrocommunity/pack/json/init.lua
+++ lua/astrocommunity/pack/json/init.lua
@@ -37,4 +37,11 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "jsonls")
     end,
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "json-lsp")
+    end,
+  },
 }
