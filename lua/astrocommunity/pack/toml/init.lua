5221762eef23805681490886b69aebef1fa23a9a
Micah Halter <micah@mehalter.com>
2024-02-08 10:24:13 -0500

    feat(toml): add `mason-tool-installer` config

--- lua/astrocommunity/pack/toml/init.lua
+++ lua/astrocommunity/pack/toml/init.lua
@@ -25,4 +25,11 @@ return {
       opts.handlers.taplo = function() end
     end,
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "taplo")
+    end,
+  },
 }
