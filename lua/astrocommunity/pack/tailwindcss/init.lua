7f480a458f7726cf71bce54316ac7d951f147ea8
Micah Halter <micah@mehalter.com>
2024-02-08 10:24:02 -0500

    feat(tailwindcss): add `mason-tool-installer` config

--- lua/astrocommunity/pack/tailwindcss/init.lua
+++ lua/astrocommunity/pack/tailwindcss/init.lua
@@ -8,6 +8,14 @@ return {
     end,
   },
   {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, "tailwindcss-language-server")
+    end,
+  },
+  {
     "hrsh7th/nvim-cmp",
     optional = true,
     dependencies = {
