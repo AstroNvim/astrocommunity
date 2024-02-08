c520cb9ef298bb40c9003ebba680fca02db81d80
Micah Halter <micah@mehalter.com>
2024-02-08 10:21:19 -0500

    feat(ansible): add `mason-tool-installer` config

--- lua/astrocommunity/pack/ansible/init.lua
+++ lua/astrocommunity/pack/ansible/init.lua
@@ -8,6 +8,14 @@ return {
     end,
   },
   {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, "ansible-lint", "ansible-language-server")
+    end,
+  },
+  {
     "williamboman/mason-lspconfig.nvim",
     optional = true,
     opts = function(_, opts)
