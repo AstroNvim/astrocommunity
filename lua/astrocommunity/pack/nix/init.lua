be76a7418620450b54929f29c4a54ef72decdbb4
Micah Halter <micah@mehalter.com>
2024-02-08 10:23:12 -0500

    feat(nix): add `mason-tool-installer` config

--- lua/astrocommunity/pack/nix/init.lua
+++ lua/astrocommunity/pack/nix/init.lua
@@ -16,6 +16,13 @@ return {
     end,
   },
   {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "rnix-lsp")
+    end,
+  },
+  {
     "nvimtools/none-ls.nvim",
     optional = true,
     opts = function(_, opts)
