f2470b050e87f75fbef20cdc50a4707a9cfe5009
Micah Halter <micah@mehalter.com>
2024-02-08 10:21:54 -0500

    feat(crystal): add `mason-tool-installer` config

--- lua/astrocommunity/pack/crystal/init.lua
+++ lua/astrocommunity/pack/crystal/init.lua
@@ -17,4 +17,11 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "codelldb")
     end,
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "crystalline", "codelldb")
+    end,
+  },
 }
