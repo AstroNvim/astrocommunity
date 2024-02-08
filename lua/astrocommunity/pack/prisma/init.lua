867430534a4f8e037a8664a087c0e9cc76d1d1f0
Micah Halter <micah@mehalter.com>
2024-02-08 10:23:22 -0500

    feat(prisma): add `mason-tool-installer` config

--- lua/astrocommunity/pack/prisma/init.lua
+++ lua/astrocommunity/pack/prisma/init.lua
@@ -15,4 +15,11 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "prismals")
     end,
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "prisma-language-server")
+    end,
+  },
 }
