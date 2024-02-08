1e12b0a09f4a5d4405703969e1aa23ea617e6eda
Micah Halter <micah@mehalter.com>
2024-02-08 10:21:26 -0500

    feat(astro): add `mason-tool-installer` config

--- lua/astrocommunity/pack/astro/init.lua
+++ lua/astrocommunity/pack/astro/init.lua
@@ -23,4 +23,12 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "js")
     end,
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, "astro-language-server", "js-debug-adapter")
+    end,
+  },
 }
