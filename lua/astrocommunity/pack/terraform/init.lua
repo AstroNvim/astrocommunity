83bd2ad278bb9b6a720c3fc068903666ece18bd6
Micah Halter <micah@mehalter.com>
2024-02-08 10:24:06 -0500

    feat(terraform): add `mason-tool-installer` config

--- lua/astrocommunity/pack/terraform/init.lua
+++ lua/astrocommunity/pack/terraform/init.lua
@@ -19,8 +19,15 @@ return {
     "jay-babu/mason-null-ls.nvim",
     optional = true,
     opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "tflint", "tfsec")
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
       opts.ensure_installed =
-        require("astrocore").list_insert_unique(opts.ensure_installed, "tflint", "tfsec", "terraform_fmt")
+        require("astrocore").list_insert_unique(opts.ensure_installed, "terraform-ls", "tflint", "tfsec")
     end,
   },
   {
