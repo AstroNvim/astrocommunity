2954c8fd005dac1c1a6bec50882512ec393fc667
Micah Halter <micah@mehalter.com>
2024-02-08 10:23:57 -0500

    feat(swift): add `mason-tool-installer` config

--- lua/astrocommunity/pack/swift/init.lua
+++ lua/astrocommunity/pack/swift/init.lua
@@ -16,6 +16,13 @@ return {
     end,
   },
   {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "codelldb")
+    end,
+  },
+  {
     "AstroNvim/astrolsp",
     ---@type AstroLSPOpts
     opts = {
