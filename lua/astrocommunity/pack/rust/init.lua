7fe2fd5d591b3960e983236c47aff07acc58eab2
Micah Halter <micah@mehalter.com>
2024-02-08 10:23:48 -0500

    feat(rust): add `mason-tool-installer` config

--- lua/astrocommunity/pack/rust/init.lua
+++ lua/astrocommunity/pack/rust/init.lua
@@ -51,6 +51,13 @@ return {
     end,
   },
   {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "codelldb")
+    end,
+  },
+  {
     "mrcjkb/rustaceanvim",
     version = "^3",
     ft = "rust",
