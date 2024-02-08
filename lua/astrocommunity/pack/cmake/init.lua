d722a9c7642229d76c006da638daa2de2fa7ac1d
Micah Halter <micah@mehalter.com>
2024-02-08 10:21:46 -0500

    feat(cmake): add `mason-tool-installer` config

--- lua/astrocommunity/pack/cmake/init.lua
+++ lua/astrocommunity/pack/cmake/init.lua
@@ -15,4 +15,11 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "neocmake")
     end,
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "neocmakelsp")
+    end,
+  },
 }
