20eeb6457b655b542300727c466d52dd58a5332a
Micah Halter <micah@mehalter.com>
2024-02-08 10:24:35 -0500

    feat(wgsl): add `mason-tool-installer` config

--- lua/astrocommunity/pack/wgsl/init.lua
+++ lua/astrocommunity/pack/wgsl/init.lua
@@ -13,4 +13,11 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "wgsl_analyzer")
     end,
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "wgsl-analyzer")
+    end,
+  },
 }
