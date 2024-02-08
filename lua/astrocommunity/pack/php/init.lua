7fe2230cdccad5c12eddb8ace62ce70164c72c0e
Micah Halter <micah@mehalter.com>
2024-02-08 10:23:16 -0500

    feat(php): add `mason-tool-installer` config

--- lua/astrocommunity/pack/php/init.lua
+++ lua/astrocommunity/pack/php/init.lua
@@ -30,6 +30,14 @@ return {
     end,
   },
   {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, "phpactor", "php-debug-adapter", "php-cs-fixer")
+    end,
+  },
+  {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
