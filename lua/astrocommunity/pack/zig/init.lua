bfbd86b94674c118a84751a6bf6191f634336c98
Micah Halter <micah@mehalter.com>
2024-02-08 10:24:42 -0500

    feat(zig): add `mason-tool-installer` config

--- lua/astrocommunity/pack/zig/init.lua
+++ lua/astrocommunity/pack/zig/init.lua
@@ -15,7 +15,13 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "zls")
     end,
   },
-
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "zls")
+    end,
+  },
   {
     "https://codeberg.org/NTBBloodbath/zig-tools.nvim",
     -- Load zig-tools.nvim only in Zig buffers
