9c1b7c98e8ab1a5aba40a11f5c8cc607da12daf7
Micah Halter <micah@mehalter.com>
2024-02-08 10:21:35 -0500

    feat(blade): add `mason-tool-installer` config

--- lua/astrocommunity/pack/blade/init.lua
+++ lua/astrocommunity/pack/blade/init.lua
@@ -46,6 +46,13 @@ return {
     end,
   },
   {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "blade-formatter")
+    end,
+  },
+  {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
