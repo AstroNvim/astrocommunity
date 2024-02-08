e7ef5b29e276d285df978bbf96493e02bbc123d1
Micah Halter <micah@mehalter.com>
2024-02-08 10:24:10 -0500

    feat(thrift): add `mason-tool-installer` config

--- lua/astrocommunity/pack/thrift/init.lua
+++ lua/astrocommunity/pack/thrift/init.lua
@@ -16,4 +16,11 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "thriftls")
     end,
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "thriftls")
+    end,
+  },
 }
