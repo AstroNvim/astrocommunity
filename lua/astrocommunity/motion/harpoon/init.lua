0151343be2066f14eef15a971e83b09671217d00
luxus <luxuspur@gmail.com>
2024-02-12 16:20:08 +0100

    feat(harpoon)!: Update to v2 (#741)
    * feat(harpoon): Update to v2 branch
    
    * fix(harpoon): Remove invalid cmd table
    
    ---------
    
    Co-authored-by: Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>

--- lua/astrocommunity/motion/harpoon/init.lua
+++ lua/astrocommunity/motion/harpoon/init.lua
@@ -7,38 +7,43 @@ require("astronvim.utils").set_mappings(maps)
 return {
   {
     "ThePrimeagen/harpoon",
+    branch = "harpoon2",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
     },
-    cmd = { "Harpoon" },
     keys = {
-      { prefix .. "a", function() require("harpoon.mark").add_file() end, desc = "Add file" },
-      { prefix .. "e", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
+      { prefix .. "a", function() require("harpoon"):list():append() end, desc = "Add file" },
+      {
+        prefix .. "e",
+        function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end,
+        desc = "Toggle quick menu",
+      },
       {
         "<C-x>",
         function()
           vim.ui.input({ prompt = "Harpoon mark index: " }, function(input)
             local num = tonumber(input)
-            if num then require("harpoon.ui").nav_file(num) end
+            if num then require("harpoon"):list():select(num) end
           end)
         end,
         desc = "Goto index of mark",
       },
-      { "<C-p>", function() require("harpoon.ui").nav_prev() end, desc = "Goto previous mark" },
-      { "<C-n>", function() require("harpoon.ui").nav_next() end, desc = "Goto next mark" },
+      { "<C-p>", function() require("harpoon"):list():prev() end, desc = "Goto previous mark" },
+      { "<C-n>", function() require("harpoon"):list():next() end, desc = "Goto next mark" },
       { prefix .. "m", "<cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" },
       {
         prefix .. "t",
         function()
           vim.ui.input({ prompt = term_string .. " window number: " }, function(input)
             local num = tonumber(input)
-            if num then require("harpoon." .. term_string).gotoTerminal(num) end
+            if num then require("harpoon").term.gotoTerminal(num) end
           end)
         end,
         desc = "Go to " .. term_string .. " window",
       },
     },
+    opts = {},
   },
   {
     "catppuccin/nvim",
