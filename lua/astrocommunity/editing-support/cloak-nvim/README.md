3b881932c27a41d4623c2d71d9774a33f5fa6f91
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2024-02-15 12:37:49 +0100

    feat(editing-support): Add cloak.nvim (#761)

---
+++ lua/astrocommunity/editing-support/cloak-nvim/README.md
@@ -0,0 +1,5 @@
+# Cloak.nvim
+
+Cloak allows you to overlay *'s over defined patterns in defined files.
+
+**Repository:** <https://github.com/laytan/cloak.nvim>
---
+++ lua/astrocommunity/editing-support/cloak-nvim/init.lua
@@ -0,0 +1,7 @@
+---@type LazySpec
+return {
+  "laytan/cloak.nvim",
+  opts = {},
+  event = "User AstroFile",
+  cmd = { "CloakDisable", "CloakEnable", "CloakToggle" },
+}
