515050b6116f7bebaffa981876763b1c9c47e8de
Micah Halter <micah@mehalter.com>
2024-02-22 08:54:56 -0500

    fix(noice-nvim): update `lsp_handlers` location in AstroLSP

--- lua/astrocommunity/utility/noice-nvim/init.lua
+++ lua/astrocommunity/utility/noice-nvim/init.lua
@@ -15,12 +15,13 @@ return {
     ---@param opts AstroLSPOpts
     opts = function(_, opts)
       local noice_opts = require("astrocore").plugin_opts "noice.nvim"
-      if -- check if noice handlers are disabled
-        vim.tbl_get(noice_opts, "lsp", "hover", "enabled") ~= false
-        or vim.tbl_get(noice_opts, "lsp", "signature", "enabled") ~= false
-      then
-        if not opts.features then opts.features = {} end
-        opts.features.lsp_handlers = false
+      -- disable the necessary handlers in AstroLSP
+      if not opts.lsp_handlers then opts.lsp_handlers = {} end
+      if vim.tbl_get(noice_opts, "lsp", "hover", "enabled") ~= false then
+        opts.lsp_handlers["textDocument/hover"] = false
+      end
+      if vim.tbl_get(noice_opts, "lsp", "signature", "enabled") ~= false then
+        opts.lsp_handlers["textDocument/signatureHelp"] = false
       end
     end,
   },
