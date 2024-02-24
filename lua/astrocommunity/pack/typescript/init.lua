6a43f648bca3345064450d58389ecf1e5c854491
Micah Halter <micah@mehalter.com>
2024-02-24 09:35:19 -0500

    fix(typescript): remove incorrect `mason-null-ls` entry

--- lua/astrocommunity/pack/typescript/init.lua
+++ lua/astrocommunity/pack/typescript/init.lua
@@ -42,8 +42,7 @@ return {
     "jay-babu/mason-null-ls.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed =
-        require("astrocore").list_insert_unique(opts.ensure_installed, { "prettierd", "eslint-lsp" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "prettierd" })
       if not opts.handlers then opts.handlers = {} end
 
       local has_prettier = function(util)
