cf4f3c171cbf7ecdee1ec62df2d5add21f4b13c0
Micah Halter <micah@mehalter.com>
2024-02-27 11:51:47 -0500

    feat(gist-nvim): configure `vim.g` options in AstroCore

--- lua/astrocommunity/git/gist-nvim/init.lua
+++ lua/astrocommunity/git/gist-nvim/init.lua
@@ -1,27 +1,28 @@
 return {
-  {
-    "Rawnly/gist.nvim",
-    dependencies = {
-      {
-        "samjwill/nvim-unception",
-        lazy = false,
-        init = function() vim.g.unception_block_while_host_edits = true end,
+  "Rawnly/gist.nvim",
+  dependencies = {
+    {
+      "samjwill/nvim-unception",
+      lazy = false,
+      dependencies = {
+        "AstroNvim/astrocore",
+        opts = { options = { g = {
+          unception_block_while_host_edits = true,
+        } } },
       },
     },
-    cmd = { "GistCreate", "GistCreateFromFile", "GistsList" },
-    config = function()
-      require("gist").setup {
-        private = false,
-        clipboard = "+",
-        list = {
-          -- If there are multiple files in a gist you can scroll them,
-          -- with vim-like bindings n/p next previous
-          mappings = {
-            next_file = "<C-n>",
-            prev_file = "<C-p>",
-          },
-        },
-      }
-    end,
+  },
+  cmd = { "GistCreate", "GistCreateFromFile", "GistsList" },
+  opts = {
+    private = false,
+    clipboard = "+",
+    list = {
+      -- If there are multiple files in a gist you can scroll them,
+      -- with vim-like bindings n/p next previous
+      mappings = {
+        next_file = "<C-n>",
+        prev_file = "<C-p>",
+      },
+    },
   },
 }
