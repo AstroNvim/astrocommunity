882ad05867d71a4ff8dfa690d7b0fed852b79e00
Micah Halter <micah@mehalter.com>
2024-02-27 18:04:16 -0500

    fix(lsp_lines-nvim): update diagnostic configuration

--- lua/astrocommunity/diagnostics/lsp_lines-nvim/init.lua
+++ lua/astrocommunity/diagnostics/lsp_lines-nvim/init.lua
@@ -12,14 +12,14 @@ return {
         },
       },
     },
+    {
+      "AstroNvim/astrolsp",
+      opts = {
+        diagnostics = {
+          virtual_text = false,
+        },
+      },
+    },
   },
   opts = {},
-  config = function(_, opts)
-    -- disable diagnostic virtual text
-    local lsp_utils = require "astrolsp"
-
-    lsp_utils.diagnostics[3].virtual_text = false
-    vim.diagnostic.config(lsp_utils.diagnostics[vim.g.diagnostics_mode])
-    require("lsp_lines").setup(opts)
-  end,
 }
