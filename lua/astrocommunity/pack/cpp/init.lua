92fcfa407d73e46ebea871edad7e92e7069e50a4
Micah Halter <micah@mehalter.com>
2024-02-27 11:53:46 -0500

    feat(cpp): configure autocommands with AstroCore

--- lua/astrocommunity/pack/cpp/init.lua
+++ lua/astrocommunity/pack/cpp/init.lua
@@ -32,20 +32,26 @@ return {
   },
   {
     "p00f/clangd_extensions.nvim",
-    init = function()
-      -- load clangd extensions when clangd attaches
-      local augroup = vim.api.nvim_create_augroup("clangd_extensions", { clear = true })
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = augroup,
-        desc = "Load clangd_extensions with clangd",
-        callback = function(args)
-          if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "clangd" then
-            require "clangd_extensions"
-            vim.api.nvim_del_augroup_by_id(augroup)
-          end
-        end,
-      })
-    end,
+    lazy = true,
+    dependencies = {
+      "AstroNvim/astrocore",
+      opts = {
+        autocmds = {
+          clangd_extensions = {
+            {
+              event = "LspAttach",
+              desc = "Load clangd_extensions with clangd",
+              callback = function(args)
+                if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "clangd" then
+                  require "clangd_extensions"
+                  vim.api.nvim_del_augroup_by_name "clangd_extensions"
+                end
+              end,
+            },
+          },
+        },
+      },
+    },
   },
   {
     "Civitasv/cmake-tools.nvim",
