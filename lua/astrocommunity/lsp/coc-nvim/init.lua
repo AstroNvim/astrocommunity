9230bd3a4e2342b6ab50ab89726e3731361e3843
Uzair Aftab <uzaaft@outlook.com>
2024-02-29 19:27:51 +0100

    feat(lsp): Add basic coc support

---
+++ lua/astrocommunity/lsp/coc-nvim/init.lua
@@ -0,0 +1,8 @@
+return {
+  { "jay-babu/mason-null-ls.nvim", enabled = false },
+  { "AstroNvim/astrolsp", enabled = false },
+  { "neovim/nvim-lspconfig", enabled = false },
+  { "nvimtools/none-ls.nvim", enabled = false },
+  { "hrsh7th/cmp-nvim-lsp", enabled = false },
+  { "neoclide/coc.nvim", branch = "release", event = "BufEnter" },
+}
