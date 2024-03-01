85c8944e938c07cc86446518ee5e5a17dad6ef40
Micah Halter <micah@mehalter.com>
2024-03-01 12:02:18 -0500

    feat(pack): add Templ language pack

---
+++ lua/astrocommunity/pack/templ/init.lua
@@ -0,0 +1,18 @@
+vim.filetype.add { extension = { templ = "templ" } }
+local utils = require "astronvim.utils"
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Ensure that opts.ensure_installed exists and is a table or string "all".
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "templ")
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "templ") end,
+  },
+}
