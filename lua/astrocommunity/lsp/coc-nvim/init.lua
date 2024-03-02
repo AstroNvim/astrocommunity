37c344404a8b44ef15be76bbbe66872d8a741def
Uzair Aftab <uzaaft@outlook.com>
2024-03-02 10:49:51 +0100

    fix(coc-nvim): Disable more plugins

--- lua/astrocommunity/lsp/coc-nvim/init.lua
+++ lua/astrocommunity/lsp/coc-nvim/init.lua
@@ -1,8 +1,84 @@
 return {
-  { "jay-babu/mason-null-ls.nvim", enabled = false },
-  { "AstroNvim/astrolsp", enabled = false },
-  { "neovim/nvim-lspconfig", enabled = false },
-  { "nvimtools/none-ls.nvim", enabled = false },
-  { "hrsh7th/cmp-nvim-lsp", enabled = false },
   { "neoclide/coc.nvim", branch = "release", event = "BufEnter" },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    enabled = false,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    enabled = false,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    enabled = false,
+  },
+  {
+    "williamboman/nvim-lsp-installer",
+    enabled = false,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    enabled = false,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    enabled = false,
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    enabled = false,
+  },
+  {
+    "lvimuser/lsp-inlayhints.nvim",
+    config = false,
+    enabled = false,
+  },
+  {
+    "rcarriga/cmp-dap",
+    enabled = false,
+  },
+  -- refactor
+  {
+    "ThePrimeagen/refactoring.nvim",
+    enabled = false,
+  },
+  -- luaship
+  {
+    "L3MON4D3/LuaSnip",
+    enabled = false,
+  },
+  {
+    "rafamadriz/friendly-snippets",
+    enabled = false,
+  },
+  -- cmp
+  {
+    "hrsh7th/nvim-cmp",
+    enabled = false,
+  },
+  {
+    "hrsh7th/cmp-buffer",
+    enabled = false,
+  },
+  {
+    "hrsh7th/cmp-path",
+    enabled = false,
+  },
+  {
+    "saadparwaiz1/cmp_luasnip",
+    enabled = false,
+  },
+  {
+    "folke/trouble.nvim",
+    enabled = false,
+  },
+  {
+    "neoclide/coc.nvim",
+    branch = "release",
+    lazy = false,
+  },
+  {
+    "folke/neoconf.nvim",
+    enabled = false,
+  },
 }
