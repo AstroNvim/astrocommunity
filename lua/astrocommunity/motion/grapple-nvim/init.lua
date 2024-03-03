c730a7ee7c6fafc75d0356230b0b378ab4327733
Calvin Bochulak <calvin.bochulak@gmail.com>
2024-03-03 01:04:01 -0700

    feat(grapple-nvim): better keymaps + add nvim-web-devicons dependency (#781)
    fix(grapple-nvim): better keymaps + add nvim-web-devicons as a lazy dependency
    
    Co-authored-by: Calvin Bochulak <calvin@Calvins-MBP.lan>

--- lua/astrocommunity/motion/grapple-nvim/init.lua
+++ lua/astrocommunity/motion/grapple-nvim/init.lua
@@ -5,14 +5,17 @@ maps.n[prefix] = { desc = icon .. "Grapple" }
 require("astronvim.utils").set_mappings(maps)
 return {
   "cbochs/grapple.nvim",
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons", lazy = true },
+  },
   cmd = { "Grapple" },
   keys = {
     { prefix .. "a", "<cmd>Grapple tag<CR>", desc = "Add file" },
     { prefix .. "d", "<cmd>Grapple untag<CR>", desc = "Remove file" },
     { prefix .. "t", "<cmd>Grapple toggle<CR>", desc = "Toggle a file" },
-    { prefix .. "e", "<cmd>Grapple open_tags<CR>", desc = "Select from tags" },
-    { prefix .. "s", "<cmd>Grapple open_scopes<CR>", desc = "Select a scope" },
-    { prefix .. "l", "<cmd>Grapple open_loaded<CR>", desc = "Select a loaded scope" },
+    { prefix .. "e", "<cmd>Grapple toggle_tags<CR>", desc = "Select from tags" },
+    { prefix .. "s", "<cmd>Grapple toggle_scopes<CR>", desc = "Select a scope" },
+    { prefix .. "l", "<cmd>Grapple toggle_loaded<CR>", desc = "Select a loaded scope" },
     { prefix .. "x", "<cmd>Grapple reset<CR>", desc = "Clear tags from current project" },
     { "<C-n>", "<cmd>Grapple cycle forward<CR>", desc = "Select next tag" },
     { "<C-p>", "<cmd>Grapple cycle backward<CR>", desc = "Select previous tag" },
