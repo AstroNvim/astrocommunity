83ab9739cb42ed3af223e132d795605b56fe5867
Micah Halter <micah@mehalter.com>
2024-03-04 10:51:17 -0500

    feat(nvim-lint): integrate fallback and global linters with core `nvim-lint` API

--- lua/astrocommunity/lsp/nvim-lint/init.lua
+++ lua/astrocommunity/lsp/nvim-lint/init.lua
@@ -17,27 +17,26 @@ return {
           or linter
       end
 
-      local function try_lint()
-        local names = lint._resolve_linter_by_ft(vim.bo.filetype)
-
+      local orig_resolve_linter_by_ft = lint._resolve_linter_by_ft
+      lint._resolve_linter_by_ft = function(...)
+        local linters = orig_resolve_linter_by_ft(...)
         -- Add fallback linters and global linters.
-        if #names == 0 then names = lint.linters_by_ft["_"] or {} end
-        vim.list_extend(names, lint.linters_by_ft["*"] or {})
+        if not linters[1] then linters = lint.linters_by_ft["_"] or {} end
+        require("astrocore").list_insert_unique(linters, lint.linters_by_ft["*"] or {})
 
         -- Filter out linters that don't exist or don't match the condition.
         local ctx = { filename = vim.api.nvim_buf_get_name(0) }
         ctx.dirname = vim.fn.fnamemodify(ctx.filename, ":h")
-        names = vim.tbl_filter(function(name)
+        linters = vim.tbl_filter(function(name)
           local linter = lint.linters[name]
           return linter
             and vim.fn.executable(linter.cmd) == 1
             and not (type(linter) == "table" and linter.condition and not linter.condition(ctx))
-        end, names)
-
-        lint.try_lint(names)
+        end, linters)
+        return linters
       end
 
-      try_lint() -- start linter immediately
+      lint.try_lint() -- start linter immediately
       local timer = vim.loop.new_timer()
       vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave", "TextChanged" }, {
         group = vim.api.nvim_create_augroup("auto_lint", { clear = true }),
@@ -45,7 +44,7 @@ return {
         callback = function()
           timer:start(100, 0, function()
             timer:stop()
-            vim.schedule(try_lint)
+            vim.schedule(lint.try_lint)
           end)
         end,
       })
