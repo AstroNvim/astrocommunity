995f1e8cbc61fc0c1d61e0aff2bbe023e7178bca
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-03-06 18:02:18 +0100

    refactor(neovide): made it cleaner (credits: @mehalter)

--- lua/astrocommunity/recipes/neovide/init.lua
+++ lua/astrocommunity/recipes/neovide/init.lua
@@ -1,98 +1,77 @@
-local g = vim.g
-if g.neovide == nil then return {} end
+if not vim.g.neovide then return {} end
 
 ---@param scale_factor number
 ---@return number
 local function clamp_scale_factor(scale_factor)
-  return math.max(math.min(scale_factor, g["neovide_max_scale_factor"]), g["neovide_min_scale_factor"])
+  return math.max(math.min(scale_factor, vim.g.neovide_max_scale_factor), vim.g.neovide_min_scale_factor)
 end
 
 ---@param scale_factor number
 ---@param clamp? boolean
----@return nil
 local function set_scale_factor(scale_factor, clamp)
   if clamp then scale_factor = clamp_scale_factor(scale_factor) end
-  g["neovide_scale_factor"] = scale_factor
+  vim.g.neovide_scale_factor = scale_factor
 end
 
----@return nil
-local function reset_scale_factor() g["neovide_scale_factor"] = g["neovide_initial_scale_factor"] end
+local function reset_scale_factor() vim.g.neovide_scale_factor = vim.g.neovide_initial_scale_factor end
 
 ---@param increment number
 ---@param clamp? boolean
----@return nil
 local change_scale_factor = function(increment, clamp)
-  local new_scale = g["neovide_scale_factor"] + increment
+  local new_scale = vim.g.neovide_scale_factor + increment
   set_scale_factor(new_scale, clamp)
 end
 
 ---@type LazySpec
 return {
-  {
-    "AstroNvim/astrocore",
-    ---@param opts AstroCoreOpts
-    opts = function(_, opts)
-      local neovide_scale_factor = (opts.options.opt_global and opts.options.opt_global["neovide_scale_factor"])
-        or (opts.options.g and opts.options.g["neovide_scale_factor"])
-        or g["neovide_scale_factor"]
-        or 1
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    options = {
+      g = {
+        neovide_change_scale_factor = vim.g.neovide_change_scale_factor or 0.1,
+        neovide_min_scale_factor = vim.g.neovide_min_scale_factor or 0.7,
+        neovide_max_scale_factor = vim.g.neovide_max_scale_factor or 2.0,
+        neovide_initial_scale_factor = vim.g.neovide_scale_factor or 1,
+        neovide_scale_factor = vim.g.neovide_scale_factor or 1,
+      },
+    },
+    commands = {
+      NeovideSetScaleFactor = {
+        function(event)
+          local scale_factor, option = tonumber(event.fargs[1]), event.fargs[2]
 
-      local options = {
-        g = {
-          neovide_change_scale_factor = g["neovide_change_scale_factor"] or 0.1,
-          neovide_min_scale_factor = g["neovide_min_scale_factor"] or 0.7,
-          neovide_max_scale_factor = g["neovide_max_scale_factor"] or 2.0,
-          neovide_initial_scale_factor = neovide_scale_factor,
-          neovide_scale_factor = neovide_scale_factor,
+          if not scale_factor then
+            vim.notify(
+              "Error: scale factor argument is nil or not a valid number.",
+              vim.log.levels.ERROR,
+              { title = "Recipe: neovide" }
+            )
+            return
+          end
+
+          set_scale_factor(scale_factor, option ~= "force")
+        end,
+        nargs = "+",
+        desc = "Set Neovide scale size",
+      },
+      NeovideResetScaleFactor = {
+        reset_scale_factor,
+        desc = "Reset Neovide scaling",
+      },
+    },
+    mappings = {
+      n = {
+        ["<C-=>"] = {
+          function() change_scale_factor(vim.g.neovide_change_scale_factor, true) end,
+          desc = "Neovide increase scale",
         },
-      }
-
-      local commands = {
-        ["NeovideSetScaleFactor"] = {
-          function(event)
-            local scale_factor = tonumber(event.fargs[1])
-            local option = event.fargs[2]
-
-            if scale_factor == nil then
-              vim.notify(
-                "Error: scale factor argument is nil or not a valid number.",
-                vim.log.levels.ERROR,
-                { title = "Recipe: neovide" }
-              )
-              return
-            end
-
-            set_scale_factor(scale_factor, option ~= "force")
-          end,
-          nargs = "+",
+        ["<C-->"] = {
+          function() change_scale_factor(-vim.g.neovide_change_scale_factor, true) end,
+          desc = "Neovide decrease scale",
         },
-        ["NeovideResetScaleFactor"] = {
-          reset_scale_factor,
-        },
-      }
-
-      local mappings = {
-        n = {
-          ["<C-=>"] = {
-            function() change_scale_factor(g["neovide_change_scale_factor"], true) end,
-            desc = "Neovide increase scale",
-          },
-          ["<C-->"] = {
-            function() change_scale_factor(-g["neovide_change_scale_factor"], true) end,
-            desc = "Neovide decrease scale",
-          },
-          ["<C-0>"] = {
-            reset_scale_factor,
-            desc = "Neovide reset scale",
-          },
-        },
-      }
-
-      return vim.tbl_deep_extend("force", opts, {
-        options = options,
-        mappings = mappings,
-        commands = commands,
-      } --[[@as AstroCoreOpts]])
-    end,
+        ["<C-0>"] = { reset_scale_factor, desc = "Neovide reset scale" },
+      },
+    },
   },
 }
