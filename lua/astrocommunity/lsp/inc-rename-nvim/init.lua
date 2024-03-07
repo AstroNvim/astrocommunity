90e40cb12c56a6e7c98097e8c71d52a36e0d53e4
Micah Halter <micah@mehalter.com>
2024-03-07 16:04:01 -0500

    feat(inc-rename-nvim): use dressing if available for input box and autofill word under cursor

--- lua/astrocommunity/lsp/inc-rename-nvim/init.lua
+++ lua/astrocommunity/lsp/inc-rename-nvim/init.lua
@@ -1,34 +1,32 @@
 return {
-  {
-    "smjonas/inc-rename.nvim",
-    event = "User AstroLspSetup",
-    dependencies = {
+  "smjonas/inc-rename.nvim",
+  event = "User AstroLspSetup",
+  dependencies = {
+    {
       "AstroNvim/astrolsp",
       optional = true,
       ---@type AstroLSPOpts
       opts = {
-        ---@diagnostic disable: missing-fields
         mappings = {
           n = {
             ["<Leader>lr"] = {
-              ":IncRename ",
-              desc = "IncRename",
+              function() return ":IncRename " .. vim.fn.expand "<cword>" end,
+              expr = true,
+              desc = "Rename current symbol",
               cond = "textDocument/rename",
             },
           },
         },
       },
     },
-    cmd = "IncRename",
-    opts = {},
-  },
-  {
-    "folke/noice.nvim",
-    optional = true,
-    opts = {
-      presets = {
-        inc_rename = true,
-      },
+    {
+      "folke/noice.nvim",
+      optional = true,
+      opts = { presets = { inc_rename = true } },
     },
   },
+  opts = function(_, opts)
+    local is_available = require("astrocore").is_available
+    if is_available "dressing.nvim" and not is_available "noice.nvim" then opts.input_buffer_type = "dressing" end
+  end,
 }
