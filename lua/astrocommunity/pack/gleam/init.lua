b6beeed30e616810063599df8d50c1790868be6d
Micah Halter <micah.halter@gtri.gatech.edu>
2024-03-19 12:13:48 -0400

    fix(gleam): move to manual gleam setup rather than mason based beacuse deprecation

--- lua/astrocommunity/pack/gleam/init.lua
+++ lua/astrocommunity/pack/gleam/init.lua
@@ -8,16 +8,11 @@ return {
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    "AstroNvim/astrolsp",
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "gleam" })
-    end,
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "gleam" })
+      if vim.fn.executable "gleam" == 1 then
+        opts.servers = require("astrocore").list_insert_unique(opts.servers, { "gleam" })
+      end
     end,
   },
 }
