2a028c97de89b89d937beb3c91477bd1ae3fbbd1
fuchami <fuchami23@icloud.com>
2024-03-26 04:18:49 +0900

    feat(indent): add indent-rainbowline (#808)
    * feat(indent): add indent-rainbowline
    
    * fix(indent-rainbowline): Pass in opts from function into make_opts call
    
    * Update lua/astrocommunity/indent/indent-rainbowline/init.lua
    
    Co-authored-by: Micah Halter <micah@mehalter.com>
    
    ---------
    
    Co-authored-by: Uzair Aftab <muaf@nmbu.no>
    Co-authored-by: Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
    Co-authored-by: Micah Halter <micah@mehalter.com>

---
+++ lua/astrocommunity/indent/indent-rainbowline/init.lua
@@ -0,0 +1,7 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  opts = function(_, opts)
+    return require("indent-rainbowline").make_opts(opts, require("astrocore").plugin_opts "indent-rainbowline.nvim")
+  end,
+  dependencies = { "TheGLander/indent-rainbowline.nvim" },
+}
