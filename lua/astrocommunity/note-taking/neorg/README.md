9060d0367cb1c162e6b6043277f39bab2ca8390e
ALVAROPING1 <alvaroping1@gmail.com>
2024-03-25 13:34:58 +0100

    fix(neorg)!: fix breaking change in v8.0.0

--- lua/astrocommunity/note-taking/neorg/README.md
+++ lua/astrocommunity/note-taking/neorg/README.md
@@ -3,3 +3,5 @@
 Modernity meets insane extensibility. The future of organizing your life in Neovim.
 
 **Repository:** <https://github.com/nvim-neorg/neorg>
+
+**Requirements:** Lua 5.1/LuaJIT and Lua header files installed as system dependencies
--- lua/astrocommunity/note-taking/neorg/init.lua
+++ lua/astrocommunity/note-taking/neorg/init.lua
@@ -1,23 +1,29 @@
 return {
-  "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  event = "VeryLazy",
-  opts = {
-    load = {
-      ["core.defaults"] = {}, -- Loads default behaviour
-      ["core.concealer"] = {}, -- Adds pretty icons to your documents
-      ["core.keybinds"] = {}, -- Adds default keybindings
-      ["core.completion"] = {
-        config = {
-          engine = "nvim-cmp",
-        },
-      }, -- Enables support for completion plugins
-      ["core.journal"] = {}, -- Enables support for the journal module
-      ["core.dirman"] = { -- Manages Neorg workspaces
-        config = {
-          workspaces = {
-            notes = "~/projects/notes",
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- We'd like this plugin to load first out of the rest
+    config = true,
+  },
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    event = "VeryLazy",
+    opts = {
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.keybinds"] = {}, -- Adds default keybindings
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          },
+        }, -- Enables support for completion plugins
+        ["core.journal"] = {}, -- Enables support for the journal module
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/projects/notes",
+            },
           },
         },
       },
