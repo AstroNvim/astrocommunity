0dd68e064b9e4cdda06044809a6fc39051bf7c21
Uzair Aftab <muaf@nmbu.no>
2024-03-25 09:38:22 +0100

    fix(indent-rainbowline): Pass in opts from function into make_opts call

--- lua/astrocommunity/indent/indent-rainbowline/init.lua
+++ lua/astrocommunity/indent/indent-rainbowline/init.lua
@@ -2,7 +2,7 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     event = "User AstroFile",
-    opts = function() return require("indent-rainbowline").make_opts() end,
+    opts = function(_, opts) return require("indent-rainbowline").make_opts(opts) end,
     dependencies = {
       "TheGLander/indent-rainbowline.nvim",
     },
