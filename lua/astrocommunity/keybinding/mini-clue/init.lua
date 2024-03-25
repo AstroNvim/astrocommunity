6b272683452fd7079b0fe3f4c36398995cc81993
William Roy <wroy@proton.me>
2024-03-25 16:41:22 -0400

    feat(mini.clue): add windows submode (#814)

--- lua/astrocommunity/keybinding/mini-clue/init.lua
+++ lua/astrocommunity/keybinding/mini-clue/init.lua
@@ -59,7 +59,11 @@ return {
           miniclue.gen_clues.g(),
           miniclue.gen_clues.marks(),
           miniclue.gen_clues.registers(),
-          miniclue.gen_clues.windows(),
+          miniclue.gen_clues.windows {
+            submode_move = true,
+            submode_navigate = true,
+            submode_resize = true,
+          },
           miniclue.gen_clues.z(),
         },
       }
