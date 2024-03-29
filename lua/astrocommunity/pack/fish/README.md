ca2d13168944a936903940934e67d4d0e35d0313
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-03-29 20:07:59 +0100

    feat(pack): add fish pack

---
+++ lua/astrocommunity/pack/fish/README.md
@@ -0,0 +1,7 @@
+# Elm Language Pack
+
+This plugin pack does the following:
+
+- Adds `fish` Treesitter parser
+- Adds `fish_indent` formatter
+- Adds `fish` linter
---
+++ lua/astrocommunity/pack/fish/init.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "fish" })
+      end
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require "null-ls"
+      if type(opts.sources) == "table" then
+        opts.sources = vim.list_extend(opts.sources, {
+          nls.builtins.formatting.fish_indent,
+          nls.builtins.diagnostics.fish,
+        })
+      end
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        fish = { "fish_indent" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        fish = { "fish" },
+      },
+    },
+  },
+}
