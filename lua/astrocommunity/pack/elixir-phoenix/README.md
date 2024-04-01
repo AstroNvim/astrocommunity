f85fb3721e78d5ba397c43a22bf4748d19585499
Tres Henry <tres@treshenry.net>
2024-04-01 23:01:14 +0200

    Proposal to add a pack for elixir/phoenix

---
+++ lua/astrocommunity/pack/elixir-phoenix/README.md
@@ -0,0 +1,10 @@
+# Elixir and Phoenix Pack
+
+Adds baseline support for Elixir/Phoenix projects.
+
+It:
+
+- Adds the `tailwindcss` AstroCommunity pack
+- Adds the `elixirls` language server
+- Adds the `elixir` and `heex` Treesitter parsers
+- Ensures `tailwindcss` is configured for html-eex and `class:` style hinting.
---
+++ lua/astrocommunity/pack/elixir-phoenix/init.lua
@@ -0,0 +1,43 @@
+return {
+  { import = "astrocommunity.pack.tailwindcss" },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "elixirls" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "elixir", "heex" })
+      end
+    end,
+  },
+  {
+    "AstroNvim/astrolsp",
+    opts = {
+      config = {
+        tailwindcss = {
+          init_options = {
+            userLanguages = {
+              heex = "html-eex",
+              elixir = "html-eex",
+            },
+          },
+          settings = {
+            tailwindCSS = {
+              experimental = {
+                classRegex = {
+                  'class[:]\\s*"([^"]*)"',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
