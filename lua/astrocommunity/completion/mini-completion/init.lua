bd12edd7e2a88a9db2bcbbc1771e06d4e5054856
Uzair Aftab <muaf@nmbu.no>
2024-04-07 11:29:46 +0200

    fix(mini-completion): Disable all cmp plugins

--- lua/astrocommunity/completion/mini-completion/init.lua
+++ lua/astrocommunity/completion/mini-completion/init.lua
@@ -1,6 +1,10 @@
 return {
   { "hrsh7th/nvim-cmp", enabled = false },
+  { "hrsh7th/cmp-buffer", enabled = false },
+  { "hrsh7th/cmp-nvim-lsp", enabled = false },
+  { "hrsh7th/cmp-path", enabled = false },
   { "L3MON4D3/LuaSnip", enabled = false },
+  { "saadparwaiz1/cmp_luasnip", enabled = false },
   { "rcarriga/cmp-dap", enabled = false },
   { "echasnovski/mini.completion", lazy = false, opts = {} },
 }
