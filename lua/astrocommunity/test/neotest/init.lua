15932f5eaf26187e44fb1345986e9326b1391ba8
PatMulligan <43773168+PatMulligan@users.noreply.github.com>
2024-04-07 18:56:12 -0600

    added dependency FixCursorHold
    Neotest uses the CursorHold event. This uses the updatetime setting which is by default very high, and lowering this can lead to excessive writes to disk. It's recommended to use https://github.com/antoinemadec/FixCursorHold.nvim which allows detaching updatetime from the frequency of the CursorHold event. The repo claims it is no longer needed but it is still recommended (See this issue)

--- lua/astrocommunity/test/neotest/init.lua
+++ lua/astrocommunity/test/neotest/init.lua
@@ -6,6 +6,7 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-neotest/nvim-nio",
+      "antoinemadec/FixCursorHold.nvim",
       {
         "AstroNvim/astrocore",
         opts = {
