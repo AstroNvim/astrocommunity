653f5eb1a8bd31d4040e2a86c3064f06a46e421a
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-09 21:21:53 +0200

    feat(lsp): add `nvim-context-vt`

---
+++ lua/astrocommunity/lsp/nvim-context-vt/init.lua
@@ -0,0 +1,19 @@
+---@type LazySpec
+return {
+  "andersevenrud/nvim_context_vt",
+  event = "User AstroFile",
+  dependencies = {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          ["<Leader>uv"] = {
+            function() require("nvim_context_vt").toggle() end,
+            name = "Toggle virutal text context",
+          },
+        },
+      },
+    },
+  },
+}
