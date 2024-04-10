3f030d5b2d8a6fb8db88ff53062aa48459d06df6
Ramesh Santhanakrishnan <rameshms@yahoo.com>
2024-04-10 10:30:25 -0700

    fix: Align the blame window properly when using heirline

--- lua/astrocommunity/git/blame-nvim/init.lua
+++ lua/astrocommunity/git/blame-nvim/init.lua
@@ -1,4 +1,21 @@
 return {
   "FabijanZulj/blame.nvim",
   event = "User AstroGitFile",
+  dependencies = {
+    { -- configure heirline with custom hook to enable winbar for 'blame' filetype
+      "rebelot/heirline.nvim",
+      optional = true,
+      opts = function(_, opts)
+        local disable_winbar_cb = vim.tbl_get(opts, "opts", "disable_winbar_cb")
+        return require("astrocore").extend_tbl(opts, {
+          opts = {
+            disable_winbar_cb = function(args)
+              if vim.bo[args.buf].filetype == "blame" then return false end
+              if disable_winbar_cb then return disable_winbar_cb(args) end
+            end,
+          },
+        })
+      end,
+    },
+  },
 }
