62c3fb2f68928e0fb5745876bce1d00caa9d71eb
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-10 21:25:27 +0200

    feat(blame-nvim): add binding to toggle git blame

--- lua/astrocommunity/git/blame-nvim/init.lua
+++ lua/astrocommunity/git/blame-nvim/init.lua
@@ -2,6 +2,20 @@ return {
   "FabijanZulj/blame.nvim",
   event = "User AstroGitFile",
   dependencies = {
+    {
+      "AstroNvim/astrocore",
+      ---@type AstroCoreOpts
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>gB"] = {
+              "<cmd>ToggleBlame<cr>",
+              desc = "Toggle git blame",
+            },
+          },
+        },
+      },
+    },
     { -- configure heirline with custom hook to enable winbar for 'blame' filetype
       "rebelot/heirline.nvim",
       optional = true,
