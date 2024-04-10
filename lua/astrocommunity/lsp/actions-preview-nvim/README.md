3f64ba35980eecfa65062cb67bca5bacfdc8bb97
Manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-04-10 19:41:34 +0200

    feat(lsp): add `actions-preview.nvim` plugin (#855)
    * feat(lsp): add `actions-preview.nvim` plugin
    
    * feat(actions-preview-nvim): Lazy load plugin
    
    Co-authored-by: Micah Halter <micah@mehalter.com>
    
    * refactor(actions-preview-nvim): made mapping smaller
    
    ---------
    
    Co-authored-by: Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
    Co-authored-by: Micah Halter <micah@mehalter.com>

---
+++ lua/astrocommunity/lsp/actions-preview-nvim/README.md
@@ -0,0 +1,5 @@
+# Actions Preview
+
+Fully customizable previewer for LSP code actions.
+
+**Repository:** <https://github.com/aznhe21/actions-preview.nvim>
---
+++ lua/astrocommunity/lsp/actions-preview-nvim/init.lua
@@ -0,0 +1,30 @@
+---@type LazySpec
+return {
+  "aznhe21/actions-preview.nvim",
+  lazy = true,
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    {
+      "AstroNvim/astrolsp",
+      ---@type AstroLSPOpts
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>la"] = {
+              function() require("actions-preview").code_actions() end,
+              desc = "LSP code action",
+              cond = "testDocument/codeAction",
+            },
+          },
+          v = {
+            ["<Leader>la"] = {
+              function() require("actions-preview").code_actions() end,
+              desc = "LSP code action",
+              cond = "testDocument/codeAction",
+            },
+          },
+        },
+      },
+    },
+  },
+}
