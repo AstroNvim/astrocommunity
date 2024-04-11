bb88a06ed0614bdeb8cf4edb191baf190503b7d1
Micah Halter <micah@mehalter.com>
2024-04-11 10:09:14 -0400

    fix(mini-move): properly load `opts` when building `keys` lazy loading

--- lua/astrocommunity/motion/mini-move/init.lua
+++ lua/astrocommunity/motion/mini-move/init.lua
@@ -1,17 +1,19 @@
 return {
   {
     "echasnovski/mini.move",
-    keys = function(plugin, keys)
+    keys = function(_, keys)
+      local plugin = require("lazy.core.config").spec.plugins["mini.move"]
+      local opts = require("lazy.core.plugin").values(plugin, "opts", false) -- resolve mini.clue options
       -- Populate the keys based on the user's options
       local mappings = {
-        { plugin.opts.mappings.line_left, desc = "Move line left" },
-        { plugin.opts.mappings.line_right, desc = "Move line right" },
-        { plugin.opts.mappings.line_down, desc = "Move line down" },
-        { plugin.opts.mappings.line_up, desc = "Move line up" },
-        { plugin.opts.mappings.left, desc = "Move selection left", mode = "v" },
-        { plugin.opts.mappings.right, desc = "Move selection right", mode = "v" },
-        { plugin.opts.mappings.down, desc = "Move selection down", mode = "v" },
-        { plugin.opts.mappings.up, desc = "Move selection up", mode = "v" },
+        { opts.mappings.line_left, desc = "Move line left" },
+        { opts.mappings.line_right, desc = "Move line right" },
+        { opts.mappings.line_down, desc = "Move line down" },
+        { opts.mappings.line_up, desc = "Move line up" },
+        { opts.mappings.left, desc = "Move selection left", mode = "v" },
+        { opts.mappings.right, desc = "Move selection right", mode = "v" },
+        { opts.mappings.down, desc = "Move selection down", mode = "v" },
+        { opts.mappings.up, desc = "Move selection up", mode = "v" },
       }
       mappings = vim.tbl_filter(function(m) return m[1] and #m[1] > 0 end, mappings)
       return vim.list_extend(mappings, keys)
