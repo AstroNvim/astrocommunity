f1456b70a82b3a71a7f7992564138357c81c6b3a
Gear J <12108619+gearj@users.noreply.github.com>
2024-04-13 00:22:34 +0700

    fix(rest-nvim)!: update to rest.nvim v2 (#877)

--- lua/astrocommunity/programming-language-support/rest-nvim/init.lua
+++ lua/astrocommunity/programming-language-support/rest-nvim/init.lua
@@ -1,31 +1,34 @@
 return {
   {
     "rest-nvim/rest.nvim",
-    ft = { "http", "json" },
-    cmd = {
-      "RestNvim",
-      "RestNvimPreview",
-      "RestNvimLast",
-    },
+    ft = "http",
+    cmd = "Rest",
     dependencies = {
-      "nvim-lua/plenary.nvim",
+      {
+        "vhyrro/luarocks.nvim",
+        priority = 1000,
+        config = true,
+      },
       {
         "AstroNvim/astrocore",
         opts = function(_, opts)
           local maps = opts.mappings
           local prefix = "<Leader>r"
           maps.n[prefix] = { desc = "RestNvim" }
-          maps.n[prefix .. "r"] = { "<Plug>RestNvim", desc = "Run request" }
+          maps.n[prefix .. "r"] = { "<cmd>Rest run<cr>", desc = "Run request under the cursor" }
+          maps.n[prefix .. "l"] = { "<cmd>Rest run last<cr>", desc = "Re-run latest request" }
         end,
       },
     },
+    main = "rest-nvim",
     opts = {},
   },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "http", "json" })
+        opts.ensure_installed =
+          require("astrocore").list_insert_unique(opts.ensure_installed, { "lua", "xml", "http", "json", "graphql" })
       end
     end,
   },
