53a2ce575edadb34e7dc993a07fd9b3e02d7bff4
Micah Halter <micah@mehalter.com>
2024-04-12 13:39:17 -0400

    fix(nvim-sftp-sync): remove lazy loading to play nicer with `:UpdateRemotePlugins`

--- lua/astrocommunity/remote-development/nvim-sftp-sync/init.lua
+++ lua/astrocommunity/remote-development/nvim-sftp-sync/init.lua
@@ -1,12 +1,5 @@
 return {
   "dcampos/nvim-sftp-sync",
   build = ":UpdateRemotePlugins",
-  cmd = {
-    "SftpDisable",
-    "SftpEnable",
-    "SftpOpenLog",
-    "SftpReset",
-    "SftpSelectServer",
-    "SftpSend",
-  },
+  event = "VeryLazy",
 }
