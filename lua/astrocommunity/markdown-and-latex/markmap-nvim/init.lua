7fec933cc86969ffcef499327e9241024daa7cd9
Javier E. <jaedns@gmail.com>
2024-04-15 12:07:10 -0300

    fix(markmap-nvim): the package in Mason is named `markmap-cli`
    * Place `"Zeioth/markmap.nvim"` with specs inside brackets.
    * And load only with markdown files.

--- lua/astrocommunity/markdown-and-latex/markmap-nvim/init.lua
+++ lua/astrocommunity/markdown-and-latex/markmap-nvim/init.lua
@@ -1,16 +1,19 @@
 --  [markdown markmap]
 --  https://github.com/Zeioth/markmap.nvim
 return {
-  "Zeioth/markmap.nvim",
-  cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
-  opts = {
-    hide_toolbar = "false",
+  {
+    "Zeioth/markmap.nvim",
+    cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
+    ft = "markdown",
+    opts = {
+      hide_toolbar = "false",
+    },
   },
   {
     "jay-babu/mason-null-ls.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "markmap" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "markmap-cli" })
     end,
   },
 }
