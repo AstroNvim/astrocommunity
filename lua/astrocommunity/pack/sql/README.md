91e6578be315dd9e70072be2c08b658a5137a938
Micah Halter <micah@mehalter.com>
2024-04-16 17:07:05 -0400

    feat(sql): add `sqls.nvim` plugin for `sqls` LSP extensions

---
+++ lua/astrocommunity/pack/sql/README.md
@@ -0,0 +1,8 @@
+# SQL Language Pack
+
+This plugin pack does the following:
+
+- Adds `sql` Treesitter parser
+- Adds `sqls` language server
+- Adds `sqlfluff` formatter
+- Adds [sqls.nvim](https://github.com/nanotee/sqls.nvim) for language specific tooling
--- lua/astrocommunity/pack/sql/init.lua
+++ lua/astrocommunity/pack/sql/init.lua
@@ -29,4 +29,25 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "sqlfluff", "sqls" })
     end,
   },
+  {
+    "nanotee/sqls.nvim",
+    lazy = true,
+    dependencies = {
+      "AstroNvim/astrocore",
+      opts = {
+        autocmds = {
+          sqls_attach = {
+            {
+              event = "LspAttach",
+              desc = "Load sqls.nvim with sqls",
+              callback = function(args)
+                local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+                if client.name == "sqls" then require("sqls").on_attach(client, args.buf) end
+              end,
+            },
+          },
+        },
+      },
+    },
+  },
 }
