738444aa292a34473a68ace6a53c144b28bc3ad6
Micah Halter <micah@mehalter.com>
2024-04-17 14:57:34 -0400

    fix(sql): drop `sqlfluff` as `sqls` supports formatting (#890)

--- lua/astrocommunity/pack/sql/init.lua
+++ lua/astrocommunity/pack/sql/init.lua
@@ -16,17 +16,10 @@ return {
     end,
   },
   {
-    "jay-babu/mason-null-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "sqlfluff" })
-    end,
-  },
-  {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "sqlfluff", "sqls" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "sqls" })
     end,
   },
   {
