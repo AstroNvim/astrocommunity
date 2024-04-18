2fd9565c33bb6289b71906e08272ce3869e3d140
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-04-18 19:30:23 +0800

    fix formatting

--- lua/astrocommunity/pack/fish/init.lua
+++ lua/astrocommunity/pack/fish/init.lua
@@ -12,9 +12,7 @@ return {
     optional = true,
     opts = function(_, opts)
       local nls = require "null-ls"
-      if not opts.sources then
-        opts.sources = {}
-      end
+      if not opts.sources then opts.sources = {} end
       opts.sources = vim.list_extend(opts.sources, {
         nls.builtins.formatting.fish_indent,
         nls.builtins.diagnostics.fish,
