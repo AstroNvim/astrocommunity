56667176518b2b36b1ccd1547719b28ade316171
Ramesh Santhanakrishnan <rameshms@yahoo.com>
2024-04-19 19:40:34 -0700

    lsp: clangd: Add <Leader>lw keymap to switch between source and header

--- lua/astrocommunity/pack/cpp/init.lua
+++ lua/astrocommunity/pack/cpp/init.lua
@@ -46,6 +46,12 @@ return {
                 if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "clangd" then
                   require "clangd_extensions"
                   vim.api.nvim_del_augroup_by_name "clangd_extensions"
+                  vim.keymap.set(
+                    "n",
+                    "<Leader>lw",
+                    "<cmd>ClangdSwitchSourceHeader<CR>",
+                    { silent = true, desc = "Clangd: Switch source/header file" }
+                  )
                 end
               end,
             },
