43341f42f16051db0b39d8b231fcc93177dd74f1
Patrick Mulligan <patjmulligan@gmail.com>
2024-04-21 16:52:51 -0600

    set completion key to <C-e>, update README

--- lua/astrocommunity/completion/tabby-nvim/init.lua
+++ lua/astrocommunity/completion/tabby-nvim/init.lua
@@ -1,3 +1,8 @@
 return {
-  { "TabbyML/vim-tabby" },
+  "TabbyML/vim-tabby",
+  init = function()
+    vim.g.tabby_keybinding_accept = "<C-e>"
+    vim.g.tabby_keybinding_trigger_or_dismiss = "<C-\\>"
+    vim.g.tabby_node_binary = "/path/to/node"
+  end,
 }
