0dd853205e74f139a9af0db1a9f880e6a56a7e2a
Patrick Mulligan <patjmulligan@gmail.com>
2024-04-21 12:00:27 -0600

    add config function to initialize parameters

--- lua/astrocommunity/completion/tabby-nvim/init.lua
+++ lua/astrocommunity/completion/tabby-nvim/init.lua
@@ -1,8 +1,12 @@
-vim.g.tabby_keybinding_accept = "<Tab>"
-vim.g.tabby_keybinding_trigger_or_dismiss = "<C-\\>"
-
-vim.g.tabby_node_binary = "/path/to/node"
-
 return {
   { "TabbyML/vim-tabby" },
+  config = function()
+    -- Configuration settings for vim-tabby
+    vim.g.tabby_keybinding_accept = "<Tab>"
+    vim.g.tabby_keybinding_trigger_or_dismiss = "<C-\\>"
+    vim.g.tabby_node_binary = "/path/to/node"
+
+    -- You can also set key mappings here if vim-tabby requires them
+    -- vim.keymap.set('n', '<your-key>', '<vim-command-or-function>')
+  end,
 }
