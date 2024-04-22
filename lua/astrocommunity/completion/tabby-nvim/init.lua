ba327758a02dab36e43dba0fb57e218fc21dbf1f
luxus <luxuspur@gmail.com>
2024-04-22 19:41:29 +0200

    move init to astrocore
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/completion/tabby-nvim/init.lua
+++ lua/astrocommunity/completion/tabby-nvim/init.lua
@@ -1,8 +1,16 @@
 return {
   "TabbyML/vim-tabby",
-  init = function()
-    vim.g.tabby_keybinding_accept = "<C-e>"
-    vim.g.tabby_keybinding_trigger_or_dismiss = "<C-\\>"
-    vim.g.tabby_node_binary = vim.fn.exepath("node")
-  end,
+  dependencies = {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      options = {
+        g = {
+          tabby_keybinding_accept = "<C-e>",
+          tabby_keybinding_trigger_or_dismiss = "<C-\\>",
+          tabby_node_binary = vim.fn.exepath "node",
+        },
+      },
+    },
+  },
 }
