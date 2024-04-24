2cc0a1396794e6c4fa4efdb2ecedc459e76bd7f3
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2024-04-24 09:11:54 +0200

    chore(harp-nvim): Format code

--- lua/astrocommunity/motion/harp-nvim/harp-nvim.lua
+++ lua/astrocommunity/motion/harp-nvim/harp-nvim.lua
@@ -1,25 +1,34 @@
 return {
-    {
-        "Thorinori/harp-nvim",
-        lazy = true,
-        dependencies = {
-            "AstroNvim/astrocore",
-            opts = {
-                mappings = {
-                    n = {
-                        ["<Leader>i"] = {function() require("harp").default_get() end,desc = ":edit file in given register"},
-                        ["<Leader>I"] = {function() require("harp").default_set() end,desc = "Store current buffer path in given register"},
-                        ["<Leader>x"] = {function() require("harp").percwd_get() end,desc = ":edit file in given register, specific to this project"},
-                        ["<Leader>X"] = {function() require("harp").percwd_set() end,desc = "Store current buffer path in given register, specific to this project"},
-                        ["'"] = {function() require("harp").perbuffer_mark_get() end,desc = "Show local marks"},
-                        ["m"] = {function() require("harp").perbuffer_mark_set() end,desc = "Set local mark"},
-                        ["<Leader>'"] = {function() require("harp").global_mark_get() end,desc = "Show global marks"},
-                        ["<Leader>m"] = {function() require("harp").global_mark_set() end,desc = "Set global mark"},
-                        ["<Leader>z"] = {function() require("harp").cd_get() end,desc = "CD to path stored in register"},
-                        ["<Leader>Z"] = {function() require("harp").cd_set() end,desc = "Set CWD in register"},
-                    }
-                },
+  {
+    "Thorinori/harp-nvim",
+    lazy = true,
+    dependencies = {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>i"] = { function() require("harp").default_get() end, desc = ":edit file in given register" },
+            ["<Leader>I"] = {
+              function() require("harp").default_set() end,
+              desc = "Store current buffer path in given register",
             },
+            ["<Leader>x"] = {
+              function() require("harp").percwd_get() end,
+              desc = ":edit file in given register, specific to this project",
+            },
+            ["<Leader>X"] = {
+              function() require("harp").percwd_set() end,
+              desc = "Store current buffer path in given register, specific to this project",
+            },
+            ["'"] = { function() require("harp").perbuffer_mark_get() end, desc = "Show local marks" },
+            ["m"] = { function() require("harp").perbuffer_mark_set() end, desc = "Set local mark" },
+            ["<Leader>'"] = { function() require("harp").global_mark_get() end, desc = "Show global marks" },
+            ["<Leader>m"] = { function() require("harp").global_mark_set() end, desc = "Set global mark" },
+            ["<Leader>z"] = { function() require("harp").cd_get() end, desc = "CD to path stored in register" },
+            ["<Leader>Z"] = { function() require("harp").cd_set() end, desc = "Set CWD in register" },
+          },
         },
-    }
+      },
+    },
+  },
 }
