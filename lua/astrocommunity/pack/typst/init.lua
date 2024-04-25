83cb7e6047c5bcbaf03700f176a45ba33ec4bccf
Ilia Bahrebar <150489740+Std-Enigma@users.noreply.github.com>
2024-04-25 19:04:02 +0330

    fix(typst): added missing command to typst-preview.nvim (#913)
    Update init.lua

--- lua/astrocommunity/pack/typst/init.lua
+++ lua/astrocommunity/pack/typst/init.lua
@@ -15,7 +15,7 @@ return {
   },
   {
     "chomosuke/typst-preview.nvim",
-    cmd = { "TypstPreview", "TypstPreviewToggle" },
+    cmd = { "TypstPreview", "TypstPreviewToggle", "TypstPreviewUpdate" },
     version = "0.1.*",
     build = function() require("typst-preview").update() end,
     opts = {},
