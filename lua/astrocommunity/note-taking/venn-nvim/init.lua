0ddc379c60df6a502c5dd36caa948c2fb845c6d3
Micah Halter <micah@mehalter.com>
2024-04-26 12:57:41 -0400

    feat(venn-nvim): allow using Hydra if it's available to create a drawing submode

--- lua/astrocommunity/note-taking/venn-nvim/init.lua
+++ lua/astrocommunity/note-taking/venn-nvim/init.lua
@@ -1,43 +1,85 @@
+local hint = [[
+ Arrow^^^^^^   Select region with <C-v> 
+ ^ ^ _K_ ^ ^   _f_: surround it with box
+ _H_ ^ ^ _L_
+ ^ ^ _J_ ^ ^                      _<Esc>_
+]]
+
 return {
-  "jbyuki/venn.nvim",
-  cmd = "VBox",
-  dependencies = {
-    "AstroNvim/astrocore",
-    ---@type AstroCoreOpts
+  {
+    "jbyuki/venn.nvim",
+    cmd = "VBox",
+    dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local astrocore = require "astrocore"
+          if not astrocore.is_available "hydra.nvim" then
+            return astrocore.extend_tbl(opts, {
+              commands = {
+                ToggleVenn = {
+                  function()
+                    local mappings = {
+                      n = { -- draw a line on HJKL keystokes
+                        H = "<C-v>h:VBox<CR>",
+                        J = "<C-v>j:VBox<CR>",
+                        K = "<C-v>k:VBox<CR>",
+                        L = "<C-v>l:VBox<CR>",
+                      },
+                      v = { -- draw a box by pressing "f" with visual selection
+                        f = ":VBox<CR>",
+                      },
+                    }
+                    if vim.b.venn_enabled then
+                      vim.opt_local.virtualedit = ""
+                      for mode, map in pairs(mappings) do
+                        for lhs, _ in pairs(map) do
+                          vim.keymap.del(mode, lhs, { buffer = true })
+                        end
+                      end
+                      vim.b.venn_enabled = nil
+                    else
+                      vim.b.venn_enabled = true
+                      vim.opt_local.virtualedit = "all"
+                      require("astrocore").set_mappings(mappings, { buffer = true })
+                    end
+                    vim.notify(("Venn Diagramming Mode: %s"):format(vim.b.venn_enabled and "Enabled" or "Disabled"))
+                  end,
+                  desc = "Toggle venn diagramming mode",
+                },
+              },
+              mappings = {
+                n = { ["<Leader>v"] = { function() vim.cmd.ToggleVenn() end, desc = "Toggle venn diagramming" } },
+              },
+            })
+          end
+        end,
+      },
+    },
+  },
+  {
+    "anuvyklack/hydra.nvim",
+    optional = true,
     opts = {
-      commands = {
-        ToggleVenn = {
-          function()
-            local mappings = {
-              n = { -- draw a line on HJKL keystokes
-                H = "<C-v>h:VBox<CR>",
-                J = "<C-v>j:VBox<CR>",
-                K = "<C-v>k:VBox<CR>",
-                L = "<C-v>l:VBox<CR>",
-              },
-              v = { -- draw a box by pressing "f" with visual selection
-                f = ":VBox<CR>",
-              },
-            }
-            if vim.b.venn_enabled then
-              vim.opt_local.virtualedit = ""
-              for mode, map in pairs(mappings) do
-                for lhs, _ in pairs(map) do
-                  vim.keymap.del(mode, lhs, { buffer = true })
-                end
-              end
-              vim.b.venn_enabled = nil
-            else
-              vim.b.venn_enabled = true
-              vim.opt_local.virtualedit = "all"
-              require("astrocore").set_mappings(mappings, { buffer = true })
-            end
-            vim.notify(("Venn Diagramming Mode: %s"):format(vim.b.venn_enabled and "Enabled" or "Disabled"))
-          end,
-          desc = "Toggle venn diagramming mode",
+      ["Draw Diagram"] = {
+        hint = hint,
+        config = {
+          color = "pink",
+          invoke_on_body = true,
+          hint = { border = "rounded" },
+          on_enter = function() vim.o.virtualedit = "all" end,
+        },
+        mode = "n",
+        body = "<leader>v",
+        heads = {
+          { "H", "<C-v>h:VBox<CR>" },
+          { "J", "<C-v>j:VBox<CR>" },
+          { "K", "<C-v>k:VBox<CR>" },
+          { "L", "<C-v>l:VBox<CR>" },
+          { "f", ":VBox<CR>", { mode = "v" } },
+          { "<Esc>", nil, { exit = true } },
         },
       },
-      mappings = { n = { ["<Leader>v"] = { function() vim.cmd.ToggleVenn() end, desc = "Toggle venn diagramming" } } },
     },
   },
 }
