f1404d22616bfb126a627b763c5eb21da4a99e84
cdanyl <cdanyl@icloud.com>
2024-04-29 14:14:46 +0200

    feat(overseer-nvim): Add <Leader>T keymaps

--- lua/astrocommunity/code-runner/overseer-nvim/init.lua
+++ lua/astrocommunity/code-runner/overseer-nvim/init.lua
@@ -17,6 +17,24 @@ return {
       "OverseerClearCache",
     },
     opts = {},
+    dependencies = {
+      { "AstroNvim/astroui", opts = { icons = { Overseer = "" } } },
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local maps = opts.mappings
+          local prefix = "<leader>T"
+          maps.n[prefix] = { desc = require("astroui").get_icon("Overseer", 1, true) .. "Overseer" }
+
+          maps.n[prefix .. "t"] = { "<Cmd>OverseerToggle<CR>", desc = "Toggle" }
+          maps.n[prefix .. "c"] = { "<Cmd>OverseerRunCmd<CR>", desc = "Run Command" }
+          maps.n[prefix .. "r"] = { "<Cmd>OverseerRun<CR>", desc = "Run Task" }
+          maps.n[prefix .. "q"] = { "<Cmd>OverseerQuickAction<CR>", desc = "Quick Action" }
+          maps.n[prefix .. "a"] = { "<Cmd>OverseerTaskAction<CR>", desc = "Task Action" }
+          maps.n[prefix .. "i"] = { "<Cmd>OverseerInfo<CR>", desc = "Info" }
+        end,
+      },
+    },
   },
   {
     "catppuccin/nvim",
