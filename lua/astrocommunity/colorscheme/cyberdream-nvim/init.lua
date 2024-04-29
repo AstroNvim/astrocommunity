a21940e6cb5b9ee2508d82bf7c93714f2107645c
0x434b <admin@0x434b.dev>
2024-04-29 09:58:27 +0400

    feat(colorscheme): add cyberdream and dogrun

---
+++ lua/astrocommunity/colorscheme/cyberdream-nvim/init.lua
@@ -0,0 +1,10 @@
+return {
+  "scottmckendry/cyberdream.nvim",
+  opts = {
+    transparent = true,
+    italic_comments = true,
+    hide_fillchars = true,
+    borderless_telescope = true,
+    terminal_colors = true,
+  },
+}
---
+++ lua/astrocommunity/colorscheme/dogrun-nvim/README.md
@@ -0,0 +1,10 @@
+# vim-dogrun
+
+🐶 A dark Neovim / Vim colorscheme for the GUI and 256 / true-color terminals.
+
+**Repository:** <https://github.com/wadackel/vim-dogrun>
+
+```vim
+" Vim Script
+colorscheme dogrun
+```
