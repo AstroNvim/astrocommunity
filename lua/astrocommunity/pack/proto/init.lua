66b6ef3119a8f4e2dce71dd819504cb9bc05ccf3
Alejandro Duarte <32496764+JADC362@users.noreply.github.com>
2024-04-29 09:56:14 -0500

    mason lsp buf was rename to buf-language-server

--- lua/astrocommunity/pack/proto/init.lua
+++ lua/astrocommunity/pack/proto/init.lua
@@ -13,7 +13,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "bufls" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "buf-language-server" })
     end,
   },
   {
