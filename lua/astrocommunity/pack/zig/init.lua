8dac43951c6d1d9d365c3088acd30330023d1b41
Ben Woodward <ben@benwoodward.me.uk>
2024-05-02 17:34:45 +0100

    fix(pack): remove non-existant branch (#936)
    * fix(pack): remove non-existant branch
    
    * fix(pack): add neotest-zig version
    
    Co-authored-by: Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>

--- lua/astrocommunity/pack/zig/init.lua
+++ lua/astrocommunity/pack/zig/init.lua
@@ -25,7 +25,7 @@ return {
   {
     "nvim-neotest/neotest",
     optional = true,
-    dependencies = { "lawrence-laz/neotest-zig", branch = "v1.2.0" },
+    dependencies = { "lawrence-laz/neotest-zig", version = "^1" },
     opts = function(_, opts)
       if not opts.adapters then opts.adapters = {} end
       table.insert(opts.adapters, require "neotest-zig")
