3c5529150854884e36324696b0bc23308277207b
Ben Woodward <ben@benwoodward.me.uk>
2024-05-02 17:24:57 +0100

    fix(pack): add neotest-zig version
    Co-authored-by: Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>

--- lua/astrocommunity/pack/zig/init.lua
+++ lua/astrocommunity/pack/zig/init.lua
@@ -25,7 +25,7 @@ return {
   {
     "nvim-neotest/neotest",
     optional = true,
-    dependencies = { "lawrence-laz/neotest-zig" },
+    dependencies = { "lawrence-laz/neotest-zig", version = "^1" },
     opts = function(_, opts)
       if not opts.adapters then opts.adapters = {} end
       table.insert(opts.adapters, require "neotest-zig")
