8cdff0b4bcd3a1f73b30e4df3f5e1ac3ea231f0b
William Roy <wroy@proton.me>
2024-05-06 18:35:09 -0400

    feat(pack/cpp): add better coc integration

--- lua/astrocommunity/pack/cpp/init.lua
+++ lua/astrocommunity/pack/cpp/init.lua
@@ -89,4 +89,44 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "clangd", "codelldb" })
     end,
   },
+  {
+    "neoclide/coc.nvim",
+    optional = true,
+    opts = function(_, opts)
+      vim.fn["coc#config"]("clangd.arguments", {
+        -- Include index symbols that are not defined in scopes (e.g.
+        -- namespaces). Such completions will insert scope qualifiers.
+        "--all-scopes-completion=true",
+        -- Index project code in the background and persist index on disk.
+        "--background-index=true",
+        -- Set thread priority for building the background index.
+        "--background-index-priority=normal",
+        -- Enable clang-tidy diagnostics.
+        "--clang-tidy",
+        -- Use clang-parser for code-completion consistently, and don't dilute
+        -- results with text-based completion.
+        "--completion-parse=always",
+        -- Combine similar completion items (e.g. function overloads). Type
+        -- information will be shown where possible.
+        "--completion-style=bundled",
+        -- Disable function parameter placeholders (e.g. std::endl(basic_ostream<_CharT, _Traits> &__os))
+        "--function-arg-placeholders=false",
+        -- Never insert headers with completion proposals. It causes more problems than it solves.
+        "--header-insertion=never",
+        -- Parse all emplace-like functions in included headers
+        "--parse-forwarding-functions",
+        -- Store PCHs in memory. Note that it may increases memory usages, but
+        -- in return it will greatly improve performance.
+        "--pch-storage=memory",
+        -- Use Decision Forest-based ranking algorithm to rank completion candidates. In practice, this gives an estimated 6% accuracy boost.
+        "--ranking-model=decision_forest",
+        -- Avoid imposing arbitrary limits. In most instances, this is rarely a
+        -- problem. If it is, it will likely require manual adjustment on a
+        -- per-project or per-hardware basis anyway.
+        "--limit-references=0",
+        "--limit-results=0",
+        "--rename-file-limit=0",
+      })
+    end,
+  },
 }
