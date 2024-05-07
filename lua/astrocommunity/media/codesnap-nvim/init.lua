392038c30c077aa80e1b3f6aab417432fbcdd196
FraI3mega <69040313+FraI3mega@users.noreply.github.com>
2024-05-07 17:35:43 +0200

    feat(codesnap-nvim): add keymaps for snapshots with highlight (#944)

--- lua/astrocommunity/media/codesnap-nvim/init.lua
+++ lua/astrocommunity/media/codesnap-nvim/init.lua
@@ -2,7 +2,7 @@ local prefix = "<Leader>c"
 return {
   "mistricky/codesnap.nvim",
   build = "make",
-  cmd = { "CodeSnap", "CodeSnapSave" },
+  cmd = { "CodeSnap", "CodeSnapSave", "CodeSnapHighlight", "CodeSnapSaveHighlight" },
   dependencies = {
     {
       "AstroNvim/astrocore",
@@ -12,6 +12,8 @@ return {
             [prefix] = { desc = " CodeSnap" },
             [prefix .. "s"] = { ":'<,'>CodeSnap<CR>", desc = "CodeSnap (clipboard)" },
             [prefix .. "S"] = { ":'<,'>CodeSnapSave<CR>", desc = "CodeSnap (save)" },
+            [prefix .. "h"] = { ":'<,'>CodeSnapHighlight<CR>", desc = "CodeSnap with highlight (clipboard)" },
+            [prefix .. "H"] = { ":'<,'>CodeSnapSaveHighlight<CR>", desc = "CodeSnap with highlight (save)" },
           },
         },
       },
