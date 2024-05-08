a22872fb929fa59fa239546cd2dee6a489d1e623
Micah Halter <micah@mehalter.com>
2024-05-08 12:19:57 -0400

    feat(completion): add `cmp-git` (#950)

---
+++ lua/astrocommunity/completion/cmp-git/init.lua
@@ -0,0 +1,21 @@
+return {
+  "petertriho/cmp-git",
+  ft = { "gitcommit", "octo" },
+  dependencies = { "hrsh7th/nvim-cmp" },
+  opts = function(_, opts)
+    opts.filetypes = require("lazy.core.plugin").values(assert(require("astrocore").get_plugin "cmp-git"), "ft")
+  end,
+  config = function(_, opts)
+    if opts.filetypes then
+      local cmp = require "cmp"
+      cmp.setup.filetype(opts.filetypes, {
+        sources = cmp.config.sources({
+          { name = "git" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end
+    require("cmp_git").setup(opts)
+  end,
+}
