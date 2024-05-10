58cf3019fb74035ef13b0ba9b00ce3c0ded05854
Cretezy <Cretezy@users.noreply.github.com>
2024-05-10 13:30:20 -0400

    feat(pack): Add Jujutsu/jj pack (#954)
    feat(jj): Add Jujutsu/jj pack

---
+++ lua/astrocommunity/pack/jj/init.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    "zschreur/telescope-jj.nvim",
+    config = function() require("telescope").load_extension "jj" end,
+    dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          mappings = {
+            n = {
+              ["<Leader>jf"] = {
+                function()
+                  local jj_pick_status, jj_res = pcall(require("telescope").extensions.jj.files)
+                  if jj_pick_status then return end
+
+                  -- Git fallback
+                  local git_files_status, git_res = pcall(require("telescope.builtin").git_files)
+                  if not git_files_status then
+                    error("Could not launch jj or git files: \n" .. jj_res .. "\n" .. git_res)
+                  end
+                end,
+                desc = "jj files",
+              },
+              ["<Leader>jc"] = { function() require("telescope").extensions.jj.conflicts() end, desc = "jj conflicts" },
+              ["<Leader>jd"] = { function() require("telescope").extensions.jj.diff() end, desc = "jj diff" },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "Cretezy/neo-tree-jj.nvim",
+    dependencies = {
+      {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = function(_, opts)
+          table.insert(opts.sources, "jj")
+
+          -- Replace git tab in neo-tree when in jj repo
+          if require("neo-tree.sources.jj.utils").get_repository_root() then
+            -- Remove git tab
+            for i, source in ipairs(opts.source_selector.sources) do
+              if source.source == "git_status" then
+                table.remove(opts.source_selector.sources, i)
+                break
+              end
+            end
+
+            -- Add jj tab
+            table.insert(opts.source_selector.sources, {
+              display_name = "󰊢 JJ",
+              source = "jj",
+            })
+          end
+        end,
+      },
+    },
+  },
+}
