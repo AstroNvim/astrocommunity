5a1ad9bf97fe6a3d2498e129d73eb9b24fe28e8a
Maarten <maartenvanbeek@gmail.com>
2024-05-15 09:54:01 +0200

    Apply best practices to configuration
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/code-runner/toggletasks-nvim/init.lua
+++ lua/astrocommunity/code-runner/toggletasks-nvim/init.lua
@@ -4,12 +4,21 @@ return {
     { "nvim-lua/plenary.nvim" },
     { "akinsho/toggleterm.nvim" },
     { "nvim-telescope/telescope.nvim" },
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>tss"] = { "<Cmd>Telescope toggletasks spawn<CR>", desc = "toggletasks: spawn" },
+            ["<Leader>tsc"] = { "<Cmd>Telescope toggletasks select<CR>", desc = "toggletasks: select" },
+            ["<Leader>tse"] = { "<Cmd>Telescope toggletasks edit<CR>", desc = "toggletasks: edit config" },
+          },
+        },
+      },
+    },
   },
   opts = {},
-  init = function() require("telescope").load_extension "toggletasks" end,
-  keys = {
-    { "<leader>tss", "<cmd>Telescope toggletasks spawn<CR>", desc = "toggletasks: spawn" },
-    { "<leader>tsc", "<cmd>Telescope toggletasks select<CR>", desc = "toggletasks: select" },
-    { "<leader>tse", "<cmd>Telescope toggletasks edit<CR>", desc = "toggletasks: edit config" },
-  },
+  init = function()
+    require("astrocore").on_load("telescope.nvim", function() require("telescope").load_extension "toggletasks" end)
+  end,
 }
