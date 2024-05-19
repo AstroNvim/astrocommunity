33d420956aeab3f3159ce1fd7ead997f45024a55
Rachel Powers <508861+Ryex@users.noreply.github.com>
2024-05-19 16:57:23 -0700

    fix(rust): preserve loading of rust-analyzer.json (#977)
    merge astrolsp settings in such a way as to preserve the default behavior
    of loading local project settings from rust-analyzer.json

--- lua/astrocommunity/pack/rust/init.lua
+++ lua/astrocommunity/pack/rust/init.lua
@@ -86,7 +86,23 @@ return {
       end
 
       local astrolsp_avail, astrolsp = pcall(require, "astrolsp")
-      return { server = astrolsp_avail and astrolsp.lsp_opts "rust_analyzer", dap = { adapter = adapter } }
+      local astrolsp_opts = (astrolsp_avail and astrolsp.lsp_opts "rust_analyzer") or {}
+      local server = {
+        ---@type table | (fun(project_root:string|nil, default_settings: table|nil):table) -- The rust-analyzer settings or a function that creates them.
+        settings = function(project_root, default_settings)
+          local astrolsp_settings = astrolsp_opts.settings or {}
+
+          local merge_table = require("astrocore").extend_tbl(default_settings or {}, astrolsp_settings)
+          local ra = require "rustaceanvim.config.server"
+          -- load_rust_analyzer_settings merges any found settings with the passed in default settings table and then returns that table
+          return ra.load_rust_analyzer_settings(project_root, {
+            settings_file_pattern = "rust-analyzer.json",
+            default_settings = merge_table,
+          })
+        end,
+      }
+      local final_server = require("astrocore").extend_tbl(astrolsp_opts, server)
+      return { server = final_server, dap = { adapter = adapter } }
     end,
     config = function(_, opts) vim.g.rustaceanvim = require("astrocore").extend_tbl(opts, vim.g.rustaceanvim) end,
   },
