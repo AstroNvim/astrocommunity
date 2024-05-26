e6cb57b1f571f8978c9ec5f1557729b7710542a3
William Parsons-Heins <williamparsons62@gmail.com>
2024-05-26 14:06:02 +0100

    refactor typescript.init to use typescript.core and then add eslint+prettier on top

--- lua/astrocommunity/pack/typescript/init.lua
+++ lua/astrocommunity/pack/typescript/init.lua
@@ -27,9 +27,7 @@ local has_prettier = function(bufnr)
   if not lsp_rooter then
     lsp_rooter = rooter.resolve("lsp", {
       ignore = {
-        servers = function(client)
-          return not vim.tbl_contains({ "vtsls", "typescript-tools", "volar", "eslint", "tsserver" }, client.name)
-        end,
+        servers = function(client) return not vim.tbl_contains({ "eslint" }, client.name) end,
       },
     })
   end
@@ -73,18 +71,7 @@ end
 local conform_formatter = function(bufnr) return has_prettier(bufnr) and { "prettierd" } or {} end
 
 return {
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.lsp.nvim-lsp-file-operations" },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    optional = true,
-    opts = function(_, opts)
-      if opts.ensure_installed ~= "all" then
-        opts.ensure_installed =
-          require("astrocore").list_insert_unique(opts.ensure_installed, { "javascript", "typescript", "tsx", "jsdoc" })
-      end
-    end,
-  },
+  { import = "astrocommunity.pack.typescript.core" },
   {
     "AstroNvim/astrolsp",
     ---@type AstroLSPOpts
@@ -99,44 +86,12 @@ return {
           },
         },
       },
-      ---@diagnostic disable: missing-fields
-      config = {
-        vtsls = {
-          settings = {
-            typescript = {
-              updateImportsOnFileMove = { enabled = "always" },
-              inlayHints = {
-                parameterNames = { enabled = "all" },
-                parameterTypes = { enabled = true },
-                variableTypes = { enabled = true },
-                propertyDeclarationTypes = { enabled = true },
-                functionLikeReturnTypes = { enabled = true },
-                enumMemberValues = { enabled = true },
-              },
-            },
-            javascript = {
-              updateImportsOnFileMove = { enabled = "always" },
-              inlayHints = {
-                parameterNames = { enabled = "literals" },
-                parameterTypes = { enabled = true },
-                variableTypes = { enabled = true },
-                propertyDeclarationTypes = { enabled = true },
-                functionLikeReturnTypes = { enabled = true },
-                enumMemberValues = { enabled = true },
-              },
-            },
-            vtsls = {
-              enableMoveToFileCodeAction = true,
-            },
-          },
-        },
-      },
     },
   },
   {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "vtsls", "eslint" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "eslint" })
     end,
   },
   {
@@ -165,55 +120,11 @@ return {
     end,
   },
   {
-    "jay-babu/mason-nvim-dap.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "js" })
-    end,
-  },
-  {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(
-        opts.ensure_installed,
-        { "vtsls", "eslint-lsp", "prettierd", "js-debug-adapter" }
-      )
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "eslint-lsp", "prettierd" })
     end,
-  },
-  {
-    "vuki656/package-info.nvim",
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {},
-    event = "BufRead package.json",
-  },
-  {
-    "yioneko/nvim-vtsls",
-    lazy = true,
-    dependencies = {
-      "AstroNvim/astrocore",
-      opts = {
-        autocmds = {
-          nvim_vtsls = {
-            {
-              event = "LspAttach",
-              desc = "Load nvim-vtsls with vtsls",
-              callback = function(args)
-                if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "vtsls" then
-                  require("vtsls")._on_attach(args.data.client_id, args.buf)
-                  vim.api.nvim_del_augroup_by_name "nvim_vtsls"
-                end
-              end,
-            },
-          },
-        },
-      },
-    },
-    config = function(_, opts) require("vtsls").config(opts) end,
-  },
-  {
-    "dmmulroy/tsc.nvim",
-    cmd = "TSC",
-    opts = {},
   },
 }
