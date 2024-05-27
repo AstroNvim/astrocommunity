7de5e2fcc06f6bcc7073451474a113583d90067a
Micah Halter <micah@mehalter.com>
2024-05-27 17:41:04 -0400

    fix(cpp): fix support for `linux/arm64` architecture
    Implements #997
    
    Co-authored-by: Josh Heyse <josh@heyse.us>

--- lua/astrocommunity/pack/cpp/init.lua
+++ lua/astrocommunity/pack/cpp/init.lua
@@ -1,18 +1,20 @@
+local machine = vim.loop.os_uname().machine
+local is_arm = machine == "aarch64" or vim.startswith(machine, "arm")
+
 return {
   {
     "AstroNvim/astrolsp",
     optional = true,
-    ---@type AstroLSPOpts
-    opts = {
-      ---@diagnostic disable: missing-fields
-      config = {
+    opts = function(_, opts)
+      opts.config = vim.tbl_deep_extend("keep", opts, {
         clangd = {
           capabilities = {
             offsetEncoding = "utf-8",
           },
         },
-      },
-    },
+      })
+      if is_arm then opts.servers = require("astrocore").list_insert_unique(opts.servers, { "clangd" }) end
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -28,7 +30,9 @@ return {
     "williamboman/mason-lspconfig.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "clangd" })
+      if not is_arm then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "clangd" })
+      end
     end,
   },
   {
@@ -86,7 +90,9 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "clangd", "codelldb" })
+      local tools = { "codelldb" }
+      if not is_arm then table.insert(tools, "clangd") end
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, tools)
     end,
   },
 }
