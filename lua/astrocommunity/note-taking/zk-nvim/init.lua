042f8513b80ea21f9aed163518d7a1043b7b7364
Micah Halter <micah@mehalter.com>
2024-05-27 19:48:51 -0400

    fix(zk-nvim): update to new organization and setup

--- lua/astrocommunity/note-taking/zk-nvim/init.lua
+++ lua/astrocommunity/note-taking/zk-nvim/init.lua
@@ -1,11 +1,33 @@
 return {
   {
-    "mickael-menu/zk-nvim",
-    opts = {
-      picker = "telescope",
+    "zk-org/zk-nvim",
+    main = "zk",
+    event = "User AstroFile",
+    cmd = {
+      "ZkIndex",
+      "ZkNew",
+      "ZkNewFromTitleSelection",
+      "ZkNewFromContentSelection",
+      "ZkCd",
+      "ZkNotes",
+      "ZkBacklinks",
+      "ZkLinks",
+      "ZkInsertLinkAtSelection",
+      "ZkInsertLink",
+      "ZkMatch",
+      "ZkTags",
     },
+    dependencies = { "AstroNvim/astrolsp", opts = { handlers = { zk = function() end } } },
+    opts = function()
+      local lsp_config = require("astrolsp").lsp_opts "zk"
+      return {
+        lsp = {
+          config = lsp_config,
+          auto_attach = { filetypes = lsp_config.filetypes },
+        },
+      }
+    end,
   },
-
   {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
