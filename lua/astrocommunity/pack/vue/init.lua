ac9457fbcc2b802d3edfe4ef8aae1f958a0aa038
Micah Halter <micah@mehalter.com>
2024-05-28 12:44:33 -0400

    feat(vue): enable inlay hints by default

--- lua/astrocommunity/pack/vue/init.lua
+++ lua/astrocommunity/pack/vue/init.lua
@@ -12,6 +12,30 @@ return {
               hybridMode = false,
             },
           },
+          settings = {
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              inlayHints = {
+                parameterNames = { enabled = "all" },
+                parameterTypes = { enabled = true },
+                variableTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                enumMemberValues = { enabled = true },
+              },
+            },
+            javascript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              inlayHints = {
+                parameterNames = { enabled = "literals" },
+                parameterTypes = { enabled = true },
+                variableTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                enumMemberValues = { enabled = true },
+              },
+            },
+          },
         },
       },
     },
