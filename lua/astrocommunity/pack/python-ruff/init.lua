52242e9dc19e5a5ff9ab01fcd4e33c0b167eca6f
Reledia <jacopo778.raineri@gmail.com>
2024-06-03 22:05:49 +0200

    feat(python-ruff): change from ruff_lsp to ruff

--- lua/astrocommunity/pack/python-ruff/init.lua
+++ lua/astrocommunity/pack/python-ruff/init.lua
@@ -4,7 +4,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ruff_lsp" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ruff" })
     end,
   },
   {
@@ -21,7 +21,7 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ruff-lsp" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ruff" })
       opts.ensure_installed = vim.tbl_filter(
         function(v) return not vim.tbl_contains({ "black", "isort" }, v) end,
         opts.ensure_installed
