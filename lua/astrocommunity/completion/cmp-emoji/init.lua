bc375b8fead0565c523d95d7f829ced6aac14313
Micah Halter <micah@mehalter.com>
2024-06-03 17:11:43 -0400

    fix(completion): fix typo in repos for `cmp-emoji` and `cmp-latex-symbols`

--- lua/astrocommunity/completion/cmp-emoji/init.lua
+++ lua/astrocommunity/completion/cmp-emoji/init.lua
@@ -1,7 +1,7 @@
 return {
   "hrsh7th/nvim-cmp",
   optional = true,
-  dependencies = { "kdheepak/cmp-emoji" },
+  dependencies = { "hrsh7th/cmp-emoji" },
   opts = function(_, opts)
     if not opts.sources then opts.sources = {} end
     table.insert(opts.sources, { name = "emoji", priority = 700 })
--- lua/astrocommunity/completion/cmp-latex-symbols/init.lua
+++ lua/astrocommunity/completion/cmp-latex-symbols/init.lua
@@ -1,7 +1,7 @@
 return {
   "hrsh7th/nvim-cmp",
   optional = true,
-  dependencies = { "kdheepak/cmp-latex-symbols" },
+  dependencies = { "hrsh7th/cmp-latex-symbols" },
   opts = function(_, opts)
     if not opts.sources then opts.sources = {} end
     table.insert(opts.sources, { name = "latex_symbols", priority = 700 })
