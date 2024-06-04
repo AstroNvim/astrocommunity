35ba0cf17237d05b509f54490dde9b3df73ebc45
Micah Halter <micah@mehalter.com>
2024-06-04 09:08:03 -0400

    feat(lazydev-nvim): add AstroNvim types automatically

--- lua/astrocommunity/neovim-lua-development/lazydev-nvim/init.lua
+++ lua/astrocommunity/neovim-lua-development/lazydev-nvim/init.lua
@@ -6,8 +6,12 @@ return {
     ft = "lua",
     opts = {
       library = {
-        uv = "luvit-meta/library",
-        lazyvim = "LazyVim",
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "astrocore", words = { "AstroCore" } },
+        { path = "astrolsp", words = { "AstroLSP" } },
+        { path = "astroui", words = { "AstroUI" } },
+        { path = "astrotheme", words = { "AstroTheme" } },
+        { path = "lazy.nvim", words = { "Lazy" } },
       },
     },
   },
