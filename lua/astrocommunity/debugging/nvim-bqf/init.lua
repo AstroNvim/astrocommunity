2ea93187a63fb9eab0f9fa3d3f13c7c70f7ea053
Micah Halter <micah@mehalter.com>
2024-06-04 08:57:21 -0400

    feat(nvim-bqf): use the AstroUI "Selected" icon for selection

--- lua/astrocommunity/debugging/nvim-bqf/init.lua
+++ lua/astrocommunity/debugging/nvim-bqf/init.lua
@@ -1,1 +1,13 @@
-return { "kevinhwang91/nvim-bqf", ft = "qf", opts = {} }
+return {
+  "kevinhwang91/nvim-bqf",
+  ft = "qf",
+  dependencies = {
+    "AstroNvim/astrocore",
+    ---@param opts AstroCoreOpts
+    opts = function(_, opts)
+      if not opts.signs then opts.signs = {} end
+      opts.signs.BqfSign = { text = " " .. require("astroui").get_icon "Selected", texthl = "BqfSign" }
+    end,
+  },
+  opts = {},
+}
