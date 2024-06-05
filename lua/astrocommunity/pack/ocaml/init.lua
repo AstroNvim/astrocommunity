c5b33e2f0d53743cfa6db5484840f88bb95c8f4c
Uzair Aftab <muaf@nmbu.no>
2024-06-05 12:06:09 +0200

    feat(pack): Add `ocaml` pack

---
+++ lua/astrocommunity/pack/ocaml/init.lua
@@ -0,0 +1,28 @@
+return {
+  { import = "astrocommunity.pack.toml" },
+  { "tjdevries/ocaml.nvim", opts = {} },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(
+          opts.ensure_installed,
+          { "ocaml", "ocaml_interface", "ocamllex", "menhir" }
+        )
+      end
+    end,
+  },
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@param opts AstroLSPOpts
+    opts = {
+      config = {
+        ocamllsp = {
+          get_language_id = function(_, ftype) return ftype end,
+        },
+      },
+    },
+  },
+}
