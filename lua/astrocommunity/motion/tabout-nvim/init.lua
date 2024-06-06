4a0dbf0c79b1d0871bb7cd6815335d0deee82a22
Micah Halter <micah@mehalter.com>
2024-06-06 10:51:40 -0400

    fix(tabout-nvim): add support for native snippets

--- lua/astrocommunity/motion/tabout-nvim/init.lua
+++ lua/astrocommunity/motion/tabout-nvim/init.lua
@@ -6,12 +6,15 @@ return {
     {
       "hrsh7th/nvim-cmp",
       opts = function(_, opts)
-        local cmp, luasnip = require "cmp", require "luasnip"
+        local cmp = require "cmp"
+        local luasnip_avail, luasnip = pcall(require, "luasnip")
         opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
+          elseif luasnip_avail and luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
+          elseif not luasnip_avail and pcall(vim.snippet.active, { direction = 1 }) then
+            vim.snippet.jump(1)
           else
             fallback()
           end
