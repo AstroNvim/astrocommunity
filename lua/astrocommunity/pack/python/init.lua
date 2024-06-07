51eb1c1efd58ac35242070db1ec484c7c910a5a7
Micah Halter <micah@mehalter.com>
2024-06-07 14:45:26 -0400

    fix(python): add protection against `debugpy` not being installed through mason

--- lua/astrocommunity/pack/python/init.lua
+++ lua/astrocommunity/pack/python/init.lua
@@ -76,11 +76,15 @@ return {
     dependencies = "mfussenegger/nvim-dap",
     ft = "python", -- NOTE: ft: lazy-load on filetype
     config = function(_, opts)
-      local path = require("mason-registry").get_package("debugpy"):get_install_path()
-      if vim.fn.has "win32" == 1 then
-        path = path .. "/venv/Scripts/python"
-      else
-        path = path .. "/venv/bin/python"
+      local path = vim.fn.exepath "python"
+      local debugpy = require("mason-registry").get_package "debugpy"
+      if debugpy:is_installed() then
+        path = debugpy:get_install_path()
+        if vim.fn.has "win32" == 1 then
+          path = path .. "/venv/Scripts/python"
+        else
+          path = path .. "/venv/bin/python"
+        end
       end
       require("dap-python").setup(path, opts)
     end,
