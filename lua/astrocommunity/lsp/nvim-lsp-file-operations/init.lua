79ed791e66b664687b5d52a3698f626232324301
Micah Halter <micah@mehalter.com>
2024-06-16 09:22:02 -0400

    fix(nvim-lsp-file-operations): conditionally enable capabilities to language servers

--- lua/astrocommunity/lsp/nvim-lsp-file-operations/init.lua
+++ lua/astrocommunity/lsp/nvim-lsp-file-operations/init.lua
@@ -4,6 +4,20 @@ return {
   lazy = true,
   -- lazily load plugin after a tree plugin is loaded
   init = function(plugin) require("astrocore").on_load({ "neo-tree.nvim", "nvim-tree.lua" }, plugin.name) end,
+  dependencies = {
+    "AstroNvim/astrolsp",
+    opts = function(_, opts)
+      local operations = vim.tbl_get(require("astrocore").plugin_opts "nvim-lsp-file-operations", "operations") or {}
+      local fileOperations = {}
+      for _, operation in ipairs { "willRename", "didRename", "willCreate", "didCreate", "willDelete", "didDelete" } do
+        local enabled = vim.tbl_get(operations, operation .. "Files")
+        if enabled == nil then enabled = true end
+        fileOperations[operation] = enabled
+      end
+      opts.capabilities =
+        vim.tbl_deep_extend("force", opts.capabilities or {}, { workspace = { fileOperations = fileOperations } })
+    end,
+  },
   main = "lsp-file-operations", -- set the main module name where the `setup` function is
   opts = {},
 }
