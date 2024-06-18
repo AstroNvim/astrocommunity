ec3addf9b1cf1bb5a8d05b08bec5380717a891f1
Kamil Cukrowski <kamilcukrowski@gmail.com>
2024-06-19 00:12:11 +0200

    feat(chatgpt-nvim): add which-key mappings below <leader>G

--- lua/astrocommunity/editing-support/chatgpt-nvim/init.lua
+++ lua/astrocommunity/editing-support/chatgpt-nvim/init.lua
@@ -5,6 +5,36 @@ return {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = {
+            -- G like GPT
+            ["<Leader>G"] = {
+              name = "🤖ChatGPT",
+              c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+              e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
+              g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
+              t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
+              k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
+              d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
+              a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
+              o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
+              s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
+              f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
+              x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
+              r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
+              l = {
+                "<cmd>ChatGPTRun code_readability_analysis<CR>",
+                "Code Readability Analysis",
+                mode = { "n", "v" },
+              },
+            },
+          },
+        },
+      },
+    },
   },
   opts = {},
 }
