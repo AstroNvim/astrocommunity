6f28799ca56d12ade101389ff9be37ea68b9375e
w1_liamby <55990985+WilliamBy@users.noreply.github.com>
2024-06-21 20:53:59 +0800

    Update lua/astrocommunity/pack/hyprland/init.lua
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/pack/hyprland/init.lua
+++ lua/astrocommunity/pack/hyprland/init.lua
@@ -19,9 +19,6 @@ return {
         extension = {
           hl = "hyprlang",
         },
-        filename = {
-          ["hyprland.conf"] = "hyprlang",
-        },
         pattern = {
           ["*/hypr/*.conf"] = "hyprlang",
           ["hypr*.conf"] = "hyprlang",
