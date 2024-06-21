58be28a129aeaaad74fb608b3a9f05e0214aab99
Micah Halter <micah@mehalter.com>
2024-06-21 09:45:08 -0400

    fix(go): install go dependencies if mason isn't available

--- lua/astrocommunity/pack/go/init.lua
+++ lua/astrocommunity/pack/go/init.lua
@@ -109,6 +109,12 @@ return {
   {
     "olexsmir/gopher.nvim",
     ft = "go",
+    build = function()
+      if not require("lazy.core.config").spec.plugins["mason.nvim"] then
+        vim.print "Installing go dependencies..."
+        vim.cmd.GoInstallDeps()
+      end
+    end,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
