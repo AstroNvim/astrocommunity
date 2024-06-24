92e5937f275b6a56c1241dc090fa0a47463740a7
zhaown <51357674+chaozwn@users.noreply.github.com>
2024-06-25 00:22:17 +0800

    Update init.lua

--- lua/astrocommunity/pack/vue/init.lua
+++ lua/astrocommunity/pack/vue/init.lua
@@ -13,32 +13,17 @@ return {
               hybridMode = true,
             },
           },
-          settings = {
-            typescript = {
-              updateImportsOnFileMove = { enabled = "always" },
-              inlayHints = {
-                parameterNames = { enabled = "all" },
-                parameterTypes = { enabled = true },
-                variableTypes = { enabled = true },
-                propertyDeclarationTypes = { enabled = true },
-                functionLikeReturnTypes = { enabled = true },
-                enumMemberValues = { enabled = true },
-              },
-            },
-            javascript = {
-              updateImportsOnFileMove = { enabled = "always" },
-              inlayHints = {
-                parameterNames = { enabled = "literals" },
-                parameterTypes = { enabled = true },
-                variableTypes = { enabled = true },
-                propertyDeclarationTypes = { enabled = true },
-                functionLikeReturnTypes = { enabled = true },
-                enumMemberValues = { enabled = true },
-              },
-            },
-          },
         },
         vtsls = {
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+            "vue",
+          },
           settings = {
             vtsls = {
               tsserver = {
