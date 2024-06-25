6aa6aad7b86f59fd622dc405171d912edb734c11
Micah Halter <micah@mehalter.com>
2024-06-25 14:04:23 -0400

    fix(vim-sandwich): fix lazy loading and mapping setup

--- lua/astrocommunity/syntax/vim-sandwich/init.lua
+++ lua/astrocommunity/syntax/vim-sandwich/init.lua
@@ -1,21 +1,23 @@
 return {
   {
     "machakann/vim-sandwich",
-    event = { "User AstroFile", "InsertEnter" },
     dependencies = {
-      {
-        "AstroNvim/astrocore",
-        mappings = {
-          n = {
-            { "sa", desc = "Add surrounding" },
-            { "sd", desc = "Delete surrounding" },
-            { "sr", desc = "Replace surrounding" },
-          },
-          v = {
-            { "sa", desc = "Add surrounding" },
-          },
-        },
-      },
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        opts.options.g.sandwich_no_default_key_mappings = true
+
+        local maps = assert(opts.mappings)
+        maps.n.sa = { "<Plug>(sandwich-add)", desc = "Add surrounding" }
+        maps.o.sa = maps.n.sa
+        maps.x.sa = maps.n.sa
+        maps.n.sd = { "<Plug>(sandwich-delete)", desc = "Delete surrounding" }
+        maps.n.sr = { "<Plug>(sandwich-replace)", desc = "Replace surrounding" }
+      end,
+    },
+    keys = {
+      { "<Plug>(sandwich-add)", mode = { "n", "x", "o" } },
+      "<Plug>(sandwich-delete)",
+      "<Plug>(sandwich-replace)",
     },
   },
   {
