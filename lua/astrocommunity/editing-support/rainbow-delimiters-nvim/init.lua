afe4a726975a4cc2233cc4366c5beb2b22e52653
Micah Halter <micah@mehalter.com>
2024-06-25 14:01:45 -0400

    feat(rainbow-delimiters-nvim): add `<Leader>u(` to toggle rainbow delimiters

--- lua/astrocommunity/editing-support/rainbow-delimiters-nvim/init.lua
+++ lua/astrocommunity/editing-support/rainbow-delimiters-nvim/init.lua
@@ -1,9 +1,34 @@
 return {
   {
     "HiPhish/rainbow-delimiters.nvim",
-    dependencies = "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          mappings = {
+            n = {
+              ["<Leader>u("] = {
+                function()
+                  local bufnr = vim.api.nvim_get_current_buf()
+                  require("rainbow-delimiters").toggle(bufnr)
+                  require("astrocore").notify(
+                    string.format(
+                      "Buffer rainbow delimeters %s",
+                      require("rainbow-delimiters").is_enabled(bufnr) and "on" or "off"
+                    )
+                  )
+                end,
+                desc = "Toggle rainbow delimeters (buffer)",
+              },
+            },
+          },
+        },
+      },
+    },
     event = "User AstroFile",
     main = "rainbow-delimiters.setup",
+    opts = {},
   },
   {
     "catppuccin",
