90ff9f23f98c4265b37091c6077744b48c19e324
Micah Halter <micah@mehalter.com>
2024-06-30 14:50:18 -0400

    feat(nvim-highlight-colors): add `nvim-cmp` completion integration

--- lua/astrocommunity/color/nvim-highlight-colors/init.lua
+++ lua/astrocommunity/color/nvim-highlight-colors/init.lua
@@ -15,4 +15,25 @@ return {
     },
     opts = { enabled_named_colors = false },
   },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    opts = function(_, opts)
+      if not opts.formatting then opts.formatting = {} end
+      local formatter = vim.tbl_get(opts, "formatting", "format")
+      opts.formatting.format = function(entry, item)
+        if formatter then
+          local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
+          item = formatter(entry, item)
+          if color_item.abbr_hl_group then
+            item.kind_hl_group = color_item.abbr_hl_group
+            item.kind = color_item.abbr
+          end
+        else
+          item = require("nvim-highlight-colors").format(entry, item)
+        end
+        return item
+      end
+    end,
+  },
 }
