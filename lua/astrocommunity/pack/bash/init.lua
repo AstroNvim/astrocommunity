ce626bc1aaeb234aef14c91156d64fe3974e06bc
Micah Halter <micah@mehalter.com>
2024-07-01 14:05:02 -0400

    fix(bash): `shfmt` indentation detection implemented upstream

--- lua/astrocommunity/pack/bash/init.lua
+++ lua/astrocommunity/pack/bash/init.lua
@@ -46,21 +46,6 @@ return {
       formatters_by_ft = {
         sh = { "shfmt" },
       },
-      formatters = {
-        shfmt = {
-          prepend_args = function(_, ctx)
-            local args = {}
-            local bo = vim.bo[ctx.buf]
-            if bo.expandtab then
-              local tab_size = bo.tabstop or 2
-              local indent_size = bo.shiftwidth
-              if indent_size == 0 or not indent_size then indent_size = tab_size end
-              vim.list_extend(args, { "-i", tostring(indent_size) })
-            end
-            return args
-          end,
-        },
-      },
     },
   },
   {
