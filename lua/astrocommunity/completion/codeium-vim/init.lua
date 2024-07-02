144f5b9ac133fdb5340c3cb19f0e60b27baecd67
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-07-02 06:38:56 +0200

    refactor(codeium-vim): port mappings to `astrocore`

--- lua/astrocommunity/completion/codeium-vim/init.lua
+++ lua/astrocommunity/completion/codeium-vim/init.lua
@@ -1,17 +1,43 @@
 return {
   "Exafunction/codeium.vim",
   event = "User AstroFile",
-  config = function()
-    vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-    vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
-    vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
-    vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
-    vim.keymap.set("n", "<Leader>;", function()
-      if vim.g.codeium_enabled == true then
-        vim.cmd "CodeiumDisable"
-      else
-        vim.cmd "CodeiumEnable"
-      end
-    end, { noremap = true, desc = "Toggle Codeium active" })
-  end,
+  dependencies = {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          ["<Leader>;"] = {
+            function()
+              if vim.g.codeium_enabled == true then
+                vim.cmd "CodeiumDisable"
+              else
+                vim.cmd "CodeiumEnable"
+              end
+            end,
+            noremap = true,
+            desc = "Toggle Codeium active",
+          },
+        },
+        i = {
+          ["<C-g>"] = {
+            function() return vim.fn["codeium#Accept"]() end,
+            expr = true,
+          },
+          ["<C-;>"] = {
+            function() return vim.fn["codeium#CycleCompletions"](1) end,
+            expr = true,
+          },
+          ["<C-,>"] = {
+            function() return vim.fn["codeium#CycleCompletions"](-1) end,
+            expr = true,
+          },
+          ["<C-x>"] = {
+            function() return vim.fn["codeium#Clear"]() end,
+            expr = true,
+          },
+        },
+      },
+    },
+  },
 }
