25baea9ab9f5cb07b39f06eaa4dda701b89cd61d
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-07-02 07:36:22 +0200

    refactor(lsp-signature-nvim): corrected lazy spec and disable conflicts features from `noice-nvim`

--- lua/astrocommunity/lsp/lsp-signature-nvim/init.lua
+++ lua/astrocommunity/lsp/lsp-signature-nvim/init.lua
@@ -1,9 +1,22 @@
+---@type LazySpec
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
+  {
     "ray-x/lsp_signature.nvim",
+    event = "User AstroFile",
+    main = "lsp_signature",
     opts = {
       hint_enable = false, -- disable hints as it will crash in some terminal
+    },
+  },
+  {
+    "folke/noice.nvim",
+    optional = true,
+    ---@type NoiceConfig
+    opts = {
+      lsp = {
+        signature = { enabled = false },
+        hover = { enabled = false },
+      },
     },
   },
 }
