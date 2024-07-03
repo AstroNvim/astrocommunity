478425a7e8e1c4aa33a4d0d583826b016b30ef43
Amirkhan <dlordproudd@gmail.com>
2024-07-03 12:18:33 +0300

    fix(codeium-nvim): resolve loading issues for nvim-cmp and lspkind.nvim

--- lua/astrocommunity/completion/codeium-nvim/init.lua
+++ lua/astrocommunity/completion/codeium-nvim/init.lua
@@ -36,28 +36,28 @@ return {
         end,
       },
     },
-    {
-      "hrsh7th/nvim-cmp",
-      optional = true,
-      dependencies = { "Exafunction/codeium.nvim" },
-      opts = function(_, opts)
-        -- Inject codeium into cmp sources, with high priority
-        table.insert(opts.sources, 1, {
-          name = "codeium",
-          group_index = 1,
-          priority = 10000,
-        })
-      end,
-    },
-    {
-      "onsails/lspkind.nvim",
-      optional = true,
-      -- Adds icon for codeium using lspkind
-      opts = function(_, opts)
-        opts.symbol_map = {
-          Codeium = require("astroui").get_icon("Codeium", 1, true),
-        }
-      end,
-    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "Exafunction/codeium.nvim" },
+    opts = function(_, opts)
+      -- Inject codeium into cmp sources, with high priority
+      table.insert(opts.sources, 1, {
+        name = "codeium",
+        group_index = 1,
+        priority = 10000,
+      })
+    end,
+  },
+  {
+    "onsails/lspkind.nvim",
+    optional = true,
+    -- Adds icon for codeium using lspkind
+    opts = function(_, opts)
+      opts.symbol_map = {
+        Codeium = require("astroui").get_icon("Codeium", 1, true),
+      }
+    end,
   },
 }
