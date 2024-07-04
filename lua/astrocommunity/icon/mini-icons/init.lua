34c3decc690ef02d412d8bc62faec7e563489f56
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2024-07-04 14:04:45 +0200

    feat(icon): Add mini.icons (#1086)

---
+++ lua/astrocommunity/icon/mini-icons/init.lua
@@ -0,0 +1,14 @@
+return {
+  "echasnovski/mini.icons",
+  opts = {},
+  lazy = true,
+  specs = {
+    { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+  },
+  init = function()
+    package.preload["nvim-web-devicons"] = function()
+      require("mini.icons").mock_nvim_web_devicons()
+      return package.loaded["nvim-web-devicons"]
+    end
+  end,
+}
