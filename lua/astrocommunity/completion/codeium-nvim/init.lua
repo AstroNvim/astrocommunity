5bfcfc411a8c58d40774e25952f30effdbd78be2
khas-amir <dlordproudd@gmail.com>
2024-07-08 15:03:26 +0300

    fix(codeium-nvim): resolve loading issues for nvim-cmp and lspkind.nvim (#1082)
    * fix(codeium-nvim): resolve loading issues for nvim-cmp and lspkind.nvim
    
    * fix(codeium-nvim): fix various bugs in spec
    
    ---------
    
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/completion/codeium-nvim/init.lua
+++ lua/astrocommunity/completion/codeium-nvim/init.lua
@@ -1,45 +1,44 @@
 return {
-  {
-    "Exafunction/codeium.nvim",
-    event = "User AstroFile",
-    cmd = "Codeium",
-    opts = {
-      enable_chat = true,
-    },
-    dependencies = {
-      {
-        "AstroNvim/astroui",
-        ---@type AstroUIOpts
-        opts = {
-          icons = {
-            Codeium = "",
-          },
+  "Exafunction/codeium.nvim",
+  event = "User AstroFile",
+  cmd = "Codeium",
+  opts = {
+    enable_chat = true,
+  },
+  dependencies = {
+    {
+      "AstroNvim/astroui",
+      ---@type AstroUIOpts
+      opts = {
+        icons = {
+          Codeium = "",
         },
-      },
-      {
-        "AstroNvim/astrocore",
-        ---@param opts AstroCoreOpts
-        opts = function(_, opts)
-          return require("astrocore").extend_tbl(opts, {
-            mappings = {
-              n = {
-                ["<Leader>;"] = {
-                  name = require("astroui").get_icon("Codeium", 1, true) .. "Codeium",
-                },
-                ["<Leader>;o"] = {
-                  desc = "Open Chat",
-                  function() vim.cmd "Codeium Chat" end,
-                },
-              },
-            },
-          })
-        end,
       },
     },
     {
+      "AstroNvim/astrocore",
+      ---@param opts AstroCoreOpts
+      opts = function(_, opts)
+        return require("astrocore").extend_tbl(opts, {
+          mappings = {
+            n = {
+              ["<Leader>;"] = {
+                name = require("astroui").get_icon("Codeium", 1, true) .. "Codeium",
+              },
+              ["<Leader>;o"] = {
+                desc = "Open Chat",
+                function() vim.cmd "Codeium Chat" end,
+              },
+            },
+          },
+        })
+      end,
+    },
+  },
+  specs = {
+    {
       "hrsh7th/nvim-cmp",
       optional = true,
-      dependencies = { "Exafunction/codeium.nvim" },
       opts = function(_, opts)
         -- Inject codeium into cmp sources, with high priority
         table.insert(opts.sources, 1, {
@@ -54,9 +53,8 @@ return {
       optional = true,
       -- Adds icon for codeium using lspkind
       opts = function(_, opts)
-        opts.symbol_map = {
-          Codeium = require("astroui").get_icon("Codeium", 1, true),
-        }
+        if not opts.symbol_map then opts.symbol_map = {} end
+        opts.symbol_map.Codeium = require("astroui").get_icon("Codeium", 1, true)
       end,
     },
   },
