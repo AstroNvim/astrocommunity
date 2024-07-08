96c88877466250a4855b94d0dbf832f8415874c6
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2024-07-08 13:36:47 +0200

    feat(nix): Replace `nil_ls` with `nixd` (#1093)

--- lua/astrocommunity/pack/nix/README.md
+++ lua/astrocommunity/pack/nix/README.md
@@ -9,7 +9,7 @@ Requires the following in your `PATH`
 This plugin pack does the following:
 
 - Adds `nix` Treesitter parsers
-- Adds `nil_ls` language server
+- Adds `nixd` language server
 - Adds the following `null-ls` sources:
   - [alejandra](https://github.com/kamadorueda/alejandra)
   - [deadnix](https://github.com/astro/deadnix)
--- lua/astrocommunity/pack/nix/init.lua
+++ lua/astrocommunity/pack/nix/init.lua
@@ -12,14 +12,14 @@ return {
     "williamboman/mason-lspconfig.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "nil_ls" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "nixd" })
     end,
   },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "nil_ls" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "nixd" })
     end,
   },
   {
