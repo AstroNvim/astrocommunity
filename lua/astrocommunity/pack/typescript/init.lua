db07ec9f77d8cdafbfa3ccf75c08e13b9c4901c6
Micah Halter <micah@mehalter.com>
2024-07-09 08:14:14 -0400

    feat(typescript): configure language specific icons in `mini.icons`

--- lua/astrocommunity/pack/typescript/init.lua
+++ lua/astrocommunity/pack/typescript/init.lua
@@ -216,4 +216,20 @@ return {
     cmd = "TSC",
     opts = {},
   },
+  {
+    "echasnovski/mini.icons",
+    opts = {
+      file = {
+        [".eslintrc.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+        [".node-version"] = { glyph = "", hl = "MiniIconsGreen" },
+        [".prettierrc"] = { glyph = "", hl = "MiniIconsPurple" },
+        [".yarnrc.yml"] = { glyph = "", hl = "MiniIconsBlue" },
+        ["eslint.config.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+        ["package.json"] = { glyph = "", hl = "MiniIconsGreen" },
+        ["tsconfig.json"] = { glyph = "", hl = "MiniIconsAzure" },
+        ["tsconfig.build.json"] = { glyph = "", hl = "MiniIconsAzure" },
+        ["yarn.lock"] = { glyph = "", hl = "MiniIconsBlue" },
+      },
+    },
+  },
 }
