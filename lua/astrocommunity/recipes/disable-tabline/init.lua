71f26b6a774a0027d356d836e64bec7294f065f2
Micah Halter <micah@mehalter.com>
2024-07-18 08:06:35 -0400

    fix(disable-tabline): make sure to remove any changes to the default `showtabline`

--- lua/astrocommunity/recipes/disable-tabline/init.lua
+++ lua/astrocommunity/recipes/disable-tabline/init.lua
@@ -3,6 +3,7 @@ return {
     "AstroNvim/astrocore",
     ---@param opts AstroCoreOpts
     opts = function(_, opts)
+      if vim.tbl_get(opts, "options", "opt", "showtabline") then opts.options.opt.showtabline = nil end
       for k, _ in pairs(opts.mappings.n) do
         if k:find "^<Leader>b" then opts.mappings.n[k] = false end
       end
