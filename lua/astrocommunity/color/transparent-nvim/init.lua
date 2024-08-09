4d95d0a90896156f6f5968b4466b060d9142ce50
Micah Halter <micah@mehalter.com>
2024-08-09 12:22:09 -0400

    fix(transparent-nvim): use `opts` table so the user can extend options

--- lua/astrocommunity/color/transparent-nvim/init.lua
+++ lua/astrocommunity/color/transparent-nvim/init.lua
@@ -1,14 +1,15 @@
 return {
   "xiyaowong/transparent.nvim",
   lazy = false,
-  config = function()
+  opts = {
+    extra_groups = {
+      "NormalFloat",
+      "NvimTreeNormal",
+    },
+  },
+  config = function(_, opts)
     local transparent = require "transparent"
-    transparent.setup {
-      extra_groups = {
-        "NormalFloat",
-        "NvimTreeNormal",
-      },
-    }
+    transparent.setup(opts)
     transparent.clear_prefix "BufferLine"
     transparent.clear_prefix "NeoTree"
     transparent.clear_prefix "lualine"
