2ea16f78e2d864667277a7a35d331302e1f5707b
Micah Halter <micah@mehalter.com>
2024-08-12 19:25:44 -0400

    feat(markdown-and-latex): add `markview.nvim` (#1155)

---
+++ lua/astrocommunity/markdown-and-latex/markview-nvim/init.lua
@@ -0,0 +1,9 @@
+return {
+  "OXY2DEV/markview.nvim",
+  ft = { "markdown", "markdown.mdx" },
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {
+    hybrid_modes = { "n" },
+    headings = { shift_width = 0 },
+  },
+}
