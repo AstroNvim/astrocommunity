338382d55311116eebc1dde013e194440b6fea8c
Tianze Zhu <ztz20001117@gmail.com>
2024-08-12 17:02:52 +0800

    feat(markdown): added markdown-preview.nvim plugin.

--- lua/astrocommunity/pack/markdown/init.lua
+++ lua/astrocommunity/pack/markdown/init.lua
@@ -1,5 +1,21 @@
 return {
   {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function(plugin)
+      if vim.fn.executable "npx" then
+        vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
+      else
+        vim.cmd [[Lazy load markdown-preview.nvim]]
+        vim.fn["mkdp#util#install"]()
+      end
+    end,
+    init = function()
+      if vim.fn.executable "npx" then vim.g.mkdp_filetypes = { "markdown" } end
+    end,
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     optional = true,
     opts = function(_, opts)
