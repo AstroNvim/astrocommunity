5dac4da763aa6220757cf0864f24130ec69b54db
Micah Halter <micah@mehalter.com>
2024-08-12 15:13:56 -0400

    fix(python): disable `venv-selector` if `fd` isn't installed

--- lua/astrocommunity/pack/python/init.lua
+++ lua/astrocommunity/pack/python/init.lua
@@ -77,6 +77,7 @@ return {
   {
     "linux-cultist/venv-selector.nvim",
     branch = "regexp",
+    enabled = vim.fn.executable "fd" == 1 or vim.fn.executable "fdfind" == 1 or vim.fn.executable "fd-find" == 1,
     dependencies = {
       { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
       {
