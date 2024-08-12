7da567d6ff8d4073b05c84fce3b01dc95d0607ea
Micah Halter <micah@mehalter.com>
2024-08-12 13:21:36 -0400

    fix(markdown-and-latex): improve installation process and lazy loading

--- lua/astrocommunity/markdown-and-latex/markdown-preview-nvim/init.lua
+++ lua/astrocommunity/markdown-and-latex/markdown-preview-nvim/init.lua
@@ -1,5 +1,9 @@
 return {
   "iamcco/markdown-preview.nvim",
-  build = "cd app && yarn install",
-  ft = "markdown",
+  build = function() vim.fn["mkdp#util#install"]() end,
+  ft = { "markdown", "markdown.mdx" },
+  init = function()
+    local plugin = require("lazy.core.config").spec.plugins["markdown-preview.nvim"]
+    vim.g.mkdp_filetypes = require("lazy.core.plugin").values(plugin, "ft", true)
+  end,
 }
