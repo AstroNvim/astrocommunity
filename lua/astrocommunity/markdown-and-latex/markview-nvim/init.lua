8d9279212c537584507feac556e8720133712dfd
Manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-08-13 23:56:06 +0200

    fix(markview-nvim): Add missing treesitter parsers (#1158)
    * fix(markview-nvim): `markview-nvim` needs `html` from treesitter to parse
    
    * Update lua/astrocommunity/markdown-and-latex/markview-nvim/init.lua
    
    * chore(markview-nvim): Format code
    
    ---------
    
    Co-authored-by: Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>

--- lua/astrocommunity/markdown-and-latex/markview-nvim/init.lua
+++ lua/astrocommunity/markdown-and-latex/markview-nvim/init.lua
@@ -1,7 +1,16 @@
 return {
   "OXY2DEV/markview.nvim",
   ft = { "markdown", "markdown.mdx" },
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed =
+          require("astrocore").list_insert_unique(opts.ensure_installed, { "html", "markdown", "markdown_inline" })
+      end
+    end,
+  },
   opts = {
     hybrid_modes = { "n" },
     headings = { shift_width = 0 },
