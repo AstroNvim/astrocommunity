0f562dfcaa5df1e2336010d1f6051d2a57048072
Micah Halter <micah@mehalter.com>
2024-08-13 19:43:59 -0400

    fix(markview-nvim): fix incorrect treesitter configuration

--- lua/astrocommunity/markdown-and-latex/markview-nvim/init.lua
+++ lua/astrocommunity/markdown-and-latex/markview-nvim/init.lua
@@ -3,7 +3,6 @@ return {
   ft = { "markdown", "markdown.mdx" },
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
-    optional = true,
     opts = function(_, opts)
       if opts.ensure_installed ~= "all" then
         opts.ensure_installed =
