ee47571fdbe1872a57a6a647446cbfd9dc510f21
Manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-08-14 00:06:35 +0200

    fix(helpview-nvim): add missing treesitter parser (#1159)

--- lua/astrocommunity/neovim-lua-development/helpview-nvim/init.lua
+++ lua/astrocommunity/neovim-lua-development/helpview-nvim/init.lua
@@ -1,5 +1,13 @@
 return {
   "OXY2DEV/helpview.nvim",
   ft = "help",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "vimdoc" })
+      end
+    end,
+  },
 }
