514f307d62905c0a67fa10819b413cafb4957b09
Paul Gross <pgross@gmail.com>
2024-08-16 14:08:14 -0700

    fix(go): automatically install gofumpt and goimports
    The go configuration tells conform to use these tools for formatting,
    but they were not automatically installed. This fix adds them to the
    mason-tool-installer list so they will be installed with the rest of the
    go tools.

--- lua/astrocommunity/pack/go/init.lua
+++ lua/astrocommunity/pack/go/init.lua
@@ -87,7 +87,7 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = require("astrocore").list_insert_unique(
         opts.ensure_installed,
-        { "delve", "gopls", "gomodifytags", "gotests", "iferr", "impl" }
+        { "delve", "gopls", "gofumpt", "goimports", "gomodifytags", "gotests", "iferr", "impl" }
       )
     end,
   },
