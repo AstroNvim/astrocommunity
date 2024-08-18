764387c2bd2776811dc14790cdfa9968282569b5
Uzair Aftab <muaf@nmbu.no>
2024-08-18 11:36:40 +0200

    feat(molten-nvim): Add some basic keybindings

--- lua/astrocommunity/code-runner/molten-nvim/init.lua
+++ lua/astrocommunity/code-runner/molten-nvim/init.lua
@@ -28,4 +28,17 @@ return {
   },
   version = "^1", -- use version <2.0.0 to avoid breaking changes
   build = ":UpdateRemotePlugins",
+  dependencies = {
+    "AstroNvim/astrocore",
+    opts = function(_, opts)
+      if not opts.mappings then opts.mappings = {} end
+      local prefix = "<leader>m"
+
+      opts.mappings.n[prefix .. "mi"] = { "<Cmd>MoltenInit<CR>", desc = "Initialize the plugin" }
+      opts.mappings.n[prefix .. "e"] = { "<Cmd>MoltenEvaluateOperator<CR>", desc = "Run operator selection" }
+      opts.mappings.n[prefix .. "rl"] = { "<Cmd>MoltenEvaluateLine<CR>", desc = "Evaluate line" }
+      opts.mappings.n[prefix .. "rr"] = { "<Cmd>MoltenReevaluateCell<CR>", desc = "Re-evaluate cell" }
+      opts.mappings.v[prefix .. "r"] = { ":<C-u>MoltenEvaluateVisual<CR>gv", desc = "Evaluate visual selection" }
+    end,
+  },
 }
