8c62ac98b8f651bf3cfdfcbe899a22e3f9048909
German Lashevich <german.lashevich@gmail.com>
2024-08-21 11:06:01 +0200

    refactor opts

--- lua/astrocommunity/pack/helm/init.lua
+++ lua/astrocommunity/pack/helm/init.lua
@@ -1,53 +1,44 @@
-local M = {}
-
-function M.is_helm_file(path)
-  local check = vim.fs.find("Chart.yaml", { path = vim.fs.dirname(path), upward = true })
-  return not vim.tbl_isempty(check)
-end
-
---@private
---@return string
-function M.yaml_filetype(path, _) return M.is_helm_file(path) and "helm.yaml" or "yaml" end
-
---@private
---@return string
-function M.tmpl_filetype(path, _) return M.is_helm_file(path) and "helm.tmpl" or "template" end
-
---@private
---@return string
-function M.tpl_filetype(path, _) return M.is_helm_file(path) and "helm.tmpl" or "smarty" end
-
 return {
   {
     "AstroNvim/astrocore",
-    ---@type AstroCoreOpts
-    opts = {
-      autocmds = {
-        helm_commentstring = {
-          {
-            event = "FileType",
-            pattern = "helm",
-            callback = function() vim.opt_local.commentstring = "{{/* %s */}}" end,
+    ---@param opts AstroCoreOpts
+    opts = function(_, opts)
+      local is_helm_file = function(path)
+        local check = vim.fs.find("Chart.yaml", { path = vim.fs.dirname(path), upward = true })
+        return not vim.tbl_isempty(check)
+      end
+      local yaml_filetype = function(path, _) return is_helm_file(path) and "helm.yaml" or "yaml" end
+      local tmpl_filetype = function(path, _) return is_helm_file(path) and "helm.tmpl" or "template" end
+      local tpl_filetype = function(path, _) return is_helm_file(path) and "helm.tmpl" or "smarty" end
+
+      return require("astrocore").extend_tbl(opts, {
+        autocmds = {
+          helm_commentstring = {
+            {
+              event = "FileType",
+              pattern = "helm",
+              callback = function() vim.opt_local.commentstring = "{{/* %s */}}" end,
+            },
           },
         },
-      },
-      filetypes = {
-        extension = {
-          gotmpl = "helm",
-          yaml = M.yaml_filetype,
-          yml = M.yaml_filetype,
-          tmpl = M.tmpl_filetype,
-          tpl = M.tpl_filetype,
+        filetypes = {
+          extension = {
+            gotmpl = "helm",
+            yaml = yaml_filetype,
+            yml = yaml_filetype,
+            tmpl = tmpl_filetype,
+            tpl = tpl_filetype,
+          },
+          filename = {
+            ["Chart.yaml"] = "yaml",
+            ["Chart.lock"] = "yaml",
+          },
+          pattern = {
+            ["helmfile.*%.ya?ml"] = "helm",
+          },
         },
-        filename = {
-          ["Chart.yaml"] = "yaml",
-          ["Chart.lock"] = "yaml",
-        },
-        pattern = {
-          ["helmfile.*%.ya?ml"] = "helm",
-        },
-      },
-    },
+      })
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
