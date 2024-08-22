1b6148f47e08b239c2dbff61a2a4f9c96a387b2e
Micah Halter <micah@mehalter.com>
2024-08-22 10:47:09 -0400

    feat(split-and-window): add `neominimap.nvim`

---
+++ lua/astrocommunity/split-and-window/neominimap-nvim/README.md
@@ -0,0 +1,5 @@
+# Neominimap
+
+Yet another minimap plugin for Neovim
+
+**Repository:** <https://github.com/Isrothy/neominimap.nvim>
---
+++ lua/astrocommunity/split-and-window/neominimap-nvim/init.lua
@@ -0,0 +1,30 @@
+return {
+  "Isrothy/neominimap.nvim",
+  version = "^3",
+  lazy = false,
+  specs = {
+    { -- recommended options by the plugin
+      "AstroNvim/astrocore",
+      opts = {
+        options = { opt = {
+          wrap = false,
+        } },
+        mappings = {
+          n = {
+            ["<Leader>um"] = { "<Cmd>Neominimap toggle<CR>", desc = "Toggle minimap" },
+          },
+        },
+      },
+    },
+  },
+  opts = {
+    buf_filter = function(bufnr) return require("astrocore.buffer").is_valid(bufnr) end,
+  },
+  config = function(_, opts)
+    if opts.layout == nil or opts.layout == "float" then
+      local float_width = (vim.tbl_get(opts, "float", "minimap_width") or 20) + 2
+      if vim.opt.sidescrolloff:get() < float_width then vim.opt.sidescrolloff = float_width end
+    end
+    vim.g.neominimap = require("astrocore").extend_tbl(opts, vim.g.neominimap)
+  end,
+}
