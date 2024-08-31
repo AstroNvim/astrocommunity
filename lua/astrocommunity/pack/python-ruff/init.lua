72fad3a2cbd3112f0f30bfeecd020fe1e74e9fe9
Glenn Waters <glenn@watrs.ca>
2024-08-31 16:58:57 -0400

    Ensure formatters are called.

--- lua/astrocommunity/pack/python-ruff/init.lua
+++ lua/astrocommunity/pack/python-ruff/init.lua
@@ -40,6 +40,9 @@ return {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
+      format_on_save = {
+        lsp_format = "fallback",
+      },
       formatters_by_ft = {
         python = { "ruff_organize_imports", "ruff_format" },
       },
