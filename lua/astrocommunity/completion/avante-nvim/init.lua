b98ccba084e008e995acb0a81d3b5d064eff129c
vonhyou <moi@rfung.dev>
2024-09-01 17:00:38 -0400

    test and add render-markdown.nvim as recommended

--- lua/astrocommunity/completion/avante-nvim/init.lua
+++ lua/astrocommunity/completion/avante-nvim/init.lua
@@ -30,5 +30,13 @@ return {
         },
       },
     },
+    {
+      -- Make sure to setup it properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
   },
 }
