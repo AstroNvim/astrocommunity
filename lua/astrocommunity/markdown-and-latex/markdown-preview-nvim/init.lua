53dea1c0263638ab07678bfe97402dc1d1a4f21d
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-09-01 20:04:44 -0400

    feat(markdown-preview-nvim): add mappings

--- lua/astrocommunity/markdown-and-latex/markdown-preview-nvim/init.lua
+++ lua/astrocommunity/markdown-and-latex/markdown-preview-nvim/init.lua
@@ -1,9 +1,27 @@
+---@type LazySpec
 return {
   "iamcco/markdown-preview.nvim",
   build = function() vim.fn["mkdp#util#install"]() end,
   ft = { "markdown", "markdown.mdx" },
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   init = function()
     local plugin = require("lazy.core.config").spec.plugins["markdown-preview.nvim"]
     vim.g.mkdp_filetypes = require("lazy.core.plugin").values(plugin, "ft", true)
   end,
+  dependencies = {
+    { "AstroNvim/astroui", opts = { icons = { Markdown = "" } } },
+    {
+      "AstroNvim/astrocore",
+      optional = true,
+      opts = function(_, opts)
+        local maps = opts.mappings
+        local prefix = "<Leader>M"
+
+        maps.n[prefix] = { desc = require("astroui").get_icon("Markdown", 1, true) .. "Markdown" }
+        maps.n[prefix .. "p"] = { "<cmd>MarkdownPreview<cr>", desc = "Preview" }
+        maps.n[prefix .. "s"] = { "<cmd>MarkdownPreviewStop<cr>", desc = "Stop preview" }
+        maps.n[prefix .. "t"] = { "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle preview" }
+      end,
+    },
+  },
 }
