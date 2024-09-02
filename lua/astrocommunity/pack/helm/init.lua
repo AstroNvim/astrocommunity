bfbbe67ba22f6014619139b81d3f3223eb7e41bd
German Lashevich <german.lashevich@gmail.com>
2024-09-02 18:07:58 +0200

    fix(helm): better file type detection (#1171)
    * fix(helm): better file type detection
    
    * remove unused M.setup function
    
    * refactor opts
    
    * fix(helm): add filetype merging
    
    ---------
    
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/pack/helm/init.lua
+++ lua/astrocommunity/pack/helm/init.lua
@@ -1,26 +1,45 @@
 return {
   {
     "AstroNvim/astrocore",
-    ---@type AstroCoreOpts
-    opts = {
-      autocmds = {
-        helm_commentstring = {
-          {
-            event = "FileType",
-            pattern = "helm",
-            callback = function() vim.opt_local.commentstring = "{{/* %s */}}" end,
+    ---@param opts AstroCoreOpts
+    opts = function(_, opts)
+      local utils = require "astrocommunity"
+      local is_helm_file = function(path)
+        local check = vim.fs.find("Chart.yaml", { path = vim.fs.dirname(path), upward = true })
+        return not vim.tbl_isempty(check)
+      end
+      local yaml_filetype = function(path, _) return is_helm_file(path) and "helm" or "yaml" end
+      local tmpl_filetype = function(path, _) return is_helm_file(path) and "helm" or "template" end
+      local tpl_filetype = function(path, _) return is_helm_file(path) and "helm" or "smarty" end
+
+      return require("astrocore").extend_tbl(opts, {
+        autocmds = {
+          helm_commentstring = {
+            {
+              event = "FileType",
+              pattern = "helm",
+              callback = function() vim.opt_local.commentstring = "{{/* %s */}}" end,
+            },
           },
         },
-      },
-      filetypes = {
-        extension = { gotmpl = "helm" },
-        pattern = {
-          [".*/templates/.*%.ya?ml"] = "helm",
-          [".*/templates/.*%.tpl"] = "helm",
-          ["helmfile.*%.ya?ml"] = "helm",
+        filetypes = {
+          extension = {
+            gotmpl = "helm",
+            yaml = utils.merge_filetype("yaml", vim.tbl_get(opts, "filetypes", "extension", "yaml"), yaml_filetype),
+            yml = utils.merge_filetype("yaml", vim.tbl_get(opts, "filetypes", "extension", "yml"), yaml_filetype),
+            tmpl = utils.merge_filetype("template", vim.tbl_get(opts, "filetypes", "extension", "tmpl"), tmpl_filetype),
+            tpl = utils.merge_filetype("smarty", vim.tbl_get(opts, "filetypes", "extension", "tpl"), tpl_filetype),
+          },
+          filename = {
+            ["Chart.yaml"] = "yaml",
+            ["Chart.lock"] = "yaml",
+          },
+          pattern = {
+            ["helmfile.*%.ya?ml"] = "helm",
+          },
         },
-      },
-    },
+      })
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
