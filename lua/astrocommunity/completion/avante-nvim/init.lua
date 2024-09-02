295ce8658af392b1fbb272fe72b554e330e60833
vonhyou <moi@rfung.dev>
2024-09-01 21:39:44 -0400

    add keymaps opts that have cmds

--- lua/astrocommunity/completion/avante-nvim/init.lua
+++ lua/astrocommunity/completion/avante-nvim/init.lua
@@ -37,6 +37,31 @@ return {
 
         maps.n[prefix .. "e"] = { function() require("avante.api").edit() end, desc = "Avante edit" }
         maps.v[prefix .. "e"] = { function() require("avante.api").edit() end, desc = "Avante edit" }
+
+        -- the following key bindings do not have an official api implementation
+        maps.n.co = { ":AvanteConflictChooseOurs<CR>", desc = "Choose ours" }
+        maps.v.co = { ":AvanteConflictChooseOurs<CR>", desc = "Choose ours" }
+
+        maps.n.ct = { ":AvanteConflictChooseTheirs<CR>", desc = "Choose theirs" }
+        maps.v.ct = { ":AvanteConflictChooseTheirs<CR>", desc = "Choose theirs" }
+
+        maps.n.ca = { ":AvanteConflictChooseAllTheirs<CR>", desc = "Choose all theirs" }
+        maps.v.ca = { ":AvanteConflictChooseAllTheirs<CR>", desc = "Choose all theirs" }
+
+        maps.n.c0 = { ":AvanteConflictChooseNone<CR>", desc = "Choose none" }
+        maps.v.c0 = { ":AvanteConflictChooseNone<CR>", desc = "Choose none" }
+
+        maps.n.cb = { ":AvanteConflictChooseBoth<CR>", desc = "Choose both" }
+        maps.v.cb = { ":AvanteConflictChooseBoth<CR>", desc = "Choose both" }
+
+        maps.n.cc = { ":AvanteConflictChooseCursor<CR>", desc = "Choose cursor" }
+        maps.v.cc = { ":AvanteConflictChooseCursor<CR>", desc = "Choose cursor" }
+
+        maps.n["]x"] = { ":AvanteConflictPrevConflict<CR>", desc = "Move to previous conflict" }
+        maps.v["]x"] = { ":AvanteConflictPrevConflict<CR>", desc = "Move to previous conflict" }
+
+        maps.n["[x"] = { ":AvanteConflictNextConflict<CR>", desc = "Move to next conflict" }
+        maps.x["[x"] = { ":AvanteConflictNextConflict<CR>", desc = "Move to next conflict" }
       end,
     },
     -- optional dependencies
