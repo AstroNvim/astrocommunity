50228d3ef6480307757cd5df7c228ea6d3767e9b
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-09-01 20:32:23 -0400

    feat(nvim-coverage): add keybindings

--- lua/astrocommunity/test/nvim-coverage/init.lua
+++ lua/astrocommunity/test/nvim-coverage/init.lua
@@ -1,6 +1,42 @@
+---@type LazySpec
 return {
   "andythigpen/nvim-coverage",
   event = "User AstroFile",
-  dependencies = { "nvim-lua/plenary.nvim" },
   opts = {},
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "AstroNvim/astroui",
+      opts = {
+        icons = {
+          Tests = "󰗇",
+          Coverage = "",
+        },
+      },
+    },
+    {
+      "AstroNvim/astrocore",
+      optional = true,
+      opts = function(_, opts)
+        local astroui = require "astroui"
+        local maps = opts.mappings
+
+        local tests_prefix = "<Leader>T"
+        local coverage_prefix = tests_prefix .. "C"
+
+        -- INFO: Compatibility with `neotest` and `vim-test`
+        maps.n[tests_prefix] = { desc = astroui.get_icon("Tests", 1, true) .. "Tests" }
+
+        maps.n[coverage_prefix] = { desc = astroui.get_icon("Coverage", 1, true) .. "Coverage" }
+        maps.n[coverage_prefix .. "t"] = { function() require("coverage").toggle() end, desc = "Toggle coverage" }
+        maps.n[coverage_prefix .. "s"] =
+          { function() require("coverage").summary() end, desc = "Show coverage summary" }
+        maps.n[coverage_prefix .. "c"] = { function() require("coverage").clear() end, desc = "Clear coverage" }
+        maps.n[coverage_prefix .. "l"] = {
+          function() require("coverage").load(true) end,
+          desc = "Load and show coverage",
+        }
+      end,
+    },
+  },
 }
