11ce8f0f19e31d5b890aa2b0f831cf27f6cc1228
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-09-04 07:38:30 -0400

    refactor(neotest): replace table keybindings by opts function instead

--- lua/astrocommunity/test/neotest/init.lua
+++ lua/astrocommunity/test/neotest/init.lua
@@ -1,34 +1,58 @@
-local prefix = "<Leader>T"
+---@type LazySpec
 return {
   "nvim-neotest/neotest",
   lazy = true,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-neotest/nvim-nio",
+    { "AstroNvim/astroui", opts = { icons = { Tests = "󰗇" } } },
     {
       "AstroNvim/astrocore",
-      opts = {
-        mappings = {
-          n = {
-            [prefix] = { desc = "󰗇 Tests" },
-            [prefix .. "t"] = { function() require("neotest").run.run() end, desc = "Run test" },
-            [prefix .. "d"] = { function() require("neotest").run.run { strategy = "dap" } end, desc = "Debug test" },
-            [prefix .. "f"] = {
-              function() require("neotest").run.run(vim.fn.expand "%") end,
-              desc = "Run all tests in file",
-            },
-            [prefix .. "p"] = {
-              function() require("neotest").run.run(vim.fn.getcwd()) end,
-              desc = "Run all tests in project",
-            },
-            [prefix .. "<CR>"] = { function() require("neotest").summary.toggle() end, desc = "Test Summary" },
-            [prefix .. "o"] = { function() require("neotest").output.open() end, desc = "Output hover" },
-            [prefix .. "O"] = { function() require("neotest").output_panel.toggle() end, desc = "Output window" },
-            ["]T"] = { function() require("neotest").jump.next() end, desc = "Next test" },
-            ["[T"] = { function() require("neotest").jump.prev() end, desc = "previous test" },
-          },
-        },
-      },
+      opts = function(_, opts)
+        local maps = opts.mappings
+
+        local prefix = "<Leader>T"
+
+        maps.n[prefix] = {
+          desc = require("astroui").get_icon("Tests", 1, true) .. "Tests",
+        }
+        maps.n[prefix .. "t"] = {
+          function() require("neotest").run.run() end,
+          desc = "Run test",
+        }
+        maps.n[prefix .. "d"] = {
+          function() require("neotest").run.run { strategy = "dap" } end,
+          desc = "Debug test",
+        }
+        maps.n[prefix .. "f"] = {
+          function() require("neotest").run.run(vim.fn.expand "%") end,
+          desc = "Run all tests in file",
+        }
+        maps.n[prefix .. "p"] = {
+          function() require("neotest").run.run(vim.fn.getcwd()) end,
+          desc = "Run all tests in project",
+        }
+        maps.n[prefix .. "<CR>"] = {
+          function() require("neotest").summary.toggle() end,
+          desc = "Test Summary",
+        }
+        maps.n[prefix .. "o"] = {
+          function() require("neotest").output.open() end,
+          desc = "Output hover",
+        }
+        maps.n[prefix .. "O"] = {
+          function() require("neotest").output_panel.toggle() end,
+          desc = "Output window",
+        }
+        maps.n["]T"] = {
+          function() require("neotest").jump.next() end,
+          desc = "Next test",
+        }
+        maps.n["[T"] = {
+          function() require("neotest").jump.prev() end,
+          desc = "Previous test",
+        }
+      end,
     },
     {
       "folke/neodev.nvim",
