2cad0e523881e323afd1c4e08836a10094e3d82b
vonhyou <moi@rfung.dev>
2024-09-04 17:45:08 -0400

    remove all optional dependencies

--- lua/astrocommunity/completion/avante-nvim/init.lua
+++ lua/astrocommunity/completion/avante-nvim/init.lua
@@ -64,31 +64,6 @@ return {
         maps.x["[x"] = { ":AvanteConflictNextConflict<CR>", desc = "Move to next conflict" }
       end,
     },
-    -- optional dependencies
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      -- Make sure to setup it properly if you have lazy=true
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
   },
   opts = {},
 }
