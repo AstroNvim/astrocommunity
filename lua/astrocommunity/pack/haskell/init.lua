c9c5f7a42d83ef33301a516b28e97c01057d1fec
Micah Halter <micah@mehalter.com>
2024-09-05 12:12:11 -0400

    fix(haskell): add empty config function for neotest adapter

--- lua/astrocommunity/pack/haskell/init.lua
+++ lua/astrocommunity/pack/haskell/init.lua
@@ -66,7 +66,7 @@ local pack = {
   {
     "nvim-neotest/neotest",
     optional = true,
-    dependencies = { "mrcjkb/neotest-haskell" },
+    dependencies = { "mrcjkb/neotest-haskell", config = function() end },
     opts = function(_, opts)
       if not opts.adapters then opts.adapters = {} end
       table.insert(opts.adapters, require "neotest-haskell"(require("astrocore").plugin_opts "neotest-haskell"))
