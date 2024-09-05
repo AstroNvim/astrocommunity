5cd235f05fc9f57bf49c516a8ab0147e2ac232e8
Micah Halter <micah@mehalter.com>
2024-09-05 12:12:04 -0400

    fix(go): add empty config function for neotest adapter

--- lua/astrocommunity/pack/go/init.lua
+++ lua/astrocommunity/pack/go/init.lua
@@ -128,7 +128,7 @@ return {
   {
     "nvim-neotest/neotest",
     optional = true,
-    dependencies = { "nvim-neotest/neotest-go" },
+    dependencies = { "nvim-neotest/neotest-go", config = function() end },
     opts = function(_, opts)
       if not opts.adapters then opts.adapters = {} end
       table.insert(opts.adapters, require "neotest-go"(require("astrocore").plugin_opts "neotest-go"))
