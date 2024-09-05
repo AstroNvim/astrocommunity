d7e120b114eb73dbd459e85a6a14ec0707579914
Micah Halter <micah@mehalter.com>
2024-09-05 08:06:01 -0400

    fix(typescript): add `ts_ls` to list of typescript language servers

--- lua/astrocommunity/pack/typescript/init.lua
+++ lua/astrocommunity/pack/typescript/init.lua
@@ -28,7 +28,7 @@ local has_prettier = function(bufnr)
     lsp_rooter = rooter.resolve("lsp", {
       ignore = {
         servers = function(client)
-          return not vim.tbl_contains({ "vtsls", "typescript-tools", "volar", "eslint", "tsserver" }, client.name)
+          return not vim.tbl_contains({ "eslint", "ts_ls", "typescript-tools", "volar", "vtsls" }, client.name)
         end,
       },
     })
