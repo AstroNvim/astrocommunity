2396c66dfe1be12e08f67ee274ffcf0b681cdecc
Micah Halter <micah@mehalter.com>
2024-09-05 13:08:53 -0400

    feat(markdown-and-latex): add `render-markdown.nvim`

---
+++ lua/astrocommunity/markdown-and-latex/render-markdown-nvim/init.lua
@@ -0,0 +1,21 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  cmd = "RenderMarkdown",
+  ft = function()
+    local plugin = require("lazy.core.config").spec.plugins["render-markdown.nvim"]
+    local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+    return opts.file_types or { "markdown" }
+  end,
+  dependencies = {
+    {
+      "nvim-treesitter/nvim-treesitter",
+      opts = function(_, opts)
+        if opts.ensure_installed ~= "all" then
+          opts.ensure_installed =
+            require("astrocore").list_insert_unique(opts.ensure_installed, { "html", "markdown", "markdown_inline" })
+        end
+      end,
+    },
+  },
+  opts = {},
+}
