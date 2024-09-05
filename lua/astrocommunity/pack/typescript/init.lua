ec39e6e5c6f9b4e018cc71f3ed05438409ee08cd
Micah Halter <micah@mehalter.com>
2024-09-05 14:19:53 -0400

    add missing empty config function

--- lua/astrocommunity/pack/typescript/init.lua
+++ lua/astrocommunity/pack/typescript/init.lua
@@ -236,7 +236,7 @@ return {
   {
     "nvim-neotest/neotest",
     optional = true,
-    dependencies = { "nvim-neotest/neotest-jest" },
+    dependencies = { { "nvim-neotest/neotest-jest", config = function() end } },
     opts = function(_, opts)
       if not opts.adapters then opts.adapters = {} end
       table.insert(opts.adapters, require "neotest-jest"(require("astrocore").plugin_opts "neotest-jest"))
