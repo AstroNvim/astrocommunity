a013538447cd7bf597b43ef8e7b7b4d08f37d78d
Micah Halter <micah.halter@gtri.gatech.edu>
2024-09-19 10:47:29 -0400

    feat(grug-far-nvim)!: add more features and align with recommended `spectre.nvim` mappings

--- lua/astrocommunity/search/grug-far-nvim/init.lua
+++ lua/astrocommunity/search/grug-far-nvim/init.lua
@@ -12,17 +12,48 @@ return {
       "AstroNvim/astrocore",
       ---@param opts AstroCoreOpts
       opts = function(_, opts)
-        if not opts.mappings then opts.mappings = require("astrocore").empty_map_table() end
-        local maps, prefix = opts.mappings, "<Leader>r"
+        local maps, prefix = opts.mappings, "<Leader>s"
 
-        maps.n[prefix] = {
+        maps.n[prefix] = { desc = require("astroui").get_icon("GrugFar", 1, true) .. "Search/Replace" }
+        maps.n[prefix .. "s"] = {
           function() require("grug-far").open { transient = true } end,
-          desc = require("astroui").get_icon("GrugFar", 1, true) .. "Search and Replace",
+          desc = "Search/Replace workspace",
         }
-
+        maps.n[prefix .. "e"] = {
+          function()
+            local ext = require("astrocore.buffer").is_valid() and vim.fn.expand "%:e" or ""
+            require("grug-far").open {
+              transient = true,
+              prefills = { filesFilter = ext ~= "" and "*." .. ext or nil },
+            }
+          end,
+          desc = "Search/Replace filetype",
+        }
+        maps.n[prefix .. "f"] = {
+          function()
+            local filter = require("astrocore.buffer").is_valid() and vim.fn.expand "%" or nil
+            require("grug-far").open { transient = true, prefills = { paths = filter } }
+          end,
+          desc = "Search/Replace file",
+        }
+        maps.n[prefix .. "w"] = {
+          function()
+            local current_word = vim.fn.expand "<cword>"
+            if current_word ~= "" then
+              require("grug-far").open {
+                transient = true,
+                startCursorRow = 4,
+                prefills = { search = vim.fn.expand "<cword>" },
+              }
+            else
+              vim.notify("No word under cursor", vim.log.levels.WARN, { title = "Grug-far" })
+            end
+          end,
+          desc = "Replace current word",
+        }
         maps.x[prefix] = {
           function() require("grug-far").open { transient = true, startCursorRow = 4 } end,
-          desc = require("astroui").get_icon("GrugFar", 1, true) .. "Search and Replace (current word)",
+          desc = "Replace selection",
         }
       end,
     },
@@ -42,9 +73,18 @@ return {
       ---@type CatppuccinOptions
       opts = { integrations = { grug_far = true } },
     },
+    {
+      "folke/which-key.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if not opts.disable then opts.disable = {} end
+        opts.disable.ft = require("astrocore").list_insert_unique(opts.disable.ft, { "grug-far" })
+      end,
+    },
   },
   ---@param opts GrugFarOptionsOverride
   opts = function(_, opts)
+    opts.windowCreationCommand = "split"
     if not opts.icons then opts.icons = {} end
     opts.icons.enabled = vim.g.icons_enabled
     if not vim.g.icons_enabled then
