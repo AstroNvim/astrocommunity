5bcdfc8e1b206ddab7f8d22d42faaa8a73ff412b
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2024-10-17 13:16:33 -0400

    feat(pack): lazier load full-dadbod
    also, have dadbod use nerd fonts. be case insensitive
    
    add pg to completion which is a file type used for sql tests

--- lua/astrocommunity/pack/full-dadbod/init.lua
+++ lua/astrocommunity/pack/full-dadbod/init.lua
@@ -4,7 +4,16 @@ return {
     {
       "kristijanhusak/vim-dadbod-ui",
       dependencies = { "tpope/vim-dadbod" },
-      event = "VeryLazy",
+      cmd = {
+        "DBUI",
+        "DBUIToggle",
+        "DBUIAddConnection",
+        "DBUIFindBuffer",
+      },
+      init = function()
+        vim.g.db_ui_use_nerd_fonts = 1
+        vim.g.completion_matching_ignore_case = 1
+      end,
     },
     {
       "hrsh7th/nvim-cmp",
@@ -20,12 +29,17 @@ return {
                   {
                     event = "FileType",
                     desc = "dadbod completion",
-                    pattern = { "sql", "mysql", "plsql" },
+                    pattern = { "sql", "mysql", "plsql", "pg" },
                     callback = function()
                       require("cmp").setup.buffer { sources = { { name = "vim-dadbod-completion" } } }
                     end,
                   },
                 },
+              pg_file = {
+                    event = { "BufRead", "BufNewFile" },
+                    pattern = "*.pg",
+                    command = "set filetype=sql",
+                  },
               },
             },
           },
