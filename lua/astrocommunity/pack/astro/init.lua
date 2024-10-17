de04a80ba404adbd5a90593829dbe3e857818252
Micah Halter <micah@mehalter.com>
2024-10-17 08:04:43 -0400

    fix(astro): fix various bugs and enabled better usage of other packs

--- lua/astrocommunity/pack/astro/init.lua
+++ lua/astrocommunity/pack/astro/init.lua
@@ -1,11 +1,12 @@
 return {
+  { import = "astrocommunity.pack.typescript" },
   {
     "nvim-treesitter/nvim-treesitter",
     optional = true,
     opts = function(_, opts)
       -- Ensure that opts.ensure_installed exists and is a table or string "all".
       if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "astro", "scss" })
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "astro" })
       end
     end,
   },
@@ -17,53 +18,11 @@ return {
     end,
   },
   {
-    "AstroNvim/astrolsp",
-    ---@type AstroLSPOpts
-    opts = {
-      ---@diagnostic disable: missing-fields
-      config = {
-        svelte = {
-          settings = {
-            typescript = {
-              updateImportsOnFileMove = { enabled = "always" },
-              inlayHints = {
-                parameterNames = { enabled = "all" },
-                parameterTypes = { enabled = true },
-                variableTypes = { enabled = true },
-                propertyDeclarationTypes = { enabled = true },
-                functionLikeReturnTypes = { enabled = true },
-                enumMemberValues = { enabled = true },
-              },
-            },
-            javascript = {
-              updateImportsOnFileMove = { enabled = "always" },
-              inlayHints = {
-                parameterNames = { enabled = "literals" },
-                parameterTypes = { enabled = true },
-                variableTypes = { enabled = true },
-                propertyDeclarationTypes = { enabled = true },
-                functionLikeReturnTypes = { enabled = true },
-                enumMemberValues = { enabled = true },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "js" })
-    end,
-  },
-  {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     optional = true,
     opts = function(_, opts)
       opts.ensure_installed =
-        require("astrocore").list_insert_unique(opts.ensure_installed, { "astro-language-server", "js-debug-adapter" })
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "astro-language-server" })
     end,
   },
 }
