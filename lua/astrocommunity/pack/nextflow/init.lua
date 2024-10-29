6166e840d19b0f6665c8e02c76cba500fa4179b0
Micah Halter <micah.halter@gtri.gatech.edu>
2024-10-29 15:44:32 -0400

    fix(nextflow): pin `vscode-language-nextflow`

--- lua/astrocommunity/pack/nextflow/init.lua
+++ lua/astrocommunity/pack/nextflow/init.lua
@@ -14,6 +14,7 @@ return {
     optional = true,
     specs = {
       "nextflow-io/vscode-language-nextflow",
+      commit = "efc410e46db3518ec7693668e159fb7b148a0e1a",
       ft = "nextflow",
       dependencies = { "L3MON4D3/LuaSnip" },
       config = function(plugin)
