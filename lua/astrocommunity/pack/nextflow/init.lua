ec4d829f5c6c4f2adafa64ff124548ce00f81e5c
Micah Halter <micah@mehalter.com>
2024-11-07 08:47:09 -0500

    refactor(nextflow): `nextflow_ls` merged upstream

--- lua/astrocommunity/pack/nextflow/init.lua
+++ lua/astrocommunity/pack/nextflow/init.lua
@@ -10,35 +10,6 @@ return {
   },
   { "nextflow-io/vim-language-nextflow", ft = "nextflow" },
   {
-    "AstroNvim/astrolsp",
-    ---@type AstroLSPOpts
-    opts = {
-      ---@diagnostic disable-next-line: missing-fields
-      config = {
-        nextflow_ls = {
-          cmd = {
-            "java",
-            "-jar",
-            "nextflow-language-server.jar",
-          },
-          filetypes = { "nextflow" },
-          root_dir = function(fname)
-            local util = require "lspconfig.util"
-            return util.root_pattern "nextflow.config"(fname) or util.find_git_ancestor(fname)
-          end,
-          settings = {
-            nextflow = {
-              suppressFutureWarnings = true,
-              files = {
-                exclude = { ".git", ".nf-test", "work" },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  {
     "echasnovski/mini.icons",
     optional = true,
     opts = {
