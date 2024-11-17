917083747a24022bac4b6feeff29ea1c5febc72f
Micah Halter <micah@mehalter.com>
2024-11-17 09:08:37 -0500

    fix(cmp-latex-symbols): use correct repository
    Fixes #1236

--- lua/astrocommunity/completion/cmp-latex-symbols/init.lua
+++ lua/astrocommunity/completion/cmp-latex-symbols/init.lua
@@ -1,7 +1,7 @@
 return {
   "hrsh7th/nvim-cmp",
   optional = true,
-  dependencies = { "hrsh7th/cmp-latex-symbols" },
+  dependencies = { "kdheepak/cmp-latex-symbols" },
   opts = function(_, opts)
     if not opts.sources then opts.sources = {} end
     table.insert(opts.sources, { name = "latex_symbols", priority = 700 })
