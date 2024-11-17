9d0bd857672decf1485c059651fe7b407022c5e3
luxus <luxuspur@gmail.com>
2024-11-17 22:54:10 +0100

    adding some of the toggleterm

--- lua/astrocommunity/utility/snacks-nvim/init.lua
+++ lua/astrocommunity/utility/snacks-nvim/init.lua
@@ -24,6 +24,7 @@ return {
   end,
   specs = {
     { "rcarriga/nvim-notify", enabled = false },
+    { "akinsho/toggleterm.nvim", enabled = false },
     { "RRethy/vim-illuminate", enabled = false },
     {
       "rebelot/heirline.nvim",
@@ -46,6 +47,7 @@ return {
       opts = function(_, opts)
         local Snacks = require "snacks"
         local maps = opts.mappings
+        local astro = require "astrocore"
 
         opts.autocmds.snacks_toggle = {
           event = "User",
@@ -75,13 +77,30 @@ return {
         maps.n["<Leader>un"] = { function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" }
         maps.n["<Leader>bd"] = { function() Snacks.bufdelete() end, desc = "Delete Buffer" }
         maps.n["<Leader>gg"] = { function() Snacks.lazygit() end, desc = "Lazygit" }
+        maps.n["<Leader>tl"] = { function() Snacks.lazygit() end, desc = "Lazygit" }
         maps.n["<Leader>gb"] = { function() Snacks.git.blame_line() end, desc = "Git Blame Line" }
+        if vim.fn.executable "node" == 1 then
+          maps.n["<Leader>tn"] = { function() Snacks.terminal "node" end, desc = "ToggleTerm node" }
+        end
+        local python = vim.fn.executable "python" == 1 and "python" or vim.fn.executable "python3" == 1 and "python3"
+        if python then
+          maps.n["<Leader>tp"] = { function() Snacks.terminal "python" end, desc = "ToggleTerm python" }
+        end
+        if vim.fn.executable "btm" == 1 then
+          maps.n["<Leader>tb"] = { function() Snacks.terminal "btm" end, desc = "ToggleTerm bottom" }
+        end
+        local gdu = vim.fn.has "mac" == 1 and "gdu-go" or "gdu"
+        if vim.fn.has "win32" == 1 and vim.fn.executable(gdu) ~= 1 then gdu = "gdu_windows_amd64.exe" end
+        if vim.fn.executable(gdu) == 1 then
+          maps.n["<Leader>tu"] = { function() Snacks.terminal { "gdu" } end, desc = "ToggleTerm gdu" }
+        end
         maps.n["<Leader>gB"] = { function() Snacks.gitbrowse() end, desc = "Git Browse" }
         maps.n["<Leader>gf"] = { function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" }
         maps.n["<Leader>gl"] = { function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" }
         maps.n["<Leader>cR"] = { function() Snacks.rename() end, desc = "Rename File" }
-        maps.n["<c-/>"] = { function() Snacks.terminal() end, desc = "Toggle Terminal" }
-        maps.n["<c-_>"] = { function() Snacks.terminal() end, desc = "which_key_ignore" }
+        maps.n["<Leader>th"] = { function() Snacks.terminal() end, desc = "Toggle Terminal" }
+        maps.n["<Leader>tf"] = { function() Snacks.terminal() end, desc = "Toggle Terminal Float" }
+        maps.n["<Leader>to"] = { function() Snacks.terminal "<Cmd>execute v:count ." end, desc = "Toggle Terminal" }
         maps.n["]r"] = { function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference" }
         maps.n["[r"] = { function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference" }
       end,
