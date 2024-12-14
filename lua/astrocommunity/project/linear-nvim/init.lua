6c2ee0ec271de2cd7f1e978ce9fb51ea06205496
Uzair Aftab <muaf@nmbu.no>
2024-12-14 15:30:20 +0100

    feat(linear-nvim): Add keymaps based upon the defaults in the plugin readme

--- lua/astrocommunity/project/linear-nvim/init.lua
+++ lua/astrocommunity/project/linear-nvim/init.lua
@@ -4,6 +4,31 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
     "stevearc/dressing.nvim",
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = opts.mappings
+        local prefix = "<Leader>m"
+
+        maps.n[prefix] = { desc = "Linear" }
+        maps.n[prefix .. "m"] = {
+          function() require("linear-nvim").show_assigned_issues() end,
+          desc = "Show assigned issues",
+        }
+        maps.v[prefix .. "c"] = {
+          function() require("linear-nvim").create_issue() end,
+          desc = "Toggle local note",
+        }
+        maps.n[prefix .. "c"] = {
+          function() require("linear-nvim").create_issue() end,
+          desc = "Toggle local note",
+        }
+        maps.n[prefix .. "s"] = {
+          function() require("linear-nvim").show_issue_details() end,
+          desc = "Toggle local note",
+        }
+      end,
+    },
   },
   opts = {},
 }
