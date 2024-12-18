296f3e935452fe13ce128724cf22d85b2077db17
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-12-18 21:43:47 +0100

    remove unused function

--- lua/astrocommunity/completion/blink-cmp/init.lua
+++ lua/astrocommunity/completion/blink-cmp/init.lua
@@ -1,8 +1,3 @@
-local function has_words_before()
-  local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-end
-
 return {
   "Saghen/blink.cmp",
   event = "InsertEnter",
