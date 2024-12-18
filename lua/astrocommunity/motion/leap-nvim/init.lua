761a1cb49b0e47b0b650f5cfe1a1f341a1ea3654
Nicholas Ciechanowski <55490546+ALameLlama@users.noreply.github.com>
2024-12-19 01:55:38 +1100

    fix(leap-nvim): Fix cursor invisible bug on nvim 0.10+ (#1293)
    fix(leap-nvim): cursor invisible on nvim 0.10+

--- lua/astrocommunity/motion/leap-nvim/init.lua
+++ lua/astrocommunity/motion/leap-nvim/init.lua
@@ -5,26 +5,28 @@ return {
     {
       "AstroNvim/astrocore",
       opts = {
-        autocmds = {
-          leap_cursor = { -- https://github.com/ggandor/leap.nvim/issues/70#issuecomment-1521177534
-            {
-              event = "User",
-              pattern = "LeapEnter",
-              callback = function()
-                vim.cmd.hi("Cursor", "blend=100")
-                vim.opt.guicursor:append { "a:Cursor/lCursor" }
-              end,
-            },
-            {
-              event = "User",
-              pattern = "LeapLeave",
-              callback = function()
-                vim.cmd.hi("Cursor", "blend=0")
-                vim.opt.guicursor:remove { "a:Cursor/lCursor" }
-              end,
-            },
-          },
-        },
+        autocmds = vim.version().minor < 10
+            and {
+              leap_cursor = { -- https://github.com/ggandor/leap.nvim/issues/70#issuecomment-1521177534
+                {
+                  event = "User",
+                  pattern = "LeapEnter",
+                  callback = function()
+                    vim.cmd.hi("Cursor", "blend=100")
+                    vim.opt.guicursor:append { "a:Cursor/lCursor" }
+                  end,
+                },
+                {
+                  event = "User",
+                  pattern = "LeapLeave",
+                  callback = function()
+                    vim.cmd.hi("Cursor", "blend=0")
+                    vim.opt.guicursor:remove { "a:Cursor/lCursor" }
+                  end,
+                },
+              },
+            }
+          or {},
         mappings = {
           n = {
             ["s"] = { "<Plug>(leap-forward)", desc = "Leap forward" },
