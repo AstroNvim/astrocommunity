5d73f2f8505fd61b295fe7011c3692899ac5cdd7
Micah Halter <micah@mehalter.com>
2024-12-20 12:15:17 -0500

    refactor(vim-workspace): refactor configuration to be user configurable

--- lua/astrocommunity/session/vim-workspace/init.lua
+++ lua/astrocommunity/session/vim-workspace/init.lua
@@ -1,30 +1,46 @@
 return {
-  { "resession.nvim", enabled = false },
-  {
-    "thaerkh/vim-workspace",
-    enabled = true,
-    priority = 10000,
-    lazy = false,
-    init = function()
-      vim.cmd [[
-      let g:workspace_autosave_ignore = ['gitcommit', "neo-tree", "nerdtree", "qf", "tagbar"]
-      let g:workspace_session_disable_on_args = 1
-      let g:workspace_session_directory = stdpath("cache") . '/vim-workspace.sessions'
-      let g:workspace_undodir= stdpath("cache") . "/vim-workspace.undodir"
-      let g:workspace_autocreate = 1
-      " nnoremap <leader>W :ToggleWorkspace<CR>
-      autocmd VimLeave *
-          \ if exists(":Neotree") | execute 'Neotree close' | endif |
-          \ if exists(":NERDTreeClose") | execute 'NERDTreeClose' | endif
-	    let g:workspace_create_new_tabs = 0
-	    " Because a bug, these two populate search / history, just disable them.
-	    let g:workspace_autosave_untrailtabs = 0
-	    let g:workspace_autosave_untrailspaces = 0
-	    let g:workspace_nocompatible = 0
-	    let g:workspace_session_disable_on_args = 1
-	    " https://github.com/thaerkh/vim-workspace/issues/11
-	    set sessionoptions-=blank
-      ]]
-    end,
+  "thaerkh/vim-workspace",
+  priority = 10000,
+  lazy = false,
+  specs = {
+    { "resession.nvim", enabled = false },
+    {
+      "AstroNvim/astrocore",
+      ---@param opts AstroCoreOpts
+      opts = function(_, opts)
+        local sessionoptions = {} -- https://github.com/thaerkh/vim-workspace/issues/11
+        for _, value in ipairs(vim.tbl_get(opts, "options", "opt", "sessionoptions") or vim.opt.sessionoptions:get()) do
+          if value ~= "blank" then table.insert(sessionoptions, value) end
+        end
+        return require("astrocore").extend_tbl(opts, {
+          autocmds = {
+            autoclose_neotree = {
+              {
+                event = "VimLeave",
+                callback = function()
+                  if vim.fn.exists ":Neotree" == 2 then vim.cmd.Neotree "close" end
+                  if vim.fn.exists ":NERDTreeClose" == 2 then vim.cmd.NERDTreeClose() end
+                end,
+              },
+            },
+          },
+          options = {
+            opt = { sessionoptions = sessionoptions },
+            g = {
+              workspace_autosave_ignore = { "gitcommit", "neo-tree", "nerdtree", "qf", "tagbar" },
+              workspace_session_disable_on_args = 1,
+              workspace_session_directory = vim.fn.stdpath "cache" .. "/vim-workspace.sessions",
+              workspace_undodir = vim.fn.stdpath "cache" .. "/vim-workspace.undodir",
+              workspace_autocreate = 1,
+              workspace_create_new_tabs = 0,
+              -- Because a bug, these two populate search / history, just disable them.
+              workspace_autosave_untrailtabs = 0,
+              workspace_autosave_untrailspaces = 0,
+              workspace_nocompatible = 0,
+            },
+          },
+        })
+      end,
+    },
   },
 }
