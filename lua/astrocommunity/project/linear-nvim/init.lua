6426600f2964f350377cc3627b868fc10286b286
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2025-01-14 16:23:19 +0100

    fix(linear-nvim)!: Change leader from `<Leader>m` to `<Leader>L` (#1314)
    * fix(linear-nvim): Change leader from `<Leader>m` to `<Leader>L` to 
    
    This makes more sense semantically
    
    * Update lua/astrocommunity/project/linear-nvim/init.lua
    
    Co-authored-by: Micah Halter <micah@mehalter.com>
    
    * Update init.lua
    
    ---------
    
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrocommunity/project/linear-nvim/init.lua
+++ lua/astrocommunity/project/linear-nvim/init.lua
@@ -8,24 +8,24 @@ return {
       "AstroNvim/astrocore",
       opts = function(_, opts)
         local maps = opts.mappings
-        local prefix = "<Leader>m"
+        local prefix = "<Leader>L"
 
         maps.n[prefix] = { desc = "Linear" }
-        maps.n[prefix .. "m"] = {
+        maps.n[prefix .. "l"] = {
           function() require("linear-nvim").show_assigned_issues() end,
           desc = "Show assigned issues",
         }
         maps.v[prefix .. "c"] = {
           function() require("linear-nvim").create_issue() end,
-          desc = "Toggle local note",
+          desc = "Create issue",
         }
         maps.n[prefix .. "c"] = {
           function() require("linear-nvim").create_issue() end,
-          desc = "Toggle local note",
+          desc = "Create issue",
         }
         maps.n[prefix .. "s"] = {
           function() require("linear-nvim").show_issue_details() end,
-          desc = "Toggle local note",
+          desc = "Show issue details",
         }
       end,
     },
