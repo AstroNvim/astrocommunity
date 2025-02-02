17307e72704b76721e5a6374aeb9920a35cd3c3d
Jörg Thalheim <Mic92@users.noreply.github.com>
2025-02-02 21:53:49 +0700

    fix(nix): drop none-ls formatter (#1324)
    * fix(nix): drop none-ls formatter
    
    nixd already provides formatting builtin:
    
    https://github.com/nix-community/nixd/blob/2c25600cb9c91bc06fe8676c044814dc30435274/nixd/docs/configuration.md?plain=1#L62
    
    <!-- ps-id: 1fa3237e-80c0-4325-a86a-cda8aa832f3a -->
    
    * docs(nix): Remove mentions of Alejandra
    
    ---------
    
    Co-authored-by: luxus <luxuspur@gmail.com>
    Co-authored-by: Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>

--- lua/astrocommunity/pack/nix/init.lua
+++ lua/astrocommunity/pack/nix/init.lua
@@ -15,7 +15,6 @@ return {
       local builtins = require("null-ls").builtins
       opts.sources = require("astrocore").list_insert_unique(opts.sources, {
         builtins.code_actions.statix,
-        builtins.formatting.alejandra,
         builtins.diagnostics.deadnix,
       })
     end,
@@ -24,15 +23,6 @@ return {
     "AstroNvim/astrolsp",
     optional = true,
     opts = function(_, opts) opts.servers = require("astrocore").list_insert_unique(opts.servers, { "nixd" }) end,
-  },
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        nix = { "alejandra" },
-      },
-    },
   },
   {
     "mfussenegger/nvim-lint",
