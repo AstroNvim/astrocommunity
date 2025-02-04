c2ff9b9a665c88492857f26cdbcb60aeda84e471
Anton Ždanov <anton@azdanov.dev>
2025-02-04 20:12:09 +0200

    fix(tailwindcss): resolve root dir based on installed dependency (#1352)
    * fix(tailwindcss): resolve root dir based on installed dependency
    
    * refactor(tailwindcss): improve readability of code
    
    ---------
    
    Co-authored-by: Micah Halter <micah.halter@gtri.gatech.edu>

--- lua/astrocommunity/pack/tailwindcss/init.lua
+++ lua/astrocommunity/pack/tailwindcss/init.lua
@@ -1,3 +1,16 @@
+local function decode_json_file(filename)
+  local file = io.open(filename, "r")
+  if file then
+    local content = file:read "*all"
+    file:close()
+
+    local ok, data = pcall(vim.fn.json_decode, content)
+    if ok and type(data) == "table" then return data end
+  end
+end
+
+local function has_nested_key(json, ...) return vim.tbl_get(json, ...) ~= nil end
+
 return {
   { import = "astrocommunity.pack.html-css" },
   {
@@ -15,7 +28,10 @@ return {
           config = {
             tailwindcss = {
               root_dir = function(fname)
-                local root_pattern = require("lspconfig").util.root_pattern(
+                local root_pattern = require("lspconfig").util.root_pattern
+
+                -- First, check for common Tailwind config files
+                local root = root_pattern(
                   "tailwind.config.mjs",
                   "tailwind.config.cjs",
                   "tailwind.config.js",
@@ -23,8 +39,24 @@ return {
                   "postcss.config.js",
                   "config/tailwind.config.js",
                   "assets/tailwind.config.js"
-                )
-                return root_pattern(fname)
+                )(fname)
+                -- If not found, check for package.json dependencies
+                if not root then
+                  local package_root = root_pattern "package.json"(fname)
+                  if package_root then
+                    local package_data = decode_json_file(root .. "/package.json")
+                    if
+                      package_data
+                      and (
+                        has_nested_key(package_data, "dependencies", "tailwindcss")
+                        or has_nested_key(package_data, "devDependencies", "tailwindcss")
+                      )
+                    then
+                      root = package_root
+                    end
+                  end
+                end
+                return root
               end,
             },
           },
