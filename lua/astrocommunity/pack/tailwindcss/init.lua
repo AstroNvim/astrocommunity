41577c612a2f2616fce9144ba66be6cc8e2748c4
Micah Halter <micah.halter@gtri.gatech.edu>
2025-02-04 13:11:22 -0500

    refactor(tailwindcss): improve readability of code

--- lua/astrocommunity/pack/tailwindcss/init.lua
+++ lua/astrocommunity/pack/tailwindcss/init.lua
@@ -1,13 +1,12 @@
 local function decode_json_file(filename)
   local file = io.open(filename, "r")
-  if not file then return nil end
+  if file then
+    local content = file:read "*all"
+    file:close()
 
-  local content = file:read "*all"
-  file:close()
-
-  local ok, data = pcall(vim.fn.json_decode, content)
-  if ok and type(data) == "table" then return data end
-  return nil
+    local ok, data = pcall(vim.fn.json_decode, content)
+    if ok and type(data) == "table" then return data end
+  end
 end
 
 local function has_nested_key(json, ...) return vim.tbl_get(json, ...) ~= nil end
@@ -29,10 +28,10 @@ return {
           config = {
             tailwindcss = {
               root_dir = function(fname)
-                local lsp_util = require("lspconfig").util
+                local root_pattern = require("lspconfig").util.root_pattern
 
                 -- First, check for common Tailwind config files
-                local root_pattern = lsp_util.root_pattern(
+                local root = root_pattern(
                   "tailwind.config.mjs",
                   "tailwind.config.cjs",
                   "tailwind.config.js",
@@ -40,28 +39,24 @@ return {
                   "postcss.config.js",
                   "config/tailwind.config.js",
                   "assets/tailwind.config.js"
-                )
-                local root = root_pattern(fname)
-                if root then return root end
-
-                -- Next, check for package.json dependencies
-                root_pattern = lsp_util.root_pattern "package.json"
-                root = root_pattern(fname)
-                if root then
-                  local package_data = decode_json_file(root .. "/package.json")
-                  if
-                    package_data
-                    and (
-                      has_nested_key(package_data, "dependencies", "tailwindcss")
-                      or has_nested_key(package_data, "devDependencies", "tailwindcss")
-                    )
-                  then
-                    return root
+                )(fname)
+                -- If not found, check for package.json dependencies
+                if not root then
+                  local package_root = root_pattern "package.json"(fname)
+                  if package_root then
+                    local package_data = decode_json_file(root .. "/package.json")
+                    if
+                      package_data
+                      and (
+                        has_nested_key(package_data, "dependencies", "tailwindcss")
+                        or has_nested_key(package_data, "devDependencies", "tailwindcss")
+                      )
+                    then
+                      root = package_root
+                    end
                   end
                 end
-
-                -- Couldn't find valid root dir
-                return nil
+                return root
               end,
             },
           },
