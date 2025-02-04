be2ef475bec84ead072827d8bf140fb90cdc9ab1
Anton Ždanov <anton@azdanov.dev>
2025-02-04 21:21:33 +0200

    fix(lazydev-nvim): add deprecation notice as it was added upstream

--- lua/astrocommunity/neovim-lua-development/lazydev-nvim/README.md
+++ lua/astrocommunity/neovim-lua-development/lazydev-nvim/README.md
@@ -7,3 +7,5 @@ Requires:
 - [Neovim v0.10+](https://github.com/neovim/neovim/releases)
 
 **Repository:** <https://github.com/folke/lazydev.nvim>
+
+Note: This plugins is part of AstroNvim core as of [v4.22.0](https://github.com/AstroNvim/AstroNvim/releases/tag/v4.22.0)
--- lua/astrocommunity/neovim-lua-development/lazydev-nvim/init.lua
+++ lua/astrocommunity/neovim-lua-development/lazydev-nvim/init.lua
@@ -2,34 +2,10 @@ return {
   "folke/lazydev.nvim",
   ft = "lua",
   cmd = "LazyDev",
-  opts = function(_, opts)
-    if not opts.library then
-      opts.library = {
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
-        { path = "astrocore", words = { "AstroCore" } },
-        { path = "astrolsp", words = { "AstroLSP" } },
-        { path = "astroui", words = { "AstroUI" } },
-        { path = "astrotheme", words = { "AstroTheme" } },
-        { path = "lazy.nvim", words = { "Lazy" } },
-      }
-    end
+  init = function()
+    require("astrocore").notify(
+      "`lazydev.nvim` has been included upstream!\nPlease remove from your community plugins.",
+      vim.log.levels.WARN
+    )
   end,
-  specs = {
-    { "folke/neodev.nvim", optional = true, enabled = false },
-    { "Bilal2453/luvit-meta", lazy = true },
-    {
-      "hrsh7th/nvim-cmp",
-      optional = true,
-      opts = function(_, opts)
-        local lazydev_inserted
-        for _, source in ipairs(opts.sources or {}) do
-          if (type(source) == "table" and source.name or source) == "lazydev" then
-            lazydev_inserted = true
-            break
-          end
-        end
-        if not lazydev_inserted then table.insert(opts.sources, { name = "lazydev", group_index = 0 }) end
-      end,
-    },
-  },
 }
