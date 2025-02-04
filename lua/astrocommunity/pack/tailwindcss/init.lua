e7d664732421a8730f19a2cb28e84abc58e09253
Anton Ždanov <anton@azdanov.dev>
2025-02-04 18:23:31 +0200

    fix(tailwindcss): resolve root dir based on installed dependency

--- lua/astrocommunity/pack/tailwindcss/init.lua
+++ lua/astrocommunity/pack/tailwindcss/init.lua
@@ -1,3 +1,17 @@
+local function decode_json_file(filename)
+  local file = io.open(filename, "r")
+  if not file then return nil end
+
+  local content = file:read "*all"
+  file:close()
+
+  local ok, data = pcall(vim.fn.json_decode, content)
+  if ok and type(data) == "table" then return data end
+  return nil
+end
+
+local function has_nested_key(json, ...) return vim.tbl_get(json, ...) ~= nil end
+
 return {
   { import = "astrocommunity.pack.html-css" },
   {
@@ -15,7 +29,10 @@ return {
           config = {
             tailwindcss = {
               root_dir = function(fname)
-                local root_pattern = require("lspconfig").util.root_pattern(
+                local lsp_util = require("lspconfig").util
+
+                -- First, check for common Tailwind config files
+                local root_pattern = lsp_util.root_pattern(
                   "tailwind.config.mjs",
                   "tailwind.config.cjs",
                   "tailwind.config.js",
@@ -24,7 +41,27 @@ return {
                   "config/tailwind.config.js",
                   "assets/tailwind.config.js"
                 )
-                return root_pattern(fname)
+                local root = root_pattern(fname)
+                if root then return root end
+
+                -- Next, check for package.json dependencies
+                root_pattern = lsp_util.root_pattern "package.json"
+                root = root_pattern(fname)
+                if root then
+                  local package_data = decode_json_file(root .. "/package.json")
+                  if
+                    package_data
+                    and (
+                      has_nested_key(package_data, "dependencies", "tailwindcss")
+                      or has_nested_key(package_data, "devDependencies", "tailwindcss")
+                    )
+                  then
+                    return root
+                  end
+                end
+
+                -- Couldn't find valid root dir
+                return nil
               end,
             },
           },
