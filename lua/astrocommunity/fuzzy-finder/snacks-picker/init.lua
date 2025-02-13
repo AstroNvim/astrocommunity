8a2a0ff47f5a4d31b542562c1822733edb8607d6
Micah Halter <micah.halter@gtri.gatech.edu>
2025-02-13 14:43:27 -0500

    fix(snacks-picker): `nvim-treesitter` shouldn't be a dependency

--- lua/astrocommunity/fuzzy-finder/snacks-picker/init.lua
+++ lua/astrocommunity/fuzzy-finder/snacks-picker/init.lua
@@ -2,7 +2,6 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
   opts = { picker = { ui_select = true } },
   specs = {
     {
