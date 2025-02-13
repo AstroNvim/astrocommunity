10f38472c6ed10815098ed9be3426bb1a7a57ad9
Micah Halter <micah.halter@gtri.gatech.edu>
2025-02-13 07:54:07 -0500

    fix(rainbow-delimiters-nvim): use new large buffer detection

--- lua/astrocommunity/editing-support/rainbow-delimiters-nvim/init.lua
+++ lua/astrocommunity/editing-support/rainbow-delimiters-nvim/init.lua
@@ -36,6 +36,11 @@ return {
     },
     event = "User AstroFile",
     main = "rainbow-delimiters.setup",
-    opts = {},
+    opts = {
+      condition = function(bufnr)
+        local buf_utils = require "astrocore.buffer"
+        return buf_utils.is_valid(bufnr) and not buf_utils.is_large(bufnr)
+      end,
+    },
   },
 }
