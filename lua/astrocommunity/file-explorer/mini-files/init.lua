08128f6ec796990555c090bf3516dd71c695f10a
Micah Halter <micah.halter@gtri.gatech.edu>
2025-02-18 08:14:12 -0500

    feat(mini-files): add AstroLSP file operation integration

--- lua/astrocommunity/file-explorer/mini-files/init.lua
+++ lua/astrocommunity/file-explorer/mini-files/init.lua
@@ -26,6 +26,40 @@ return {
       ---@type CatppuccinOptions
       opts = { integrations = { mini = true } },
     },
+    {
+      "AstroNvim/astrolsp",
+      optional = true,
+      specs = {
+        {
+          "AstroNvim/astrocore",
+          ---@type AstroCoreOpts
+          opts = {
+            autocmds = {
+              mini_files_lsp_operations = {
+                {
+                  event = "User",
+                  pattern = "MiniFilesActionCreate",
+                  desc = "execute `didCreateFiles` operation when creating files",
+                  callback = function(args) require("astrolsp.file_operations").didCreateFiles(args.data.to) end,
+                },
+                {
+                  event = "User",
+                  pattern = "MiniFilesActionDelete",
+                  desc = "execute `didDeleteFiles` operation when deleting files",
+                  callback = function(args) require("astrolsp.file_operations").didDeleteFiles(args.data.from) end,
+                },
+                {
+                  event = "User",
+                  pattern = { "MiniFilesActionMove", "MiniFilesActionRename" },
+                  desc = "execute `didRenameFiles` operation when moving or renaming files",
+                  callback = function(args) require("astrolsp.file_operations").didRenameFiles(args.data) end,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   },
   opts = {},
 }
