b721eadab35bf0c67c606e0e4b0ca435b1d57faa
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2025-03-02 23:14:25 +0100

    fix(motion): remove settings opts

--- lua/astrocommunity/motion/mini-jump/init.lua
+++ lua/astrocommunity/motion/mini-jump/init.lua
@@ -1,13 +1,5 @@
 return {
   "echasnovski/mini.jump",
   event = "User AstroFile",
-  opts = {
-    delay = {
-      -- Delay between jump and highlighting all possible jumps
-      highlight = 250,
-
-      -- Delay between jump and automatic stop if idle (no jump is done)
-      idle_stop = 1000,
-    },
-  },
+  opts = {},
 }
