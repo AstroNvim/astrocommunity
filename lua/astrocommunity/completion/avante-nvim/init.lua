88e1441f52d898907a6a072b0b66fd9c3652f1d4
Manoel Neto <64708198+Yorizel@users.noreply.github.com>
2025-03-24 12:59:20 -0300

    adding mcphub integration

--- lua/astrocommunity/completion/avante-nvim/init.lua
+++ lua/astrocommunity/completion/avante-nvim/init.lua
@@ -47,6 +47,27 @@ return {
   },
   specs = { -- configure optional plugins
     { "AstroNvim/astroui", opts = { icons = { Avante = "" } } },
+    {
+        "ravitemer/mcphub.nvim",
+        optional = true,
+        specs = {
+          {
+            "yetone/avante.nvim",
+            opts = {
+              system_prompt = function()
+                local hub = require("mcphub").get_hub_instance()
+                return hub:get_active_servers_prompt()
+              end,
+              -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+              custom_tools = function()
+                return {
+                  require("mcphub.extensions.avante").mcp_tool(),
+                }
+              end,
+            },
+          },
+        },
+      },
     { -- if copilot.lua is available, default to copilot provider
       "zbirenbaum/copilot.lua",
       optional = true,
