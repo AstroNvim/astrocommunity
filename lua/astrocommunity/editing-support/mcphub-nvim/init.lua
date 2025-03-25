361329c8649c15c3c233df9cc140df08c7a56cab
Manoel Neto <64708198+Yorizel@users.noreply.github.com>
2025-03-25 08:37:53 -0300

    Create init.lua

---
+++ lua/astrocommunity/editing-support/mcphub-nvim/init.lua
@@ -0,0 +1,48 @@
+return {
+  "ravitemer/mcphub.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
+  },
+  -- cmd = "MCPHub", -- lazily start the hub when `MCPHub` is called
+  build = "sudo npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+  config = function()
+    require("mcphub").setup {
+      -- Required options
+      port = 3000,                                              -- Port for MCP Hub server
+      config = vim.fn.expand "~/Documents/mcp/mcpservers.json", -- Absolute path to config file
+
+      -- Optional options
+      on_ready = function(hub)
+        -- Called when hub is ready
+      end,
+      on_error = function(err)
+        -- Called on errors
+      end,
+      log = {
+        level = vim.log.levels.WARN,
+        to_file = false,
+        file_path = nil,
+        prefix = "MCPHub",
+      },
+    }
+  end,
+  specs = {
+    {
+      "yetone/avante.nvim",
+      optional = true,
+      opts = {
+        system_prompt = function()
+          local hub = require("mcphub").get_hub_instance()
+          return hub:get_active_servers_prompt()
+        end,
+        -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+        custom_tools = function()
+          return {
+            require("mcphub.extensions.avante").mcp_tool(),
+          }
+        end,
+      },
+    },
+  },
+}
+
