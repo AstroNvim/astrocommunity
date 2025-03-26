cf80ce103c72c24fd03fd93e8750e498c626745b
msdone <103359349+msdone-lwt@users.noreply.github.com>
2025-03-26 11:02:17 +0800

    Update init.lua(opts syntax)

--- lua/astrocommunity/completion/avante-nvim/init.lua
+++ lua/astrocommunity/completion/avante-nvim/init.lua
@@ -81,40 +81,36 @@ return {
     {
       "nvim-neo-tree/neo-tree.nvim",
       optional = true,
-      opts = function(_, opts)
-        local ok, _ = pcall(require, "neo-tree")
-        if not ok then return opts end
-        return require("astrocore").extend_tbl(opts, {
-          filesystem = {
-            commands = {
-              avante_add_files = function(state)
-                local node = state.tree:get_node()
-                local filepath = node:get_id()
-                local relative_path = require("avante.utils").relative_path(filepath)
+      opts = {
+        filesystem = {
+          commands = {
+            avante_add_files = function(state)
+              local node = state.tree:get_node()
+              local filepath = node:get_id()
+              local relative_path = require("avante.utils").relative_path(filepath)
 
-                local sidebar = require("avante").get()
+              local sidebar = require("avante").get()
 
-                local open = sidebar:is_open()
-                -- ensure avante sidebar is open
-                if not open then
-                  require("avante.api").ask()
-                  sidebar = require("avante").get()
-                end
+              local open = sidebar:is_open()
+              -- ensure avante sidebar is open
+              if not open then
+                require("avante.api").ask()
+                sidebar = require("avante").get()
+              end
 
-                sidebar.file_selector:add_selected_file(relative_path)
+              sidebar.file_selector:add_selected_file(relative_path)
 
-                -- remove neo tree buffer
-                if not open then sidebar.file_selector:remove_selected_file "neo-tree filesystem [1]" end
-              end,
-            },
-            window = {
-              mappings = {
-                ["oa"] = "avante_add_files",
-              },
-            },
+              -- remove neo tree buffer
+              if not open then sidebar.file_selector:remove_selected_file "neo-tree filesystem [1]" end
+            end,
           },
-        })
-      end,
+        },
+        window = {
+          mappings = {
+            ["oa"] = "avante_add_files",
+          },
+        },
+      },
     },
   },
 }
