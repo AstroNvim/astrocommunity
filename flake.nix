e20980c304b0bfecf3e404ff2fe90d2dd1a2b793
Uzair Aftab <uzaaft@outlook.com>
2025-03-29 20:36:24 +0100

    feat: add in flake for development, and ignore direnv folder

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "docs.astronvim.org";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {packages = with pkgs; [stylua lua-language-server selene];};
+      }
+    );
+}
