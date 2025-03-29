20b0601fd8499bbc635e238d8388d9fcd1af1537
Uzair Aftab <uzaaft@outlook.com>
2025-03-29 22:14:26 +0100

    fix(nvim-tinygit): Doesnt need dressing.nvim longer, and requires telescope.nvim

--- lua/astrocommunity/git/nvim-tinygit/init.lua
+++ lua/astrocommunity/git/nvim-tinygit/init.lua
@@ -2,7 +2,7 @@ return {
   "chrisgrieser/nvim-tinygit",
   ft = { "git_rebase", "gitcommit" }, -- so ftplugins are loaded
   dependencies = {
-    "stevearc/dressing.nvim",
+    "nvim-telescope/telescope.nvim",
     {
       "AstroNvim/astrocore",
       opts = {
