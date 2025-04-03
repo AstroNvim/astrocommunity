7c4d047f39df4c2d843500f461e3bec9cd8571e5
Ray Jameson <67468725+RayJameson@users.noreply.github.com>
2025-04-03 18:44:25 +0500

    fix(nvim-dap-repl-highlights): nvim-dap in dependencies breaks lazy loading (#1417)

--- lua/astrocommunity/debugging/nvim-dap-repl-highlights/init.lua
+++ lua/astrocommunity/debugging/nvim-dap-repl-highlights/init.lua
@@ -4,11 +4,7 @@ return {
   specs = {
     {
       "nvim-treesitter/nvim-treesitter",
-      dependencies = {
-        "LiadOz/nvim-dap-repl-highlights",
-        dependencies = { "mfussenegger/nvim-dap" },
-        opts = {},
-      },
+      dependencies = { "LiadOz/nvim-dap-repl-highlights", opts = {} },
       opts = function(_, opts)
         if opts.ensure_installed ~= "all" then
           opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "dap_repl" })
