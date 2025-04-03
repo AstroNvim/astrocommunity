a080b7d53b1a5237b043185682b41463e9dc119f
Manoel Neto <64708198+Yorizel@users.noreply.github.com>
2025-04-03 10:47:19 +0000

    Update init.lua

--- lua/astrocommunity/completion/avante-nvim/init.lua
+++ lua/astrocommunity/completion/avante-nvim/init.lua
@@ -53,25 +53,22 @@ return {
     { "AstroNvim/astroui", opts = { icons = { Avante = "" } } },
     {
         "saghen/blink.cmp",
-      optional = true,
         dependencies = {
           "Kaiser-Yang/blink-cmp-avante",
         },
-        opts = {
-          sources = {
-            -- Add 'avante' to the list
-            default = { "avante", "lsp", "path", "buffer" },
+        opts = function(_, opts)
+          opts.sources = vim.tbl_deep_extend("keep", opts.sources or {}, {
+            default = {},
             providers = {
               avante = {
+
                 module = "blink-cmp-avante",
                 name = "Avante",
-                opts = {
-                  -- options for blink-cmp-avante
-                },
               },
             },
-          },
-        },
+          })
+          table.insert(opts.sources.default, "avante")
+        end,
       },
     { -- if copilot.lua is available, default to copilot provider
       "zbirenbaum/copilot.lua",
