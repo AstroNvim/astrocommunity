9c36ff34bd83c34992f8599c7643a22610ad1be5
Micah Halter <micah@mehalter.com>
2025-04-04 09:16:48 -0400

    Update lua/astrocommunity/recipes/heirline-tabline-buffer-number/init.lua

--- lua/astrocommunity/recipes/heirline-tabline-buffer-number/init.lua
+++ lua/astrocommunity/recipes/heirline-tabline-buffer-number/init.lua
@@ -1,16 +1,24 @@
 return {
   "heirline.nvim",
+  specs = {
+    {
+      "AstroNvim/astroui",
+      ---@type AstroUIOpts
+      opts = {
+        status = {
+          attributes = {
+            bufnr = { bold = true, underline = true },
+          },
+        },
+      },
+    },
+  },
   opts = function(_, opts)
     -- overwrite https://github.com/AstroNvim/AstroNvim/blob/main/lua/astronvim/plugins/heirline.lua#L114
     local status = require "astroui.status"
-    local function my_tabline_file_info()
-      local tmp = status.component.tabline_file_info()
-      table.insert(tmp, 2, {
-        provider = function(self) return self and self.bufnr and self.bufnr or "" end,
-        hl = { bold = true, underline = true },
-      })
-      return tmp
-    end
-    opts.tabline[2] = status.heirline.make_buflist(my_tabline_file_info())
+
+    opts.tabline[2] = status.heirline.make_buflist(
+      status.component.tabline_file_info { bufnr = { hl = status.hl.get_attributes "bufnr" } }
+    )
   end,
 }
