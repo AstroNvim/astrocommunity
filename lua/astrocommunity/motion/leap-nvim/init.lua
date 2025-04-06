89c28df7d54f91db2241feac112010ef4bcd5560
Nicholas Ciechanowski <NicholasACiechanowski@gmail.com>
2025-04-06 19:05:29 +1000

    fix(leap-nvim): Fix cursor invisible bug on nvim 0.10+

--- lua/astrocommunity/motion/leap-nvim/init.lua
+++ lua/astrocommunity/motion/leap-nvim/init.lua
@@ -5,28 +5,6 @@ return {
     {
       "AstroNvim/astrocore",
       opts = {
-        autocmds = vim.fn.has "nvim-0.10" == 0
-            and {
-              leap_cursor = { -- https://github.com/ggandor/leap.nvim/issues/70#issuecomment-1521177534
-                {
-                  event = "User",
-                  pattern = "LeapEnter",
-                  callback = function()
-                    vim.cmd.hi("Cursor", "blend=100")
-                    vim.opt.guicursor:append { "a:Cursor/lCursor" }
-                  end,
-                },
-                {
-                  event = "User",
-                  pattern = "LeapLeave",
-                  callback = function()
-                    vim.cmd.hi("Cursor", "blend=0")
-                    vim.opt.guicursor:remove { "a:Cursor/lCursor" }
-                  end,
-                },
-              },
-            }
-          or {},
         mappings = {
           n = {
             ["s"] = { "<Plug>(leap-forward)", desc = "Leap forward" },
