c9148bf971be6c284cb07bfc2002c020dacd02a3
Anton Ždanov <anton@azdanov.dev>
2025-04-12 07:21:29 +0300

    feat(pack): add golangci-lint

---
+++ lua/astrocommunity/pack/golangci-lint/golangci-lint.lua
@@ -0,0 +1,33 @@
+---@type LazySpec
+return {
+  {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      ---@diagnostic disable: missing-fields
+      config = {
+        -- Overrides the default config to support golangci-lint v2
+        golangci_lint_ls = {
+          init_options = {
+            command = {
+              "golangci-lint",
+              "run",
+              "--output.json.path",
+              "stdout",
+              "--show-stats=false",
+              "--issues-exit-code=1",
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "golangci-lint-langserver" })
+    end,
+  },
+}
