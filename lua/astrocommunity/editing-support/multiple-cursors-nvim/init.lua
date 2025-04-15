c32ae3f29ad7036ff565d879fb1985a61d5eb1cb
Krivoshapkin Eduard <90126102+ButterSus@users.noreply.github.com>
2025-04-15 14:37:00 +0300

    feat(multiple-cursors-nvim): Add icon for mappings prefix (#1444)

--- lua/astrocommunity/editing-support/multiple-cursors-nvim/init.lua
+++ lua/astrocommunity/editing-support/multiple-cursors-nvim/init.lua
@@ -11,29 +11,36 @@ return {
     "MultipleCursorsLock",
   },
   dependencies = {
-    "AstroNvim/astrocore",
-    opts = function(_, opts)
-      local maps = opts.mappings
-      for lhs, map in pairs {
-        ["<C-Down>"] = { "<Cmd>MultipleCursorsAddDown<CR>", desc = "Add cursor down" },
-        ["<C-Up>"] = { "<Cmd>MultipleCursorsAddUp<CR>", desc = "Add cursor up" },
-        ["<C-LeftMouse>"] = { "<Cmd>MultipleCursorsMouseAddDelete<CR>", desc = "Add cursor with mouse" },
-      } do
-        maps.n[lhs] = map
-        maps.i[lhs] = map
-      end
-      local prefix = "<Leader>m"
-      for lhs, map in pairs {
-        [prefix .. "a"] = { "<Cmd>MultipleCursorsAddMatches<CR>", desc = "Add cursor matches" },
-        [prefix .. "A"] = { "<Cmd>MultipleCursorsAddMatchesV<CR>", desc = "Add cursor matches in previous visual area" },
-        [prefix .. "j"] = { "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", desc = "Add cursor and jump to next match" },
-        [prefix .. "J"] = { "<Cmd>MultipleCursorsJumpNextMatch<CR>", desc = "Move cursor to next match" },
-        [prefix .. "l"] = { "<Cmd>MultipleCursorsLock<CR>", desc = "Lock virtual cursors" },
-      } do
-        maps.n[lhs] = map
-        maps.x[lhs] = map
-      end
-    end,
+    { "AstroNvim/astroui", opts = { icons = { MultipleCursors = "󰗧" } } },
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = opts.mappings
+        for lhs, map in pairs {
+          ["<C-Down>"] = { "<Cmd>MultipleCursorsAddDown<CR>", desc = "Add cursor down" },
+          ["<C-Up>"] = { "<Cmd>MultipleCursorsAddUp<CR>", desc = "Add cursor up" },
+          ["<C-LeftMouse>"] = { "<Cmd>MultipleCursorsMouseAddDelete<CR>", desc = "Add cursor with mouse" },
+        } do
+          maps.n[lhs] = map
+          maps.i[lhs] = map
+        end
+        local prefix = "<Leader>m"
+        for lhs, map in pairs {
+          [prefix] = { desc = require("astroui").get_icon("MultipleCursors", 1, true) .. "MultipleCursors" },
+          [prefix .. "a"] = { "<Cmd>MultipleCursorsAddMatches<CR>", desc = "Add cursor matches" },
+          [prefix .. "A"] = {
+            "<Cmd>MultipleCursorsAddMatchesV<CR>",
+            desc = "Add cursor matches in previous visual area",
+          },
+          [prefix .. "j"] = { "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", desc = "Add cursor and jump to next match" },
+          [prefix .. "J"] = { "<Cmd>MultipleCursorsJumpNextMatch<CR>", desc = "Move cursor to next match" },
+          [prefix .. "l"] = { "<Cmd>MultipleCursorsLock<CR>", desc = "Lock virtual cursors" },
+        } do
+          maps.n[lhs] = map
+          maps.x[lhs] = map
+        end
+      end,
+    },
   },
   opts = {},
 }
