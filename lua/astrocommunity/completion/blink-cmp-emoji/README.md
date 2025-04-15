85ea256b24eac7de0b555559c65ec626d9495e3b
Nicholas Ciechanowski <nicholasaciechanowski@gmail.com>
2025-04-15 22:07:32 +1000

    feat(completion): Add blink-cmp-emoji

---
+++ lua/astrocommunity/completion/blink-cmp-emoji/README.md
@@ -0,0 +1,5 @@
+# Emoji source for blink.cmp completion plugin.
+
+This enables emoji completion in the blink.cmp plugin using `:`
+
+**Repository:** <https://github.com/moyiz/blink-emoji.nvim>
---
+++ lua/astrocommunity/completion/blink-cmp-emoji/init.lua
@@ -0,0 +1,18 @@
+return {
+  "moyiz/blink-emoji.nvim",
+  lazy = true,
+  specs = {
+    {
+      "Saghen/blink.cmp",
+      optional = true,
+      opts = {
+        sources = {
+          default = { "emoji" },
+          providers = {
+            emoji = { name = "emoji", module = "blink-emoji" },
+          },
+        },
+      },
+    },
+  },
+}
