1dfc91088ea9897f915e6ee34f5bde1a0f9d10ca
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2025-04-16 10:03:18 +0200

    Update init.lua

--- lua/astrocommunity/note-taking/neorg/init.lua
+++ lua/astrocommunity/note-taking/neorg/init.lua
@@ -3,32 +3,27 @@ return {
   version = "^9",
   event = "VeryLazy",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  opts = function(_, opts)
-    local astrocore = require "astrocore"
-    return astrocore.extend_tbl(opts, {
-      load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.keybinds"] = {}, -- Adds default keybindings
-        ["core.completion"] = {
-          config = {
-            engine = (astrocore.is_available "nvim-cmp" and "nvim-cmp")
-              or (astrocore.is_available "coq_nvim" and "coq_nvim")
-              or (astrocore.is_available "nvim-compe" and "nvim-compe"),
+  opts = {
+    load = {
+      ["core.defaults"] = {}, -- Loads default behaviour
+      ["core.concealer"] = {}, -- Adds pretty icons to your documents
+      ["core.keybinds"] = {}, -- Adds default keybindings
+      ["core.completion"] = {
+        config = {
+          engine = "nvim-cmp",
+        },
+      }, -- Enables support for completion plugins
+      ["core.journal"] = {}, -- Enables support for the journal module
+      ["core.dirman"] = { -- Manages Neorg workspaces
+        config = {
+          workspaces = {
+            notes = "~/projects/notes",
           },
-        }, -- Enables support for completion plugins
-        ["core.journal"] = {}, -- Enables support for the journal module
-        ["core.dirman"] = { -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              notes = "~/projects/notes",
-            },
-            default_workspace = "notes",
-          },
+          default_workspace = "notes",
         },
       },
-    })
-  end,
+    },
+  },
   specs = {
     {
       "nvim-treesitter/nvim-treesitter",
