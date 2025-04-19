3fc2a3f8d8a7a09425a2e34a62069cf3f253d6b7
Cretezy <Cretezy@users.noreply.github.com>
2025-04-18 21:34:29 -0400

    feat(godot): Add .uid/.import file handling and godot-server.nvim (#1450)
    * feat(godot): Add .uid file handling and godot-server
    
    * fix(godot): use neo-tree nested files and clean up event handlers
    
    * chore(godot): improve notification formatting
    
    * refactor(godot): `neo-tree` events can share an id for easier unsubscribe
    
    ---------
    
    Co-authored-by: Micah Halter <micah.halter@gtri.gatech.edu>

--- lua/astrocommunity/pack/godot/init.lua
+++ lua/astrocommunity/pack/godot/init.lua
@@ -1,3 +1,19 @@
+local function gd_move_handler(source, destination)
+  if not vim.fs.root(source, "project.godot") then return end -- only run in godot projects
+  local suffix = source:match "%.gd$" and "uid" or "import"
+  local tmp_file = ("%s.%s"):format(source, suffix)
+  local tmp_stat = vim.uv.fs_stat(tmp_file)
+
+  if tmp_stat then
+    -- Rename the temporary file
+    local new_tmp_file = ("%s.%s"):format(destination, suffix)
+    local success, err = vim.uv.fs_rename(tmp_file, new_tmp_file)
+    if not success then
+      vim.notify(("Failed to rename Godot file: `%s`\n```\n%s\n```"):format(tmp_file, err or "unknown error"))
+    end
+  end
+end
+
 return {
   {
     "AstroNvim/astrocore",
@@ -72,5 +88,40 @@ return {
           require("astrocore").list_insert_unique(opts.ensure_installed, { "gdscript", "glsl", "godot_resource" })
       end
     end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    optional = true,
+    opts = {
+      event_handlers = {
+        {
+          event = "file_renamed",
+          handler = function(args) gd_move_handler(args.source, args.destination) end,
+          id = "godot_handler",
+        },
+        {
+          event = "file_moved",
+          handler = function(args) gd_move_handler(args.source, args.destination) end,
+          id = "godot_handler",
+        },
+      },
+      nesting_rules = {
+        godot_import = {
+          pattern = "^(.*)$",
+          files = { -- pretty loose, but is probably a specific enough pattern to be alright
+            "%1.import",
+          },
+        },
+        godot_uid = {
+          pattern = "^(.*%.gd)$",
+          files = {
+            "%1.uid",
+          },
+        },
+      },
+    },
+  },
+  {
+    "Cretezy/godot-server.nvim",
   },
 }
