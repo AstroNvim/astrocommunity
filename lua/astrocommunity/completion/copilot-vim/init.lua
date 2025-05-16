bcceb23eeb59f4d430e390ffd16bcf9204aee52c
Anton Ždanov <anton@azdanov.dev>
2025-05-16 07:37:28 +0300

    feat(copilot-vim): add catppuccin integration

--- lua/astrocommunity/completion/copilot-vim/init.lua
+++ lua/astrocommunity/completion/copilot-vim/init.lua
@@ -1,5 +1,15 @@
+---@type LazySpec
 return {
   "github/copilot.vim",
   cmd = "Copilot",
   event = "User AstroFile",
+  specs = {
+    {
+      "catppuccin",
+      optional = true,
+      ---@module 'catppuccin'
+      ---@type CatppuccinOptions
+      opts = { integrations = { copilot_vim = true } },
+    },
+  },
 }
