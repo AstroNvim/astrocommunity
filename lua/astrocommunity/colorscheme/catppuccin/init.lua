88d27a9e53253c4a9a752e07bd96fdc40d9b693d
omagarwal25 <lidagarwal@gmail.com>
2025-05-20 23:42:42 +0100

    removed commented out code

--- lua/astrocommunity/colorscheme/catppuccin/init.lua
+++ lua/astrocommunity/colorscheme/catppuccin/init.lua
@@ -34,25 +34,4 @@ return {
       },
     },
   },
-  specs = {
-    {
-      "akinsho/bufferline.nvim",
-      optional = true,
-      opts = function(_, opts)
-        return require("astrocore").extend_tbl(opts, {
-          highlights = require("catppuccin.groups.integrations.bufferline").get(),
-        })
-      end,
-    },
-    {
-      "nvim-telescope/telescope.nvim",
-      optional = true,
-      opts = {
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-      },
-    },
-  },
 }
