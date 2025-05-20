2d388dfad7da35343c7b436dd63f4cb3d3652f76
Anton Ždanov <anton@azdanov.dev>
2025-05-20 06:58:53 +0300

    feat(pack): add biome

---
+++ lua/astrocommunity/pack/biome/README.md
@@ -0,0 +1,16 @@
+# Biome
+
+[Biome](https://biomejs.dev) is a fast toolchain for web projects, providing formatting and linting via CLI and LSP.
+
+## Features
+
+- **Formatter**
+
+  - Supports JavaScript, TypeScript, JSX, TSX, JSON, CSS, and GraphQL
+  - 97% compatibility with Prettier
+  - Can format malformed code as you write
+
+- **Linter**
+  - Covers JavaScript, TypeScript, JSX, CSS, and GraphQL
+  - Includes 293 rules from ESLint, TypeScript ESLint, and more
+  - Outputs detailed, contextual diagnostics to help improve your code
---
+++ lua/astrocommunity/pack/biome/init.lua
@@ -0,0 +1,58 @@
+-- https://biomejs.dev/internals/language-support/
+local supported_ft = {
+  "astro",
+  "css",
+  "graphql",
+  -- "html",
+  "javascript",
+  "javascriptreact",
+  "json",
+  "jsonc",
+  -- "markdown",
+  "svelte",
+  "typescript",
+  "typescriptreact",
+  "vue",
+  -- "yaml",
+}
+
+---@type LazySpec
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "biome" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "biome" })
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "biome" })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = function(_, opts)
+      if not opts.formatters_by_ft then opts.formatters_by_ft = {} end
+
+      for _, ft in ipairs(supported_ft) do
+        opts.formatters_by_ft[ft] = { "biome" }
+      end
+
+      opts.formatters = opts.formatters or {}
+      opts.formatters.biome = {
+        -- Only use the `biome` formatter if biome config is present
+        require_cwd = true,
+      }
+    end,
+  },
+}
