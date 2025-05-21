c0517361f1d8f231880234f4377020aa2e2877c9
kevinCDW <123593272+kevinCDW@users.noreply.github.com>
2025-05-21 16:41:21 -0400

    chore: update based on stylua feedback

--- lua/astrocommunity/indent/indent-rainbowline/init.lua
+++ lua/astrocommunity/indent/indent-rainbowline/init.lua
@@ -1,6 +1,6 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-  main = 'ibl',
+  main = "ibl",
   opts = function(_, opts)
     return require("indent-rainbowline").make_opts(opts, require("astrocore").plugin_opts "indent-rainbowline.nvim")
   end,
