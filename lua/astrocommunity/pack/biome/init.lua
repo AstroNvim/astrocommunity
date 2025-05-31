32d74f7536b5aea53d65586a3bbec4d26dee9cbe
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2025-05-31 12:41:17 +0200

    refactor: move filetype table to the opts function that uses it

--- lua/astrocommunity/pack/biome/init.lua
+++ lua/astrocommunity/pack/biome/init.lua
@@ -1,20 +1,4 @@
--- https://biomejs.dev/internals/language-support/
-local supported_ft = {
-  "astro",
-  "css",
-  "graphql",
-  -- "html",
-  "javascript",
-  "javascriptreact",
-  "json",
-  "jsonc",
-  -- "markdown",
-  "svelte",
-  "typescript",
-  "typescriptreact",
-  "vue",
-  -- "yaml",
-}
+
 
 ---@type LazySpec
 return {
@@ -43,7 +27,23 @@ return {
     optional = true,
     opts = function(_, opts)
       if not opts.formatters_by_ft then opts.formatters_by_ft = {} end
-
+      -- https://biomejs.dev/internals/language-support/
+      local supported_ft = {
+        "astro",
+        "css",
+        "graphql",
+        -- "html",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "jsonc",
+        -- "markdown",
+        "svelte",
+        "typescript",
+        "typescriptreact",
+        "vue",
+        -- "yaml",
+      }
       for _, ft in ipairs(supported_ft) do
         opts.formatters_by_ft[ft] = { "biome" }
       end
