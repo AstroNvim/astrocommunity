2f3113fb161d8d5cee1cb6f01f08cc661478acfc
Manoel Neto <64708198+Yorizel@users.noreply.github.com>
2025-06-20 17:04:40 -0300

    Create init.lua

---
+++ lua/astrocommunity/pack/oxlint/init.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "oxlint" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "oxlint" })
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "oxlint" })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = function(_, opts)
+      if not opts.formatters_by_ft then opts.formatters_by_ft = {} end
+      -- https://oxc.rs/docs/guide/usage/linter.html
+      local supported_ft = {
+        "astro",
+        "css",
+        "graphql",
+        -- "html",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "jsonc",
+        -- "markdown",
+        "svelte",
+        "typescript",
+        "typescriptreact",
+        "vue",
+        -- "yaml",
+      }
+      for _, ft in ipairs(supported_ft) do
+        opts.formatters_by_ft[ft] = { "oxlint" }
+      end
+    end,
+  },
+}
