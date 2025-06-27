bca9af8d787a3687c2ed5ab874cdea19dcebcb48
Manoel Neto <64708198+Yorizel@users.noreply.github.com>
2025-06-27 20:15:07 -0300

    Create init.lua

---
+++ lua/astrocommunity/editing-support/codecompanion-nvim/init.lua
@@ -0,0 +1,71 @@
+local prefix = "<Leader>A"
+
+---@type LazySpec
+return {
+  "olimorris/codecompanion.nvim",
+  event = "User AstroFile",
+  cmd = {
+    "CodeCompanion",
+    "CodeCompanionActions",
+    "CodeCompanionChat",
+    "CodeCompanionCmd",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "ravitemer/mcphub.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        if not opts.mappings then opts.mappings = {} end
+        opts.mappings.n[prefix] = { desc = require("astroui").get_icon("CodeCompanion", 1, true) .. "CodeCompanion" }
+        opts.mappings.v[prefix] = { desc = require("astroui").get_icon("CodeCompanion", 1, true) .. "CodeCompanion" }
+        opts.mappings.n[prefix .. "c"] = { "<cmd>CodeCompanionChat Toggle<cr>", desc = "Toggle chat" }
+        opts.mappings.v[prefix .. "c"] = { "<cmd>CodeCompanionChat Toggle<cr>", desc = "Toggle chat" }
+        opts.mappings.n[prefix .. "p"] = { "<cmd>CodeCompanionActions<cr>", desc = "Open action palette" }
+        opts.mappings.v[prefix .. "p"] = { "<cmd>CodeCompanionActions<cr>", desc = "Open action palette" }
+        opts.mappings.n[prefix .. "q"] = { "<cmd>CodeCompanion<cr>", desc = "Open inline assistant" }
+        opts.mappings.v[prefix .. "q"] = { "<cmd>CodeCompanion<cr>", desc = "Open inline assistant" }
+        opts.mappings.v[prefix .. "a"] = { "<cmd>CodeCompanionChat Add<cr>", desc = "Add selection to chat" }
+        vim.cmd [[cab cc CodeCompanion]]
+      end,
+    },
+  },
+  opts = {},
+  specs = {
+    { "AstroNvim/astroui", opts = { icons = { CodeCompanion = "󱙺" } } },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if not opts.file_types then opts.file_types = { "markdown" } end
+        opts.file_types = require("astrocore").list_insert_unique(opts.file_types, { "codecompanion" })
+      end,
+    },
+    {
+      "OXY2DEV/markview.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if not opts.preview then opts.preview = {} end
+        if not opts.preview.filetypes then opts.preview.filetypes = { "markdown", "quarto", "rmd" } end
+        opts.preview.filetypes = require("astrocore").list_insert_unique(opts.preview.filetypes, { "codecompanion" })
+      end,
+    },
+    {
+      "folke/snacks.nvim",
+      optional = true,
+      specs = {
+        {
+          "olimorris/codecompanion.nvim",
+          opts = {
+            display = {
+              action_palette = {
+                provider = "snacks",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
