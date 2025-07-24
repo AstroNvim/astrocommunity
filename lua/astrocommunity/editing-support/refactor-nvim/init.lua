7fdb54ee5a1b133875d19ddc2d41e9d39ae6bac9
ShifatHasanGNS <shifathasangns@gmail.com>
2025-07-25 02:38:29 +0600

    fixed formatting of refactor.nvim/init.lua

--- lua/astrocommunity/editing-support/refactor-nvim/init.lua
+++ lua/astrocommunity/editing-support/refactor-nvim/init.lua
@@ -1,8 +1,8 @@
 return {
-	"ShifatHasanGNS/refactor.nvim",
-	config = function()
-		require("refactor").setup({
-			default_flags = "", -- Case-insensitive, Partial-match, Literal-text, Normal-case
-		})
-	end,
+  "ShifatHasanGNS/refactor.nvim",
+  config = function()
+    require("refactor").setup({
+      default_flags = "", -- Case-insensitive, Partial-match, Literal-text, Normal-case
+    })
+  end,
 }
