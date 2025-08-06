8e359d7a2ce1ae0222f5b27f650d7cb897b2df5d
Tomas Slusny <slusnucky@gmail.com>
2025-08-06 08:40:08 +0200

    feat(copilotchat-nvim): update to v4 and add mcphub.nvim integration (#1599)
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- lua/astrocommunity/editing-support/mcphub-nvim/init.lua
+++ lua/astrocommunity/editing-support/mcphub-nvim/init.lua
@@ -40,4 +40,18 @@ return {
       },
     },
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    optional = true,
+    opts = {
+      extensions = {
+        copilotchat = {
+          enabled = true,
+          convert_tools_to_functions = true,
+          convert_resources_to_functions = true,
+          add_mcp_prefix = false,
+        },
+      },
+    },
+  },
 }
