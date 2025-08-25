de3cff09e8b64afb886366e8f7119ccf226d733d
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2025-08-25 14:39:12 +0200

    feat(html-css): Remove prettierd & prettier (#1627)

--- lua/astrocommunity/pack/html-css/init.lua
+++ lua/astrocommunity/pack/html-css/init.lua
@@ -38,34 +38,12 @@ return {
     },
   },
   {
-    "jay-babu/mason-null-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "prettierd" })
-    end,
-  },
-  {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(
-        opts.ensure_installed,
-        { "html-lsp", "css-lsp", "emmet-ls", "prettierd" }
-      )
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "html-lsp", "css-lsp", "emmet-ls" })
     end,
-  },
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        html = { "prettierd", "prettier", stop_after_first = true },
-        css = { "prettierd", "prettier", stop_after_first = true },
-        scss = { "prettierd", "prettier", stop_after_first = true },
-        less = { "prettierd", "prettier", stop_after_first = true },
-        postcss = { "prettierd", "prettier", stop_after_first = true },
-      },
-    },
   },
   {
     "echasnovski/mini.icons",
