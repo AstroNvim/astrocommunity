c29c34768e8b36399618ee019765907ad243c355
Uzair Aftab <uzaaft@outlook.com>
2025-08-25 10:01:36 +0200

    chore: remove nvim-dap

--- lua/astrocommunity/pack/typescript/init.lua
+++ lua/astrocommunity/pack/typescript/init.lua
@@ -71,7 +71,7 @@ return {
     "jay-babu/mason-nvim-dap.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed or {}, { "js" }) -- 'js' for js-debug-adapter
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed or {}, { "js" })
     end,
   },
   {
@@ -137,58 +137,6 @@ return {
     opts = function(_, opts)
       if not opts.adapters then opts.adapters = {} end
       table.insert(opts.adapters, require "neotest-jest"(require("astrocore").plugin_opts "neotest-jest"))
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap",
-    optional = true,
-    opts = function()
-      local dap = require "dap"
-      if not dap.adapters["pwa-node"] then
-        dap.adapters["pwa-node"] = {
-          type = "server",
-          host = "localhost",
-          port = "${port}",
-          executable = { command = vim.fn.exepath "js-debug-adapter", args = { "${port}" } },
-        }
-      end
-      if not dap.adapters.node then
-        dap.adapters.node = function(cb, config)
-          if config.type == "node" then config.type = "pwa-node" end
-          local pwa_adapter = dap.adapters["pwa-node"]
-          if type(pwa_adapter) == "function" then
-            pwa_adapter(cb, config)
-          else
-            cb(pwa_adapter)
-          end
-        end
-      end
-
-      local js_filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" }
-      local js_config = {
-        {
-          type = "pwa-node",
-          request = "launch",
-          name = "Launch file",
-          program = "${file}",
-          cwd = "${workspaceFolder}",
-        },
-        {
-          type = "pwa-node",
-          request = "attach",
-          name = "Attach",
-          processId = require("dap.utils").pick_process,
-          cwd = "${workspaceFolder}",
-        },
-      }
-
-      for _, language in ipairs(js_filetypes) do
-        if not dap.configurations[language] then dap.configurations[language] = js_config end
-      end
-
-      local vscode_filetypes = require("dap.ext.vscode").type_to_filetypes
-      vscode_filetypes["node"] = js_filetypes
-      vscode_filetypes["pwa-node"] = js_filetypes
     end,
   },
 }
