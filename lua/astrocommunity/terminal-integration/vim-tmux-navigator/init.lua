c564d49301635a623b461065f6897a6c4a0029f3
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2025-08-29 22:50:40 +0700

    fix(vim-tmux-navigator): make it work

--- lua/astrocommunity/terminal-integration/vim-tmux-navigator/init.lua
+++ lua/astrocommunity/terminal-integration/vim-tmux-navigator/init.lua
@@ -1,5 +1,18 @@
----@type LazySpec
 return {
   "christoomey/vim-tmux-navigator",
-  lazy = false,
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
 }
