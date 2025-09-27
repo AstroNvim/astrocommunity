729924b747df24207a5cc2729f8829b5a50a181a
Uzair Aftab <git@uzaaft.me>
2025-09-27 22:48:39 +0200

    refactor(diff-keybindings): Refactor diffkeybindings to use astroui, and fix visual bug

--- lua/astrocommunity/pack/diff-keybindings/init.lua
+++ lua/astrocommunity/pack/diff-keybindings/init.lua
@@ -1,23 +1,22 @@
-local menu = "<Leader>D"
 return {
   {
     "AstroNvim/astrocore",
     ---@type AstroCoreOpts
-    opts = {
-      mappings = {
-        n = {
-          [menu] = { desc = "  Diff" },
-          [menu .. "v"] = { "<cmd>vert diffsplit<cr>", desc = "  Vertical split diff" },
-          [menu .. "h"] = { "<cmd>diffsplit<cr>", desc = "  Horizontal split diff" },
-          [menu .. "t"] = { "<cmd>diffthis<cr>", desc = "  Diff this" },
-          [menu .. "p"] = { "<cmd>diffpatch<cr>", desc = "  Patch diff" },
-          [menu .. "u"] = { "<cmd>diffupdate<cr>", desc = "󰚰  Update diff" },
-          [menu .. "g"] = { "<cmd>diffget<cr>", desc = "  Get diff" },
-          [menu .. "P"] = { "<cmd>diffput<cr>", desc = "  Put diff" },
-          [menu .. "q"] = { "<cmd>diffoff<cr>", desc = "󰘪  Stop diff" },
-          [menu .. "?"] = { "<cmd>h diff<cr>", desc = "󰋗  Help" },
-        },
-      },
-    },
+    opts = function(_, opts)
+      local mappings = assert(opts.mappings)
+
+      local prefix = "<Leader>D"
+      mappings.n[prefix] = { desc = require("astroui").get_icon("Diff", 1, true) .. "Diff" }
+      mappings.n[prefix .. "v"] = { "<cmd>vert diffsplit<cr>", desc = "  Vertical split diff" }
+      mappings.n[prefix .. "h"] = { "<cmd>diffsplit<cr>", desc = "  Horizontal split diff" }
+      mappings.n[prefix .. "t"] = { "<cmd>diffthis<cr>", desc = "  Diff this" }
+      mappings.n[prefix .. "p"] = { "<cmd>diffpatch<cr>", desc = "  Patch diff" }
+      mappings.n[prefix .. "u"] = { "<cmd>diffupdate<cr>", desc = "󰚰  Update diff" }
+      mappings.n[prefix .. "g"] = { "<cmd>diffget<cr>", desc = "  Get diff" }
+      mappings.n[prefix .. "P"] = { "<cmd>diffput<cr>", desc = "  Put diff" }
+      mappings.n[prefix .. "q"] = { "<cmd>diffoff<cr>", desc = "󰘪  Stop diff" }
+      mappings.n[prefix .. "?"] = { "<cmd>h diff<cr>", desc = "󰋗  Help" }
+    end,
   },
+  { "AstroNvim/astroui", opts = { icons = { Diff = "" } } },
 }
