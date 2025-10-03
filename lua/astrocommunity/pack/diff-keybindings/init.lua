c2805644809245ac9a74d6afec20cc2e8545d570
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2025-10-03 11:48:34 +0200

    Update lua/astrocommunity/pack/diff-keybindings/init.lua
    Co-authored-by: Anton Ždanov <anton@azdanov.dev>

--- lua/astrocommunity/pack/diff-keybindings/init.lua
+++ lua/astrocommunity/pack/diff-keybindings/init.lua
@@ -7,15 +7,15 @@ return {
 
       local prefix = "<Leader>D"
       mappings.n[prefix] = { desc = require("astroui").get_icon("Diff", 1, true) .. "Diff" }
-      mappings.n[prefix .. "v"] = { "<cmd>vert diffsplit<cr>", desc = "  Vertical split diff" }
-      mappings.n[prefix .. "h"] = { "<cmd>diffsplit<cr>", desc = "  Horizontal split diff" }
-      mappings.n[prefix .. "t"] = { "<cmd>diffthis<cr>", desc = "  Diff this" }
-      mappings.n[prefix .. "p"] = { "<cmd>diffpatch<cr>", desc = "  Patch diff" }
-      mappings.n[prefix .. "u"] = { "<cmd>diffupdate<cr>", desc = "󰚰  Update diff" }
-      mappings.n[prefix .. "g"] = { "<cmd>diffget<cr>", desc = "  Get diff" }
-      mappings.n[prefix .. "P"] = { "<cmd>diffput<cr>", desc = "  Put diff" }
-      mappings.n[prefix .. "q"] = { "<cmd>diffoff<cr>", desc = "󰘪  Stop diff" }
-      mappings.n[prefix .. "?"] = { "<cmd>h diff<cr>", desc = "󰋗  Help" }
+      mappings.n[prefix .. "v"] = { "<cmd>vert diffsplit<cr>", desc = " Vertical split diff" }
+      mappings.n[prefix .. "h"] = { "<cmd>diffsplit<cr>", desc = " Horizontal split diff" }
+      mappings.n[prefix .. "t"] = { "<cmd>diffthis<cr>", desc = " Diff this" }
+      mappings.n[prefix .. "p"] = { "<cmd>diffpatch<cr>", desc = " Patch diff" }
+      mappings.n[prefix .. "u"] = { "<cmd>diffupdate<cr>", desc = "󰚰 Update diff" }
+      mappings.n[prefix .. "g"] = { "<cmd>diffget<cr>", desc = " Get diff" }
+      mappings.n[prefix .. "P"] = { "<cmd>diffput<cr>", desc = " Put diff" }
+      mappings.n[prefix .. "q"] = { "<cmd>diffoff<cr>", desc = "󰘪 Stop diff" }
+      mappings.n[prefix .. "?"] = { "<cmd>h diff<cr>", desc = "󰋗 Help" }
     end,
   },
   { "AstroNvim/astroui", opts = { icons = { Diff = "" } } },
