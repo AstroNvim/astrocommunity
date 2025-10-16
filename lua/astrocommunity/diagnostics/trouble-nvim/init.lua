9f513c065557ea8209b8c74450458603c3c0fe44
std-enigma <std.ilia.bahrebar@gmail.com>
2025-10-16 17:04:04 +0330

    feat(trouble-nvim): integrate snacks.nvim picker

--- lua/astrocommunity/diagnostics/trouble-nvim/init.lua
+++ lua/astrocommunity/diagnostics/trouble-nvim/init.lua
@@ -52,6 +52,30 @@ return {
       end,
     },
     {
+      "folke/trouble.nvim",
+      optional = true,
+      specs = {
+        "folke/snacks.nvim",
+        opts = function(_, opts)
+          return vim.tbl_deep_extend("force", opts or {}, {
+            picker = {
+              actions = require("trouble.sources.snacks").actions,
+              win = {
+                input = {
+                  keys = {
+                    ["<c-t>"] = {
+                      "trouble_open",
+                      mode = { "n", "i" },
+                    },
+                  },
+                },
+              },
+            },
+          })
+        end,
+      },
+    },
+    {
       "catppuccin",
       optional = true,
       ---@type CatppuccinOptions
