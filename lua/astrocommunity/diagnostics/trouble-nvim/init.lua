267b5817f08b56e7ef22d4ba842ac8343092b682
std-enigma <std.ilia.bahrebar@gmail.com>
2025-10-18 08:52:39 +0330

    refactor(trouble-nvim): wrong plugin spec

--- lua/astrocommunity/diagnostics/trouble-nvim/init.lua
+++ lua/astrocommunity/diagnostics/trouble-nvim/init.lua
@@ -52,28 +52,25 @@ return {
       end,
     },
     {
-      "folke/trouble.nvim",
+      "folke/snacks.nvim",
       optional = true,
-      specs = {
-        "folke/snacks.nvim",
-        opts = function(_, opts)
-          return vim.tbl_deep_extend("force", opts or {}, {
-            picker = {
-              actions = require("trouble.sources.snacks").actions,
-              win = {
-                input = {
-                  keys = {
-                    ["<c-t>"] = {
-                      "trouble_open",
-                      mode = { "n", "i" },
-                    },
+      opts = function(_, opts)
+        return vim.tbl_deep_extend("force", opts or {}, {
+          picker = {
+            actions = require("trouble.sources.snacks").actions,
+            win = {
+              input = {
+                keys = {
+                  ["<c-t>"] = {
+                    "trouble_open",
+                    mode = { "n", "i" },
                   },
                 },
               },
             },
-          })
-        end,
-      },
+          },
+        })
+      end,
     },
     {
       "catppuccin",
