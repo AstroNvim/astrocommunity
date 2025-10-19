3f89cb0cb64cdaf8f8a673022b93a0b11c7d309b
Jacopo Zacchigna <59306950+Jac-Zac@users.noreply.github.com>
2025-10-19 12:41:17 +0200

    feat(fyler-nvim): update fyler.nvim config to replace neotree.nvim (#1666)
    * feat(file-explorer): Updated fyler.nvim config
    
    More configuration to make it the default file explorer
    
    * Update lua/astrocommunity/file-explorer/fyler-nvim/init.lua
    
    Co-authored-by: Nicholas Ciechanowski <NicholasACiechanowski@gmail.com>
    
    * Update lua/astrocommunity/file-explorer/fyler-nvim/init.lua
    
    Co-authored-by: Nicholas Ciechanowski <NicholasACiechanowski@gmail.com>
    
    * Update lua/astrocommunity/file-explorer/fyler-nvim/init.lua
    
    Co-authored-by: Nicholas Ciechanowski <NicholasACiechanowski@gmail.com>
    
    * Update lua/astrocommunity/file-explorer/fyler-nvim/init.lua
    
    Co-authored-by: Nicholas Ciechanowski <NicholasACiechanowski@gmail.com>
    
    * fix(fyler-nvim): make neo-tree table optional
    
    ---------
    
    Co-authored-by: Nicholas Ciechanowski <NicholasACiechanowski@gmail.com>
    Co-authored-by: Uzair Aftab <git@uzaaft.me>

--- lua/astrocommunity/file-explorer/fyler-nvim/init.lua
+++ lua/astrocommunity/file-explorer/fyler-nvim/init.lua
@@ -1,15 +1,31 @@
 return {
-  "A7Lavinraj/fyler.nvim",
-  dependencies = {
-    "echasnovski/mini.icons",
-    {
-      "AstroNvim/astrocore",
-      opts = function(_, opts)
-        local maps = assert(opts.mappings)
-        maps.n["<Leader>y"] = { "<Cmd>Fyler<CR>", desc = "Fyler" }
-      end,
+  { "nvim-neo-tree/neo-tree.nvim", optional = true, enabled = false },
+  {
+    "A7Lavinraj/fyler.nvim",
+    dependencies = {
+      "echasnovski/mini.icons",
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local maps = opts.mappings or {}
+          maps.n["<Leader>e"] = {
+            function() require("fyler").toggle { kind = "float" } end,
+            desc = "Open with fyler (floating)",
+          }
+        end,
+      },
+    },
+
+    opts = {
+      default_explorer = true,
+
+      win = {
+        border = "rounded",
+        kind = "replace",
+      },
+      indentscope = {
+        marker = "┊",
+      },
     },
   },
-  cmd = "Fyler",
-  opts = {},
 }
