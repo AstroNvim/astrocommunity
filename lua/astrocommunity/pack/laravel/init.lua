4572a34e44caf1eb427e9fbe6a156183e4c2620a
Serhii Shvets <wert2all@gmail.com>
2025-12-02 02:18:39 +0200

    feat(laravel): add Pint formatter support

--- lua/astrocommunity/pack/laravel/init.lua
+++ lua/astrocommunity/pack/laravel/init.lua
@@ -71,4 +71,20 @@ return {
     },
     ft = { "blade", "php" },
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "pint" })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { "pint" },
+      },
+    },
+  },
 }
