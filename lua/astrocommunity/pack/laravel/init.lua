0c9051e4e17a80015f1ddc5ab8470eef58806c85
Uzair Aftab <git@uzaaft.me>
2025-12-16 13:32:30 +0100

    fix: add pint to null-ls

--- lua/astrocommunity/pack/laravel/init.lua
+++ lua/astrocommunity/pack/laravel/init.lua
@@ -79,6 +79,13 @@ return {
     end,
   },
   {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "pint" })
+    end,
+  },
+  {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
