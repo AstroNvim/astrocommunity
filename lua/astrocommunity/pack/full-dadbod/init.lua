ab6bf9a5d5ba3b8e987a44554bb639cb2f467d7c
rami3l <rami3l@outlook.com>
2026-01-01 13:59:38 +0100

    fix(full-dadbod): fix `blink.cmp` integration (#1712)

--- lua/astrocommunity/pack/full-dadbod/init.lua
+++ lua/astrocommunity/pack/full-dadbod/init.lua
@@ -51,18 +51,19 @@ return {
                 },
               },
             },
-            {
-              "saghen/blink.cmp",
-              opts = {
-                sources = {
-                  per_filetype = {
-                    sql = { "snippets", "dadbod", "buffer" },
-                  },
-                  -- add vim-dadbod-completion to your completion providers
-                  providers = {
-                    dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
-                  },
-                },
+          },
+        },
+        {
+          "saghen/blink.cmp",
+          optional = true,
+          opts = {
+            sources = {
+              per_filetype = {
+                sql = { "snippets", "dadbod", "buffer" },
+              },
+              -- add vim-dadbod-completion to your completion providers
+              providers = {
+                dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
               },
             },
           },
