fe7c263bf722921a7d244cc703a567219ef5e374
Micah Halter <micah.halter@gtri.gatech.edu>
2026-01-09 14:15:50 -0500

    feat(python): move base setup to a `base` submodule and allow `python` pack to still deliver a sane default setup

---
+++ lua/astrocommunity/pack/python/base.lua
@@ -0,0 +1,69 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "python", "toml" })
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    opts = function(_, opts)
+      if not opts.handlers then opts.handlers = {} end
+      opts.handlers.python = function() end -- make sure python doesn't get set up by mason-nvim-dap, it's being set up by nvim-dap-python
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "debugpy" })
+    end,
+  },
+  {
+    "linux-cultist/venv-selector.nvim",
+    enabled = vim.fn.executable "fd" == 1 or vim.fn.executable "fdfind" == 1 or vim.fn.executable "fd-find" == 1,
+    dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          mappings = {
+            n = {
+              ["<Leader>lv"] = { "<Cmd>VenvSelect<CR>", desc = "Select VirtualEnv" },
+            },
+          },
+        },
+      },
+    },
+    opts = {},
+    cmd = "VenvSelect",
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    specs = {
+      {
+        "mfussenegger/nvim-dap-python",
+        dependencies = "mfussenegger/nvim-dap",
+        ft = "python", -- NOTE: ft: lazy-load on filetype
+        config = function(_, opts)
+          local path = vim.fn.exepath "debugpy-adapter"
+          if path == "" then path = vim.fn.exepath "python" end
+          require("dap-python").setup(path, opts)
+        end,
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = { "nvim-neotest/neotest-python", config = function() end },
+    opts = function(_, opts)
+      if not opts.adapters then opts.adapters = {} end
+      table.insert(opts.adapters, require "neotest-python"(require("astrocore").plugin_opts "neotest-python"))
+    end,
+  },
+}
