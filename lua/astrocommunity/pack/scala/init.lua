2b5e0040bcffa2ef1cbdc66d5c69c4ba8501339e
Uzair Aftab <git@uzaaft.me>
2026-01-27 19:59:43 +0100

    fix(scala): use vim.lsp.config for v6 compatibility

--- lua/astrocommunity/pack/scala/init.lua
+++ lua/astrocommunity/pack/scala/init.lua
@@ -24,8 +24,7 @@ return {
     },
     opts = function()
       local metals, astrocore = require "metals", require "astrocore"
-      local astrolsp_avail, astrolsp = pcall(require, "astrolsp")
-      local user_config = astrolsp_avail and astrolsp.lsp_opts "metals" or {}
+      local user_config = vim.lsp.config["metals"] or {}
       if astrocore.is_available "nvim-dap" then
         user_config.on_attach = astrocore.patch_func(user_config.on_attach, function(orig, ...)
           orig(...)
