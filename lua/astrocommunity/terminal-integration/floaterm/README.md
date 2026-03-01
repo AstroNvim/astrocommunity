e3228cd280fc9c0cebd5bac23cd5fde811bf9c59
Reverse <reeversesoft@gmail.com>
2026-03-01 14:33:10 +0100

    feat(terminal-integration): add floaterm (#1740)
    * feat(terminal-integration): add floaterm
    
    * Apply suggestion from @Uzaaft
    
    * Apply suggestion from @Uzaaft
    
    ---------
    
    Co-authored-by: Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>

---
+++ lua/astrocommunity/terminal-integration/floaterm/README.md
@@ -0,0 +1,5 @@
+# floaterm
+
+Beautiful floating terminal manager for Neovim. 
+
+**Repository:** <https://github.com/nvzone/floaterm>
---
+++ lua/astrocommunity/terminal-integration/floaterm/init.lua
@@ -0,0 +1,19 @@
+return {
+  "nvzone/floaterm",
+  dependencies = {
+    { "nvzone/volt", lazy = true },
+    {
+      "AstroNvim/astrocore",
+      ---@type AstroCoreOpts
+      opts = {
+        mappings = {
+          n = {
+            ["<leader>tF"] = { "<cmd>FloatermToggle<cr>", desc = "Toggle Floaterm (NvZone)" },
+          },
+        },
+      },
+    },
+  },
+  cmd = "FloatermToggle",
+  opts = {},
+}
