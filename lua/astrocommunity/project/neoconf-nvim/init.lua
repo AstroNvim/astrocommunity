571594bd3984b327cb0ad453d7937e9f02eb97e9
Uzair Aftab <git@uzaaft.me>
2026-04-02 13:39:38 +0200

    deprecate(neoconf-nvim): disable pack for v6 compatibility
    neoconf.nvim does not support the vim.lsp.config API used in
    AstroNvim v6 and is no longer maintained. The pack is now disabled
    with a deprecation notice directing users to project-local
    lsp/<server>.lua files or language server config files instead.

--- lua/astrocommunity/project/neoconf-nvim/init.lua
+++ lua/astrocommunity/project/neoconf-nvim/init.lua
@@ -1,5 +1,7 @@
+-- neoconf.nvim does not support vim.lsp.config and has been removed from AstroNvim v6.
+-- This pack is deprecated. Consider using project-local lsp/<server>.lua files
+-- or language server config files (e.g., .luarc.json) instead.
 return {
   "folke/neoconf.nvim",
-  cmd = "Neoconf",
-  opts = {},
+  enabled = false,
 }
