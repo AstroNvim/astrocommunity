00f09b344fd5ed64feec5b3b040d55034d79f5f2
vonhyou <shuo.feng@ray.works>
2026-04-13 17:51:59 -0400

    add AstroNvim v6 compatibility

--- lua/astrocommunity/pack/elixir/README.md
+++ lua/astrocommunity/pack/elixir/README.md
@@ -7,9 +7,8 @@ This plugin does the following:
 - Adds official [elixir-lang/expert](https://github.com/elixir-lang/expert) LSP.
 - Adds `mix format` formatter via `conform.nvim`
 - Adds `elixir`, `heex`, and `eex` Treesitter parsers
+- AstroNvim v6 compatiblity
 
 ## Note 
 
-Expert is registered manually at runtime because AstroNvim pins `nvim-lspconfig` to a version predating `lsp/expert.lua`.
-
-This pack uses [Expert](https://expert-lsp.org), the **official** Elixir language server maintained under `elixir-lang`. It is currently in [release candidate stage](https://github.com/elixir-lang/expert/milestone/1).
+This pack uses [Expert](https://expert-lsp.org), the **official** Elixir language server maintained under `elixir-lang`.
--- lua/astrocommunity/pack/elixir/init.lua
+++ lua/astrocommunity/pack/elixir/init.lua
@@ -1,54 +1,36 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    optional = true,
-    opts = function(_, opts)
-      if opts.ensure_installed ~= "all" then
-        opts.ensure_installed =
-          require("astrocore").list_insert_unique(opts.ensure_installed, { "elixir", "heex", "eex" })
-      end
-    end,
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "mason-org/mason-lspconfig.nvim",
     optional = true,
     opts = function(_, opts)
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "expert" })
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    "AstroNvim/astrocore",
     optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "expert" })
-    end,
+    ---@type AstroCoreOpts
+    opts = {
+      treesitter = { ensure_installed = { "elixir", "heex", "eex" } },
+    },
   },
   {
     "AstroNvim/astrolsp",
     optional = true,
-    opts = function(_, opts)
-      local configs = require "lspconfig.configs"
-      if not configs.expert then
-        configs.expert = {
-          default_config = {
-            cmd = { "expert", "--stdio" },
-            root_dir = function(fname) return require("lspconfig").util.root_pattern("mix.exs", ".git")(fname) end,
-            filetypes = { "elixir", "eelixir", "heex" },
-            settings = {},
-          },
-        }
-      end
-      opts.servers = require("astrocore").list_insert_unique(opts.servers, { "expert" })
-    end,
+    ---@type AstroLSPOpts
+    opts = {
+      servers = { "expert" },
+    },
   },
   {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
       formatters_by_ft = {
-        elixir = { "mix" },
-        heex = { "mix" },
-        eex = { "mix" },
+        elixir = { "mix_format" },
+        heex = { "mix_format" },
+        eelixir = { "mix_format" },
+        surface = { "mix_format" },
       },
     },
   },
