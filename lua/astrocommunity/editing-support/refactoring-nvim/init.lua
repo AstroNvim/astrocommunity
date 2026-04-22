cddc36cbe890f3aa7b2d8a299aafbafc35cb28df
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-04-22 13:02:38 +0400

    fix(refactoring-nvim): add async.nvim dependency
    https://github.com/ThePrimeagen/refactoring.nvim#installation

--- lua/astrocommunity/editing-support/refactoring-nvim/init.lua
+++ lua/astrocommunity/editing-support/refactoring-nvim/init.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
+    "lewis6991/async.nvim",
     {
       "AstroNvim/astrocore",
       ---@param opts AstroCoreOpts
