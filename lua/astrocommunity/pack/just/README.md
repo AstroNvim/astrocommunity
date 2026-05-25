07ff75482385cc9524a46428e403ea241ad0fe0a
TakiMoysha <36836047+TakiMoysha@users.noreply.github.com>
2026-05-25 15:35:18 +0200

    feat(just): added autocmd for replacing make with just, if find justfile
    chore(just): refactored just_as_make

--- lua/astrocommunity/pack/just/README.md
+++ lua/astrocommunity/pack/just/README.md
@@ -4,3 +4,4 @@ This plugin does the following:
 
 - Adds `just` Treesitter parser: <https://github.com/IndianBoy42/tree-sitter-just>
 - Adds `just-lsp`: <https://github.com/terror/just-lsp>
+- Adds autocmds `just_as_make`: `:make` now calls just if there is justfile in workdir. And redirects the output to quickfix like make
--- lua/astrocommunity/pack/just/init.lua
+++ lua/astrocommunity/pack/just/init.lua
@@ -13,6 +13,19 @@ return {
         },
       },
       treesitter = { ensure_installed = { "just" } },
+      autocmds = {
+        just_as_make = {
+          {
+            event = "BufReadPost",
+            pattern = "*",
+            desc = "Set just as :make if workdir have justfile",
+            callback = function()
+              local justfile = vim.fn.findfile("justfile", ".;")
+              if justfile ~= "" then vim.bo.makeprg = "just" end
+            end,
+          },
+        },
+      },
     },
   },
   {
