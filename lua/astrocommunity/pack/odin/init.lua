710aa77fd8942de92be79a80ccea906fc7e3da8a
kuroski <daniel.kuroski@jetbrains.com>
2026-06-15 13:46:42 -0300

    feat(pack): add odin language pack

---
+++ lua/astrocommunity/pack/odin/init.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    optional = true,
+    ---@type AstroCoreOpts
+    opts = {
+      treesitter = { ensure_installed = { "odin" } },
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ols" })
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ols" })
+    end,
+  },
+}
