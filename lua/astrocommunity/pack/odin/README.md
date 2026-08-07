6b940dbc898019ee25d1de71ba8a0c0b3bec38bd
Dahren Ericsson <dahrenericsson@proton.me>
2026-08-07 22:31:34 +0700

    feat(pack): Added odin language pack

---
+++ lua/astrocommunity/pack/odin/README.md
@@ -0,0 +1,5 @@
+# Odin Language Pack
+
+This plugin pack does the following:
+- Adds `odin` Treesitter parser
+- Adds `ols` language server
---
+++ lua/astrocommunity/pack/odin/init.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    optional = true,
+    ---@type AstroCoreOpts
+    opts = {
+      treesitter = { ensure_installed = { "odin" } },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ols" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ols", "codelldb" })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        lua = { "ols" },
+      },
+    },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
+    end,
+  },
+}
