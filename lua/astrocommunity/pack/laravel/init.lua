8bc277f306b3687cc11784c7fa6f49f0ace63465
Angelo Gambara <282856609+angelogambaradev@users.noreply.github.com>
2026-08-08 08:19:30 +0200

    fix: apply suggestion from @ALameLlama
    Co-authored-by: Nicholas Ciechanowski <NicholasACiechanowski@gmail.com>

--- lua/astrocommunity/pack/laravel/init.lua
+++ lua/astrocommunity/pack/laravel/init.lua
@@ -4,9 +4,7 @@ return {
   { import = "astrocommunity.pack.blade" },
   {
     "adalessa/laravel.nvim",
-    cond = function()
-      return vim.loop.fs_stat(vim.fn.getcwd() .. "/artisan") ~= nil
-    end,
+    cond = function() return vim.loop.fs_stat(vim.fn.getcwd() .. "/artisan") ~= nil end,
     cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
     dependencies = {
       "tpope/vim-dotenv",
